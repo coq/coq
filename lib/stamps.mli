610caabdaac2f9ca635737839f645cc870d83975
filliatr <filliatr@85f007b7-540e-0410-9357-904b9bb8a0f7>
1999-10-08 08:17:29 +0000

    time stamps
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@92 85f007b7-540e-0410-9357-904b9bb8a0f7

---
+++ lib/stamps.mli
@@ -0,0 +1,21 @@
+
+(* $Id$ *)
+
+(* Time stamps. *)
+
+type 'a timestamped
+
+(* [ts_mod] gives a ['b timestamped] with a new stamp *)
+val ts_mod : ('a -> 'b) -> 'a timestamped -> 'b timestamped
+val ts_it : 'a timestamped -> 'a
+val ts_mk : 'a -> 'a timestamped
+val ts_eq : 'a timestamped -> 'a timestamped -> bool
+val ts_stamp : 'a timestamped -> int
+
+type 'a idstamped
+
+(* [ids_mod] gives a ['b stamped] with the same stamp *)
+val ids_mod : ('a -> 'b) -> 'a idstamped -> 'b idstamped
+val ids_it : 'a idstamped -> 'a
+val ids_mk : 'a -> 'a idstamped
+val ids_eq : 'a idstamped -> 'a idstamped -> bool
