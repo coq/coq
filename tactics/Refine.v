33d14679b922ae0a15c10e788335f23a3a64c643
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2000-10-03 18:10:27 +0000

    L'argument de Refine est un terme ouvert
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@637 85f007b7-540e-0410-9357-904b9bb8a0f7

--- tactics/Refine.v
+++ tactics/Refine.v
@@ -4,7 +4,7 @@
 Declare ML Module "refine".
 
 Grammar tactic simple_tactic :=
-  tcc [ "Refine" constrarg($c) ] -> [(Tcc $c)].
+  tcc [ "Refine" castedopenconstrarg($c) ] -> [(Tcc $c)].
 
 Syntax tactic level 0:
   tcc [(Tcc $C)] -> ["Refine " $C].
