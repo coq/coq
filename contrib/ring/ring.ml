75effdc265e716651c860d0878ffbfc205646cbd
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2000-10-26 12:27:42 +0000

    Ajout de la mthode load_function pour exporter les 'tactic-ring-theory'
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@765 85f007b7-540e-0410-9357-904b9bb8a0f7

--- contrib/ring/ring.ml
+++ contrib/ring/ring.ml
@@ -147,7 +147,7 @@ let (theory_to_obj, obj_to_theory) =
   let cache_th (_,(c, th)) = theories_map_add (c,th)
   and spec_th x = x in
   declare_object ("tactic-ring-theory",
-		  { load_function = (fun _ -> ());
+		  { load_function = cache_th;
 		    open_function = (fun _ -> ());
                     cache_function = cache_th;
 		    specification_function = spec_th })
