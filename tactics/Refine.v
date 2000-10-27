e1933dac58fc125f23c05ae88b8b57a54008dab6
mayero <mayero@85f007b7-540e-0410-9357-904b9bb8a0f7>
2000-10-27 15:15:29 +0000

    Ajoute : Ast dans la regle de grammaire
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@774 85f007b7-540e-0410-9357-904b9bb8a0f7

--- tactics/Refine.v
+++ tactics/Refine.v
@@ -3,7 +3,7 @@
 
 Declare ML Module "refine".
 
-Grammar tactic simple_tactic :=
+Grammar tactic simple_tactic: Ast :=
   tcc [ "Refine" castedopenconstrarg($c) ] -> [(Tcc $c)].
 
 Syntax tactic level 0:
