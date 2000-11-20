2c82358cdfdcebfa9a39a6259b67f0a611f7cf22
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2000-11-20 08:40:03 +0000

    Mieux à sa place dans toplevel
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@858 85f007b7-540e-0410-9357-904b9bb8a0f7

---
+++ toplevel/class.mli
@@ -0,0 +1,24 @@
+
+(* $Id$ *)
+
+(*i*)
+open Names
+open Term
+open Classops
+open Declare
+(*i*)
+
+(* Classes and coercions. *)
+
+val try_add_new_coercion : identifier -> strength -> unit
+val try_add_new_coercion_subclass : identifier -> strength -> unit
+val try_add_new_coercion_record : identifier -> strength -> identifier -> unit
+val try_add_new_coercion_with_target : identifier -> strength ->
+  identifier -> identifier -> bool -> unit
+
+val try_add_new_class : identifier -> strength -> unit
+val process_class :
+  dir_path -> (cl_typ * cl_info_typ) -> (cl_typ * cl_info_typ)
+val process_coercion :
+  dir_path -> (coe_typ * coe_info_typ) * cl_typ * cl_typ -> 
+    ((coe_typ * coe_info_typ) * cl_typ * cl_typ) * identifier * int 
