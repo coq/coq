6c9c73e4c0de61bbf728aa4e0e87c51e79ec3efc
bertot <bertot@85f007b7-540e-0410-9357-904b9bb8a0f7>
2001-02-09 08:15:57 +0000

    exported a few functions that are used in graphical interface pcoq.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@1361 85f007b7-540e-0410-9357-904b9bb8a0f7

--- parsing/search.mli
+++ parsing/search.mli
@@ -12,3 +12,18 @@ open Pattern
 val search_by_head : global_reference -> dir_path list * bool -> unit
 val search_rewrite : constr_pattern -> dir_path list * bool -> unit
 val search_pattern : constr_pattern -> dir_path list * bool -> unit
+
+(* The filtering function that is by standard search facilities.
+   It can be passed as argument to the raw search functions.
+   It is used in pcoq. *)
+
+val filter_by_module : dir_path list -> bool -> global_reference -> env -> 'a -> bool
+
+(* raw search functions can be used for various extensions.
+   They are also used for pcoq. *)
+val filtered_search : (global_reference -> env -> constr -> bool) -> 
+  (global_reference -> env -> constr -> unit) -> global_reference -> unit
+val raw_pattern_search : (global_reference -> env -> constr -> bool) ->
+  (global_reference -> env -> constr -> unit) -> constr_pattern -> unit
+val raw_search_rewrite : (global_reference -> env -> constr -> bool) ->
+  (global_reference -> env -> constr -> unit) -> constr_pattern -> unit
