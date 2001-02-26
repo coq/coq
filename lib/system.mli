f9f808fd7044592a7d4c520c77f43e87ba245cae
filliatr <filliatr@85f007b7-540e-0410-9357-904b9bb8a0f7>
2001-02-26 08:34:36 +0000

    export open_trapping_failure pour contrib/extraction
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@1401 85f007b7-540e-0410-9357-904b9bb8a0f7

--- lib/system.mli
+++ lib/system.mli
@@ -42,6 +42,8 @@ val raw_extern_intern : int -> string ->
 val extern_intern : 
   int -> string -> (string -> 'a -> unit) * (load_path -> string -> 'a)
 
+val open_trapping_failure : (string -> 'a) -> string -> string -> 'a
+
 (*s Time stamps. *)
 
 type time
