085807162a7acf72fc060f3720eef43eacde7fe5
bertot <bertot@85f007b7-540e-0410-9357-904b9bb8a0f7>
2001-04-04 12:51:36 +0000

    adding entry points for the arguments of the Comment command.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@1534 85f007b7-540e-0410-9357-904b9bb8a0f7

--- parsing/pcoq.mli
+++ parsing/pcoq.mli
@@ -165,6 +165,8 @@ module Vernac :
     val ne_identarg_list : Coqast.t list Gram.Entry.e
     val qualidarg : Coqast.t Gram.Entry.e
     val qualidconstarg : Coqast.t Gram.Entry.e
+    val commentarg : Coqast.t Gram.Entry.e
+    val commentarg_list : Coqast.t list Gram.Entry.e
     val ne_qualidarg_list : Coqast.t list Gram.Entry.e
     val numarg : Coqast.t Gram.Entry.e
     val numarg_list : Coqast.t list Gram.Entry.e
