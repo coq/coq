9dffee324277ac229634e9f21e3f46694ec61ca8
filliatr <filliatr@85f007b7-540e-0410-9357-904b9bb8a0f7>
2001-04-19 12:45:22 +0000

    modifs des scripts de test auto
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@1621 85f007b7-540e-0410-9357-904b9bb8a0f7

--- contrib/extraction/test/ml2v.ml
+++ contrib/extraction/test/ml2v.ml
@@ -1,17 +1,13 @@
-let main () = begin
-  let j = Array.length (Sys.argv) in
-  if j>0 then begin
-	let s = Sys.argv.(1) in
-	let b = Filename.chop_extension (Filename.basename s) in 
-	let b0 = String.sub b 0 1 in
-	let b1 = String.capitalize b0 in  
-	let b = String.sub b 1 ((String.length b) -1) in 
-	let d = Filename.dirname s
-	in print_string (d^"/["^b0^b1^"]"^b^".v");
-	print_newline()
-  end;
-  exit(0)	
-end;;	
-
-main();;
- 		
+let _ =
+  let j = Array.length Sys.argv in
+  if j > 0 then
+    let fml = Sys.argv.(1) in
+    let f = Filename.chop_extension fml in 
+    let fv = f ^ ".v" in
+    if Sys.file_exists fv then
+      print_endline fv
+    else
+      let d = Filename.dirname f in
+      let b = String.capitalize (Filename.basename f) in
+      let fv = Filename.concat d (b ^ ".v") in
+      print_endline fv
--- contrib/extraction/test/v2ml.ml
+++ contrib/extraction/test/v2ml.ml
@@ -1,15 +1,9 @@
-let main () = begin
+let _ = 
   let j = Array.length (Sys.argv) in
-  if j>0 then begin
-	let s = Sys.argv.(1) in
-	let b = Filename.chop_extension (Filename.basename s) in 
-	let b = String.uncapitalize b in
-	let d = Filename.dirname s
-	in print_string (d^"/"^b^".ml");
-	print_newline()
-  end;
-  exit(0)	
-end;;	
+  if j>0 then 
+    let s = Sys.argv.(1) in
+    let b = Filename.chop_extension (Filename.basename s) in 
+    let b = String.uncapitalize b in
+    let d = Filename.dirname s in 
+    print_endline (Filename.concat d (b ^ ".ml"))
 
-main();;
- 		
