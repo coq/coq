2be53251b7997eda2557a8bb938590f4c8ba24b8
mayero <mayero@85f007b7-540e-0410-9357-904b9bb8a0f7>
2001-04-20 19:53:19 +0000

    test Fourier, DiscrR
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@1659 85f007b7-540e-0410-9357-904b9bb8a0f7

---
+++ test-suite/success/Fourier.v
@@ -0,0 +1,16 @@
+Require SplitAbsolu.
+Require Fourier.
+ 
+Lemma l1:
+ (x, y, z : R)
+  ``(Rabsolu x-z) <= (Rabsolu x-y)+(Rabsolu y-z)``.
+Intros; SplitAbsolu; Fourier.
+Qed.
+ 
+Lemma l2:
+ (x, y : R)
+ ``x < (Rabsolu y)`` ->
+ ``y < 1`` -> ``x >= 0`` -> ``-y <= 1`` ->  ``(Rabsolu x) <= 1``.
+Intros.
+SplitAbsolu; Fourier.
+Qed.
