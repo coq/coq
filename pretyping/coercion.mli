818874e865d091a9ec5b3e65aa257dc77939e11b
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2001-09-14 13:12:08 +0000

    L'instantiation des evars quand un produit ou une sorte étaient attendus n'était pas fait
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@1967 85f007b7-540e-0410-9357-904b9bb8a0f7

--- pretyping/coercion.mli
+++ pretyping/coercion.mli
@@ -36,10 +36,11 @@ val inh_coerce_to_sort :
 val inh_conv_coerce_to : Rawterm.loc -> 
   env -> 'a evar_defs -> unsafe_judgment -> constr -> unsafe_judgment
 
+(*i
 (* [inh_apply_rel_list loc env isevars args f tycon] tries to type [(f
    args)] of type [tycon] (if any) by inserting coercions in front of
    each arg$_i$, if necessary *)
 val inh_apply_rel_list : Rawterm.loc -> env -> 'a evar_defs ->
   (Rawterm.loc * unsafe_judgment) list ->
     (Rawterm.loc * unsafe_judgment) -> constr option -> unsafe_judgment
-
+i*)
