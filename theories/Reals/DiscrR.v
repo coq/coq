8a2e6b944ecf4ea688ef4b8d8fac8ef76feabd3b
mayero <mayero@85f007b7-540e-0410-9357-904b9bb8a0f7>
2001-09-18 10:14:57 +0000

    modif test const
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@1977 85f007b7-540e-0410-9357-904b9bb8a0f7

--- theories/Reals/DiscrR.v
+++ theories/Reals/DiscrR.v
@@ -10,6 +10,17 @@
 
 Require Rbase.
 
+Recursive Tactic Definition Isrealint trm:=
+  Match trm With
+  | [R0] -> Idtac
+  | [R1] -> Idtac
+  | [(Rplus ?1 ?2)] -> (Isrealint ?1);(Isrealint ?2)
+  | [(Rminus ?1 ?2)] -> (Isrealint ?1);(Isrealint ?2)
+  | [(Rmult ?1 ?2)] -> (Isrealint ?1);(Isrealint ?2)
+  | [(Ropp ?1)] -> (Isrealint ?1)
+  | _ -> Fail.
+
+
 Tactic Definition Sup0 :=
   Match Context With
   | [ |- (Rgt R1 R0) ] -> Unfold Rgt;Apply Rlt_R0_R1
@@ -22,12 +33,12 @@ Tactic Definition Sup0 :=
 Tactic Definition DiscrR :=
   Match Context With
   | [ |- ~R1==R0 ] -> Red;Intro;Apply R1_neq_R0;Assumption
-  | [ |- ~((Rplus R1 ?1)==R0) ] -> Apply Rgt_not_eq;Sup0
-  | [ |- ~(Ropp ?1)==R0 ] -> Apply Ropp_neq; DiscrR
+  | [ |- ~((Rplus R1 ?1)==R0) ] -> (Isrealint ?1);Apply Rgt_not_eq;Sup0
+  | [ |- ~(Ropp ?1)==R0 ] -> (Isrealint ?1);Apply Ropp_neq; DiscrR
   | [ |- ~(?1==?1) ] -> ElimType False
-  | [ |- ~(Rminus R0 ?1)==R0 ] -> Rewrite Rminus_Ropp; DiscrR
-  | [ |- ~(?1==?2) ] -> 
-    (Apply Rminus_not_eq; Ring (Rminus ?1 ?2); 
+  | [ |- ~(Rminus R0 ?1)==R0 ] -> (Isrealint ?1);Rewrite Rminus_Ropp; DiscrR
+  | [ |- ~(?1==?2) ] -> ((Isrealint ?1);(Isrealint ?2);
+    Apply Rminus_not_eq; Ring (Rminus ?1 ?2); 
       (Match Context With
       | [ |- ~(Rplus (Ropp R1) ?)==R0 ] -> 
         Repeat Rewrite <-Ropp_distr1; DiscrR
