90e4bb226594b3f44febb369bb36d28af4fa2079
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2001-09-20 15:47:05 +0000

    Nombre magique pour la V7.1
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@2026 85f007b7-540e-0410-9357-904b9bb8a0f7

--- library/library.ml
+++ library/library.ml
@@ -162,7 +162,7 @@ let module_segment = function
 
 let module_full_filename m = (find_module m).module_filename
 
-let vo_magic_number = 0700
+let vo_magic_number = 0701 (* V7.1 *)
 
 let (raw_extern_module, raw_intern_module) =
   System.raw_extern_intern vo_magic_number ".vo"
