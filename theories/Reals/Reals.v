0f89e0158458aeea267437dd8388c05ceb609308
desmettr <desmettr@85f007b7-540e-0410-9357-904b9bb8a0f7>
2001-11-30 16:10:49 +0000

    Modification de Reals pour integrer les modifications
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@2255 85f007b7-540e-0410-9357-904b9bb8a0f7

--- theories/Reals/Reals.v
+++ theories/Reals/Reals.v
@@ -14,6 +14,7 @@ Require Export Raxioms.
 Require Export Rbase.
 Require Export R_Ifp.
 Require Export Rbasic_fun.
+Require Export R_sqr.
 Require Export Rlimit.
 Require Export Rfunctions.
 Require Export Rderiv.
