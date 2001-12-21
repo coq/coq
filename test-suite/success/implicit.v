7af730c8408f294ebe568240a3288f24a0959847
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2001-12-21 16:13:44 +0000

    Ajout d'un exemple de Christine
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@2365 85f007b7-540e-0410-9357-904b9bb8a0f7

--- test-suite/success/implicit.v
+++ test-suite/success/implicit.v
@@ -1,9 +1,18 @@
 (* Implicit on section variables *)
-(* Example submitted by David Nowak *)
 
 Set Implicit Arguments.
+
+(* Example submitted by David Nowak *)
+
 Section Spec.
 Variable A:Set.
 Variable op : (A:Set)A->A->Set.
 Infix 6 "#" op.
 Check (x:A)(x # x).
+
+(* Example submitted by Christine *)
+Record stack : Type := {type : Set; elt : type; 
+                        empty : type -> bool; proof : (empty elt)=true }.
+
+Check (type:Set; elt:type; empty:(type->bool))(empty elt)=true->stack.
+
