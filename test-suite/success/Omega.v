6f0399df592d58987a65e9e8fce857d80a2c4c22
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2002-01-21 09:32:08 +0000

    Ajout test de Pierre Crégut
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@2420 85f007b7-540e-0410-9357-904b9bb8a0f7

--- test-suite/success/Omega.v
+++ test-suite/success/Omega.v
@@ -1,11 +1,19 @@
+Require Omega.
+
 (* Submitted by Xavier Urbain 18 Jan 2002 *)
 
-Require Omega.
-Lemma lem : (x,y:Z) 
+Lemma lem1 : (x,y:Z) 
   `-5 < x < 5` ->
   `-5 < y` ->
   `-5 < x+y+5`.
 Proof.
 Intros x y.
+Omega.
+Qed.
+
+(* Proposed by Pierre Crégut *)
+
+Lemma lem2 : (x:Z) `x < 4` -> `x > 2` -> `x=3`.
+Intro.
 Omega.
 Qed.
