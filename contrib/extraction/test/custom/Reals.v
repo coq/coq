88ff300b20833b71ec5d31fffcc766dda9aba365
letouzey <letouzey@85f007b7-540e-0410-9357-904b9bb8a0f7>
2002-03-21 17:06:15 +0000

    reparation du test des reals
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@2561 85f007b7-540e-0410-9357-904b9bb8a0f7

--- contrib/extraction/test/custom/Reals.v
+++ contrib/extraction/test/custom/Reals.v
@@ -14,3 +14,4 @@ Extract Inlined Constant sigma => "(fun l h -> sigma_aux l h (Minus.minus h l))"
 Extract Inlined Constant PI => "3.141593".
 Extract Inlined Constant cos => cos.
 Extract Inlined Constant sin => sin.
+Extract Inlined Constant derive_pt => "(fun f x -> ((f (x+.1E-5))-.(f x))*.1E5)".
