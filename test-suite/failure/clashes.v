b1f64fc34b3d845571830970cf8c8ff0f6525530
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2002-04-12 12:55:01 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@2638 85f007b7-540e-0410-9357-904b9bb8a0f7

---
+++ test-suite/failure/clashes.v
@@ -0,0 +1,8 @@
+(* Submitted by David Nowak *)
+
+(* Simpler to forbid the definition of n as a global than to write it
+   S.n to keep n accessible... *)
+
+Section S.
+Variable n:nat.
+Inductive P : Set := n : P.
