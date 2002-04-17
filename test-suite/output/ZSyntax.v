cda6963b3ca8fd75c00a0c65dd9ecffdeadfb3c3
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2002-04-17 11:49:07 +0000

    Quelques bugs avec inject_nat
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@2653 85f007b7-540e-0410-9357-904b9bb8a0f7

--- test-suite/output/ZSyntax.v
+++ test-suite/output/ZSyntax.v
@@ -1,3 +1,17 @@
 Require ZArith.
 Check `32`.
 Check [f:nat->Z]`(f O) + 0`.
+Check [x:positive]`(POS (xO x))`.
+Check [x:positive]`(POS x)+1`.
+Check [x:positive]`(POS x)`.
+Check [x:positive]`(NEG (xO x))`.
+Check [x:positive]`(POS (xO x))+0`.
+Check [x:positive]`(Zopp (POS (xO x)))`.
+Check [x:positive]`(Zopp (POS (xO x)))+0`.
+Check `(inject_nat O)+1`.
+Check (Zplus `0` (inject_nat (plus O O))).
+Check `(inject_nat O)=0`.
+
+(* Submitted by Pierre Casteran *)
+Require Arith.
+Check (Zplus `0` (inject_nat (789))).
