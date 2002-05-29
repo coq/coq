26bf42a793d556047aea897cdeed8b4214d5177f
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2002-05-29 10:59:25 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@2726 85f007b7-540e-0410-9357-904b9bb8a0f7

---
+++ test-suite/success/Destruct.v
@@ -0,0 +1,9 @@
+(* Submitted by Robert Schneck *)
+
+Parameter A,B,C,D : Prop.
+Axiom X : A->B->C/\D.
+
+Lemma foo : A->B->C.
+Proof.
+Intros. 
+NewDestruct X. (* Should find axiom X and should handle arguments of X *)
