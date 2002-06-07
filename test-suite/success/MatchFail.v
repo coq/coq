1198561c5038b7f8609c84eb676b396483ce15e4
bertot <bertot@85f007b7-540e-0410-9357-904b9bb8a0f7>
2002-06-07 06:56:35 +0000

    I added a comment on the tactic compute_POS.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@2767 85f007b7-540e-0410-9357-904b9bb8a0f7

--- test-suite/success/MatchFail.v
+++ test-suite/success/MatchFail.v
@@ -1,6 +1,11 @@
 Require Export ZArith.
 Require Export ZArithRing.
 
+(* Cette tactique a pour objectif de remplacer toute instance
+   de (POS (xI e)) ou de (POS (xO e)) par
+   2*(POS e)+1  ou 2*(POS e), pour rendre les expressions plus
+   à même d'être utilisées par Ring, lorsque ces expressions contiennent
+   des variables de type positive. *)
 Tactic Definition compute_POS :=
  (Match Context With
  | [|- [(POS (xI ?1))]] -> Let v = ?1 In 
