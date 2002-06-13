626e6f8bb47df7845aac639c327191e30ad2b17d
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2002-06-13 16:21:05 +0000

    Test de l'interprétation des fermetures de Match Context
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@2780 85f007b7-540e-0410-9357-904b9bb8a0f7

--- test-suite/success/ltac.v
+++ test-suite/success/ltac.v
@@ -4,5 +4,13 @@
 (* Checks substitution of x *)
 Tactic Definition f x := Unfold x; Idtac.
  
-Goal (plus O O) = O.
+Lemma lem1 : (plus O O) = O.
 f plus.
+Qed.
+
+(* Check that Match Context keeps a closure *)
+Tactic Definition U := Let a = 'I In Match Context With [ |- ? ] -> Apply a.
+
+Lemma lem2 : True.
+U.
+Qed
