8b26fd6ba739d4f49fae99ed764b086022e44b50
desmettr <desmettr@85f007b7-540e-0410-9357-904b9bb8a0f7>
2002-07-31 14:34:11 +0000

    MAJ pour TAF
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@2950 85f007b7-540e-0410-9357-904b9bb8a0f7

--- theories/Reals/Reals.v
+++ theories/Reals/Reals.v
@@ -29,6 +29,7 @@ Require Export Rcomplet.
 Require Export Alembert_compl.
 Require Export Rtrigo_fun.
 Require Export Ranalysis.
+Require Export TAF.
 Require Export DiscrR.
 Require Export SplitRmult.
 Require Export SplitAbsolu.
