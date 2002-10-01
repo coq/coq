a716e3b141e08c43d10ab3418b6d2b8f6b85445c
clrenard <clrenard@85f007b7-540e-0410-9357-904b9bb8a0f7>
2002-10-01 07:44:02 +0000

    bug de noms long pour eqT.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@3053 85f007b7-540e-0410-9357-904b9bb8a0f7

--- tactics/setoid_replace.ml
+++ tactics/setoid_replace.ml
@@ -82,7 +82,7 @@ let coq_fleche = lazy(constant ["Setoid"] "fleche")
 
 (* Coq constants *)
 
-let coqeq = lazy(global_constant ["Logic"] "eqT")
+let coqeq = lazy(global_constant ["Logic_Type"] "eqT")
 
 let coqconj = lazy(global_constant ["Logic"] "conj")
 let coqand = lazy(global_constant ["Logic"] "and")
