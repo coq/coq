e41d89dd460d98df11a1d6b7fe17aaac6238c652
desmettr <desmettr@85f007b7-540e-0410-9357-904b9bb8a0f7>
2002-11-26 18:40:39 +0000

    Theorie 'light' des réels
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@3297 85f007b7-540e-0410-9357-904b9bb8a0f7

---
+++ theories/Reals/RealsB.v
@@ -0,0 +1,15 @@
+(***********************************************************************)
+(*  v      *   The Coq Proof Assistant  /  The Coq Development Team    *)
+(* <O___,, *        INRIA-Rocquencourt  &  LRI-CNRS-Orsay              *)
+(*   \VV/  *************************************************************)
+(*    //   *      This file is distributed under the terms of the      *)
+(*         *       GNU Lesser General Public License Version 2.1       *)
+(***********************************************************************)
+
+(*i $Id$ i*)
+
+Require Export Rdefinitions.
+Require Export TypeSyntax.
+Require Export Raxioms.
+Require Export Rbase.
+Require Export DiscrR.
