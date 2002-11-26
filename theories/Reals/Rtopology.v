22c9662db9caef7fbb3f51d89e17fb4aa3d52646
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2002-11-26 15:19:16 +0000

    Explicitation de NONA car sinon LEFTA par défaut; déplacement dans 5 qui est le niveau des relations
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@3294 85f007b7-540e-0410-9357-904b9bb8a0f7

--- theories/Reals/Rtopology.v
+++ theories/Reals/Rtopology.v
@@ -95,7 +95,7 @@ Qed.
 
 Definition eq_Dom [D1,D2:R->Prop] : Prop := (inclus D1 D2)/\(inclus D2 D1).
 
-Infix 6 "=_D" eq_Dom.
+Infix 5 "=_D" eq_Dom.
 
 Lemma ouvert_P1 : (D:R->Prop) (ouvert D) <-> D =_D (interieur D).
 Intro; Split.
