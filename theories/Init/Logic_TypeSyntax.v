450ef302a756c7ea29c06bb2cdb8053836f2a67c
barras <barras@85f007b7-540e-0410-9357-904b9bb8a0f7>
2003-03-14 10:54:34 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@3770 85f007b7-540e-0410-9357-904b9bb8a0f7

--- theories/Init/Logic_TypeSyntax.v
+++ theories/Init/Logic_TypeSyntax.v
@@ -19,25 +19,25 @@ Notation "x === y" := (identityT ? x y) (at level 5, no associativity)
 
 (* Order is important to give printing priority to fully typed ALL and EX *)
 
-Notation "'ALLT' x : t | p" := (allT t [x:t]p) (at level 10, p at level 8)
-  V8only (at level 200, x at level 80).
+Notation AllT := (allT ?).
 Notation "'ALLT' x | p"     := (allT ? [x]p)   (at level 10, p at level 8)
   V8only (at level 200, x at level 80).
-Notation AllT := (allT ?).
-
-Notation "'EXT' x : t | p"  := (exT t [x:t]p)  (at level 10, p at level 8)
+Notation "'ALLT' x : t | p" := (allT t [x:t]p) (at level 10, p at level 8)
   V8only (at level 200, x at level 80).
+
+Notation ExT  := (exT ?).
 Notation "'EXT' x | p"      := (exT ? [x]p)    (at level 10, p at level 8)
   V8only (at level 200, x at level 80).
-Notation ExT  := (exT ?).
+Notation "'EXT' x : t | p"  := (exT t [x:t]p)  (at level 10, p at level 8)
+  V8only (at level 200, x at level 80).
 
-Notation "'EXT' x : t | p & q" := (exT2 t [x:t]p [x:t]q)
-  (at level 10, p, q at level 8)
-  V8only "'EXT2' x : t | p & q" (at level 200, x at level 80).
+Notation ExT2 := (exT2 ?).
 Notation "'EXT' x | p & q"     := (exT2 ? [x]p [x]q)
   (at level 10, p, q at level 8)
   V8only "'EXT2' x | p & q" (at level 200, x at level 80).
-Notation ExT2 := (exT2 ?).
+Notation "'EXT' x : t | p & q" := (exT2 t [x:t]p [x:t]q)
+  (at level 10, p, q at level 8)
+  V8only "'EXT2' x : t | p & q" (at level 200, x at level 80).
 
 (** Parsing only of things in [Logic_type.v] *)
 
