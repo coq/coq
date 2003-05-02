35e296c8f2b90cdc8b4490ad1ea8d93b97f1417e
blanqui <blanqui@85f007b7-540e-0410-9357-904b9bb8a0f7>
2003-05-02 14:21:48 +0000

    Ajout du fichier 'kernel/symbol.ml': informations sur les symboles.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/branches/recriture@3986 85f007b7-540e-0410-9357-904b9bb8a0f7

---
+++ kernel/symbol.ml
@@ -0,0 +1,57 @@
+(***********************************************************************)
+(*  v      *   The Coq Proof Assistant  /  The Coq Development Team    *)
+(* <O___,, *        INRIA-Rocquencourt  &  LRI-CNRS-Orsay              *)
+(*   \VV/  *************************************************************)
+(*    //   *      This file is distributed under the terms of the      *)
+(*         *       GNU Lesser General Public License Version 2.1       *)
+(***********************************************************************)
+
+(* $Id$ *)
+
+open Util
+
+(* equational theories *)
+type eqth = Free | C | AC
+
+(* statuses *)
+type status = Mul | Lex | RevLex | Comb of (int list) list
+
+(* say if a combination is linear *)
+let is_linear_comb =
+  let indices = ref [] in
+  let rec is_linear_mul = function
+    | i::m' ->
+	if List.mem i !indices then false
+	else (indices := i::!indices; is_linear_mul m')
+    | _ -> true
+  in
+  let rec is_linear_lex = function
+    | m::l' -> is_linear_mul m & is_linear_lex l'
+    | _ -> true
+  in
+    fun l -> indices := []; is_linear_lex l
+
+(* say if a status is linear *)
+let is_linear = function
+  | Comb l -> is_linear_comb l
+  | _ -> true
+
+(* kinds of occurrences *)
+type delta = Pos | Neg | Nul
+
+let opp = function
+  | Pos -> Neg
+  | Neg -> Pos
+  | _ -> Nul
+
+(* termination criteria *)
+type termin = General_Schema
+
+(* information about symbols *)
+type symbol_info = {
+  symb_arity : int;
+  symb_eqth : eqth;
+  symb_status : status;
+  symb_mons : delta array;
+  symb_termin : termin;
+}
---
+++ kernel/symbol.mli
@@ -0,0 +1,35 @@
+(***********************************************************************)
+(*  v      *   The Coq Proof Assistant  /  The Coq Development Team    *)
+(* <O___,, *        INRIA-Rocquencourt  &  LRI-CNRS-Orsay              *)
+(*   \VV/  *************************************************************)
+(*    //   *      This file is distributed under the terms of the      *)
+(*         *       GNU Lesser General Public License Version 2.1       *)
+(***********************************************************************)
+
+(* $Id$ *)
+
+(* equational theories *)
+type eqth = Free | C | AC
+
+(* statuses *)
+type status = Mul | Lex | RevLex | Comb of (int list) list
+
+(* say if a status is linear *)
+val is_linear : status -> bool
+
+(* kinds of occurrences *)
+type delta = Pos | Neg | Nul
+
+val opp : delta -> delta
+
+(* termination criteria *)
+type termin = General_Schema
+
+(* information about symbols *)
+type symbol_info = {
+  symb_arity : int;
+  symb_eqth : eqth;
+  symb_status : status;
+  symb_mons : delta array;
+  symb_termin : termin;
+}
