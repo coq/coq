af841b20c9aed32226a6e977d1ff87435783b574
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2003-10-29 09:01:19 +0000

    Choix sous sa forme relationnelle
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@4741 85f007b7-540e-0410-9357-904b9bb8a0f7

---
+++ theories/Logic/RelationalChoice.v
@@ -0,0 +1,17 @@
+(***********************************************************************)
+(*  v      *   The Coq Proof Assistant  /  The Coq Development Team    *)
+(* <O___,, *        INRIA-Rocquencourt  &  LRI-CNRS-Orsay              *)
+(*   \VV/  *************************************************************)
+(*    //   *      This file is distributed under the terms of the      *)
+(*         *       GNU Lesser General Public License Version 2.1       *)
+(***********************************************************************)
+
+(*i $Id$ i*)
+
+(* This file axiomatizes the relational form of the axiom of choice *)
+
+Axiom relational_choice :
+ (A:Type;B:Type;R: A->B->Prop) 
+  ((x:A)(EX y:B|(R x y)))
+   -> (EXT R':A->B->Prop | 
+         ((x:A)(EX y:B|(R x y)/\(R' x y)/\ ((y':B) (R' x y') -> y=y')))).
