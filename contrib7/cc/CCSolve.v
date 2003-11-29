7ee14e9fb95e4e182f9983278275a6052d9b60da
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2003-11-29 18:56:22 +0000

    Deplacement des fichiers ancienne syntaxe dans theories7, contrib7 et states7
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@5033 85f007b7-540e-0410-9357-904b9bb8a0f7

---
+++ contrib7/cc/CCSolve.v
@@ -0,0 +1,20 @@
+(***********************************************************************)
+(*  v      *   The Coq Proof Assistant  /  The Coq Development Team    *)
+(* <O___,, *        INRIA-Rocquencourt  &  LRI-CNRS-Orsay              *)
+(*   \VV/  *************************************************************)
+(*    //   *      This file is distributed under the terms of the      *)
+(*         *       GNU Lesser General Public License Version 2.1       *)
+(***********************************************************************)
+
+(* $Id$ *)
+
+Tactic Definition CCsolve :=
+ Repeat (Match Context With
+ [ H: ?1 |- ?2] -> 
+  Let Heq = FreshId "Heq" In
+ (Assert Heq:(?2==?1);[Congruence|(Rewrite Heq;Exact H)])
+ |[ H: ?1; G: ?2 -> ?3 |- ?] ->                        
+  Let Heq = FreshId "Heq" In
+ (Assert Heq:(?2==?1) ;[Congruence|                              
+     (Rewrite Heq in G;Generalize (G H);Clear G;Intro G)])).  
+
