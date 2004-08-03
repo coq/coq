5fdbd65d54f6ce49aea432ce460db0ec94d87562
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2004-08-03 21:14:32 +0000

    bibtex et hevea
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@8581 85f007b7-540e-0410-9357-904b9bb8a0f7

--- doc/newfaq/run.sh
+++ doc/newfaq/run.sh
@@ -1,2 +1,6 @@
 #/bin/sh
-coq-tex -n 72 -v -sl -small main.tex && latex main.v.tex 
+coq-tex -n 72 -v -sl -small main.tex && latex main.v.tex && bibtex main.v && latex main.v.tex && latex main.v.tex && hevea -fix -nosymb main.v.tex
+
+# Commands for installation on pauillac
+# scp main.v001.gif interval_discr.v pauillac.inria.fr:/net/pauillac/infosystems/www/coq/doc
+# scp main.v.html pauillac.inria.fr:/net/pauillac/infosystems/www/coq/doc/faq.html
