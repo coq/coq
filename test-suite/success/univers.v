59cfce595a93abad9ec50f9f72c0aebfb374d124
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2004-09-24 18:06:12 +0000

    Ajout bug #255
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/branches/V8-0-bugfix@6128 85f007b7-540e-0410-9357-904b9bb8a0f7

--- test-suite/success/univers.v
+++ test-suite/success/univers.v
@@ -17,3 +17,24 @@ Lemma lem3 : (P:Prop)P.
 Intro P ; Pattern P.
 Apply lem2.
 Abort.
+
+(* Check managing of universe constraints in inversion *)
+(* Bug report #855 *)
+
+Inductive dep_eq : (X:Type) X -> X -> Prop :=
+   | intro_eq : (X:Type) (f:X)(dep_eq X f f)
+   | intro_feq : (A:Type) (B:A->Type)
+                 let T = (x:A)(B x) in
+                 (f, g:T) (x:A)
+                 (dep_eq (B x) (f x) (g x)) ->
+                 (dep_eq T f g).
+                                                                                
+Require Import Relations.
+                                                                                
+Theorem dep_eq_trans : (X:Type) (transitive X (dep_eq X)).
+Proof.
+  Unfold transitive.
+  Intros X f g h H1 H2.
+  Inversion H1.
+Abort.
+
