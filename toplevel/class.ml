02dfcb0c12ec84b46751b45a0fdc78b5c38ab285
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2004-11-26 18:06:54 +0000

    Réduire pour trouver l'arité d'une classe
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/branches/V8-0-bugfix@6354 85f007b7-540e-0410-9357-904b9bb8a0f7

--- toplevel/class.ml
+++ toplevel/class.ml
@@ -92,15 +92,13 @@ let explain_coercion_error g = function
 
 (* Verifications pour l'ajout d'une classe *)
 
-let rec arity_sort a = match kind_of_term a with
-  | Sort (Prop _ | Type _) -> 0
-  | Prod (_,_,c) -> (arity_sort c) +1
-  | LetIn (_,_,_,c) -> arity_sort c    (* Utile ?? *)
-  | Cast (c,_) -> arity_sort c
+let rec arity_sort (ctx,a) = match kind_of_term a with
+  | Sort (Prop _ | Type _) -> List.length ctx
   | _ -> raise Not_found
 
 let check_reference_arity ref =
-  try arity_sort (Global.type_of_global ref)
+  let t = Global.type_of_global ref in
+  try arity_sort (Reductionops.splay_prod (Global.env()) Evd.empty t)
   with Not_found -> raise (CoercionError (NotAClass ref))
 
 let check_arity = function
