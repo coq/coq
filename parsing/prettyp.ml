2b039e21cc3a166dd42c99967a5adfae35219a1e
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2005-01-13 16:44:00 +0000

    Amélioration message Locate
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@6583 85f007b7-540e-0410-9357-904b9bb8a0f7

--- parsing/prettyp.ml
+++ parsing/prettyp.ml
@@ -216,7 +216,7 @@ let print_located_qualid ref =
 	(fun (o,oqid) ->
 	  hov 2 (pr_located_qualid o ++
 	  (if oqid <> qid then
-	    spc() ++ str "(shorter name to refer to it is " ++ pr_qualid oqid ++ str")"
+	    spc() ++ str "(shorter name to refer to it in current context is " ++ pr_qualid oqid ++ str")"
 	  else
 	    mt ()))) l
 
