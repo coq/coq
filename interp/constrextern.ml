bcd3da0a8dc3e74de3a0996319a4c250387f3011
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2005-02-07 14:01:50 +0000

    Bug affichage rawconstr
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/branches/V8-0-bugfix@6697 85f007b7-540e-0410-9357-904b9bb8a0f7

--- interp/constrextern.ml
+++ interp/constrextern.ml
@@ -1605,13 +1605,15 @@ let rec extern inctx scopes vars r =
 
   | RLetTuple (loc,nal,(na,typopt),tm,b) ->
       CLetTuple (loc,nal,
-        (Some na,option_app (extern_type scopes (add_vname vars na)) typopt),
+        (option_app (fun _ -> na) typopt,
+`        option_app (extern_type scopes (add_vname vars na)) typopt),
         sub_extern false scopes vars tm,
         extern false scopes (List.fold_left add_vname vars nal) b)
 
   | RIf (loc,c,(na,typopt),b1,b2) ->
       CIf (loc,sub_extern false scopes vars c,
-        (Some na,option_app (extern_type scopes (add_vname vars na)) typopt),
+        (option_app (fun _ -> na) typopt,
+         option_app (extern_type scopes (add_vname vars na)) typopt),
         sub_extern false scopes vars b1, sub_extern false scopes vars b2)
 
   | RRec (loc,fk,idv,blv,tyv,bv) ->
