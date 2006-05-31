d5b8d356ca0441dc96b87bd7615147813d7aa64a
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2006-05-31 22:32:33 +0000

    Fusion destruct.v et Destruct.v (MacOS X ne sait pas distinguer la casse
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@8884 85f007b7-540e-0410-9357-904b9bb8a0f7

--- test-suite/success/destruct.v
+++ test-suite/success/destruct.v
@@ -1,3 +1,17 @@
+(* Submitted by Robert Schneck *)
+
+Parameter A B C D : Prop.
+Axiom X : A -> B -> C /\ D.
+
+Lemma foo : A -> B -> C.
+Proof.
+intros. 
+destruct X. (* Should find axiom X and should handle arguments of X *)
+assumption.
+assumption.
+assumption.
+Qed.
+
 (* Simplification of bug 711 *)
 
 Parameter f : true = false.
@@ -7,3 +21,5 @@ set (b := true) in *.
 (* Check that it doesn't fail with an anomaly *)
 (* Ultimately, adapt destruct to make it succeeding *)
 try destruct b.
+Abort.
+
