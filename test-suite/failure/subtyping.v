e4dfd24e4a4d4251c79694d82b1aa860657ee6dc
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2007-01-17 14:47:50 +0000

    Correction bug #1302
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@9494 85f007b7-540e-0410-9357-904b9bb8a0f7

---
+++ test-suite/failure/subtyping.v
@@ -0,0 +1,21 @@
+(* A variant of bug #1302 that must fail *)
+
+Module Type T.
+
+ Parameter A : Type.
+
+ Inductive L : Prop := 
+ | L0
+ | L1 :  (A -> Prop) -> L.
+
+End T.
+
+Module TT : T. 
+
+ Parameter A : Type.
+
+ Inductive L : Type := 
+ | L0
+ | L1 :  (A -> Prop) -> L.
+
+End TT.
