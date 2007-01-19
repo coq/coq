d185c837846520db04383f84a65ae13bc2074955
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2007-01-19 17:11:39 +0000

    Export de l'afficheur de substitutions de noms de modules pour le débogueur
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@9504 85f007b7-540e-0410-9357-904b9bb8a0f7

--- dev/top_printers.ml
+++ dev/top_printers.ml
@@ -81,6 +81,8 @@ let genppj f j = let (c,t) = f j in (c ++ str " : " ++ t)
 
 let ppj j = pp (genppj pr_ljudge j)
 
+let prsubst s = pp (Mod_subst.debug_pr_subst s)
+
 (* proof printers *)
 let ppevm evd = pp(pr_evar_map evd)
 let ppevd evd = pp(pr_evar_defs evd)
