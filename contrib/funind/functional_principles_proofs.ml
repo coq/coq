33e5b607ff8a6cf798798ab58f6dee32a91e41f7
jforest <jforest@85f007b7-540e-0410-9357-904b9bb8a0f7>
2007-02-11 23:50:39 +0000

    correction d'un bug dans la génération des principes d'induction
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/branches/v8.1@9640 85f007b7-540e-0410-9357-904b9bb8a0f7

--- contrib/funind/functional_principles_proofs.ml
+++ contrib/funind/functional_principles_proofs.ml
@@ -1474,7 +1474,7 @@ let prove_principle_for_gen
  	 (mkApp (delayed_force acc_rel,[|input_type;relation;mkVar rec_arg_id|]))
       );
       observe_tac "reverting" (revert (List.rev (acc_rec_arg_id::args_ids)));
-      observe_tac "h_fix" (h_fix (Some fix_id) (npost_rec_arg + 1)); 
+      observe_tac "h_fix" (h_fix (Some fix_id) (List.length args_ids + 1)); 
       h_intros (List.rev (acc_rec_arg_id::args_ids));
       Equality.rewriteLR (mkConst eq_ref);
       observe_tac "finish" (fun gl' -> 
