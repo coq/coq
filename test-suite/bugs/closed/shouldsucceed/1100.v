f1306fd7d7ed91d9fb3e0eb8d12fa628783ec392
notin <notin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2007-12-12 12:27:48 +0000

    Ajout d'un test pour le bug #1100
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@10370 85f007b7-540e-0410-9357-904b9bb8a0f7

---
+++ test-suite/bugs/closed/shouldsucceed/1100.v
@@ -0,0 +1,12 @@
+Require Import Setoid.
+
+Parameter P : nat -> Prop.
+Parameter Q : nat -> Prop.
+Parameter PQ : forall n, P n <-> Q n.
+
+Lemma PQ2 : forall n, P n -> Q n.
+  intros.
+  rewrite PQ in H. 
+  trivial.
+Qed.
+
