c61b48f8b123e572b33c6d080a2b09aa5ecde979
notin <notin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2008-02-01 19:22:41 +0000

    Ajout de 2 tests
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@10499 85f007b7-540e-0410-9357-904b9bb8a0f7

---
+++ test-suite/bugs/closed/1519.v
@@ -0,0 +1,23 @@
+Section S.
+
+  Variable A:Prop.
+  Variable W:A.
+  
+  Remark T: A -> A.
+    intro Z.
+    rename W into Z_.
+    rename Z into W.
+    rename Z_ into Z.
+    exact Z.
+  Qed.
+
+  (* bug :
+     Error:
+     Unbound reference: In environment
+     A : Prop
+     W : A
+     Z : A
+     The reference 2 is free
+     *)
+
+End S.
