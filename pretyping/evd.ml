coq/coq

commit d081dcfaedb5b7e2ad78574a053bcebc4bfb564a

Proper implicit arguments handling for assumptions (Axiom/Variable...). New tactic clapply to apply unapplied class methods in tactic mode, simple solution to the fact that apply does not work up-to classes yet. Add Functions.v for class definitions related to functional morphisms.
git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@10589 85f007b7-540e-0410-9357-904b9bb8a0f7

toplevel/command.ml
@@ -177,7 +177,7 @@ let syntax_definition ident c local onlyparse =
 let assumption_message id =
   if_verbose message ((string_of_id id) ^ " is assumed")
 
-let declare_one_assumption is_coe (local,kind) c nl (_,ident) =
+let declare_one_assumption is_coe (local,kind) c imps nl (_,ident) =
   let r = match local with
     | Local when Lib.sections_are_opened () ->
         let _ = 
@@ -191,11 +191,14 @@ let declare_one_assumption is_coe (local,kind) c nl (_,ident) =
     | (Global|Local) ->
         let kn =
           declare_constant ident (ParameterEntry (c,nl), IsAssumption kind) in
+	let gr = ConstRef kn in
+	  if Impargs.is_implicit_args () || imps <> [] then
+	    declare_manual_implicits false gr (Impargs.is_implicit_args ()) imps;
         assumption_message ident;
         if local=Local & Flags.is_verbose () then
           msg_warning (pr_id ident ++ str" is declared as a parameter" ++
           str" because it is at a global level");
-        ConstRef kn in
+        gr in
   if is_coe then Class.try_add_new_coercion r local
 
 let declare_assumption_hook = ref ignore
@@ -204,9 +207,12 @@ let set_declare_assumption_hook = (:=) declare_assumption_hook
 let declare_assumption idl is_coe k bl c nl=
   if not (Pfedit.refining ()) then 
     let c = generalize_constr_expr c bl in
-    let c = interp_type Evd.empty (Global.env()) c in
-      !declare_assumption_hook c;
-      List.iter (declare_one_assumption is_coe k c nl) idl
+    let sigma = Evd.empty and env = Global.env () in
+    let ic = intern_type sigma env c in
+    let imps = Implicit_quantifiers.implicits_of_rawterm ic in
+    let j = Default.understand_judgment sigma env ic in
+      !declare_assumption_hook j.uj_val;
+      List.iter (declare_one_assumption is_coe k j.uj_val imps nl) idl
   else
     errorlabstrm "Command.Assumption"
 	(str "Cannot declare an assumption while in proof editing mode.")
@@ -1026,9 +1032,16 @@ let start_proof_com sopt kind (bl,t) hook =
  	  (Pfedit.get_all_proof_names ())
   in
   let env = Global.env () in
-  let c = interp_type Evd.empty env (generalize_constr_expr t bl) in
-  let _ = Typeops.infer_type env c in
-  start_proof id kind c hook
+  let sigma = Evd.empty in
+  let b = generalize_constr_expr t bl in
+  let ib = intern_type sigma env b in
+  let imps = Implicit_quantifiers.implicits_of_rawterm ib in
+  let j = Default.understand_judgment sigma env ib in
+    start_proof id kind j.uj_val
+      (fun str cst -> 
+	if Impargs.is_implicit_args () || imps <> [] then
+	  declare_manual_implicits false cst (Impargs.is_implicit_args ()) imps;
+	hook str cst)
 
 let check_anonymity id save_ident =
   if atompart_of_id id <> "Unnamed_thm" then

Makefile.common
@@ -748,7 +748,8 @@ SETOIDSVO:= theories/Setoids/Setoid_tac.vo theories/Setoids/Setoid_Prop.vo theor
 UNICODEVO:= theories/Unicode/Utf8.vo
 
 CLASSESVO:= theories/Classes/Init.vo theories/Classes/Relations.vo theories/Classes/Morphisms.vo \
-	 theories/Classes/SetoidClass.vo theories/Classes/SetoidTactics.vo theories/Classes/SetoidDec.vo
+	theories/Classes/Functions.vo theories/Classes/Equivalence.vo \
+	theories/Classes/SetoidClass.vo theories/Classes/SetoidTactics.vo theories/Classes/SetoidDec.vo
 
 THEORIESVO:=\
   $(INITVO) $(LOGICVO) $(ARITHVO) $(BOOLVO) $(NARITHVO) $(ZARITHVO) \

contrib/subtac/subtac.ml
@@ -100,7 +100,7 @@ let declare_assumption env isevars idl is_coe k bl c nl =
       Subtac_pretyping.subtac_process env isevars id [] (Command.generalize_constr_expr c bl) None 
     in
     let c = solve_tccs_in_type env id isevars evm c typ in
-      List.iter (Command.declare_one_assumption is_coe k c nl) idl
+      List.iter (Command.declare_one_assumption is_coe k c imps nl) idl
   else
     errorlabstrm "Command.Assumption"
 	(str "Cannot declare an assumption while in proof editing mode.")
@@ -149,7 +149,7 @@ let subtac (loc, command) =
 	  vernac_assumption env isevars stre l nl
 
       | VernacInstance (sup, is, props) ->
-	  Subtac_classes.new_instance sup is props
+	  ignore(Subtac_classes.new_instance sup is props)
 
       | VernacCoFixpoint (l, b) ->
 	  let _ = trace (str "Building cofixpoint") in

contrib/subtac/subtac_classes.ml
@@ -221,4 +221,5 @@ let new_instance ctx (instid, bk, cl) props =
   in
   let evm = Subtac_utils.evars_of_term (Evd.evars_of !isevars) Evd.empty term in
   let obls, constr, typ = Eterm.eterm_obligations env id !isevars evm 0 term termtype in
-    ignore (Subtac_obligations.add_definition id constr typ ~kind:Instance ~hook obls)
+    ignore(Subtac_obligations.add_definition id constr typ ~kind:Instance ~hook obls);
+    id

contrib/subtac/subtac_classes.mli
@@ -36,4 +36,4 @@ val new_instance :
   Topconstr.local_binder list ->
   typeclass_constraint ->
   binder_def_list ->
-  unit
+  identifier

tactics/class_tactics.ml4
@@ -276,16 +276,21 @@ let valid evm p res_sigma l =
 	else (gls, sigma))
       !res_sigma (l, Evd.create_evar_defs !res_sigma)
   in raise (Found (snd evd'))
+
+let refresh_evi evi = 
+  { evi with 
+(*     evar_hyps = Environ.map_named_val Termops.refresh_universes evi.evar_hyps ; *)
+    evar_concl = Termops.refresh_universes evi.evar_concl }
     
 let resolve_all_evars_once debug (mode, depth) env p evd =
   let evm = Evd.evars_of evd in
-  let goals = 
+  let goals, evm = 
     Evd.fold
-      (fun ev evi gls ->
-	if evi.evar_body = Evar_empty && p ev evi then
-	  (evi :: gls)
-	else gls)
-      evm []
+      (fun ev evi (gls, evm) ->
+	let evi = refresh_evi evi in
+	  (if evi.evar_body = Evar_empty && p ev evi then evi :: gls else gls), 
+	Evd.add evm ev evi)
+      evm ([], Evd.empty)
   in
   let gls = { it = List.rev goals; sigma = evm } in
   let res_sigma = ref evm in
@@ -691,3 +696,22 @@ END
 
 let _ = 
   Classes.refine_ref := Refine.refine
+
+open Pretype_errors
+
+let typeclass_error e = 
+  match e with
+    | UnsolvableImplicit (evi, (InternalHole | ImplicitArg _)) ->
+	(match class_of_constr evi.evar_concl with
+	    Some c -> true
+	  | None -> false)
+    | _ -> false
+	
+let class_apply c = 
+  try Tactics.apply_with_ebindings c
+  with PretypeError (env, e) when typeclass_error e -> 
+    tclFAIL 1 (str"typeclass resolution failed")
+
+TACTIC EXTEND class_apply
+| [ "class_apply" constr_with_bindings(t) ] -> [ class_apply t ]
+END

theories/Classes/Functions.v
@@ -1,0 +1,43 @@
+(* -*- coq-prog-args: ("-emacs-U" "-nois") -*- *)
+(************************************************************************)
+(*  v      *   The Coq Proof Assistant  /  The Coq Development Team     *)
+(* <O___,, * CNRS-Ecole Polytechnique-INRIA Futurs-Universite Paris Sud *)
+(*   \VV/  **************************************************************)
+(*    //   *      This file is distributed under the terms of the       *)
+(*         *       GNU Lesser General Public License Version 2.1        *)
+(************************************************************************)
+
+(* Functional morphisms.
+ 
+   Author: Matthieu Sozeau
+   Institution: LRI, CNRS UMR 8623 - UniversitÃcopyright Paris Sud
+   91405 Orsay, France *)
+
+(* $Id: FSetAVL_prog.v 616 2007-08-08 12:28:10Z msozeau $ *)
+
+Require Import Coq.Program.Program.
+Require Export Coq.Classes.Relations.
+Require Export Coq.Classes.Morphisms.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+
+Class [ m : Morphism (A -> B) (RA ++> RB) f ] => Injective :=
+  injective : forall x y : A, RB (f x) (f y) -> RA x y.
+
+Class [ m : Morphism (A -> B) (RA ++> RB) f ] => Surjective :=
+  surjective : forall y, exists x : A, RB y (f x).
+
+Definition Bijective [ m : Morphism (A -> B) (RA ++> RB) (f : A -> B) ] :=
+  Injective m /\ Surjective m.
+
+Class [ m : Morphism (A -> B) (eqA ++> eqB) ] => MonoMorphism :=
+  monic :> Injective m.
+
+Class [ m : Morphism (A -> B) (eqA ++> eqB) ] => EpiMorphism :=
+  epic :> Surjective m.
+
+Class [ m : Morphism (A -> B) (eqA ++> eqB) ] => IsoMorphism :=
+  monomorphism :> MonoMorphism m ; epimorphism :> EpiMorphism m.
+
+Class [ m : Morphism (A -> A) (eqA ++> eqA), ? IsoMorphism m ] => AutoMorphism.

theories/Classes/Init.v
@@ -16,3 +16,6 @@
 (* $Id: Init.v 616 2007-08-08 12:28:10Z msozeau $ *)
 
 (* Ltac typeclass_instantiation := typeclasses eauto || eauto. *)
+
+Tactic Notation "clapply" ident(c) :=
+  eapply @c ; eauto with typeclass_instances.

commit 123c8683a25b34070d4874df4cdd9381d5ceac24

Correct bug introduced in r10589, where we lost information that assumption types are types when type-checking them and necessary coercions were not inserted. Add empty_evar_defs definition in Evd and call the new helper function in constrintern that performs interpretation and gives back implicit argument information.
git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@10706 85f007b7-540e-0410-9357-904b9bb8a0f7

pretyping/evd.ml
@@ -414,6 +414,7 @@ let create_evar_defs sigma =
 let create_goal_evar_defs sigma =
   let h = fold (fun mv _ l -> (mv,(dummy_loc,GoalEvar))::l) sigma [] in
   { evars=sigma; conv_pbs=[]; last_mods = []; history=h; metas=Metamap.empty }
+let empty_evar_defs = create_evar_defs empty
 let evars_of d = d.evars
 let evars_reset_evd evd d = {d with evars = evd}
 let reset_evd (sigma,mmap) d = {d with evars = sigma; metas=mmap}

pretyping/evd.mli
@@ -143,6 +143,7 @@ val subst_evar_defs_light : substitution -> evar_defs -> evar_defs
 (* create an [evar_defs] with empty meta map: *)
 val create_evar_defs      : evar_map -> evar_defs
 val create_goal_evar_defs : evar_map -> evar_defs
+val empty_evar_defs : evar_defs
 val evars_of         : evar_defs -> evar_map
 val evars_reset_evd  : evar_map ->  evar_defs -> evar_defs
 

toplevel/command.ml
@@ -205,12 +205,10 @@ let set_declare_assumption_hook = (:=) declare_assumption_hook
 let declare_assumption idl is_coe k bl c nl=
   if not (Pfedit.refining ()) then 
     let c = generalize_constr_expr c bl in
-    let sigma = Evd.empty and env = Global.env () in
-    let ic = intern_type sigma env c in
-    let imps = Implicit_quantifiers.implicits_of_rawterm ic in
-    let j = Default.understand_judgment sigma env ic in
-      !declare_assumption_hook j.uj_val;
-      List.iter (declare_one_assumption is_coe k j.uj_val imps nl) idl
+    let sigma = ref Evd.empty_evar_defs and env = Global.env () in
+    let c', imps = interp_type_evars_impls sigma env c in
+      !declare_assumption_hook c';
+      List.iter (declare_one_assumption is_coe k c' imps nl) idl
   else
     errorlabstrm "Command.Assumption"
 	(str "Cannot declare an assumption while in proof editing mode.")