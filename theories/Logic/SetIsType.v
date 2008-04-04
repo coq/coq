a418f664cbb289c4b3bf72aa2111ee02b8d44013
letouzey <letouzey@85f007b7-540e-0410-9357-904b9bb8a0f7>
2008-04-04 19:10:01 +0000

    A file that can be loaded when a migration from Set to Type is desired (for instance as a consequence of the Set/Type switch of a library such as FSets). After a "Require Export SetIsType.", Set becomes a mere notation for Type (I love notations !).
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@10757 85f007b7-540e-0410-9357-904b9bb8a0f7

---
+++ theories/Logic/SetIsType.v
@@ -0,0 +1,17 @@
+(************************************************************************)
+(*  v      *   The Coq Proof Assistant  /  The Coq Development Team     *)
+(* <O___,, * CNRS-Ecole Polytechnique-INRIA Futurs-Universite Paris Sud *)
+(*   \VV/  **************************************************************)
+(*    //   *      This file is distributed under the terms of the       *)
+(*         *       GNU Lesser General Public License Version 2.1        *)
+(************************************************************************)
+
+(** * The Set universe seen as a synonym for Type *)
+
+(** After loading this file, Set becomes just another name for Type.
+    This allows to easily perform a Set-to-Type migration, or at least
+    test whether a development relies or not on specific features of
+    Set: simply insert some Require Export of this file at starting
+    points of the development and try to recompile...  *)
+
+Notation "'Set'" := Type (only parsing).
