46f35c939cb0a17b68a0aef8119c73a6c1144a4d
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2008-04-05 22:25:00 +0000

    Suite 10760
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@10761 85f007b7-540e-0410-9357-904b9bb8a0f7

--- theories/Reals/Rfunctions.v
+++ theories/Reals/Rfunctions.v
@@ -350,7 +350,6 @@ Proof.
   rewrite <- Rinv_pow; auto.
   rewrite RPow_abs; auto.
   rewrite H'0; rewrite Rabs_right; auto with real.
-  apply Rle_ge; auto with real.
   apply Rlt_pow; auto with arith.
   rewrite Rabs_Rinv; auto.
   apply Rmult_lt_reg_l with (r := Rabs r).
