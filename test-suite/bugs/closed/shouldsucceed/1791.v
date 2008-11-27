4f95d3b90ea07f0b26fc09f4b4abb769c682069f
barras <barras@85f007b7-540e-0410-9357-904b9bb8a0f7>
2008-11-27 17:19:55 +0000

    ported r11636: fix of big 1791
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/branches/v8.2@11637 85f007b7-540e-0410-9357-904b9bb8a0f7

---
+++ test-suite/bugs/closed/shouldsucceed/1791.v
@@ -0,0 +1,38 @@
+(* simpl performs eta expansion *)
+
+Set Implicit Arguments.
+Require Import List.
+
+Definition k0 := Set.
+Definition k1 := k0 -> k0.
+
+(** iterating X n times *)
+Fixpoint Pow (X:k1)(k:nat){struct k}:k1:=
+  match k with 0 => fun X => X
+             |  S k' => fun A => X (Pow X k' A)   
+  end.
+
+Parameter Bush: k1.
+Parameter BushToList: forall (A:k0), Bush A ->  list A.
+
+Definition BushnToList (n:nat)(A:k0)(t:Pow Bush n A): list A.
+Proof.
+  intros.
+  induction n.
+  exact (t::nil).
+  simpl in t.
+  exact (flat_map IHn (BushToList t)).
+Defined.
+
+Parameter bnil : forall (A:k0), Bush A.
+Axiom BushToList_bnil: forall (A:k0), BushToList (bnil A) = nil(A:=A).
+
+Lemma BushnToList_bnil (n:nat)(A:k0):
+  BushnToList (S n) A (bnil (Pow Bush n A)) = nil.
+Proof.
+  intros.
+  simpl.
+  rewrite BushToList_bnil.
+  simpl.
+  reflexivity.
+Qed.
