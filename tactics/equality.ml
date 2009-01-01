coq/coq

commit b18a6d179903546cbf5745e601ab221f06e30078

- Added support for subterm matching in SearchAbout. - Backtrack on precise unfolding of "iff" in "tauto": it has effects on   the naming of hypotheses (especially when doing "case H" with H of   type "{x|P<->Q}" since not unfolding will eventually introduce a name   "i" while unfolding will eventually introduce a name "a" (deep sigh). - Miscellaneous (error when a plugin is missing, doc hnf, standardization   of names manipulating type constr_pattern, ...).
git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@11725 85f007b7-540e-0410-9357-904b9bb8a0f7

doc/common/macros.tex
@@ -47,6 +47,7 @@
 \newcommand{\Rem}{\medskip \noindent {\bf Remark: }}
 \newcommand{\Rems}{\medskip \noindent {\bf Remarks: }}
 \newcommand{\Example}{\medskip \noindent {\bf Example: }}
+\newcommand{\Examples}{\medskip \noindent {\bf Examples: }}
 \newcommand{\Warning}{\medskip \noindent {\bf Warning: }}
 \newcommand{\Warns}{\medskip \noindent {\bf Warnings: }}
 \newcounter{ex}
@@ -60,8 +61,6 @@
 \newenvironment{ErrMsgs}{\ErrMsgx\begin{enumerate}}{\end{enumerate}}
 \newenvironment{Remarks}{\Rems\begin{enumerate}}{\end{enumerate}}
 \newenvironment{Warnings}{\Warns\begin{enumerate}}{\end{enumerate}}
-\newenvironment{Examples}{\medskip\noindent{\bf Examples:}
-\begin{enumerate}}{\end{enumerate}}
 
 %\newcommand{\bd}{\noindent\bf}
 %\newcommand{\sbd}{\vspace{8pt}\noindent\bf}
@@ -191,11 +190,12 @@
 \newcommand{\nestedpattern}{\nterm{nested\_pattern}}
 \newcommand{\name}{\nterm{name}}
 \newcommand{\num}{\nterm{num}}
-\newcommand{\pattern}{\nterm{pattern}}
+\newcommand{\pattern}{\nterm{pattern}} % pattern for pattern-matching
 \newcommand{\orpattern}{\nterm{or\_pattern}}
 \newcommand{\intropattern}{\nterm{intro\_pattern}}
 \newcommand{\disjconjintropattern}{\nterm{disj\_conj\_intro\_pattern}}
 \newcommand{\namingintropattern}{\nterm{naming\_intro\_pattern}}
+\newcommand{\termpattern}{\nterm{term\_pattern}} % term with holes
 \newcommand{\pat}{\nterm{pat}}
 \newcommand{\pgs}{\nterm{pgms}}
 \newcommand{\pg}{\nterm{pgm}}

doc/refman/RefMan-oth.tex
@@ -134,6 +134,8 @@ environment}\\
     There is no constant in the environment named \qualid.
 \end{ErrMsgs}
 
+\newcommand{\termpatternorstr}{{\termpattern}\textrm{\textsl{-}}{\str}}
+
 \begin{Variants}
 \item {\tt SearchAbout {\str}.}
 
@@ -151,30 +153,32 @@ The string {\str} must be a notation or the main symbol of a notation
 which is then interpreted in the scope bound to the delimiting key
 {\delimkey} (see Section~\ref{scopechange}).
 
-\item {\tt SearchAbout [ \nelist{\zeroone{-}\textrm{\textsl{qualid-or-string}}}{}
+\item {\tt SearchAbout {\termpattern}.}
+
+This searches for all statements or types of definition that contains
+a subterm that matches the pattern {\termpattern} (holes of the
+pattern are either denoted by ``{\texttt \_}'' or
+by ``{\texttt ?{\ident}}'' when non linear patterns are expected).
+
+\item {\tt SearchAbout [ \nelist{\zeroone{-}{\termpatternorstr}}{}
 ].}\\
-\noindent where {\textrm{\textsl{qualid-or-string}}} is a {\qualid} or
-a {\str}, or a {\str} followed by a scope delimiting key 
-{\tt \%{\delimkey}}.
+
+\noindent where {\termpatternorstr} is a
+{\termpattern} or a {\str}, or a {\str} followed by a scope
+delimiting key {\tt \%{\delimkey}}.
 
 This generalization of {\tt SearchAbout} searches for all objects
-whose statement mentions all of {\qualid} (or {\str} if {\str} is the
-notation for a reference) and whose name contains all {\str} that
-correspond to valid identifiers. If a {\qualid} or a {\str} is
-prefixed by ``-'', the search excludes the objects that mention that
-{\qualid} or that {\str}.
-
-\Example
-
-\begin{coq_example}
-Require Import ZArith.
-SearchAbout [ Zmult Zplus "distr" ].
-\end{coq_example}
+whose statement or type contains a subterm matching {\termpattern} (or
+{\qualid} if {\str} is the notation for a reference {\qualid}) and
+whose name contains all {\str} of the request that correspond to valid
+identifiers. If a {\termpattern} or a {\str} is prefixed by ``-'', the
+search excludes the objects that mention that {\termpattern} or that
+{\str}.
 
 \item
 \begin{tabular}[t]{@{}l}
-  {\tt SearchAbout {\term} inside {\module$_1$} \ldots{} {\module$_n$}.} \\
-  {\tt SearchAbout [ \nelist{\textrm{\textsl{qualid-or-string}}}{} ]
+  {\tt SearchAbout {\termpatternorstr} inside {\module$_1$} \ldots{} {\module$_n$}.} \\
+  {\tt SearchAbout [ \nelist{{\termpatternorstr}}{} ]
     inside {\module$_1$} \ldots{} {\module$_n$}.}
 \end{tabular}
 
@@ -183,8 +187,8 @@ This restricts the search to constructions defined in modules
 
 \item
 \begin{tabular}[t]{@{}l}
-  {\tt SearchAbout {\term} outside {\module$_1$}...{\module$_n$}.} \\
-  {\tt SearchAbout [ \nelist{\textrm{\textsl{qualid-or-string}}}{} ]
+  {\tt SearchAbout {\termpatternorstr} outside {\module$_1$}...{\module$_n$}.} \\
+  {\tt SearchAbout [ \nelist{{\termpatternorstr}}{} ]
      outside {\module$_1$}...{\module$_n$}.}
 \end{tabular}
 
@@ -193,11 +197,25 @@ This restricts the search to constructions not defined in modules
 
 \end{Variants}
 
-\subsection[\tt SearchPattern {\term}.]{\tt SearchPattern {\term}.\comindex{SearchPattern}}
+\Examples
+
+\begin{coq_example*}
+Require Import ZArith.
+\end{coq_example*}
+\begin{coq_example}
+SearchAbout [ Zmult Zplus "distr" ].
+SearchAbout [ "+"%Z "*"%Z "distr" -positive -Prop].
+SearchAbout (?x * _ + ?x * _)%Z outside OmegaLemmas.
+\end{coq_example}
+
+\subsection[\tt SearchPattern {\termpattern}.]{\tt SearchPattern {\term}.\comindex{SearchPattern}}
 
 This command displays the name and type of all theorems of the current
 context whose statement's conclusion matches the expression {\term}
-where holes in the latter are denoted by ``{\texttt \_}''.
+where holes in the latter are denoted by ``{\texttt \_}''. It is a
+variant of {\tt SearchAbout {\termpattern}} that does not look for
+subterms but searches for statements whose conclusion has exactly the
+expected form.
 
 \begin{coq_example}
 Require Import Arith.
@@ -215,13 +233,12 @@ SearchPattern (?X1 + _ = _ + ?X1).
 
 \begin{Variants}
 \item {\tt SearchPattern {\term} inside
-{\module$_1$} \ldots{} {\module$_n$}.}\comindex{SearchPattern \ldots{} inside
-\ldots{}}
+{\module$_1$} \ldots{} {\module$_n$}.}
 
 This restricts the search to constructions defined in modules
 {\module$_1$} \ldots{} {\module$_n$}.
 
-\item {\tt SearchPattern {\term} outside {\module$_1$} \ldots{} {\module$_n$}.}\comindex{SearchPattern \ldots{} outside \ldots{}}
+\item {\tt SearchPattern {\term} outside {\module$_1$} \ldots{} {\module$_n$}.}
 
 This restricts the search to constructions not defined in modules
 {\module$_1$} \ldots{} {\module$_n$}.
@@ -232,7 +249,7 @@ This restricts the search to constructions not defined in modules
 
 This command displays the name and type of all theorems of the current
 context whose statement's conclusion is an equality of which one side matches
-the expression {\term =}. Holes in {\term} are denoted by ``{\texttt \_}''.
+the expression {\term}. Holes in {\term} are denoted by ``{\texttt \_}''.
 
 \begin{coq_example}
 Require Import Arith.

contrib/interface/centaur.ml4
@@ -545,7 +545,9 @@ let solve_hook n =
 let abort_hook s = output_results_nl (ctf_AbortedMessage !global_request_id s)
 
 let interp_search_about_item = function
-  | SearchRef qid -> GlobSearchRef (Nametab.global qid)
+  | SearchSubPattern pat ->
+      let _,pat = Constrintern.intern_constr_pattern Evd.empty (Global.env()) pat in
+      GlobSearchSubPattern pat
   | SearchString (s,_) ->
       warning "Notation case not taken into account";
       GlobSearchString s
@@ -563,10 +565,10 @@ let pcoq_search s l =
       raw_search_about (filter_by_module_from_list l) add_search
 	(List.map (on_snd interp_search_about_item) sl)
   | SearchPattern c ->
-      let _,pat = interp_constrpattern Evd.empty (Global.env()) c in
+      let _,pat = intern_constr_pattern Evd.empty (Global.env()) c in
       raw_pattern_search (filter_by_module_from_list l) add_search pat
   | SearchRewrite c ->
-      let _,pat = interp_constrpattern Evd.empty (Global.env()) c in
+      let _,pat = intern_constr_pattern Evd.empty (Global.env()) c in
       raw_search_rewrite (filter_by_module_from_list l) add_search pat;
   | SearchHead locqid ->
       filtered_search
@@ -581,7 +583,7 @@ let rec hyp_pattern_filter pat name a c =
       | Prod(_, hyp, c2) -> 
 	  (try
 (*	     let _ = msgnl ((str "WHOLE ") ++ (Printer.pr_lconstr c)) in
-             let _ = msgnl ((str "PAT ") ++ (Printer.pr_pattern pat)) in *)
+             let _ = msgnl ((str "PAT ") ++ (Printer.pr_constr_pattern pat)) in *)
              if Matching.is_matching pat hyp then
 	       (msgnl (str "ok"); true)
 	     else
@@ -591,7 +593,7 @@ let rec hyp_pattern_filter pat name a c =
       | _ -> false;;
 
 let hyp_search_pattern c l =
-  let _, pat = interp_constrpattern Evd.empty (Global.env()) c in
+  let _, pat = intern_constr_pattern Evd.empty (Global.env()) c in
     ctv_SEARCH_LIST := [];
     gen_filtered_search
       (fun s a c -> (filter_by_module_from_list l s a c &&
@@ -640,8 +642,8 @@ let pcoq_term_pr = {
    * Except with right bool/env which I'll get :)
    *)
   pr_lconstr_expr  = (fun c -> fFORMULA (xlate_formula c) ++ str "(pcoq_lconstr_expr of " ++ (default_term_pr.pr_lconstr_expr  c) ++ str ")");
-  pr_pattern_expr  = (fun c -> str "pcoq_pattern_expr\n" ++ (default_term_pr.pr_pattern_expr  c));
-  pr_lpattern_expr = (fun c -> str "pcoq_constr_expr\n"  ++ (default_term_pr.pr_lpattern_expr c))
+  pr_constr_pattern_expr  = (fun c -> str "pcoq_pattern_expr\n" ++ (default_term_pr.pr_constr_pattern_expr  c));
+  pr_lconstr_pattern_expr = (fun c -> str "pcoq_constr_expr\n"  ++ (default_term_pr.pr_lconstr_pattern_expr c))
 }
 
 let start_pcoq_trees () =

contrib/interface/dad.ml
@@ -99,7 +99,7 @@ let rec find_cmd (l:(string * dad_rule) list) env constr p p1 p2 =
              with
                 Failure s -> failwith "internal" in
          let _, constr_pat = 
-            interp_constrpattern Evd.empty (Global.env())
+            intern_constr_pattern Evd.empty (Global.env())
                ((*ct_to_ast*) pat) in
          let subst = matches constr_pat term_to_match in
          if (is_prefix p_f (p_r@p1)) & (is_prefix p_l (p_r@p2)) then

commit 88770a6a1814eb57a161188cda1f4b9ae639c252

- Fixed bug #2021 (uncaught exception with injection/discriminate when   the inductive status of a projectable position comes from a   dependency). - Improved doc of the stdlib chapter (see bug #2018), and fixed tex   bugs introduced in r11725. - Applied wish #2012 (max_dec transparent).
git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@11728 85f007b7-540e-0410-9357-904b9bb8a0f7

doc/common/macros.tex
@@ -47,7 +47,7 @@
 \newcommand{\Rem}{\medskip \noindent {\bf Remark: }}
 \newcommand{\Rems}{\medskip \noindent {\bf Remarks: }}
 \newcommand{\Example}{\medskip \noindent {\bf Example: }}
-\newcommand{\Examples}{\medskip \noindent {\bf Examples: }}
+\newcommand{\examples}{\medskip \noindent {\bf Examples: }}
 \newcommand{\Warning}{\medskip \noindent {\bf Warning: }}
 \newcommand{\Warns}{\medskip \noindent {\bf Warnings: }}
 \newcounter{ex}
@@ -61,6 +61,8 @@
 \newenvironment{ErrMsgs}{\ErrMsgx\begin{enumerate}}{\end{enumerate}}
 \newenvironment{Remarks}{\Rems\begin{enumerate}}{\end{enumerate}}
 \newenvironment{Warnings}{\Warns\begin{enumerate}}{\end{enumerate}}
+\newenvironment{Examples}{\medskip\noindent{\bf Examples:}
+\begin{enumerate}}{\end{enumerate}}
 
 %\newcommand{\bd}{\noindent\bf}
 %\newcommand{\sbd}{\vspace{8pt}\noindent\bf}

doc/refman/RefMan-lib.tex
@@ -1,10 +1,10 @@
 \chapter[The {\Coq} library]{The {\Coq} library\index{Theories}\label{Theories}}
 
-The \Coq\ library is structured into three parts:
+The \Coq\ library is structured into two parts:
 
 \begin{description}
 \item[The initial library:] it contains
-  elementary logical notions and datatypes. It constitutes the
+  elementary logical notions and data-types. It constitutes the
   basic state of the system directly available when running
   \Coq;
 
@@ -13,14 +13,14 @@ The \Coq\ library is structured into three parts:
   sorting, arithmetic, etc. This library comes with the system and its
   modules are directly accessible through the \verb!Require! command
   (see Section~\ref{Require});
-
-\item[User contributions:] Other specification and proof developments
-  coming from the \Coq\ users' community. These libraries are
-  available for download at \texttt{http://coq.inria.fr} (see
-  Section~\ref{Contributions}).
 \end{description}
 
-This chapter briefly reviews these libraries.
+In addition, user-provided libraries or developments are provided by
+\Coq\ users' community. These libraries and developments are available
+for download at \texttt{http://coq.inria.fr} (see
+Section~\ref{Contributions}).
+
+The chapter briefly reviews the \Coq\ libraries.
 
 \section[The basic library]{The basic library\label{Prelude}}
 
@@ -34,16 +34,18 @@ root directory; this includes the modules
 {\tt Datatypes},
 {\tt Specif},
 {\tt Peano},
-and {\tt Wf}.
+{\tt Wf} and 
+{\tt Tactics}.
 Module {\tt Logic\_Type} also makes it in the initial state}.
 
 \subsection[Notations]{Notations\label{Notations}}
 
 This module defines the parsing and pretty-printing of many symbols
-(infixes, prefixes, etc.). However, it does not assign a meaning to these
-notations. The purpose of this is to define precedence and
-associativity of very common notations, and avoid users to use them
-with other precedence, which may be confusing.
+(infixes, prefixes, etc.). However, it does not assign a meaning to
+these notations. The purpose of this is to define and fix once for all
+the precedence and associativity of very common notations. The main
+notations fixed in the initial state are listed on
+Figure~\ref{init-notations}.
 
 \begin{figure}
 \begin{center}
@@ -69,8 +71,10 @@ Notation & Precedence & Associativity \\
 \verb!_ <= _ < _! & 70 & no \\
 \verb!_ <= _ <= _! & 70 & no \\
 \verb!_ + _!   & 50 & left \\
+\verb!_ || _!  & 50 & left \\
 \verb!_ - _!   & 50 & left \\
 \verb!_ * _!   & 40 & left \\
+\verb!_ && _!  & 40 & left \\
 \verb!_ / _!   & 40 & left \\
 \verb!- _!  & 35 & right \\
 \verb!/ _!  & 35 & right \\
@@ -202,14 +206,14 @@ The following abbreviations are allowed:
   \end{tabular}
 \end{center}
 
-The type annotation \texttt{:A} can be omitted when \texttt{A} can be
+The type annotation ``\texttt{:A}'' can be omitted when \texttt{A} can be
 synthesized by the system.
 
 \subsubsection[Equality]{Equality\label{Equality}
 \index{Equality}}
 
 Then, we find equality, defined as an inductive relation. That is,
-given a \verb:Type: \verb:A: and an \verb:x: of type \verb:A:, the
+given a type \verb:A: and an \verb:x: of type \verb:A:, the
 predicate \verb:(eq A x): is the smallest one which contains \verb:x:.
 This definition, due to Christine Paulin-Mohring, is equivalent to
 define \verb:eq: as the smallest reflexive relation, and it is also
@@ -309,14 +313,14 @@ Abort.
   & $|$ & {\tt \{} {\ident} {\tt :} {\specif} {\tt |} {\form}  {\tt \&}
   {\form} {\tt \}} & ({\tt sig2})\\
   & $|$ & {\tt \{} {\ident} {\tt :} {\specif} {\tt \&} {\specif} {\tt
-    \}} & ({\tt sigS})\\
+    \}} & ({\tt sigT})\\
   & $|$ & {\tt \{} {\ident} {\tt :} {\specif} {\tt \&} {\specif} {\tt
-    \&} {\specif} {\tt \}} & ({\tt sigS2})\\
+    \&} {\specif} {\tt \}} & ({\tt sigT2})\\
   &  & & \\
 {\term} & ::= & {\tt (} {\term} {\tt ,} {\term} {\tt )} & ({\tt pair})
 \end{tabular}
 \end{centerframe}
-\caption{Syntax of datatypes and specifications}
+\caption{Syntax of data-types and specifications}
 \label{specif-syntax}
 \end{figure}
 
@@ -355,8 +359,9 @@ Inductive identity (A:Type) (a:A) : A -> Type :=
 Note that zero is the letter \verb:O:, and {\sl not} the numeral
 \verb:0:.
 
-{\tt identity} is logically equivalent to equality but it lives in
-sort {\tt Set}. Computationaly, it behaves like {\tt unit}.
+The predicate {\tt identity} is logically 
+equivalent to equality but it lives in sort {\tt
+  Type}. It is mainly maintained for compatibility.
 
 We then define the disjoint sum of \verb:A+B: of two sets \verb:A: and
 \verb:B:, and their product \verb:A*B:.
@@ -386,24 +391,27 @@ Definition snd (H: prod A B) := match H with
 End projections.
 \end{coq_example*}
 
+Some operations on {\tt bool} are also provided: {\tt andb} (with
+infix notation {\tt \&\&}), {\tt orb} (with
+infix notation {\tt ||}), {\tt xorb}, {\tt implb} and {\tt negb}.
+
 \subsection{Specification}
 
 The following notions\footnote{They are defined in module {\tt
-Specif.v}} allows to build new datatypes and specifications. 
+Specif.v}} allow to build new data-types and specifications. 
 They are available with the syntax shown on
-Figure~\ref{specif-syntax}\footnote{This syntax can be found in the module
-{\tt SpecifSyntax.v}}.
+Figure~\ref{specif-syntax}.
 
-For instance, given \verb|A:Set| and \verb|P:A->Prop|, the construct
+For instance, given \verb|A:Type| and \verb|P:A->Prop|, the construct
 \verb+{x:A | P x}+ (in abstract syntax \verb+(sig A P)+) is a
-\verb:Set:. We may build elements of this set as \verb:(exist x p):
+\verb:Type:. We may build elements of this set as \verb:(exist x p):
 whenever we have a witness \verb|x:A| with its justification
 \verb|p:P x|.
 
 From such a \verb:(exist x p): we may in turn extract its witness
 \verb|x:A| (using an elimination construct such as \verb:match:) but
 {\sl not} its justification, which stays hidden, like in an abstract
-data type. In technical terms, one says that \verb:sig: is a ``weak
+data-type. In technical terms, one says that \verb:sig: is a ``weak
 (dependent) sum''.  A variant \verb:sig2: with two predicates is also
 provided.
 
@@ -420,32 +428,32 @@ Inductive sig2 (A:Set) (P Q:A -> Prop) : Set :=
   exist2 (x:A) (_:P x) (_:Q x).
 \end{coq_example*}
 
-A ``strong (dependent) sum'' \verb+{x:A & (P x)}+ may be also defined,
-when the predicate \verb:P: is now defined as a \verb:Set:
-constructor.
+A ``strong (dependent) sum'' \verb+{x:A & P x}+ may be also defined,
+when the predicate \verb:P: is now defined as a 
+constructor of types in \verb:Type:.
 
 \ttindex{\{x:A \& (P x)\}}
 \ttindex{\&}
-\ttindex{sigS}
-\ttindex{existS}
-\ttindex{projS1}
-\ttindex{projS2}
-\ttindex{sigS2}
-\ttindex{existS2}
+\ttindex{sigT}
+\ttindex{existT}
+\ttindex{projT1}
+\ttindex{projT2}
+\ttindex{sigT2}
+\ttindex{existT2}
 
 \begin{coq_example*}
-Inductive sigS (A:Set) (P:A -> Set) : Set := existS (x:A) (_:P x).
-Section sigSprojections.
-Variable A : Set.
-Variable P : A -> Set.
-Definition projS1 (H:sigS A P) := let (x, h) := H in x.
-Definition projS2 (H:sigS A P) :=
-  match H return P (projS1 H) with
-    existS x h => h
+Inductive sigT (A:Type) (P:A -> Type) : Type := existT (x:A) (_:P x).
+Section Projections.
+Variable A : Type.
+Variable P : A -> Type.
+Definition projT1 (H:sigT A P) := let (x, h) := H in x.
+Definition projT2 (H:sigT A P) :=
+  match H return P (projT1 H) with
+    existT x h => h
   end.
-End sigSprojections.
-Inductive sigS2 (A: Set) (P Q:A -> Set) : Set :=
-    existS2 (x:A) (_:P x) (_:Q x).
+End Projections.
+Inductive sigT2 (A: Type) (P Q:A -> Type) : Type :=
+    existT2 (x:A) (_:P x) (_:Q x).
 \end{coq_example*}
 
 A related non-dependent construct is the constructive sum
@@ -461,7 +469,7 @@ Inductive sumbool (A B:Prop) : Set := left (_:A) | right (_:B).
 \end{coq_example*}
 
 This \verb"sumbool" construct may be used as a kind of indexed boolean
-data type. An intermediate between \verb"sumbool" and \verb"sum" is
+data-type. An intermediate between \verb"sumbool" and \verb"sum" is
 the mixed \verb"sumor" which combines \verb"A:Set" and \verb"B:Prop"
 in the \verb"Set" \verb"A+{B}".
 \ttindex{sumor}
@@ -500,7 +508,7 @@ Abort.
 Abort.
 \end{coq_eval}
 
-The next constructs builds a sum between a data type \verb|A:Set| and
+The next construct builds a sum between a data-type \verb|A:Type| and
 an exceptional value encoding errors:
 
 \ttindex{Exc}
@@ -515,23 +523,20 @@ Definition error := None.
 
 
 This module ends with theorems, 
-relating the sorts \verb:Set: and
+relating the sorts \verb:Set: or \verb:Type: and
 \verb:Prop: in a way which is consistent with the realizability
 interpretation.
+\ttindex{False\_rect}
 \ttindex{False\_rec}
-\ttindex{eq\_rec}
-\ttindex{Except}
+\ttindex{eq\_rect}
 \ttindex{absurd\_set}
-\ttindex{and\_rec}
+\ttindex{and\_rect}
 
-%Lemma False_rec : (P:Set)False->P.
-%Lemma False_rect : (P:Type)False->P.
 \begin{coq_example*}
 Definition except := False_rec.
-Notation Except := (except _).
 Theorem absurd_set : forall (A:Prop) (C:Set), A -> ~ A -> C.
-Theorem and_rec :
- forall (A B:Prop) (P:Set), (A -> B -> P) -> A /\ B -> P.
+Theorem and_rect :
+ forall (A B:Prop) (P:Type), (A -> B -> P) -> A /\ B -> P.
 \end{coq_example*}
 %\begin{coq_eval}
 %Abort.
@@ -544,7 +549,7 @@ The basic library includes a few elementary properties of natural
 numbers, together with the definitions of predecessor, addition and
 multiplication\footnote{This is in module {\tt Peano.v}}. It also
 provides a scope {\tt nat\_scope} gathering standard notations for
-common operations (+,*) and a decimal notation for numbers. That is he
+common operations (+, *) and a decimal notation for numbers. That is he
 can write \texttt{3} for \texttt{(S (S (S O)))}. This also works on
 the left hand side of a \texttt{match} expression (see for example
 section~\ref{refine-example}). This scope is opened by default.
@@ -618,10 +623,11 @@ Abort All.
 Fixpoint plus (n m:nat) {struct n} : nat :=
   match n with
   | 0 => m
-  | S p => S (plus p m)
+  | S p => S (p + m)
   end.
-Lemma plus_n_O : forall n:nat, n = plus n 0.
-Lemma plus_n_Sm : forall n m:nat, S (plus n m) = plus n (S m).
+where "n + m" := (plus n m) : nat_scope.
+Lemma plus_n_O : forall n:nat, n = n + 0.
+Lemma plus_n_Sm : forall n m:nat, S (n + m) = n + S m.
 \end{coq_example*}
 \begin{coq_eval}
 Abort All.
@@ -630,10 +636,11 @@ Abort All.
 Fixpoint mult (n m:nat) {struct n} : nat :=
   match n with
   | 0 => 0
-  | S p => m + mult p m
+  | S p => m + p * m
   end.
-Lemma mult_n_O : forall n:nat, 0 = mult n 0.
-Lemma mult_n_Sm : forall n m:nat, plus (mult n m) n = mult n (S m).
+where "n * m" := (mult n m) : nat_scope.
+Lemma mult_n_O : forall n:nat, 0 = n * 0.
+Lemma mult_n_Sm : forall n m:nat, n * m + n = n * (S m).
 \end{coq_example*}
 \begin{coq_eval}
 Abort All.
@@ -651,8 +658,8 @@ Finally, it gives the definition of the usual orderings \verb:le:,
 \begin{coq_example*}
 Inductive le (n:nat) : nat -> Prop :=
   | le_n : le n n
-  | le_S : forall m:nat, le n m -> le n (S m).
-Infix "+" := plus : nat_scope.
+  | le_S : forall m:nat, n <= m -> n <= (S m).
+where "n <= m" := (le n m) : nat_scope.
 Definition lt (n m:nat) := S n <= m.
 Definition ge (n m:nat) := m <= n.
 Definition gt (n m:nat) := m < n.
@@ -693,31 +700,32 @@ well-founded induction\footnote{This is defined in module {\tt Wf.v}}.
 \index{Well founded induction}
 \ttindex{Acc}
 \ttindex{Acc\_inv}
-\ttindex{Acc\_rec}
+\ttindex{Acc\_rect}
 \ttindex{well\_founded}
 
 \begin{coq_example*}
 Section Well_founded.
-Variable A : Set.
+Variable A : Type.
 Variable R : A -> A -> Prop.
-Inductive Acc : A -> Prop :=
-    Acc_intro : forall x:A, (forall y:A, R y x -> Acc y) -> Acc x.
-Lemma Acc_inv : forall x:A, Acc x -> forall y:A, R y x -> Acc y.
+Inductive Acc (x:A) : Prop :=
+    Acc_intro : (forall y:A, R y x -> Acc y) -> Acc x.
+Lemma Acc_inv : Acc x -> forall y:A, R y x -> Acc y.
 \end{coq_example*}
 \begin{coq_eval}
-simple destruct 1; trivial.
+destruct 1; trivial.
 Defined.
 \end{coq_eval}
+%% Acc_rect now primitively defined
+%% Section AccRec.
+%% Variable P : A -> Set.
+%% Variable F :
+%%     forall x:A,
+%%       (forall y:A, R y x -> Acc y) -> (forall y:A, R y x -> P y) -> P x.
+%% Fixpoint Acc_rec (x:A) (a:Acc x) {struct a} : P x :=
+%%   F x (Acc_inv x a)
+%%     (fun (y:A) (h:R y x) => Acc_rec y (Acc_inv x a y h)).
+%% End AccRec.
 \begin{coq_example*}
-Section AccRec.
-Variable P : A -> Set.
-Variable F :
-    forall x:A,
-      (forall y:A, R y x -> Acc y) -> (forall y:A, R y x -> P y) -> P x.
-Fixpoint Acc_rec (x:A) (a:Acc x) {struct a} : P x :=
-  F x (Acc_inv x a)
-    (fun (y:A) (h:R y x) => Acc_rec y (Acc_inv x a y h)).
-End AccRec.
 Definition well_founded := forall a:A, Acc a.
 Hypothesis Rwf : well_founded.
 Theorem well_founded_induction :
@@ -730,7 +738,8 @@ Theorem well_founded_ind :
 \begin{coq_eval}
 Abort All.
 \end{coq_eval}
-{\tt Acc\_rec} can be used to define functions by fixpoints using
+The automatically generated scheme {\tt Acc\_rect} 
+can be used to define functions by fixpoints using
 well-founded relations to  justify termination. Assuming
 extensionality of the functional used for the recursive call, the
 fixpoint equation can be proved.
@@ -740,7 +749,7 @@ fixpoint equation can be proved.
 \ttindex{Fix\_F\_eq}
 \begin{coq_example*}
 Section FixPoint.
-Variable P : A -> Set.
+Variable P : A -> Type.
 Variable F : forall x:A, (forall y:A, R y x -> P y) -> P x.
 Fixpoint Fix_F (x:A) (r:Acc x) {struct r} : P x :=
   F x (fun (y:A) (p:R y x) => Fix_F y (Acc_inv x r y p)).
@@ -765,7 +774,7 @@ End Well_founded.
 \subsection{Accessing the {\Type} level}
 
 The basic library includes the definitions\footnote{This is in module
-{\tt Logic\_Type.v}} of the counterparts of some datatypes and logical
+{\tt Logic\_Type.v}} of the counterparts of some data-types and logical
 quantifiers at the \verb:Type: level: negation, pair, and properties
 of {\tt identity}.
 
@@ -780,8 +789,12 @@ Definition notT (A:Type) := A -> False.
 Inductive prodT (A B:Type) : Type := pairT (_:A) (_:B).
 \end{coq_example*}
 
+At the end, it defines data-types at the {\Type} level.
 
-At the end, it defines datatypes at the {\Type} level.
+\subsection{Tactics}
+
+A few tactics defined at the user level are provided in the initial
+state\footnote{This is in module {\tt Tactics.v}}.
 
 \section{The standard library}
 
@@ -795,6 +808,7 @@ subdirectories:
   {\bf Arith}   & Basic Peano arithmetic \\
   {\bf NArith}  & Basic positive integer arithmetic \\
   {\bf ZArith}  & Basic relative integer arithmetic \\
+  {\bf Numbers} & Various approaches to natural, integer and cyclic numbers (currently axiomatically and on top of 2$^{31}$ binary words) \\
   {\bf Bool}    & Booleans (basic functions and results) \\
   {\bf Lists}   & Monomorphic and polymorphic lists (basic functions and
             results), Streams (infinite sequences defined with co-inductive

doc/refman/RefMan-oth.tex
@@ -197,7 +197,7 @@ This restricts the search to constructions not defined in modules
 
 \end{Variants}
 
-\Examples
+\examples
 
 \begin{coq_example*}
 Require Import ZArith.

tactics/equality.ml
@@ -457,8 +457,9 @@ let injectable env sigma t1 t2 =
 let descend_then sigma env head dirn =
   let IndType (indf,_) =
     try find_rectype env sigma (get_type_of env sigma head)
-    with Not_found -> assert false in
-  let ind,_ = dest_ind_family indf in
+    with Not_found ->
+      error "Cannot project on an inductive type derived from a dependency." in
+   let ind,_ = dest_ind_family indf in
   let (mib,mip) = lookup_mind_specif env ind in
   let cstr = get_constructors env indf in
   let dirn_nlams = cstr.(dirn-1).cs_nargs in
@@ -477,7 +478,7 @@ let descend_then sigma env head dirn =
           (interval 1 (Array.length mip.mind_consnames)) in
       let ci = make_case_info env ind RegularStyle in
       mkCase (ci, p, head, Array.of_list brl)))
-  
+
 (* Now we need to construct the discriminator, given a discriminable
    position.  This boils down to:
 
@@ -828,11 +829,14 @@ let make_iterated_tuple env sigma dflt (z,zty) =
 let rec build_injrec sigma env dflt c = function
   | [] -> make_iterated_tuple env sigma dflt (c,type_of env sigma c)
   | ((sp,cnum),argnum)::l ->
+    try
       let (cnum_nlams,cnum_env,kont) = descend_then sigma env c cnum in
       let newc = mkRel(cnum_nlams-argnum) in
       let (subval,tuplety,dfltval) = build_injrec sigma cnum_env dflt newc l in
       (kont subval (dfltval,tuplety),
-       tuplety,dfltval)
+      tuplety,dfltval)
+    with
+	UserError _ -> failwith "caught"
 
 let build_injector sigma env dflt c cpath =
   let (injcode,resty,_) = build_injrec sigma env dflt c cpath in

theories/Arith/Max.v
@@ -74,13 +74,13 @@ Lemma max_dec : forall n m, {max n m = n} + {max n m = m}.
 Proof.
   induction n; induction m; simpl in |- *; auto with arith.
   elim (IHn m); intro H; elim H; auto.
-Qed.
+Defined.
 
 Lemma max_case : forall n m (P:nat -> Type), P n -> P m -> P (max n m).
 Proof.
   induction n; simpl in |- *; auto with arith.
   induction m; intros; simpl in |- *; auto with arith.
   pattern (max n m) in |- *; apply IHn; auto with arith.
-Qed.
+Defined.
 
 Notation max_case2 := max_case (only parsing).

test-suite/bugs/closed/shouldsucceed/2021.v
@@ -1,0 +1,23 @@
+(* correct failure of injection/discriminate on types whose inductive
+   status derives from the substitution of an argument *)
+
+Inductive t : nat -> Type :=
+| M : forall n: nat, nat -> t n.
+
+Lemma eq_t : forall n n' m m',
+   existT (fun B : Type => B) (t n) (M n m) =
+   existT (fun B : Type => B) (t n') (M n' m') -> True.
+Proof.
+  intros.
+  injection H.
+  intro Ht.
+  exact I.
+Qed.
+
+Lemma eq_t' : forall n n' : nat,
+   existT (fun B : Type => B) (t n) (M n 0) =
+   existT (fun B : Type => B) (t n') (M n' 1) -> True.
+Proof.
+  intros.
+  discriminate H || exact I.
+Qed.