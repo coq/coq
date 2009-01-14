237f49f32519d536ed2f175b8e8c5e5b78b9c784
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2009-01-14 13:07:34 +0000

    Backporting 11785: fixing #1960 (xml bug) and #1961 (anomaly while parsing $).
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/branches/v8.2@11786 85f007b7-540e-0410-9357-904b9bb8a0f7

--- contrib/xml/cic2Xml.ml
+++ contrib/xml/cic2Xml.ml
@@ -7,7 +7,7 @@ let print_xml_term ch env sigma cic =
   let seed = ref 0 in
   let acic =
    Cic2acic.acic_of_cic_context' true seed ids_to_terms constr_to_ids 
-    ids_to_father_ids ids_to_inner_sorts ids_to_inner_types []
+    ids_to_father_ids ids_to_inner_sorts ids_to_inner_types
     env [] sigma (Unshare.unshare cic) None in
   let xml = Acic2Xml.print_term ids_to_inner_sorts acic in
   Xml.pp_ch xml ch
