7529f1422c794837b0beb0066ad5ef79ce798e86
letouzey <letouzey@85f007b7-540e-0410-9357-904b9bb8a0f7>
2009-02-20 13:39:46 +0000

    coq-interface and coq-parser can be calls to coqtop with adequate code dynlink
     From files in contrib/interface, we create (if natdynlink is available) two
     plugins named coqinterface_plugin.{cma,cmxs} and coqparser_plugin.{cma,cmxs}.
     These plugins are loaded respectively by CoqInterface.v and CoqParser.v.
     So coq-interface can be "coqtop -require CoqInterface", and coq-parser can be
     "coqtop -batch -l CoqParser" (this one cannot be compiled into a .vo, otherwise
     a customized toplevel is launched during the compilation). Turing coq-interface
     and coq-parser and their .opt versions into shell scripts allow to spare around
     40 Mb of disk space...
    
     Nota: at dynlink, parse.ml was conflicting with the module Parse of the ocaml
     runtime, so I renamed it into coqparser.ml
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@11940 85f007b7-540e-0410-9357-904b9bb8a0f7

---
+++ contrib/interface/CoqParser.v
@@ -0,0 +1,9 @@
+(************************************************************************)
+(*  v      *   The Coq Proof Assistant  /  The Coq Development Team     *)
+(* <O___,, * CNRS-Ecole Polytechnique-INRIA Futurs-Universite Paris Sud *)
+(*   \VV/  **************************************************************)
+(*    //   *      This file is distributed under the terms of the       *)
+(*         *       GNU Lesser General Public License Version 2.1        *)
+(************************************************************************)
+
+Declare ML Module "coqparser_plugin".
