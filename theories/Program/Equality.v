9ad69545bb1bac89cb467d1a5aef97c81a6bc9e3
msozeau <msozeau@85f007b7-540e-0410-9357-904b9bb8a0f7>
2009-04-08 21:04:42 +0000

    Report r11881: proper introduction of the variables to destruct in do_case, do_ind.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/branches/v8.2@12073 85f007b7-540e-0410-9357-904b9bb8a0f7

--- theories/Program/Equality.v
+++ theories/Program/Equality.v
@@ -479,8 +479,12 @@ Ltac intro_prototypes :=
     | _ => idtac
   end.
 
-Ltac do_case p := destruct p || elim_case p || (case p ; clear p).
-Ltac do_ind p := induction p || elim_ind p.
+Ltac introduce p := 
+  first [ match p with _ => idtac end (* Already there *)
+    | intros until p | intros ].
+
+Ltac do_case p := introduce p ; (destruct p || elim_case p || (case p ; clear p)).
+Ltac do_ind p := introduce p ; (induction p || elim_ind p).
 
 Ltac dep_elimify := match goal with [ |- ?T ] => change (block_dep_elim T) end.
 
