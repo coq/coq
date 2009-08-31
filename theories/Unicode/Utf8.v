c7254961b3a851fc5412e1db88a24a13b43773a7
glondu <glondu@85f007b7-540e-0410-9357-904b9bb8a0f7>
2009-08-31 10:03:18 +0000

    Fix notation for ~x in theories/Unicode/Utf8.v
    Before this change, it had used:
      U+2309 RIGHT CEILING ("⌉")
    after this change, it uses:
      U+00AC NOT SIGN ("¬")
    
    Patch submitted on coq-club by Samuel Bronson.
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@12301 85f007b7-540e-0410-9357-904b9bb8a0f7

--- theories/Unicode/Utf8.v
+++ theories/Unicode/Utf8.v
@@ -36,7 +36,7 @@ Notation "x ∨ y" := (x \/ y) (at level 85, right associativity) : type_scope.
 Notation "x ∧ y" := (x /\ y) (at level 80, right associativity) : type_scope.
 Notation "x → y" := (x -> y) (at level 90, right associativity): type_scope.
 Notation "x ↔ y" := (x <-> y) (at level 95, no associativity): type_scope.
-Notation "⌉ x" := (~x) (at level 75, right associativity) : type_scope.
+Notation "¬ x" := (~x) (at level 75, right associativity) : type_scope.
 Notation "x ≠ y" := (x <> y) (at level 70) : type_scope.
 
 (* Abstraction *)
