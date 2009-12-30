4c825c9df1643f9478d8ceced5447171cdb0c937
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2009-12-30 15:19:34 +0000

    Regression test for bug #2145 (Groebner failing with "not eq" hypotheses).
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@12617 85f007b7-540e-0410-9357-904b9bb8a0f7

---
+++ test-suite/bugs/closed/shouldsucceed/2145.v
@@ -0,0 +1,20 @@
+(* Test robustness of Groebner tactic in presence of disequalities *)
+
+Require Export Reals.
+Require Export GroebnerR.
+
+Open Scope R_scope.
+
+Lemma essai :
+ forall yb xb m1 m2 xa ya,
+  xa <> xb ->
+ yb - 2 * m2 * xb = ya - m2 * xa -> 
+ yb - m1 * xb = ya - m1 * xa ->
+ yb - ya = (2 * xb - xa) * m2 -> 
+ yb - ya = (xb - xa) * m1.
+Proof.
+intros.
+(* clear H. groebner used not to work when H was not cleared *)
+groebnerR.
+Qed.
+
