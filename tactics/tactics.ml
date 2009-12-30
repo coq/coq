a31b785e6d9c22735ababf64c8691581f83ad9f4
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2009-12-30 12:02:32 +0000

    Fixing bug #2146 (broken selection of occurrences in "change"). In trunk the different possible combinations of "at" and "in" with occurrences are taken into account. In 8.2 branch, it remains fragile (syntaxes that were accepted remain accepted and a message warns if the occurrences coming after the "with" are not taken into account).
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/branches/v8.2@12614 85f007b7-540e-0410-9357-904b9bb8a0f7

--- tactics/tactics.ml
+++ tactics/tactics.ml
@@ -249,14 +249,51 @@ let change_option occl t = function
     Some id -> change_in_hyp occl t id
   | None -> change_in_concl occl t
 
-let change occl c cls =
+let out_arg = function
+  | ArgVar _ -> anomaly "Unevaluated or_var variable"
+  | ArgArg x -> x
+
+let adjust_clause occl cls =
+  (* warn as much as possible on loss of occurrence information *)
   (match cls, occl with
       ({onhyps=(Some(_::_::_)|None)}
       |{onhyps=Some(_::_);concl_occs=((false,_)|(true,_::_))}),
       Some _ ->
 	error "No occurrences expected when changing several hypotheses."
     | _ -> ());
-  onClauses (change_option occl c) cls
+  (* get at clause from cls if only goal or one hyp specified *)
+  let occl,cls = match occl with
+    | None -> None,cls
+    | Some (occs,c) ->
+	if cls.onhyps=Some[] && occs=all_occurrences then
+	  Some (on_snd (List.map out_arg) cls.concl_occs,c),
+	  {cls with concl_occs=all_occurrences_expr}
+	else
+	  match cls.onhyps with
+	  | Some[(occs',id),l] when 
+	      cls.concl_occs=no_occurrences_expr && occs=all_occurrences -> 
+	      Some (on_snd (List.map out_arg) occs',c),
+	      {cls with onhyps=Some[(all_occurrences_expr,id),l]}
+	  | _ ->
+	      occl,cls in
+  (* check if cls has still specified occs *)
+  if cls.onhyps <> None && 
+    List.exists (fun ((occs,_),_) -> occs <> all_occurrences_expr)
+    (Option.get cls.onhyps)
+    || cls.concl_occs <> all_occurrences_expr && 
+       cls.concl_occs <> no_occurrences_expr
+  then
+    Flags.if_verbose Pp.msg_warning
+      (if cls.onhyps=Some[] then
+	str "Trailing \"at\" modifier not taken into account."
+      else
+	str "\"at\" modifier in clause \"in\" not taken into account.");
+  (* Anticipate on onClauses which removes concl if not at all occs *)
+  if cls.concl_occs=no_occurrences_expr then cls
+  else {cls with concl_occs=all_occurrences_expr}
+
+let change occl c cls =
+  onClauses (change_option occl c) (adjust_clause occl cls)
 
 (* Pour usage interne (le niveau User est pris en compte par reduce) *)
 let red_in_concl        = reduct_in_concl (red_product,DEFAULTcast)
@@ -1396,10 +1433,6 @@ let quantify lconstr =
    used to remember the place of x1, x2, ...: it is the list of hypotheses on
    the left of each x1, ...).
 *)
-
-let out_arg = function
-  | ArgVar _ -> anomaly "Unevaluated or_var variable"
-  | ArgArg x -> x
 
 let occurrences_of_hyp id cls =
   let rec hyp_occ = function
