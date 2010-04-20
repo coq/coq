edfef624c211b5649106f73e7c0f8c9454bbfdb3
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2010-04-20 08:10:14 +0000

    Propagated fix to bug 2127 (Hint Rewrite badly globalized in modules) to 8.2 (the bug was fixed for trunk/8.3 in revision 12354).
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/branches/v8.3@12955 85f007b7-540e-0410-9357-904b9bb8a0f7

---
+++ test-suite/bugs/closed/shouldsucceed/2127.v
@@ -0,0 +1,11 @@
+(* Check that "apply refl_equal" is not exported as an interactive
+   tactic but as a statically globalized one *)
+
+(* (this is a simplification of the original bug report) *)
+
+Module A.
+Hint Rewrite sym_equal using apply refl_equal : foo.
+End A.
+
+
+
