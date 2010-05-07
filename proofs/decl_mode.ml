27792e91ffaedc3921565224b0fd8607a9ddfe97
jforest <jforest@85f007b7-540e-0410-9357-904b9bb8a0f7>
2010-05-07 10:10:32 +0000

    report of commit r 12298
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/branches/v8.3@12999 85f007b7-540e-0410-9357-904b9bb8a0f7

--- proofs/decl_mode.ml
+++ proofs/decl_mode.ml
@@ -6,7 +6,7 @@
 (*         *       GNU Lesser General Public License Version 2.1        *)
 (************************************************************************)
 
-(*i $Id:$ i*)
+(*i $Id$ i*)
 
 open Names
 open Term
