7036ca39c6eab65a9baaf5f36fbfe8004dd327fa
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2010-06-13 18:45:09 +0000

    Fixing bug 2300 (ltac pattern-matching returning terms with concrete universes).
    By the way, there is an open problem of which conversion to use (conv,
    evarconv, with or w/o universes levels) when trying to unify multiple
    instances of the same variable in ltac pattern-matching.
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/branches/v8.2@13130 85f007b7-540e-0410-9357-904b9bb8a0f7

--- tactics/tacinterp.ml
+++ tactics/tacinterp.ml
@@ -1096,7 +1096,9 @@ let apply_one_mhyp_context ist env gl lmatch (hypname,patv,pat) lhyps =
                  match_next_pattern find_next')
               with
                 | PatternMatchingFailure -> apply_one_mhyp_context_rec tl in
-            match_next_pattern (fun () -> match_pat lmatch hyp pat) ()
+            match_next_pattern (fun () ->
+	      let hyp = if b<>None then refresh_universes_strict hyp else hyp in
+	      match_pat lmatch hyp pat) ()
 	| Some patv -> 
 	    match b with
 	    | Some body -> 
@@ -1112,7 +1114,9 @@ let apply_one_mhyp_context ist env gl lmatch (hypname,patv,pat) lhyps =
                         | PatternMatchingFailure ->
                             match_next_pattern_in_body next_in_body' () in
                     match_next_pattern_in_typ 
-                      (fun () -> match_pat lmeta hyp pat) ()
+                      (fun () ->
+			let hyp = refresh_universes_strict hyp in
+			match_pat lmeta hyp pat) ()
                   with PatternMatchingFailure -> apply_one_mhyp_context_rec tl
                 in
                 match_next_pattern_in_body 
