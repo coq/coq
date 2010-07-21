5f4c858933a3e7f466d26c6edc68f9b38defa90b
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2010-07-21 16:13:43 +0000

    Backporting fix to bug #2353 (fixpoint with recursively non-uniform parameters) to branch 8.2.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/branches/v8.3@13302 85f007b7-540e-0410-9357-904b9bb8a0f7

---
+++ test-suite/bugs/closed/shouldsucceed/2353.v
@@ -0,0 +1,12 @@
+(* Are recursively non-uniform params correctly treated? *)
+Inductive list (A:nat -> Type) n := cons : A n -> list A (S n) -> list A n.
+Inductive term n := app (l : list term n).
+Definition term_list :=
+  fix term_size n (t : term n) (acc : nat) {struct t} : nat :=
+    match t with
+    | app l =>
+      (fix term_list_size n (l : list term n) (acc : nat) {struct l} : nat :=
+      match l with
+      | cons t q => term_list_size (S n) q (term_size n t acc)
+      end) n l (S acc)
+    end.
