309a53f2e1aa9b2a39654cf5fa23eb632a04c22f
glondu <glondu@85f007b7-540e-0410-9357-904b9bb8a0f7>
2010-09-22 19:31:50 +0000

    Fix inconsistency in Prop/Set conversion check
    This commit fixes a bug that made the system inconsistent with proof
    irrelevance (the main idea being that Set = Prop by reflexivity).
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/branches/v8.3@13449 85f007b7-540e-0410-9357-904b9bb8a0f7

---
+++ test-suite/failure/prop-set-proof-irrelevance.v
@@ -0,0 +1,12 @@
+Require Import ProofIrrelevance.
+
+Lemma proof_irrelevance_set : forall (P : Set) (p1 p2 : P), p1 = p2.
+  exact proof_irrelevance.
+Qed.
+
+Lemma paradox : False.
+  assert (H : 0 <> 1) by discriminate.
+  apply H.
+  Fail apply proof_irrelevance. (* inlined version is rejected *)
+  apply proof_irrelevance_set.
+Qed.
