62e9842aac2367ea8594af5e4ec49b35b4df92e5
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2010-10-03 17:01:43 +0000

    Fixing printing of module_path names (was using a debugging message since r11177).
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/branches/v8.3@13486 85f007b7-540e-0410-9357-904b9bb8a0f7

--- kernel/names.ml
+++ kernel/names.ml
@@ -108,7 +108,7 @@ let rec check_bound_mp = function
   | _ -> false
 
 let rec string_of_mp = function
-  | MPfile sl -> "MPfile (" ^ string_of_dirpath sl ^ ")"
+  | MPfile sl -> string_of_dirpath sl
   | MPbound uid -> string_of_uid uid
  (* | MPapp (mp1,mp2) -> 
       "("^string_of_mp mp ^ " " ^ 
