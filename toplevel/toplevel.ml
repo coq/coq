ab62ff1a221a7b6631ad5a452c89fbabbd98e33b
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2010-12-02 17:43:59 +0000

    Backport of r13667 to branch 8.3: fixing a bug on the interpretation of Local/Global modifiers and resulting in an anomaly when the modifiers were not allowed at all.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/branches/v8.3@13668 85f007b7-540e-0410-9357-904b9bb8a0f7

--- toplevel/toplevel.ml
+++ toplevel/toplevel.ml
@@ -276,7 +276,6 @@ let rec is_pervasive_exn = function
   | Error_in_file (_,_,e) -> is_pervasive_exn e
   | Stdpp.Exc_located (_,e) -> is_pervasive_exn e
   | DuringCommandInterp (_,e) -> is_pervasive_exn e
-  | DuringSyntaxChecking (_,e) -> is_pervasive_exn e
   | _ -> false
 
 (* Toplevel error explanation, dealing with locations, Drop, Ctrl-D
@@ -285,8 +284,7 @@ let rec is_pervasive_exn = function
 let print_toplevel_error exc =
   let (dloc,exc) =
     match exc with
-      | DuringCommandInterp (loc,ie)
-      | DuringSyntaxChecking (loc,ie) ->
+      | DuringCommandInterp (loc,ie) ->
           if loc = dummy_loc then (None,ie) else (Some loc, ie)
       | _ -> (None, exc)
   in
@@ -335,8 +333,7 @@ let rec discard_to_dot () =
  * in encountered. *)
 
 let process_error = function
-  | DuringCommandInterp _
-  | DuringSyntaxChecking _ as e -> e
+  | DuringCommandInterp _ as e -> e
   | e ->
       if is_pervasive_exn e then
 	e
