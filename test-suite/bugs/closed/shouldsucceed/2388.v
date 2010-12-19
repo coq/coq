735fc0c1c77f197505202ad2462e3a997a9b911e
msozeau <msozeau@85f007b7-540e-0410-9357-904b9bb8a0f7>
2010-12-19 12:27:00 +0000

    The Program Instance no longer fails as the hole is not filled automatically.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/branches/v8.3@13729 85f007b7-540e-0410-9357-904b9bb8a0f7

--- test-suite/bugs/closed/shouldsucceed/2388.v
+++ test-suite/bugs/closed/shouldsucceed/2388.v
@@ -2,9 +2,3 @@
 
 Fail Parameters (A:Prop) (a:A A).
 
-(* This is a variant (reported as part of bug #2347) *)
-
-Require Import EquivDec.                                              
-Fail Program Instance bool_eq_eqdec : EqDec bool eq := 
-   {equiv_dec x y := (fix aux (x y : bool) {struct x}:= aux _ y) x y}.
-
