441298e1f205fafb1dfb8599c7b144294a3b150c
letouzey <letouzey@85f007b7-540e-0410-9357-904b9bb8a0f7>
2010-12-24 09:55:54 +0000

    Win32: remove the need for Coq.bat and Coqide.bat
     - coqlib is currently computed relatively of Sys.executable_name,
       hence no need to set it manually
    
     - in Win32, better detection of user home dir : in System.ml,
       if HOME isn't set, we look now for HOMEDRIVE\HOMEPATH, and then
       for USERPROFILE
    
     - concerning PATH, in Win32 we now add coqbin to PATH during the
       initialization.
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/branches/v8.3@13750 85f007b7-540e-0410-9357-904b9bb8a0f7

--- Coq.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set COQBIN=%~0\..\bin
-set COQLIB=%~0\..\lib
-echo Using COQBIN= %COQBIN%
-echo and   COQLIB= %COQLIB%
-echo Starting Coq
-%~0\..\bin\coqtop.opt.exe
-pause
--- Coqide.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set COQBIN=%~0\..\bin
-set COQLIB=%~0\..\lib
-echo Using COQBIN= %COQBIN%
-echo and   COQLIB= %COQLIB%
-echo Starting Coqide
-%~0\..\bin\coqide.opt.exe
