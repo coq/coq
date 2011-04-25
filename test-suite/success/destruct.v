b77191b6286fa88f64567086c94229a18b497a0f
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2011-04-25 12:08:25 +0000

    Merged a few test-suite files in old branch recdef and V8.1-APP tag to please case-insensitive file systems.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/branches/recdef@14062 85f007b7-540e-0410-9357-904b9bb8a0f7

--- test-suite/success/destruct.v
+++ test-suite/success/destruct.v
@@ -1,3 +1,17 @@
+(* Submitted by Robert Schneck *)
+
+Parameter A,B,C,D : Prop.
+Axiom X : A->B->C/\D.
+
+Lemma foo : A->B->C.
+Proof.
+Intros. 
+NewDestruct X. (* Should find axiom X and should handle arguments of X *)
+Assumption.
+Assumption.
+Assumption.
+Qed.
+
 (* Simplification of bug 711 *)
 
 Parameter f:true=false.
