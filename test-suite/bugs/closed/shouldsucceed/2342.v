449a02d3a3fb4c63cd6732b28c9c629c8bb3bb5d
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2011-05-15 11:40:05 +0000

    Test for bug 462 and 2342 fixed by Matthieu's 13990 (or so).
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@14124 85f007b7-540e-0410-9357-904b9bb8a0f7

---
+++ test-suite/bugs/closed/shouldsucceed/2342.v
@@ -0,0 +1,8 @@
+(* Checking that the type inference algoithme does not commit to an
+   equality over sorts when only a subtyping constraint is around *)
+
+Parameter A : Set.
+Parameter B : A -> Set.
+Parameter F : Set -> Prop. 
+Check (F (forall x, B x)). 
+
