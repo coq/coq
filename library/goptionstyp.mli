a2c18c0369de7ca302ad398b348ab429fe8f3fbe
letouzey <letouzey@85f007b7-540e-0410-9357-904b9bb8a0f7>
2011-12-21 23:16:48 +0000

    Isolate a few types of Goptions in a pure .mli, solving a dep issue with ocamlbuild
    A few types of Goptions are mentioned in toplevel/interface.mli.
    Since the latter is a pure .mli, this shouldn't trigger a dependency
    with respect to goptions.ml, but apparently ocamlbuild isn't clever
    enough to notice this, and hence wants to link a pile of useless stuff
    with coqide. I'll discuss with Pierre-Marie about the best long-term
    way to avoid this mess, but in the meantime I split the concerned types
    of Goptions in a separate Goptionstyp.mli.
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@14841 85f007b7-540e-0410-9357-904b9bb8a0f7

---
+++ library/goptionstyp.mli
@@ -0,0 +1,26 @@
+(************************************************************************)
+(*  v      *   The Coq Proof Assistant  /  The Coq Development Team     *)
+(* <O___,, *   INRIA - CNRS - LIX - LRI - PPS - Copyright 1999-2010     *)
+(*   \VV/  **************************************************************)
+(*    //   *      This file is distributed under the terms of the       *)
+(*         *       GNU Lesser General Public License Version 2.1        *)
+(************************************************************************)
+
+(** Some types used in the generic option mechanism (Goption) *)
+
+(** Placing them here in a pure interface avoid some dependency issues
+    when compiling CoqIDE *)
+
+type option_name = string list
+
+type option_value =
+  | BoolValue   of bool
+  | IntValue    of int option
+  | StringValue of string
+
+type option_state = {
+  opt_sync  : bool;
+  opt_depr  : bool;
+  opt_name  : string;
+  opt_value : option_value;
+}
