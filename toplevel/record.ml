043669e582870be26eff682560187698bbc9e101
msozeau <msozeau@85f007b7-540e-0410-9357-904b9bb8a0f7>
2012-01-25 17:12:38 +0000

    Check for unresolved evars in textual order of the params and fields declarations (fixes bug #2278).
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@14943 85f007b7-540e-0410-9357-904b9bb8a0f7

--- toplevel/record.ml
+++ toplevel/record.ml
@@ -81,8 +81,8 @@ let typecheck_params_and_fields id t ps nots fs =
   let newps = Evarutil.nf_rel_context_evar sigma newps in
   let newfs = Evarutil.nf_rel_context_evar sigma newfs in
   let ce t = Evarutil.check_evars env0 Evd.empty evars t in
-    List.iter (fun (n, b, t) -> Option.iter ce b; ce t) newps;
-    List.iter (fun (n, b, t) -> Option.iter ce b; ce t) newfs;
+    List.iter (fun (n, b, t) -> Option.iter ce b; ce t) (List.rev newps);
+    List.iter (fun (n, b, t) -> Option.iter ce b; ce t) (List.rev newfs);
     imps, newps, impls, newfs
 
 let degenerate_decl (na,b,t) =
