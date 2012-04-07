be8c8efe3a4921ee6b10ec454035eaa191abc7ef
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2012-04-07 20:24:14 +0000

    v8.4: Fixing a few bugs (see #2571) related to the interpretation of multiple binders: - fixing missing spaces in the format of the exists' notations (Logic.v); - fixing wrong variable name in check_is_hole error message (topconstr.ml); - interpret expressions with open binders such as "forall x y, t" as   "forall (x:_) (y:_),t" instead of "forall (x y:_),t" to avoid   the "implicit type" of a variable being propagated to the type of   another variable of different base name.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/branches/v8.4@15124 85f007b7-540e-0410-9357-904b9bb8a0f7

--- test-suite/output/Notations2.v
+++ test-suite/output/Notations2.v
@@ -25,6 +25,11 @@ Remove Printing Let prod.
 Check match (0,0,0) with (x,y,z) => x+y+z end.
 Check let '(a,b,c) := ((2,3),4) in a.
 
+(* Check printing of notations with mixed reserved binders (see bug #2571) *)
+
+Implicit Type myx : bool.
+Check exists myx y, myx = y.
+
 (* Test notation for anonymous functions up to eta-expansion *)
 
 Check fun P:nat->nat->Prop => fun x:nat => ex (P x). 
