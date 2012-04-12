26166ad42e971d2d5b09601e82d0506c7f1b464d
letouzey <letouzey@85f007b7-540e-0410-9357-904b9bb8a0f7>
2012-04-12 18:17:06 +0000

    Repair two tests
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/branches/v8.4@15143 85f007b7-540e-0410-9357-904b9bb8a0f7

--- test-suite/bugs/closed/shouldsucceed/2603.v
+++ test-suite/bugs/closed/shouldsucceed/2603.v
@@ -1,3 +1,18 @@
+(** Namespace of module vs. namescope of definitions/constructors/...
+
+As noticed by A. Appel in bug #2603, module names and definition
+names used to be in the same namespace. But conflict with names
+of constructors (or 2nd mutual inductive...) used to not be checked
+enough, leading to stange situations.
+
+- In 8.3pl3 we introduced checks that forbid uniformly the following
+  situations.
+
+- For 8.4 we finally managed to make module names and other names
+  live in two separate namespace, hence allowing all of the following
+  situations.
+*)
+
 Module Type T.
 End T.
 
@@ -9,10 +24,10 @@ End L.
 
 Module M1 : L with Module E:=K.
 Module E := K.
-Fail Inductive t := E. (* Used to be accepted, but End M1 below was failing *)
+Inductive t := E. (* Used to be accepted, but End M1 below was failing *)
 End M1.
 
 Module M2 : L with Module E:=K.
 Inductive t := E.
-Fail Module E := K. (* Used to be accepted *)
-Fail End M2. (* Used to be accepted *)
+Module E := K. (* Used to be accepted *)
+End M2. (* Used to be accepted *)
