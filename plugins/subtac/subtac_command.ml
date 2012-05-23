84bac85548bba13eca7ed219031c5fd58629797e
msozeau <msozeau@85f007b7-540e-0410-9357-904b9bb8a0f7>
2012-05-23 13:09:51 +0000

    Fix bug #2778, checking that struct annotations refer to existing arguments in Program.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/branches/v8.4@15345 85f007b7-540e-0410-9357-904b9bb8a0f7

--- plugins/subtac/subtac_command.ml
+++ plugins/subtac/subtac_command.ml
@@ -380,9 +380,16 @@ let rec unfold f b =
     | Some (x, b') -> x :: unfold f b'
     | None -> []
 
+
+let find_annot loc id ctx = 
+  try rel_index id ctx
+  with Not_found -> 
+  user_err_loc(loc,"",
+	       str "No parameter named " ++ Nameops.pr_id id ++ str".")
+
 let compute_possible_guardness_evidences (n,_) (_, fixctx) fixtype =
   match n with
-  | Some (loc, n) -> [rel_index n fixctx]
+  | Some (loc, id) -> [find_annot loc id fixctx]
   | None ->
       (* If recursive argument was not given by user, we try all args.
 	 An earlier approach was to look only for inductive arguments,
