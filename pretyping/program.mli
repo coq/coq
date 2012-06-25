94a8d080d4a802ffb092fa19b84d3cd470f1e444
ppedrot <ppedrot@85f007b7-540e-0410-9357-904b9bb8a0f7>
2012-06-25 13:15:46 +0000

    Added a .mli to pretyping/program.ml
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@15490 85f007b7-540e-0410-9357-904b9bb8a0f7

---
+++ pretyping/program.mli
@@ -0,0 +1,35 @@
+(************************************************************************)
+(*  v      *   The Coq Proof Assistant  /  The Coq Development Team     *)
+(* <O___,, *   INRIA - CNRS - LIX - LRI - PPS - Copyright 1999-2010     *)
+(*   \VV/  **************************************************************)
+(*    //   *      This file is distributed under the terms of the       *)
+(*         *       GNU Lesser General Public License Version 2.1        *)
+(************************************************************************)
+
+open Term
+
+(** A bunch of Coq constants used by Progam *)
+
+val sig_typ : unit -> constr
+val sig_intro : unit -> constr
+val sig_proj1 : unit -> constr
+val sigT_typ : unit -> constr
+val sigT_intro : unit -> constr
+val sigT_proj1 : unit -> constr
+val sigT_proj2 : unit -> constr
+
+val prod_typ : unit -> constr
+val prod_intro : unit -> constr
+val prod_proj1 : unit -> constr
+val prod_proj2 : unit -> constr
+
+val coq_eq_ind : unit -> constr
+val coq_eq_refl : unit -> constr
+val coq_eq_refl_ref : unit -> Globnames.global_reference
+val coq_eq_rect : unit -> constr
+
+val coq_JMeq_ind : unit -> constr
+val coq_JMeq_refl : unit -> constr
+
+val mk_coq_and : constr list -> constr
+val mk_coq_not : constr -> constr
