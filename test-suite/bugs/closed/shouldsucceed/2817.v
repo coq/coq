73d5a4fd2d54c21a7d3b493993313d13618ce831
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2012-07-06 20:44:46 +0000

    Continuing r15459: it helps testing occur-check early in some situations (see rewrite MonoidMonadTrans.bind_toLower' in Misc/QuicksortComplexity/monoid_expec.v).
    Also fixing badly designed test 2817.v.
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@15543 85f007b7-540e-0410-9357-904b9bb8a0f7

--- test-suite/bugs/closed/shouldsucceed/2817.v
+++ test-suite/bugs/closed/shouldsucceed/2817.v
@@ -5,4 +5,5 @@ Goal forall (f: nat -> nat -> Prop) (x:bool)
   (H0: forall x0, f (if x then x0 else x0) x0),
 False.
 
-intros; apply H in H0. (* crashes *)
+intros.
+Fail apply H in H0. (* should fail without exhausting the stack *)
