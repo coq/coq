coq/coq

commit bb7e5aa54afa577da7661fb43cefc9711ccfe4af

Forward-port fixes from 8.4 (15358, 15353, 15333).
git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@15418 85f007b7-540e-0410-9357-904b9bb8a0f7

pretyping/coercion.ml
@@ -323,7 +323,7 @@ let coerce_itf loc env isevars v t c1 =
 
 let saturate_evd env evd =
   Typeclasses.resolve_typeclasses
-    ~with_goals:false ~split:true ~fail:false env evd
+    ~filter:Typeclasses.no_goals ~split:true ~fail:false env evd
 
 (* appliquer le chemin de coercions p à hj *)
 let apply_coercion env sigma p hj typ_cl =

interp/constrintern.ml
@@ -1854,7 +1854,7 @@ let interp_rawcontext_gen understand_type understand_judgment env bl =
 		(push_rel d env, d::params, succ n, impls)
 	  | Some b ->
 	      let c = understand_judgment env b in
-	      let d = (na, Some c.uj_val, c.uj_type) in
+	      let d = (na, Some c.uj_val, Termops.refresh_universes c.uj_type) in
 		(push_rel d env, d::params, succ n, impls))
       (env,[],1,[]) (List.rev bl)
   in (env, par), impls

pretyping/pretyping.ml
@@ -109,8 +109,14 @@ let interp_elimination_sort = function
 
 let resolve_evars env evdref fail_evar resolve_classes =
   if resolve_classes then
-    (evdref := Typeclasses.resolve_typeclasses ~with_goals:false
-     ~split:true ~fail:fail_evar env !evdref);
+    (evdref := Typeclasses.resolve_typeclasses 
+     ~filter:(if Flags.is_program_mode () 
+	      then Typeclasses.no_goals_or_obligations else Typeclasses.no_goals)
+     ~split:true ~fail:fail_evar env !evdref;
+     if Flags.is_program_mode () then (* Try optionally solving the obligations *)
+       evdref := Typeclasses.resolve_typeclasses 
+       ~filter:Typeclasses.all_evars ~split:true ~fail:false env !evdref;     
+    );
   (* Resolve eagerly, potentially making wrong choices *)
   evdref := (try consider_remaining_unif_problems
 	       ~ts:(Typeclasses.classes_transparent_state ()) env !evdref

pretyping/typeclasses.ml
@@ -500,6 +500,15 @@ let has_typeclasses evd =
 
 let solve_instanciations_problem = ref (fun _ _ _ _ _ -> assert false)
 
-let resolve_typeclasses ?(with_goals=false) ?(split=true) ?(fail=true) env evd =
+open Evar_kinds
+type evar_filter = Evar_kinds.t -> bool
+
+let all_evars _ = true
+let no_goals = function GoalEvar -> false | _ -> true
+let no_goals_or_obligations = function
+  | GoalEvar | QuestionMark _ -> false
+  | _ -> true
+
+let resolve_typeclasses ?(filter=no_goals) ?(split=true) ?(fail=true) env evd =
   if not (has_typeclasses evd) then evd
-  else !solve_instanciations_problem env evd with_goals split fail
+  else !solve_instanciations_problem env evd filter split fail

pretyping/typeclasses.mli
@@ -87,7 +87,13 @@ val mark_resolvable : evar_info -> evar_info
 val mark_resolvables : evar_map -> evar_map
 val is_class_evar : evar_map -> evar_info -> bool
 
-val resolve_typeclasses : ?with_goals:bool -> ?split:bool -> ?fail:bool ->
+(** Filter which evars to consider for resolution. *)
+type evar_filter = Evar_kinds.t -> bool
+val all_evars : evar_filter
+val no_goals : evar_filter
+val no_goals_or_obligations : evar_filter
+
+val resolve_typeclasses : ?filter:evar_filter -> ?split:bool -> ?fail:bool ->
   env -> evar_map -> evar_map
 val resolve_one_typeclass : env -> evar_map -> types -> open_constr
 
@@ -102,7 +108,7 @@ val register_remove_instance_hint : (global_reference -> unit) -> unit
 val add_instance_hint : constr -> bool -> int option -> unit
 val remove_instance_hint : global_reference -> unit
 
-val solve_instanciations_problem : (env -> evar_map -> bool -> bool -> bool -> evar_map) ref
+val solve_instanciations_problem : (env -> evar_map -> evar_filter -> bool -> bool -> evar_map) ref
 val solve_instanciation_problem : (env -> evar_map -> types -> open_constr) ref
 
 val declare_instance : int option -> bool -> global_reference -> unit

pretyping/unification.ml
@@ -967,7 +967,7 @@ let check_types env flags (sigma,_,_ as subst) m n =
 
 let try_resolve_typeclasses env evd flags m n =
   if flags.resolve_evars then
-    try Typeclasses.resolve_typeclasses ~with_goals:false ~split:false
+    try Typeclasses.resolve_typeclasses ~filter:Typeclasses.no_goals ~split:false
 	  ~fail:true env evd
     with e when Typeclasses_errors.unsatisfiable_exception e ->
       error_cannot_unify env evd (m, n)

proofs/clenvtac.ml
@@ -72,7 +72,7 @@ let clenv_refine with_evars ?(with_classes=true) clenv gls =
   let clenv = clenv_pose_dependent_evars with_evars clenv in
   let evd' =
     if with_classes then
-      Typeclasses.resolve_typeclasses ~with_goals:true
+      Typeclasses.resolve_typeclasses ~filter:Typeclasses.all_evars
         ~fail:(not with_evars) clenv.env clenv.evd
     else clenv.evd
   in

proofs/goal.ml
@@ -162,8 +162,8 @@ module Refinable = struct
   (* spiwack: it is not very fine grain since it solves all typeclasses holes,
       not only those containing the current goal, or a given term. But it
       seems to fit our needs so far. *)
-  let resolve_typeclasses ?with_goals ?split ?(fail=false) () env rdefs _ _ =
-    rdefs:=Typeclasses.resolve_typeclasses ?with_goals ?split ~fail env !rdefs;
+  let resolve_typeclasses ?filter ?split ?(fail=false) () env rdefs _ _ =
+    rdefs:=Typeclasses.resolve_typeclasses ?filter ?split ~fail env !rdefs;
     ()
 
 

commit e6bf1b6936f8bd61eb1266f7f4dd67181f3630f1

Fix regression introduced in r15418 that broke MathClasses. In program mode, try program coercions before the last resort typeclass resolution to resolve a conversion problem.
git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@15595 85f007b7-540e-0410-9357-904b9bb8a0f7

pretyping/coercion.ml
@@ -319,11 +319,9 @@ let coerce_itf loc env isevars v t c1 =
   let t = Option.map (app_opt env evars coercion) v in
     !evars, t
 
-
-
 let saturate_evd env evd =
   Typeclasses.resolve_typeclasses
-    ~filter:Typeclasses.no_goals ~split:true ~fail:false env evd
+    ~filter:Typeclasses.no_goals ~split:false ~fail:false env evd
 
 (* appliquer le chemin de coercions p à hj *)
 let apply_coercion env sigma p hj typ_cl =
@@ -461,16 +459,16 @@ let inh_conv_coerce_to_gen rigidonly loc env evd cj t =
     try
       inh_conv_coerce_to_fail loc env evd rigidonly (Some cj.uj_val) cj.uj_type t
     with NoCoercion ->
-      let evd = saturate_evd env evd in
-	try
-	  inh_conv_coerce_to_fail loc env evd rigidonly (Some cj.uj_val) cj.uj_type t
-	with NoCoercion ->
-	  (if Flags.is_program_mode () then
-	     try
-	       coerce_itf loc env evd (Some cj.uj_val) cj.uj_type t
-	     with NoSubtacCoercion ->
-	       error_actual_type_loc loc env evd cj t
-	   else error_actual_type_loc loc env evd cj t)
+      try
+	if Flags.is_program_mode () then
+	  coerce_itf loc env evd (Some cj.uj_val) cj.uj_type t
+	else raise NoSubtacCoercion
+      with NoSubtacCoercion ->
+	let evd = saturate_evd env evd in
+      	  try
+      	    inh_conv_coerce_to_fail loc env evd rigidonly (Some cj.uj_val) cj.uj_type t
+      	  with NoCoercion ->
+	    error_actual_type_loc loc env evd cj t
   in
   let val' = match val' with Some v -> v | None -> assert(false) in
     (evd',{ uj_val = val'; uj_type = t })