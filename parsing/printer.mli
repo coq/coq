3a1d1b456274a77b8107db9d6e8e13a81f8c9569
aspiwack <aspiwack@85f007b7-540e-0410-9357-904b9bb8a0f7>
2012-07-11 09:39:02 +0000

    A friendlier printing of remaining goals when no goal is focused.
    Only their conclusion is printed (with a "subgoal n" header) like every goal
    but the first in the usual case.
    
    
    Conflicts:
    
    	parsing/printer.ml
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/branches/v8.4@15592 85f007b7-540e-0410-9357-904b9bb8a0f7

--- parsing/printer.mli
+++ parsing/printer.mli
@@ -103,7 +103,7 @@ val pr_transparent_state   : transparent_state -> std_ppcmds
 (** Proofs *)
 
 val pr_goal                : goal sigma -> std_ppcmds
-val pr_subgoals            : string option -> evar_map -> evar list -> int list -> goal list -> std_ppcmds
+val pr_subgoals            : ?pr_first:bool -> string option -> evar_map -> evar list -> int list -> goal list -> std_ppcmds
 val pr_subgoal             : int -> evar_map -> goal list -> std_ppcmds
 val pr_concl               : int -> evar_map -> goal -> std_ppcmds
 
@@ -140,7 +140,7 @@ val pr_assumptionset :
 val pr_goal_by_id : string -> std_ppcmds
 
 type printer_pr = {
- pr_subgoals            : string option -> evar_map -> evar list -> int list -> goal list -> std_ppcmds;
+ pr_subgoals            : ?pr_first:bool -> string option -> evar_map -> evar list -> int list -> goal list -> std_ppcmds;
  pr_subgoal             : int -> evar_map -> goal list -> std_ppcmds;
  pr_goal                : goal sigma -> std_ppcmds;
 };;
