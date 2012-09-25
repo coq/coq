0e7800c3f9fbb7313c8f1ad194e8163988ec6ecd
ppedrot <ppedrot@85f007b7-540e-0410-9357-904b9bb8a0f7>
2012-09-25 14:07:39 +0000

    v8.4: Fixing #2865.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/branches/v8.4@15829 85f007b7-540e-0410-9357-904b9bb8a0f7

--- toplevel/vernacentries.ml
+++ toplevel/vernacentries.ml
@@ -208,7 +208,7 @@ let show_match id =
     str "| " ++ hov 1 (prlist_with_sep spc str l) ++ str " =>"
   in
   msg (v 1 (str "match # with" ++ fnl () ++
-	    prlist_with_sep fnl pr_branch patterns ++ fnl ()))
+	    prlist_with_sep fnl pr_branch patterns ++ fnl () ++ str "end" ++ fnl ()))
 
 (* "Print" commands *)
 
