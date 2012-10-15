coq/coq

commit 5893703b77fb17885fd66dbf575b9533cbf96a90

Fixed broken globalization of identifiers containing utf8 letters without knowing it.
Note: location tables have grown a lot, a better representation of the
contents of the glob files in coqdoc might improve efficiency.

Also added keywords.

Information is now obtained from the glob file to know the exact span
of identifiers. Kept a class of identifiers (and enriched them) for
the main purpose of distinguishing between idents and symbols in the
absence of a glob file.

Still a lot of work to do in coqdoc to make it more robust...

git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@14624 85f007b7-540e-0410-9357-904b9bb8a0f7

tools/coqdoc/index.ml
@@ -58,8 +58,10 @@ let full_ident sp id =
   then id
   else ""
 
-let add_def loc ty sp id =
-  Hashtbl.add reftable (!current_library, loc) (Def (full_ident sp id, ty));
+let add_def loc1 loc2 ty sp id =
+  for loc = loc1 to loc2 do
+    Hashtbl.add reftable (!current_library, loc) (Def (full_ident sp id, ty))
+  done;
   Hashtbl.add deftable id (Ref (!current_library, full_ident sp id, ty))
 
 let add_ref m loc m' sp id ty =
@@ -359,16 +361,16 @@ let read_glob vfile f =
 		    for loc=loc1 to loc2 do
 		      add_ref !cur_mod loc lib_dp sp id (type_of_string ty)
 		    done)
-	       with _ ->
-	       try
-		Scanf.sscanf s "R%d %s %s %s %s"
-		  (fun loc lib_dp sp id ty ->
-		    add_ref !cur_mod loc lib_dp sp id (type_of_string ty))
 	       with _ -> ())
 	  | _ ->
-	      try Scanf.sscanf s "%s %d %s %s"
-		(fun ty loc sp id -> add_def loc (type_of_string ty) sp id)
+	      try Scanf.sscanf s "not %d %s %s"
+		(fun loc sp id -> add_def loc loc (type_of_string "not") sp id)
+	      with Scanf.Scan_failure _ ->
+	      try Scanf.sscanf s "%s %d:%d %s %s"
+		(fun ty loc1 loc2 sp id ->
+                  add_def loc1 loc2 (type_of_string ty) sp id)
 	      with Scanf.Scan_failure _ -> ()
+
       end
     done; assert false
   with End_of_file ->

interp/dumpglob.ml
@@ -123,9 +123,14 @@ let remove_sections dir =
     (* Theorem/Lemma outside its outer section of definition *)
     dir
 
+let interval loc =
+  let loc1,loc2 = Util.unloc loc in
+  loc1, loc2-1
+
 let dump_ref loc filepath modpath ident ty =
-  dump_string (Printf.sprintf "R%d %s %s %s %s\n"
-		  (fst (Util.unloc loc)) filepath modpath ident ty)
+  let bl,el = interval loc in
+  dump_string (Printf.sprintf "R%d:%d %s %s %s %s\n"
+		  bl el filepath modpath ident ty)
 
 let add_glob_gen loc sp lib_dp ty =
   if dump () then
@@ -157,12 +162,14 @@ let add_glob_kn loc kn =
 let dump_binding loc id = ()
 
 let dump_definition (loc, id) sec s =
-  dump_string (Printf.sprintf "%s %d %s %s\n" s (fst (Util.unloc loc))
+  let bl,el = interval loc in
+  dump_string (Printf.sprintf "%s %d:%d %s %s\n" s bl el
 			(Names.string_of_dirpath (Lib.current_dirpath sec)) (Names.string_of_id id))
 
 let dump_reference loc modpath ident ty =
-  dump_string (Printf.sprintf "R%d %s %s %s %s\n"
-		  (fst (Util.unloc loc)) (Names.string_of_dirpath (Lib.library_dp ())) modpath ident ty)
+  let bl,el = interval loc in
+  dump_string (Printf.sprintf "R%d:%d %s %s %s %s\n"
+		  bl el (Names.string_of_dirpath (Lib.library_dp ())) modpath ident ty)
 
 let dump_constraint ((loc, n), _, _) sec ty =
   match n with
@@ -175,18 +182,21 @@ let dump_modref loc mp ty =
     let l = if l = [] then l else Util.list_drop_last l in
     let fp = Names.string_of_dirpath dp in
     let mp = Names.string_of_dirpath (Names.make_dirpath l) in
-      dump_string (Printf.sprintf "R%d %s %s %s %s\n"
-		      (fst (Util.unloc loc)) fp mp "<>" ty)
+    let bl,el = interval loc in
+      dump_string (Printf.sprintf "R%d:%d %s %s %s %s\n"
+		      bl el fp mp "<>" ty)
 
 let dump_moddef loc mp ty =
   if dump () then
+    let bl,el = interval loc in
     let (dp, l) = Lib.split_modpath mp in
     let mp = Names.string_of_dirpath (Names.make_dirpath l) in
-      dump_string (Printf.sprintf "%s %d %s %s\n" ty (fst (Util.unloc loc)) "<>" mp)
+      dump_string (Printf.sprintf "%s %d:%d %s %s\n" ty bl el "<>" mp)
 
 let dump_libref loc dp ty =
-  dump_string (Printf.sprintf "R%d %s <> <> %s\n"
-		  (fst (Util.unloc loc)) (Names.string_of_dirpath dp) ty)
+  let bl,el = interval loc in
+  dump_string (Printf.sprintf "R%d:%d %s <> <> %s\n"
+		  bl el (Names.string_of_dirpath dp) ty)
 
 let cook_notation df sc =
   (* We encode notations so that they are space-free and still human-readable *)

tools/coqdoc/cpretty.mll
@@ -276,7 +276,7 @@ let firstchar =
   (* '\206' ([ '\145' - '\183'] | '\187') | *)
   (* '\xCF' [ '\x00' - '\xCE' ] |  *)
   (* utf-8 letterlike symbols *)
-  '\206' ('\160' | [ '\177'-'\183'] | '\187') |
+  '\206' (['\145'-'\161'] | ['\163'-'\187']) |
   '\226' ('\130' [ '\128'-'\137' ] (* subscripts *)
     | '\129' [ '\176'-'\187' ] (* superscripts *)
     | '\132' ['\128'-'\191'] | '\133' ['\128'-'\143'])

tools/coqdoc/output.ml
@@ -45,6 +45,7 @@ let is_keyword =
       "Implicit Arguments"; "Add"; "Strict";
       "Typeclasses"; "Instance"; "Global Instance"; "Class"; "Instantiation";
       "subgoal";
+      "Opaque"; "Transparent";
       (* Program *)
       "Program Definition"; "Program Example"; "Program Fixpoint"; "Program Lemma";
       "Obligation"; "Obligations"; "Solve"; "using"; "Next Obligation"; "Next";

commit a27c9e7bfe59dba76f1ae7ee139532ce1d4df6f7

Fixing coqdoc index bugs introduced in r14624 and r15053. Revision r14624 introduced bug #2709 on duplicate entries in index and tentative fix of it in r15053 mixed up names of files and names of constants in index (as reported by P. Castéran on coqdev).
Patch by Hugo Herbelin :-)

git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@15885 85f007b7-540e-0410-9357-904b9bb8a0f7

tools/coqdoc/index.ml
@@ -38,8 +38,9 @@ type index_entry =
 let current_library = ref ""
   (** refers to the file being parsed *)
 
-(** [deftable] stores only definitions and is used to interpolate idents
-    inside comments, which are not globalized otherwise. *)
+(** [deftable] stores only definitions and is used to build the index
+    and to interpolate idents inside comments, which are not
+    globalized otherwise. *)
 
 let deftable = Hashtbl.create 97
 
@@ -59,18 +60,18 @@ let add_def loc1 loc2 ty sp id =
   for loc = loc1 to loc2 do
     Hashtbl.add reftable (!current_library, loc) (Def (full_ident sp id, ty))
   done;
-  Hashtbl.add deftable id (Def (full_ident sp id, ty))
+  Hashtbl.add deftable id (!current_library, full_ident sp id, ty)
 
 let add_ref m loc m' sp id ty =
   if Hashtbl.mem reftable (m, loc) then ()
   else Hashtbl.add reftable (m, loc) (Ref (m', full_ident sp id, ty));
   let idx = if id = "<>" then m' else id in
     if Hashtbl.mem deftable idx then ()
-    else Hashtbl.add deftable idx (Ref (m', full_ident sp id, ty))
+    else Hashtbl.add deftable idx (m', full_ident sp id, ty)
 
 let find m l = Hashtbl.find reftable (m, l)
 
-let find_string m s = Hashtbl.find deftable s
+let find_string m s = let (m,s,t) = Hashtbl.find deftable s in Ref (m,s,t)
 
 (*s Manipulating path prefixes *)
 
@@ -254,9 +255,8 @@ let all_entries () =
     let l = try Hashtbl.find bt t with Not_found -> [] in
       Hashtbl.replace bt t ((s,m) :: l)
   in
-  let classify m e = match e with
-    | Def (s,t) -> add_g s m t; add_bt t s m
-    | Ref _ | Mod _ -> ()
+  let classify _ (m,s,t) =
+    if s <> "" && Hashtbl.mem local_modules m then (add_g s m t; add_bt t s m)
   in
     Hashtbl.iter classify deftable;
     Hashtbl.iter (fun id m -> add_g id m Library; add_bt Library id m) modules;