coq/coq

commit 86357b63200368c818bbade20f2d71a3ddbaacb5

Native compiler: hash-consing of generated code and values.
git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@16363 85f007b7-540e-0410-9357-904b9bb8a0f7

kernel/nativecode.mli
@@ -24,6 +24,8 @@ val mk_open : string -> global
 
 type symbol
 
+val clear_symb_tbl : unit -> unit
+
 val get_value : symbol array -> int -> Nativevalues.t
 
 val get_sort : symbol array -> int -> sorts
@@ -47,14 +49,14 @@ val empty_updates : code_location_updates
 val register_native_file : string -> unit
 
 val compile_constant_field : env -> string -> constant ->
-  global list * symbol list * code_location_updates ->
+  global list * code_location_updates ->
   constant_body ->
-    global list * symbol list * code_location_updates
+    global list * code_location_updates
 
 val compile_mind_field : string -> module_path -> label ->
-  global list * symbol list * code_location_updates ->
+  global list * code_location_updates ->
   mutual_inductive_body ->
-    global list * symbol list * code_location_updates
+    global list * code_location_updates
 
 val mk_conv_code : env -> string -> constr -> constr -> linkable_code
 val mk_norm_code : env -> string -> constr -> linkable_code
@@ -64,3 +66,5 @@ val mk_library_header : dir_path -> global list
 val mod_uid_of_dirpath : dir_path -> string
 
 val update_locations : code_location_updates -> unit
+
+val add_header_comment : global list -> string -> global list

kernel/nativelambda.ml
@@ -20,13 +20,11 @@ type lambda =
   | Lvar          of identifier
   | Lprod         of lambda * lambda 
   | Llam          of name array * lambda  
-  | Lrec          of name * lambda
   | Llet          of name * lambda * lambda
   | Lapp          of lambda * lambda array
   | Lconst        of string * constant (* prefix, constant name *)
   | Lcase         of annot_sw * lambda * lambda * lam_branches 
                   (* annotations, term being matched, accu, branches *)
-  | Lareint       of lambda array 
   | Lif           of lambda * lambda * lambda
   | Lfix          of (int array * int) * fix_decl 
   | Lcofix        of int * fix_decl 
@@ -117,9 +115,6 @@ let map_lam_with_binders g f n lam =
   | Llam(ids,body) ->
       let body' = f (g (Array.length ids) n) body in
       if body == body' then lam else mkLlam ids body'
-  | Lrec(id,body) ->
-      let body' = f (g 1 n) body in
-      if body == body' then lam else Lrec(id,body')
   | Llet(id,def,body) ->
       let def' = f n def in
       let body' = f (g 1 n) body in
@@ -139,9 +134,6 @@ let map_lam_with_binders g f n lam =
 	if body == body' then b else (cn,ids,body') in
       let br' = Array.smartmap on_b br in
       if t == t' && a == a' && br == br' then lam else Lcase(annot,t',a',br')
-  | Lareint a ->
-      let a' = Array.smartmap (f n) a in
-      if a == a' then lam else Lareint a' 
   | Lif(t,bt,bf) ->
       let t' = f n t in
       let bt' = f n bt in
@@ -323,8 +315,6 @@ let rec occurence k kind lam =
       occurence k (occurence k kind dom) codom
   | Llam(ids,body) ->
       let _ = occurence (k+Array.length ids) false body in kind
-  | Lrec(id,body) ->
-      let _ = occurence (k+1) false body in kind
   | Llet(_,def,body) ->
       occurence (k+1) (occurence k kind def) body
   | Lapp(f, args) ->
@@ -337,8 +327,6 @@ let rec occurence k kind lam =
       Array.iter (fun (_,ids,c) -> 
 	r := occurence (k+Array.length ids) kind c && !r) br;
       !r 
-  | Lareint a -> 
-      occurence_args k kind a
   | Lif (t, bt, bf) ->
       let kind = occurence k kind t in
       kind && occurence k kind bt && occurence k kind bf

kernel/nativelibrary.ml
@@ -26,7 +26,7 @@ let rec translate_mod prefix mp env mod_expr acc =
   | SEBfunctor (mbid,mtb,meb) -> acc
   | SEBapply (f,x,_) -> assert false
   | SEBwith _ -> assert false
-and translate_field prefix mp env (code, values, upds as acc) (l,x) =
+and translate_field prefix mp env (code, upds as acc) (l,x) =
   match x with
   | SFBconst cb ->
       let con = make_con mp empty_dirpath l in
@@ -45,14 +45,14 @@ let dump_library mp dp env mod_expr =
       let env = add_signature mp msb empty_delta_resolver env in
       let prefix = mod_uid_of_dirpath dp ^ "." in
       let t0 = Sys.time () in 
-      let mlcode, values, upds =
-        List.fold_left (translate_field prefix mp env) ([],[],empty_updates)
-          msb
+      clear_symb_tbl ();
+      let mlcode, upds =
+        List.fold_left (translate_field prefix mp env) ([],empty_updates) msb
       in
       let t1 = Sys.time () in
-      let time_info = Format.sprintf "Compiled library. Time: %.5f@." (t1-.t0) in
-      if !Flags.debug then Pp.msg_debug (Pp.str time_info);
-      List.rev mlcode, Array.of_list (List.rev values), upds
+      let time_info = Format.sprintf "Time spent generating this code: %.5fs" (t1-.t0) in
+      let mlcode = add_header_comment (List.rev mlcode) time_info in
+      mlcode, get_symbols_tbl (), upds
   | _ -> assert false
 
 

kernel/nativelambda.mli
@@ -20,13 +20,11 @@ type lambda =
   | Lvar          of identifier
   | Lprod         of lambda * lambda 
   | Llam          of name array * lambda  
-  | Lrec          of name * lambda
   | Llet          of name * lambda * lambda
   | Lapp          of lambda * lambda array
   | Lconst        of string * constant (* prefix, constant name *)
   | Lcase         of annot_sw * lambda * lambda * lam_branches 
                   (* annotations, term being matched, accu, branches *)
-  | Lareint       of lambda array 
   | Lif           of lambda * lambda * lambda
   | Lfix          of (int array * int) * fix_decl 
   | Lcofix        of int * fix_decl 

kernel/nativevalues.ml
@@ -8,6 +8,7 @@
 open Term
 open Names
 open Errors
+open Util
 
 (** This modules defines the representation of values internally used by
 the native compiler *)
@@ -30,9 +31,21 @@ type annot_sw = {
     asw_prefix : string
   }
 
+(* We compare only what is relevant for generation of ml code *)
+let eq_annot_sw asw1 asw2 =
+  eq_ind asw1.asw_ind asw2.asw_ind &&
+  String.equal asw1.asw_prefix asw2.asw_prefix
+
+open Hashset.Combine
+
+let hash_annot_sw asw =
+  combine (Hashtbl.hash asw.asw_ind) (Hashtbl.hash asw.asw_prefix)
+
 type sort_annot = string * int
 
 type rec_pos = int array
+
+let eq_rec_pos = Array.equal Int.equal
 
 type atom = 
   | Arel of int

kernel/nativevalues.mli
@@ -29,9 +29,15 @@ type annot_sw = {
     asw_prefix : string
   }
 
+val eq_annot_sw : annot_sw -> annot_sw -> bool
+
+val hash_annot_sw : annot_sw -> int
+
 type sort_annot = string * int
       
 type rec_pos = int array
+
+val eq_rec_pos : rec_pos -> rec_pos -> bool
 
 type atom =
   | Arel of int

commit d135b02a31b0ec84b61726a3d0aa7301b08a48ad

Fixing a bug in the native compiler, introduced by r16363, leading to undefined variables in the generated code.
git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@16619 85f007b7-540e-0410-9357-904b9bb8a0f7

kernel/declarations.mli
@@ -40,10 +40,12 @@ type constant_def =
   | Def of Lazyconstr.constr_substituted
   | OpaqueDef of Lazyconstr.lazy_constr
 
+(** Linking information for the native compiler. The boolean flag indicates if
+  the term is protected by a lazy tag *)
+
 type native_name =
-  | Linked of string
-  | LinkedLazy of string
-  | LinkedInteractive of string
+  | Linked of string * bool
+  | LinkedInteractive of string * bool
   | NotLinked
 
 type constant_body = {

kernel/nativecode.ml
@@ -556,6 +556,8 @@ module HashtblGlobal = Hashtbl.Make(HashedTypeGlobal)
 
 let global_tbl = HashtblGlobal.create 19991
 
+let clear_global_tbl () = HashtblGlobal.clear global_tbl
+
 let push_global gn t =
   try HashtblGlobal.find global_tbl t
   with Not_found ->
@@ -1473,25 +1475,28 @@ and compile_named env auxdefs id =
   | None -> 
       Glet(Gnamed id, MLprimitive (Mk_var id))::auxdefs
 
-let compile_constant env prefix con body =
+let compile_constant env prefix ~interactive con body =
   match body with
   | Def t ->
       let t = Lazyconstr.force t in
       let code = lambda_of_constr env t in
-      let code, name =
-        if is_lazy t then mk_lazy code, LinkedLazy prefix
-        else code, Linked prefix
+      let is_lazy = is_lazy t in
+      let code = if is_lazy then mk_lazy code else code in
+      let name =
+        if interactive then LinkedInteractive (prefix, is_lazy)
+        else Linked (prefix, is_lazy)
       in
       let l = con_label con in
       let auxdefs,code = compile_with_fv env [] (Some l) code in
-      let l =
+      let code =
         optimize_stk (Glet(Gconstant ("",con),code)::auxdefs)
       in
-      l, name
+      code, name
   | _ -> 
       let i = push_symbol (SymbConst con) in
       [Glet(Gconstant ("",con), MLapp (MLprimitive Mk_const, [|get_const_code i|]))],
-      Linked prefix
+      if interactive then LinkedInteractive (prefix, false)
+      else Linked (prefix, false)
 
 let loaded_native_files = ref ([] : string list)
 
@@ -1502,16 +1507,17 @@ let register_native_file s =
 let is_code_loaded ~interactive name =
   match !name with
   | NotLinked -> false
-  | LinkedInteractive s ->
+  | LinkedInteractive (s,_) ->
       if (interactive && List.mem s !loaded_native_files) then true
       else (name := NotLinked; false)
-  | LinkedLazy s | Linked s ->
-   if List.mem s !loaded_native_files then true else (name := NotLinked; false)
+  | Linked (s,_) ->
+      if List.mem s !loaded_native_files then true
+      else (name := NotLinked; false)
 
 let param_name = Name (id_of_string "params")
 let arg_name = Name (id_of_string "arg")
 
-let compile_mind prefix mb mind stack =
+let compile_mind prefix ~interactive mb mind stack =
   let f i stack ob =
     let gtype = Gtype((mind, i), Array.map snd ob.mind_reloc_tbl) in
     let j = push_symbol (SymbInd (mind,i)) in
@@ -1531,7 +1537,11 @@ let compile_mind prefix mb mind stack =
     in
     Array.fold_left_i add_construct (gtype::accu::stack) ob.mind_reloc_tbl
   in
-  let upd = (mb.mind_native_name, Linked prefix) in
+  let name =
+    if interactive then LinkedInteractive (prefix, false)
+    else Linked (prefix, false)
+  in
+  let upd = (mb.mind_native_name, name) in
   Array.fold_left_i f stack mb.mind_packets, upd
 
 type code_location_update =
@@ -1550,7 +1560,7 @@ let compile_mind_deps env prefix ~interactive
     || Mindmap_env.mem mind mind_updates
   then init
   else
-    let comp_stack, upd = compile_mind prefix mib mind comp_stack in
+    let comp_stack, upd = compile_mind prefix ~interactive mib mind comp_stack in
     let mind_updates = Mindmap_env.add mind upd mind_updates in
     (comp_stack, (mind_updates, const_updates))
 
@@ -1558,8 +1568,8 @@ let compile_mind_deps env prefix ~interactive
    reverse order, as well as linking information updates *)
 let rec compile_deps env prefix ~interactive init t =
   match kind_of_term t with
-  | Meta _ -> invalid_arg "Nativecode.get_deps: Meta"
-  | Evar _ -> invalid_arg "Nativecode.get_deps: Evar"
+  | Meta _ -> invalid_arg "Nativecode.compile_deps: Meta"
+  | Evar _ -> invalid_arg "Nativecode.compile_deps: Evar"
   | Ind (mind,_) -> compile_mind_deps env prefix ~interactive init mind
   | Const c ->
       let c = get_allias env c in
@@ -1573,7 +1583,7 @@ let rec compile_deps env prefix ~interactive init t =
         | Def t -> compile_deps env prefix ~interactive init (Lazyconstr.force t)
         | _ -> init
       in
-      let code, name = compile_constant env prefix c cb.const_body in
+      let code, name = compile_constant env prefix ~interactive c cb.const_body in
       let comp_stack = code@comp_stack in
       let const_updates = Cmap_env.add c (cb.const_native_name, name) const_updates in
       comp_stack, (mind_updates, const_updates)
@@ -1589,18 +1599,24 @@ let compile_constant_field env prefix con (code, (mupds, cupds)) cb =
   match cb.const_body with
   | Def t ->
     let t = Lazyconstr.force t in
-    let (code, (mupds, cupds)) = compile_deps env prefix ~interactive:false acc t in
-    let (gl, name) = compile_constant env prefix con cb.const_body in
+    let (code, (mupds, cupds)) =
+      compile_deps env prefix ~interactive:false acc t
+    in
+    let (gl, name) =
+      compile_constant ~interactive:false env prefix con cb.const_body
+    in
     let cupds = Cmap_env.add con (cb.const_native_name, name) cupds in
     gl@code, (mupds, cupds)
   | _ ->
-    let (gl, name) = compile_constant env prefix con cb.const_body in
+    let (gl, name) =
+      compile_constant env prefix ~interactive:false con cb.const_body
+    in
     let cupds = Cmap_env.add con (cb.const_native_name, name) cupds in
     gl@code, (mupds, cupds)
 
 let compile_mind_field prefix mp l (code, (mupds, cupds)) mb =
   let mind = MutInd.make2 mp l in
-  let code, upd = compile_mind prefix mb mind code in
+  let code, upd = compile_mind prefix ~interactive:false mb mind code in
   let mupds = Mindmap_env.add mind upd mupds in
   code, (mupds, cupds)
 

kernel/nativecode.mli
@@ -44,6 +44,8 @@ type code_location_update
 type code_location_updates
 type linkable_code = global list * code_location_updates
 
+val clear_global_tbl : unit -> unit
+
 val empty_updates : code_location_updates
 
 val register_native_file : string -> unit

kernel/nativelambda.ml
@@ -90,17 +90,15 @@ let get_mind_prefix env mind =
    let mib = lookup_mind mind env in
    match !(mib.mind_native_name) with
    | NotLinked -> ""
-   | Linked s -> s
-   | LinkedLazy s -> s
-   | LinkedInteractive s -> s
+   | Linked (s,_) -> s
+   | LinkedInteractive (s,_) -> s
 
 let get_const_prefix env c =
    let cb = lookup_constant c env in
    match !(cb.const_native_name) with
    | NotLinked -> ""
-   | Linked s -> s
-   | LinkedLazy s -> s
-   | LinkedInteractive s -> s
+   | Linked (s,_) -> s
+   | LinkedInteractive (s,_) -> s
     
 (* A generic map function *)
 

kernel/nativelibrary.ml
@@ -45,6 +45,7 @@ let dump_library mp dp env mod_expr =
       let env = add_signature mp msb empty_delta_resolver env in
       let prefix = mod_uid_of_dirpath dp ^ "." in
       let t0 = Sys.time () in 
+      clear_global_tbl ();
       clear_symb_tbl ();
       let mlcode, upds =
         List.fold_left (translate_field prefix mp env) ([],empty_updates) msb