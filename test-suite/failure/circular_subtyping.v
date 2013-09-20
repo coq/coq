f361ba32a28796514b8d3469c7bee3f43ad61fc1
xclerc <xclerc@85f007b7-540e-0410-9357-904b9bb8a0f7>
2013-09-20 12:57:59 +0000

    Merge "circular_subtyping?.v" tests into a single "circular_subtyping.v" test.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@16799 85f007b7-540e-0410-9357-904b9bb8a0f7

--- test-suite/failure/circular_subtyping.v
+++ test-suite/failure/circular_subtyping.v
@@ -1,7 +1,10 @@
 (* subtyping verification in presence of  pseudo-circularity*)
 Module Type S. End S.
 Module Type T. Declare Module M:S. End T.
-
 Module N:S. End N.
 Module NN <: T. Module M:=N. End NN.
+
 Fail Module P <: T with Module M:=NN := NN.
+
+Module F (X:S) (Y:T with Module M:=X). End F.
+Fail Module G := F N N.
--- test-suite/failure/circular_subtyping2.v
+++
@@ -1,8 +0,0 @@
-(*subtyping verification in presence of  pseudo-circularity at functor application *)
-Module Type S. End S.
-Module Type T. Declare Module M:S. End T.
-Module N:S. End N.
-
-Module F (X:S) (Y:T with Module M:=X). End F.
-
-Fail Module G := F N N.
