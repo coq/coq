0db8c26797d0f95292d0089e40bfd2dcc78030f2
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2013-11-28 13:28:47 +0100

    v8.4: Fixing bug #3169 and avoiding anomaly in bug #2885 (vm_compute not supporting metas/evars). Fix of #3169 is by calling pretyping retyper rather than the non-evar-aware kernel type-checker (since terms is supposed to be already typable).
    Support of metas/evars in vm is still to be done...

--- proofs/redexpr.ml
+++ proofs/redexpr.ml
@@ -22,8 +22,10 @@ open Libobject
 open Summary
 
 (* call by value normalisation function using the virtual machine *)
-let cbv_vm env _ c =
-  let ctyp = (fst (Typeops.infer env c)).Environ.uj_type in
+let cbv_vm env sigma c =
+  let ctyp = Retyping.get_type_of env sigma c in
+  if Termops.occur_meta_or_existential c then
+    error "vm_compute does not support existential variables.";
   Vnorm.cbv_vm env c ctyp
 
 
