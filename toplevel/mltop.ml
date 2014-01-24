84b09aae2c727877c98e02508ddcd3b6a3ee9db7
Pierre Letouzey <pierre.letouzey@inria.fr>
2014-01-24 17:12:32 +0100

    Mltop: explicitly qualify calls to CUnix

--- toplevel/mltop.ml
+++ toplevel/mltop.ml
@@ -10,7 +10,6 @@ open Errors
 open Util
 open Pp
 open Flags
-open CUnix
 open Libobject
 open System
 
@@ -48,8 +47,9 @@ open System
 (* This path is where we look for .cmo *)
 let coq_mlpath_copy = ref ["."]
 let keep_copy_mlpath path =
-  let cpath = canonical_path_name path in
-  let filter path' = not (String.equal cpath (canonical_path_name path')) in
+  let cpath = CUnix.canonical_path_name path in
+  let filter path' = not (String.equal cpath (CUnix.canonical_path_name path'))
+  in
   coq_mlpath_copy := path :: List.filter filter !coq_mlpath_copy
 
 (* If there is a toplevel under Coq *)
