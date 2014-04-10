4ec840d145927b0dd6ec1040a8a5ff573a0d7685
Pierre Boutillier <pierre.boutillier@ens-lyon.org>
2014-04-10 16:17:12 +0200

    coqtop -batch refuses Back 1 but accepts Undo.

--- test-suite/bugs/closed/3251.v
+++ test-suite/bugs/closed/3251.v
@@ -5,7 +5,7 @@ foo is defined
 foo is defined
 
 ... that's fishy.  But E. Tassi tells me that it's correct, because it happens on two threads. *)
-Back 1.
+Undo.
 Ltac foo := idtac.
 (* Before 5b39c3535f7b3383d89d7b844537244a4e7c0eca, this would print out: *)
 (* Anomaly: Backtrack.backto to a state with no vcs_backup. Please report. *)
