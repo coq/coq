35f5a3fce53c8517d897660df6b66294d8662e46
Enrico Tassi <Enrico.Tassi@inria.fr>
2014-04-10 19:45:30 +0200

    better description of bug 3251

--- test-suite/bugs/closed/3251.v
+++ test-suite/bugs/closed/3251.v
@@ -4,7 +4,9 @@ Ltac foo := idtac.
 foo is defined
 foo is defined
 
-... that's fishy.  But E. Tassi tells me that it's correct, because it happens on two threads. *)
+... that's fishy.  But E. Tassi tells me that it's expected since "Ltac" generates a side
+effect that escapes the proof.  In the STM model this means the command is executed twice,
+once in the proof branch, and another time in the main branch *)
 Undo.
 Ltac foo := idtac.
 (* Before 5b39c3535f7b3383d89d7b844537244a4e7c0eca, this would print out: *)
