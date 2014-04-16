635f559018f6a16caf5c6a75a1d38bb807e6a4c8
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2014-04-16 12:15:42 +0200

    Fixing missing headers.

--- theories/Reals/Ranalysis5.v
+++ theories/Reals/Ranalysis5.v
@@ -1,3 +1,11 @@
+(************************************************************************)
+(*  v      *   The Coq Proof Assistant  /  The Coq Development Team     *)
+(* <O___,, *   INRIA - CNRS - LIX - LRI - PPS - Copyright 1999-2012     *)
+(*   \VV/  **************************************************************)
+(*    //   *      This file is distributed under the terms of the       *)
+(*         *       GNU Lesser General Public License Version 2.1        *)
+(************************************************************************)
+
 Require Import Rbase.
 Require Import Ranalysis_reg.
 Require Import Rfunctions.
