5ca744aef261972e3f0c6bbed1ef5bfe1c8cff2e
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2014-05-08 15:44:27 +0200

    Fixing test-suite for bug #3043.

--- test-suite/bugs/closed/3043.v
+++ test-suite/bugs/closed/3043.v
@@ -1,4 +1,4 @@
-Goal (fun A (P : A -> Prop) (X : sigT P) => proj1_sig X) = 
+Goal (fun A (P : A -> Prop) (X : sigT P) => proj1_sig (sig_of_sigT X)) =
      (fun A (P : A -> Prop) (X : sigT P) => projT1 X).
   reflexivity.
 Qed.
