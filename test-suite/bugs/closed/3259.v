ad4c15f22abcb473dfa4cfc31e2ad5bf37c22a92
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2014-05-13 16:03:41 +0200

    Test-suite for bug #3259.

---
+++ test-suite/bugs/closed/3259.v
@@ -0,0 +1,21 @@
+Goal forall m n, n+n = m+m -> m+m = m+m.
+Proof.
+intros.
+set (k := n+n) in *.
+cut (n=m).
+intro.
+subst n.
+admit.
+admit.
+Qed.
+
+Goal forall m n, n+n = m+m -> n+n = m+m.
+Proof.
+intros.
+set (k := n+n).
+cut (n=m).
+intro.
+subst n.
+admit.
+admit.
+Qed.
