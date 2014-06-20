a57c7612767f7bd55704639f03c0899d5c3edfbb
Matthieu Sozeau <mattam@mattam.org>
2014-06-20 16:06:06 +0200

    Fixed bug 3332.

--- test-suite/bugs/opened/3332.v
+++
@@ -1,6 +0,0 @@
-(* -*- mode: coq; coq-prog-args: ("-emacs" "-time") -*- *)
-Definition foo : True.
-Proof.
-Abort. (* Toplevel input, characters 15-21:
-Anomaly: Backtrack.backto to a state with no vcs_backup. Please report. *)
-(* Anomaly: VernacAbort not handled by Stm. Please report. *)
