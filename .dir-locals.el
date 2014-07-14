737b01ab2db22762a8013b40a69dfd1d77da1fcd
Matthieu Sozeau <matthieu.sozeau@inria.fr>
2014-07-14 22:53:21 +0200

    Don't set global variables from a hidden file. (!)

--- .dir-locals.el
+++ .dir-locals.el
@@ -11,16 +11,19 @@
 		(coq-project-find-file
 		 (and (boundp 'coq-project-find-file) coq-project-find-file)))
 	    ;; coq tags file and coq debugger executable
-	    (setq tags-file-name (concat coq-root-directory "TAGS")
-		  camldebug-command-name (concat coq-root-directory
+	    (set (make-local-variable 'tags-file-name)
+		 (concat coq-root-directory "TAGS"))
+	    (setq camldebug-command-name (concat coq-root-directory
 						 "dev/ocamldebug-coq"))
 
 	    ;; Setting the compilation directory to coq root. This is
 	    ;; mutually exclusive with the setting of default-directory
 	    ;; below. Also setting the path for next error.
 	    (unless coq-project-find-file
-	      (setq compile-command (concat "make -C " coq-root-directory))
-	      (setq compilation-search-path (cons coq-root-directory nil)))
+	      (set (make-local-variable 'compile-command)
+		   (concat "make -C " coq-root-directory))
+	      (set (make-local-variable 'compilation-search-path)
+		   (cons coq-root-directory nil)))
 
 	    ;; Set default directory to coq root ONLY IF variable
 	    ;; coq-project-find-file is non nil. This should remain a
