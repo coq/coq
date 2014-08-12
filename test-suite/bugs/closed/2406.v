305c70d55f8305e8cca8daf8796ff27a142332fe
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2014-08-12 10:49:07 +0200

    In bug #2406, renouncing to test failure of parsing error. (AFAIU, it is the table of supported unicode characters which has to be upgraded anyway.)

--- test-suite/bugs/closed/2406.v
+++ test-suite/bugs/closed/2406.v
@@ -1,3 +1,6 @@
 (* Check correct handling of unsupported notations *)
 Notation "''" := (fun x => x) (at level 20).
+
+(* This fails with a syntax error but it is not catched by Fail
 Fail Definition crash_the_rooster f := .
+*)
