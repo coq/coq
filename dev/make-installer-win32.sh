938dd9ba81d0bba5a9358627405d3110fc4ee335
Enrico Tassi <Enrico.Tassi@inria.fr>
2014-09-05 15:04:49 +0200

    Installer for win32
    Not 100% functional, but coqide works.
    
    The native compiler is embedded but:
    - some path mangling problem prevents it from working even when run via
      cygwin (like in the build process)
    - CAMLLIB must be exported to ${COQ}\ocaml\lib to have it run
      (coq should do it).
    
    fix

---
+++ dev/make-installer-win32.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+NSIS="/cygdrive/c/Program Files/NSIS/makensis"
+ZIP=_make.zip
+URL1=http://sourceforge.net/projects/gnuwin32/files/make/3.81/make-3.81-bin.zip/download
+URL2=http://sourceforge.net/projects/gnuwin32/files/make/3.81/make-3.81-dep.zip/download
+
+./configure -prefix ./ -with-doc no
+make 
+if [ ! -e bin/make.exe ]; then
+  wget -O $ZIP $URL1 && 7z x $ZIP "bin/*"
+  wget -O $ZIP $URL2 && 7z x $ZIP "bin/*"
+  rm -rf $ZIP
+fi
+ocamlc unix.cma tools/mkwinapp.ml -o bin/mkwinapp.exe
+bin/mkwinapp.exe bin/coqide.exe
+bin/mkwinapp.exe bin/coqide.byte.exe
+VERSION=`grep ^VERSION= config/Makefile | cut -d = -f 2`
+cd dev/nsis
+"$NSIS" -DVERSION=$VERSION -DGTK_RUNTIME="`cygpath -w $BASE`" coq.nsi
+ls *exe
+cd ../..
