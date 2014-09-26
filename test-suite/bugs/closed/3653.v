7115d86684a6215a0bb0197f112d22d5387a4ae3
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2014-09-26 13:17:53 +0200

    Adding a test for bug #3653.

---
+++ test-suite/bugs/closed/3653.v
@@ -0,0 +1,12 @@
+Require Setoid.
+
+Variables P Q : forall {T : Set}, T -> Prop.
+
+Lemma rule{T : Set}{x : T} : Q x <-> P x. admit. Qed.
+
+Goal forall (T : Set)(x : T), Q x <-> P x.
+Proof.
+intros T x.
+setoid_rewrite rule.
+reflexivity.
+Qed.
