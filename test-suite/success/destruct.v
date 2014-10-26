coq/coq

commit bf018569405c0ae1c5d08dfa27600102b9d77977

This commit introduces changes in induction and destruct.
The main change is that selection of subterm is made similar whether
the given term is fully applied or not.
- The selection of subterm now works as follows depending on whether
  the "at" is given, of whether the subterm is fully applied or not,
  and whether there are incompatible subterms matching the pattern. In
  particular, we have:
  "at" given
  | subterm fully applied
  | | incompatible subterms
  | | |
  Y Y - it works like in 8.4
  Y N - this was broken in 8.4 ("at" was ineffective and it was finding
        all subterms syntactically equal to the first one which matches)
  N Y Y it now finds all subterms like the first one which matches
        while in 8.4 it used to fail (I hope it is not a too risky in-draft
        for a semantics we would regret...) (e.g. "destruct (S _)" on
        goal "S x = S y + S x" now selects the two occurrences of "S x"
        while it was failing before)
  N Y N it works like in 8.4
  N N - it works like in 8.4, selecting all subterms like the
        first one which matches
- Note that the "historical" semantics, when looking for a subterm, to
  select all subterms that syntactically match the first subterm to
  match the pattern (looking from left to right) is now internally called
  "like first".
- Selection of subterms can now find the type by pattern-matching (useful e.g.
  for "induction (nat_rect _ _ _ _)")
- A version of Unification.w_unify w/o any conversion is used for
  finding the subterm: it could be easily replaced by an other
  matching algorithm.

In particular, "destruct H" now works on a goal such as "H:True -> x<=y |- P y".

Secondary change is in the interpretation of terms with existential
variables:
- When several arguments are given, interpretation is delayed at the
  time of execution
- Because we aim at eventually accepting "edestruct c" with unresolved
  holes in c, we need the sigma obtained from c to be an extension of

pretyping/unification.ml
@@ -90,7 +90,7 @@ let abstract_scheme env evd c l lname_typ =
 
 let abstract_list_all env evd typ c l =
   let ctxt,_ = splay_prod_n env evd (List.length l) typ in
-  let l_with_all_occs = List.map (function a -> (AllOccurrences,a)) l in
+  let l_with_all_occs = List.map (function a -> (LikeFirst,a)) l in
   let p,evd = abstract_scheme env evd c l_with_all_occs ctxt in
   let evd,typp =
     try Typing.e_type_of env evd p
@@ -1362,9 +1362,10 @@ let indirectly_dependent c d decls =
 let indirect_dependency d decls =
   pi1 (List.hd (List.filter (fun (id,_,_) -> dependent_in_decl (mkVar id) d) decls))
 
-let finish_evar_resolution ?(flags=Pretyping.all_and_fail_flags) env initial_sigma (sigma,c) =
-  let sigma = Pretyping.solve_remaining_evars flags env initial_sigma sigma
-  in Evd.evar_universe_context sigma, nf_evar sigma c
+let finish_evar_resolution ?(flags=Pretyping.all_and_fail_flags) env current_sigma (pending,c) =
+  let sigma = Pretyping.solve_remaining_evars flags env current_sigma pending in
+  let sigma, subst = nf_univ_variables sigma in
+  sigma, subst_univs_constr subst (nf_evar sigma c)
 
 let default_matching_core_flags sigma =
   let ts = Names.full_transparent_state in {
@@ -1394,7 +1395,7 @@ let default_matching_merge_flags sigma =
     use_pattern_unification = true;
 }
 
-let default_matching_flags sigma =
+let default_matching_flags (sigma,_) =
   let flags = default_matching_core_flags sigma in {
   core_unify_flags = flags;
   merge_unify_flags = default_matching_merge_flags sigma;
@@ -1404,12 +1405,35 @@ let default_matching_flags sigma =
 }
 
 (* This supports search of occurrences of term from a pattern *)
+(* from_prefix is useful e.g. for subterms in an inductive type: we can say *)
+(* "destruct t" and it finds "t u" *)
 
-let make_pattern_test inf_flags env sigma0 (sigma,c) =
-  let flags = default_matching_flags sigma0 in
+exception PatternNotFound
+
+let make_pattern_test from_prefix_of_ind env sigma (pending,c) =
+  let flags = default_matching_flags pending in
+  let n = List.length (snd (decompose_app c)) in
   let matching_fun _ t =
-    try let sigma = w_typed_unify env sigma Reduction.CONV flags c t in
-	  Some(sigma, t)
+    try
+      let t' =
+        if from_prefix_of_ind then
+          (* We check for fully applied subterms of the form "u u1 .. un" *)
+          (* of inductive type knowning only a prefix "u u1 .. ui" *)
+          let t,l = decompose_app t in
+          let l1,l2 =
+            try List.chop n l with Failure _ -> raise (NotUnifiable None) in
+          if not (List.for_all closed0 l2) then raise (NotUnifiable None)
+          else
+            applist (t,l1)
+        else t in
+      let sigma = w_typed_unify env sigma Reduction.CONV flags c t' in
+      let _ =
+        if from_prefix_of_ind then
+          (* We check that the subterm we found from prefix is applied *)
+          (* enough to be of inductive type *)
+          try ignore (Inductiveops.find_mrectype env sigma (Retyping.get_type_of env sigma t))
+          with UserError _ -> raise (NotUnifiable None) in
+      Some(sigma, t)
     with
     | PretypeError (_,_,CannotUnify (c1,c2,Some e)) ->
         raise (NotUnifiable (Some (c1,c2,e)))
@@ -1425,22 +1449,25 @@ let make_pattern_test inf_flags env sigma0 (sigma,c) =
   { match_fun = matching_fun; merge_fun = merge_fun;
     testing_state = None; last_found = None },
   (fun test -> match test.testing_state with
-  | None ->
-      finish_evar_resolution ~flags:inf_flags env sigma0 (sigma,c)
+  | None -> None
+(*
+     let sigma, c = finish_evar_resolution ~flags:inf_flags env sigma (pending,c) in
+     sigma,c
+*)
   | Some (sigma,_) ->
+     let c = nf_evar sigma (local_strong whd_meta sigma c) in
      let univs, subst = nf_univ_variables sigma in
-     Evd.evar_universe_context univs,
-     subst_univs_constr subst (nf_evar sigma c))
+     Some (sigma,subst_univs_constr subst c))
 
 let make_eq_test env evd c =
   let out cstr =
-    Evd.evar_universe_context cstr.testing_state, c
+    match cstr.last_found with None -> None | _ -> Some (cstr.testing_state, c)
   in
-    (make_eq_univs_test env evd c, out)
+  (make_eq_univs_test env evd c, out)
 
-let make_abstraction_core name (test,out) (sigmac,c) ty occs check_occs env concl =
+let make_abstraction_core name (test,out) env sigma c ty occs check_occs concl =
   let id =
-    let t = match ty with Some t -> t | None -> get_type_of env sigmac c in
+    let t = match ty with Some t -> t | None -> get_type_of env sigma c in
     let x = id_of_name_using_hdchar (Global.env()) t name in
     let ids = ids_of_named_context (named_context env) in
     if name == Anonymous then next_ident_away_in_goal x ids else
@@ -1450,9 +1477,9 @@ let make_abstraction_core name (test,out) (sigmac,c) ty occs check_occs env conc
       x
   in
   let mkvarid () = mkVar id in
-  let compute_dependency _ (hyp,_,_ as d) depdecls =
+  let compute_dependency _ (hyp,_,_ as d) (sign,depdecls) =
     match occurrences_of_hyp hyp occs with
-    | NoOccurrences, InHyp ->
+    | AtOccs (NoOccurrences, InHyp) ->
         if indirectly_dependent c d depdecls then
           (* Told explicitly not to abstract over [d], but it is dependent *)
           let id' = indirect_dependency d depdecls in
@@ -1460,31 +1487,34 @@ let make_abstraction_core name (test,out) (sigmac,c) ty occs check_occs env conc
             ++ str " without also abstracting or erasing " ++ Nameops.pr_id hyp
             ++ str ".")
         else
-          depdecls
-    | (AllOccurrences, InHyp) as occ ->
+          (push_named_context_val d sign,depdecls)
+    | (AtOccs (AllOccurrences, InHyp) | LikeFirst) as occ ->
         let newdecl = replace_term_occ_decl_modulo occ test mkvarid d in
         if Context.eq_named_declaration d newdecl
            && not (indirectly_dependent c d depdecls)
         then
           if check_occs && not (in_every_hyp occs)
-          then raise (PretypeError (env,sigmac,NoOccurrenceFound (c,Some hyp)))
-          else depdecls
+          then raise (PretypeError (env,sigma,NoOccurrenceFound (c,Some hyp)))
+          else (push_named_context_val d sign, depdecls)
         else
-          newdecl :: depdecls
+          (push_named_context_val newdecl sign, newdecl :: depdecls)
     | occ ->
-        replace_term_occ_decl_modulo occ test mkvarid d :: depdecls in
+        let newdecl = replace_term_occ_decl_modulo occ test mkvarid d in
+        (push_named_context_val newdecl sign, newdecl :: depdecls) in
   try
-    let depdecls = fold_named_context compute_dependency env ~init:[] in
+    let sign,depdecls =
+      fold_named_context compute_dependency env
+        ~init:(empty_named_context_val,[]) in
     let ccl = match occurrences_of_goal occs with
-      | NoOccurrences -> concl
+      | AtOccs NoOccurrences -> concl
       | occ -> replace_term_occ_modulo occ test mkvarid concl
     in
     let lastlhyp =
       if List.is_empty depdecls then None else Some (pi1(List.last depdecls)) in
-    (id,depdecls,lastlhyp,ccl,out test)
+      (id,sign,depdecls,lastlhyp,ccl,out test)
   with
     SubtermUnificationError e ->
-      raise (PretypeError (env,sigmac,CannotUnifyOccurrences e))
+      raise (PretypeError (env,sigma,CannotUnifyOccurrences e))
 
 (** [make_abstraction] is the main entry point to abstract over a term
     or pattern at some occurrences; it returns:
@@ -1497,22 +1527,27 @@ let make_abstraction_core name (test,out) (sigmac,c) ty occs check_occs env conc
     - the term or pattern to abstract fully instantiated
 *)
 
+type prefix_of_inductive_support_flag = bool
+
 type abstraction_request =
-| AbstractPattern of Name.t * (evar_map * constr) * clause * bool * Pretyping.inference_flags
+| AbstractPattern of prefix_of_inductive_support_flag * Name.t * pending_constr * clause or_like_first * bool
 | AbstractExact of Name.t * constr * types option * clause * bool
 
 type abstraction_result =
-  Names.Id.t * Context.named_declaration list * Names.Id.t option *
-    constr * (Evd.evar_universe_context * constr)
+  Names.Id.t * named_context_val *
+    Context.named_declaration list * Names.Id.t option *
+    types * (Evd.evar_map * constr) option
 
 let make_abstraction env evd ccl abs =
   match abs with
-  | AbstractPattern (name,c,occs,check_occs,flags) ->
+  | AbstractPattern (from_prefix,name,c,occs,check_occs) ->
       make_abstraction_core name
-        (make_pattern_test flags env evd c) c None occs check_occs env ccl
+        (make_pattern_test from_prefix env evd c)
+        env evd (snd c) None occs check_occs ccl
   | AbstractExact (name,c,ty,occs,check_occs) ->
       make_abstraction_core name
-        (make_eq_test env evd c) (evd,c) ty occs check_occs env ccl
+        (make_eq_test env evd c)
+        env evd c ty (AtOccs occs) check_occs ccl
 
 let keyed_unify env evd kop = 
   if not !keyed_unification then fun cl -> true

pretyping/unification.mli
@@ -45,7 +45,7 @@ val elim_no_delta_flags : unit -> unify_flags
 val w_unify :
   env -> evar_map -> conv_pb -> ?flags:unify_flags -> constr -> constr -> evar_map
 
-(** [w_unify_to_subterm env (c,t) m] performs unification of [c] with a
+(** [w_unify_to_subterm env m (c,t)] performs unification of [c] with a
    subterm of [t]. Constraints are added to [m] and the matched
    subterm of [t] is also returned. *)
 val w_unify_to_subterm :
@@ -63,19 +63,24 @@ val w_coerce_to_type : env -> evar_map -> constr -> types -> types ->
 
 (* Looking for subterms in contexts at some occurrences, possibly with pattern*)
 
+exception PatternNotFound
+
 type abstraction_request =
-| AbstractPattern of Names.Name.t * (evar_map * constr) * Locus.clause * bool * Pretyping.inference_flags
+| AbstractPattern of bool * Names.Name.t * pending_constr * Locus.clause Locus.or_like_first * bool
 | AbstractExact of Names.Name.t * constr * types option * Locus.clause * bool
 
 val finish_evar_resolution : ?flags:Pretyping.inference_flags ->
-  env -> Evd.evar_map -> open_constr -> Evd.evar_universe_context * constr
+  env -> Evd.evar_map -> pending_constr -> Evd.evar_map * constr
 
 type abstraction_result =
-  Names.Id.t * Context.named_declaration list * Names.Id.t option *
-    constr * (Evd.evar_universe_context * constr)
+  Names.Id.t * named_context_val *
+    Context.named_declaration list * Names.Id.t option *
+    types * (Evd.evar_map * constr) option
 
 val make_abstraction : env -> Evd.evar_map -> constr ->
   abstraction_request -> abstraction_result
+
+val pose_all_metas_as_evars : env -> evar_map -> constr -> evar_map * constr
 
 (*i This should be in another module i*)
 

test-suite/success/destruct.v
@@ -98,25 +98,105 @@ Abort.
 
 Goal exists x, S x = S 0.
 eexists.
-Fail destruct (S _). (* Incompatible occurrences *)
+destruct (S _). (* Incompatible occurrences but takes the first one since Oct 2014 *)
+change (0 = S 0).
 Abort.
 
 Goal exists x, S 0 = S x.
 eexists.
-Fail destruct (S _). (* Incompatible occurrences *)
+destruct (S _). (* Incompatible occurrences but takes the first one since Oct 2014 *)
+change (0 = S ?x).
 Abort.
 
 Goal exists n p:nat, (S n,S n) = (S p,S p) /\ p = n.
 do 2 eexists.
-Fail destruct (_, S _). (* Was succeeding at some time in trunk *)
-Show Proof.
+destruct (_, S _). (* Was unifying at some time in trunk, now takes the first occurrence *)
+change ((n, n0) = (S ?p, S ?p) /\ ?p = ?n0).
+Abort.
 
 (* Avoid unnatural selection of a subterm larger than expected *)
 
 Goal let g := fun x:nat => x in g (S 0) = 0.
 intro.
 destruct S.
-(* Check that it is not the larger subterm "f (S 0)" which is
+(* Check that it is not the larger subterm "g (S 0)" which is
    selected, as it was the case in 8.4 *)
 unfold g at 1.
 Abort.
+
+(* Some tricky examples convenient to support *)
+
+Goal forall x, nat_rect (fun _ => nat) O (fun x y => S x) x = nat_rect (fun _ => nat) O (fun x y => S x) x.
+intros.
+destruct (nat_rect _ _ _ _).
+Abort.
+(* Check compatibility in selecting what is open or "shelved" *)
+
+Goal (forall x, x=0 -> nat) -> True.
+intros.
+Fail destruct H.
+edestruct H.
+- reflexivity.
+- exact Logic.I.
+- exact Logic.I.
+Qed.
+
+(* Check an example which was working with case/elim in 8.4 but not with
+   destruct/induction *)
+
+Goal forall x, (True -> x = 0) -> 0=0. 
+intros.
+destruct H.
+- trivial.
+- apply (eq_refl x).
+Qed.
+
+(* Check an example which was working with case/elim in 8.4 but not with
+   destruct/induction (not the different order between induction/destruct) *)
+
+Goal forall x, (True -> x = 0) -> 0=0. 
+intros.
+induction H.
+- apply (eq_refl x).
+- trivial.
+Qed.
+
+(* This test assumes that destruct/induction on non-dependent hypotheses behave the same
+   when using holes or not
+
+Goal forall x, (True -> x = 0) -> 0=0. 
+intros.
+destruct (H _).
+- apply I.
+- apply (eq_refl x).
+Qed.
+*)
+
+(* Check destruct vs edestruct *)
+
+Goal forall x, (forall y, y = 0 -> x = 0) -> 0=0.
+intros.
+Fail destruct H.
+edestruct H.
+- trivial.
+- apply (eq_refl x).
+Qed.
+
+Goal forall x, (forall y, y = 0 -> x = 0) -> 0=0.
+intros.
+Fail destruct (H _).
+edestruct (H _).
+- trivial.
+- apply (eq_refl x).
+Qed.
+
+Goal forall x, (forall y, y = 0 -> x = 0) -> 0=0.
+intros.
+Fail destruct (H _ _).
+(* Now a test which assumes that destruct/induction on non-dependent hypotheses behave the same
+   when using holes or not
+edestruct (H _ _).
+- trivial.
+- apply (eq_refl x).
+Qed.
+*)

grammar/q_coqast.ml4
@@ -367,17 +367,18 @@ let rec mlexpr_of_atomic_tactic = function
   (* Derived basic tactics *)
   | Tacexpr.TacInductionDestruct (isrec,ev,l) ->
       <:expr< Tacexpr.TacInductionDestruct $mlexpr_of_bool isrec$ $mlexpr_of_bool ev$
-	$mlexpr_of_triple
+	$mlexpr_of_pair
 	(mlexpr_of_list
-	   (mlexpr_of_pair
-              (mlexpr_of_pair
+	   (mlexpr_of_triple
+	      (mlexpr_of_pair
                  (mlexpr_of_option mlexpr_of_bool)
-	         mlexpr_of_induction_arg)
+                 mlexpr_of_induction_arg)
 	      (mlexpr_of_pair
 		 (mlexpr_of_option (mlexpr_of_located mlexpr_of_intro_pattern_naming))
-		 (mlexpr_of_option (mlexpr_of_intro_pattern_disjunctive)))))
+		 (mlexpr_of_option (mlexpr_of_intro_pattern_disjunctive)))
+	      (mlexpr_of_option mlexpr_of_clause)))
 	(mlexpr_of_option mlexpr_of_constr_with_binding)
-	(mlexpr_of_option mlexpr_of_clause) l$ >>
+ l$ >>
 
   (* Context management *)
   | Tacexpr.TacClear (b,l) ->

intf/locus.mli
@@ -86,3 +86,9 @@ type goal_location = hyp_location option
    the conclusion (conclusion is represented by None) *)
 
 type simple_clause = Id.t option list
+
+(** {6 A notion of occurrences allowing to express "all occurrences
+       convertible to the first which matches"} *)
+
+type 'a or_like_first = AtOccs of 'a | LikeFirst
+

intf/tacexpr.mli
@@ -56,15 +56,15 @@ type 'id message_token =
   | MsgInt of int
   | MsgIdent of 'id
 
-type 'constr induction_clause =
+type ('constr,'id) induction_clause =
     'constr with_bindings induction_arg *
     (intro_pattern_naming_expr located option (* eqn:... *)
     * 'constr or_and_intro_pattern_expr located or_var option) (* as ... *)
+    * 'id clause_expr option (* in ... *)
 
 type ('constr,'id) induction_clause_list =
-    'constr induction_clause list
+    ('constr,'id) induction_clause list
     * 'constr with_bindings option (* using ... *)
-    * 'id clause_expr option (* in ... *)
 
 type 'a with_bindings_arg = clear_flag * 'a with_bindings
 

commit 29a5127dac6d4fbc317e38452cf0fe05916adc56

Fixing destruct/induction with a using clause on a non-inductive type, that was broken by commit bf01856940 + use types from induction scheme to restrict selection of pattern + accept matching from partially applied term when using "using".

pretyping/unification.ml
@@ -1410,7 +1410,7 @@ let default_matching_flags (sigma,_) =
 
 exception PatternNotFound
 
-let make_pattern_test from_prefix_of_ind env sigma (pending,c) =
+let make_pattern_test from_prefix_of_ind is_correct_type env sigma (pending,c) =
   let flags = default_matching_flags pending in
   let n = List.length (snd (decompose_app c)) in
   let matching_fun _ t =
@@ -1427,12 +1427,8 @@ let make_pattern_test from_prefix_of_ind env sigma (pending,c) =
             applist (t,l1)
         else t in
       let sigma = w_typed_unify env sigma Reduction.CONV flags c t' in
-      let _ =
-        if from_prefix_of_ind then
-          (* We check that the subterm we found from prefix is applied *)
-          (* enough to be of inductive type *)
-          try ignore (Inductiveops.find_mrectype env sigma (Retyping.get_type_of env sigma t))
-          with UserError _ -> raise (NotUnifiable None) in
+      let ty = Retyping.get_type_of env sigma t in
+      if not (is_correct_type ty) then raise (NotUnifiable None);
       Some(sigma, t)
     with
     | PretypeError (_,_,CannotUnify (c1,c2,Some e)) ->
@@ -1526,7 +1522,7 @@ let make_abstraction_core name (test,out) env sigma c ty occs check_occs concl =
 type prefix_of_inductive_support_flag = bool
 
 type abstraction_request =
-| AbstractPattern of prefix_of_inductive_support_flag * Name.t * pending_constr * clause or_like_first * bool
+| AbstractPattern of prefix_of_inductive_support_flag * (types -> bool) * Name.t * pending_constr * clause or_like_first * bool
 | AbstractExact of Name.t * constr * types option * clause * bool
 
 type abstraction_result =
@@ -1536,9 +1532,9 @@ type abstraction_result =
 
 let make_abstraction env evd ccl abs =
   match abs with
-  | AbstractPattern (from_prefix,name,c,occs,check_occs) ->
+  | AbstractPattern (from_prefix,check,name,c,occs,check_occs) ->
       make_abstraction_core name
-        (make_pattern_test from_prefix env evd c)
+        (make_pattern_test from_prefix check env evd c)
         env evd (snd c) None occs check_occs ccl
   | AbstractExact (name,c,ty,occs,check_occs) ->
       make_abstraction_core name

pretyping/unification.mli
@@ -65,8 +65,10 @@ val w_coerce_to_type : env -> evar_map -> constr -> types -> types ->
 
 exception PatternNotFound
 
+type prefix_of_inductive_support_flag = bool
+
 type abstraction_request =
-| AbstractPattern of bool * Names.Name.t * pending_constr * Locus.clause Locus.or_like_first * bool
+| AbstractPattern of prefix_of_inductive_support_flag * (types -> bool) * Names.Name.t * pending_constr * Locus.clause Locus.or_like_first * bool
 | AbstractExact of Names.Name.t * constr * types option * Locus.clause * bool
 
 val finish_evar_resolution : ?flags:Pretyping.inference_flags ->

tactics/tactics.ml
@@ -2254,7 +2254,8 @@ let letin_pat_tac with_eq id c occs =
     let env = Proofview.Goal.env gl in
     let sigma = Proofview.Goal.sigma gl in
     let ccl = Proofview.Goal.concl gl in
-    let abs = AbstractPattern (false,id,c,AtOccs occs,false) in
+    let check t = true in
+    let abs = AbstractPattern (false,check,id,c,AtOccs occs,false) in
     let (id,_,depdecls,lastlhyp,ccl,res) = make_abstraction env sigma ccl abs in
     let sigma,c = match res with
     | None -> finish_evar_resolution ~flags:(tactic_infer_flags false) env sigma c
@@ -3789,8 +3790,9 @@ let clear_induction_arg_if_hyp is_arg_pure_hyp c env =
   else
     env
 
-let make_body env sigma is_arg_pure_hyp c lbind t =
-  let mind,typ = reduce_to_quantified_ind env sigma t in
+let make_body env sigma (is_arg_pure_hyp,_) c lbind t =
+  let decls,t = splay_prod env sigma t in
+  let typ = it_mkProd t decls in
   let indclause = make_clenv_binding env sigma (c,typ) lbind in
   let env = clear_induction_arg_if_hyp is_arg_pure_hyp c env in
   let evdref = ref indclause.evd in
@@ -3811,7 +3813,37 @@ let make_body env sigma is_arg_pure_hyp c lbind t =
   (* side-effect *)
   (!evdref, c)
 
-let pose_induction_arg clear_flag isrec with_evars is_arg_pure_hyp elim
+let check_expected_type env sigma (elimc,bl) t =
+  (* Compute the expected template type of the term in case a using
+     clause is given *)
+  let elimt = typ_of env sigma elimc in
+  let sign,_ = splay_prod env sigma elimt in
+  let n = List.length sign in
+  if n == 0 then error "Scheme cannot be applied.";
+  let sigma,cl = make_evar_clause env sigma (Some (n-1)) elimt in
+  let sigma = solve_evar_clause env sigma true cl bl in
+  let (_,u,_) = destProd cl.cl_concl in
+  Evarconv.e_cumul env (ref sigma) t u
+
+let check_enough_applied env sigma elim =
+  (* A heuristic to decide whether the induction arg is enough applied *)
+  match elim with
+  | None ->
+      (* No eliminator given *)
+      fun u ->
+      let t,_ = decompose_app (whd_betadeltaiota env sigma u) in isInd t
+  | Some elimc ->
+      let elimt = typ_of env sigma (fst elimc) in
+      let scheme = compute_elim_sig ~elimc elimt in
+      fun u -> match scheme.indref with
+      | None ->
+         (* in the absence of information, do not assume it may be
+            partially applied *)
+          true
+      | Some gr ->
+          check_expected_type env sigma elimc u
+
+let pose_induction_arg clear_flag isrec with_evars info_arg elim
      id ((pending,(c0,lbind)),(eqname,names)) t inhyps cls =
   Proofview.Goal.nf_enter begin fun gl ->
   let env = Proofview.Goal.env gl in
@@ -3819,16 +3851,17 @@ let pose_induction_arg clear_flag isrec with_evars is_arg_pure_hyp elim
   Proofview.Refine.refine ~unsafe:true begin fun sigma ->
   let (sigma',c') = use_bindings env sigma (c0,lbind) t in
   let occs = match cls with None -> LikeFirst | Some occs -> AtOccs occs in
-  let abs = AbstractPattern (true,Name id,(pending,c'),occs,false) in
+  let check = check_enough_applied env sigma elim in
+  let abs = AbstractPattern (snd info_arg,check,Name id,(pending,c'),occs,false) in
   let (id,sign,_,lastlhyp,ccl,res) = make_abstraction env sigma' ccl abs in
   match res with
   | None ->
       (* pattern not found *)
       let (sigma,c0) = finish_evar_resolution ~flags:(tactic_infer_flags with_evars) env sigma (pending,c0) in
-      let (sigma,c) = make_body env sigma is_arg_pure_hyp c0 lbind t in
+      let (sigma,c) = make_body env sigma info_arg c0 lbind t in
       let t = Retyping.get_type_of env sigma c in
       let with_eq = Option.map (fun eq -> (false,eq)) eqname in
-      let env = clear_induction_arg_if_hyp is_arg_pure_hyp c0 env in
+      let env = clear_induction_arg_if_hyp (fst info_arg) c0 env in
       mkletin_goal env sigma with_eq false (id,lastlhyp,ccl,c) (Some t)
 
   | Some (sigma',c) ->
@@ -3839,9 +3872,6 @@ let pose_induction_arg clear_flag isrec with_evars is_arg_pure_hyp elim
       (mkletin_goal env sigma' with_eq true (id,lastlhyp,ccl,c) None)
   end
 end
-
-let isIndType env sigma t =
-  try let _ = find_mrectype env sigma t in true with Not_found -> false
 
 let induction_gen clear_flag isrec with_evars elim
     ((_pending,(c,lbind)),(eqname,names) as arg) cls =
@@ -3856,7 +3886,8 @@ let induction_gen clear_flag isrec with_evars elim
     isVar c && not (mem_named_context (destVar c) (Global.named_context()))
     && lbind == NoBindings && not with_evars && Option.is_empty eqname
     && not (has_selected_occurrences cls) in
-  if is_arg_pure_hyp && isIndType env sigma t then
+  let enough_applied = check_enough_applied env sigma elim t in
+  if is_arg_pure_hyp && enough_applied then
     (* First case: induction on a variable already in an inductive type and
        with maximal abstraction over the variable.
        This is a situation where the induction argument is a
@@ -3874,11 +3905,12 @@ let induction_gen clear_flag isrec with_evars elim
     (* Type not the right one if partially applied but anyway for internal use*)
       let x = id_of_name_using_hdchar (Global.env()) t Anonymous in
       new_fresh_id [] x gl in
+    let info_arg = (is_arg_pure_hyp, not enough_applied) in
     if isrec then
       (* Historically, induction has side conditions last *)
       Tacticals.New.tclTHENFIRST
         (Tacticals.New.tclTHENLIST [
-          pose_induction_arg clear_flag isrec with_evars is_arg_pure_hyp elim id arg t inhyps cls;
+          pose_induction_arg clear_flag isrec with_evars info_arg elim id arg t inhyps cls;
           if with_evars
              (* do not give a success semantics to edestruct on an
                 open term yet *)
@@ -3893,7 +3925,7 @@ let induction_gen clear_flag isrec with_evars elim
       (* Historically, destruct has side conditions first *)
       Tacticals.New.tclTHENLAST
         (Tacticals.New.tclTHENLIST [
-          pose_induction_arg clear_flag isrec with_evars is_arg_pure_hyp elim id arg t inhyps cls;
+          pose_induction_arg clear_flag isrec with_evars info_arg elim id arg t inhyps cls;
           if with_evars
              (* do not give a success semantics to edestruct on an
                 open term yet *)

test-suite/success/destruct.v
@@ -200,3 +200,38 @@ edestruct (H _ _).
 - apply (eq_refl x).
 Qed.
 *)
+Abort.
+
+(* Test selection when not in an inductive type *)
+Parameter A:Type.
+Axiom elim: forall P, A -> P.
+Goal forall a:A, a = a.
+induction a using elim.
+Qed.
+
+Goal forall a:nat -> A, a 0 = a 1.
+intro a.
+induction (a 0) using elim.
+Qed.
+
+(* From Oct 2014, a subterm is found, as if without "using"; in 8.4,
+   it did not find a subterm *)
+
+Goal forall a:nat -> A, a 0 = a 1.
+intro a.
+induction a using elim.
+Qed.
+
+Goal forall a:nat -> A, forall b, a 0 = b.
+intros a b.
+induction a using elim.
+Qed.
+
+(* From Oct 2014, first subterm is found; in 8.4, it failed because it
+   found "a 0" and wanted to clear a *)
+
+Goal forall a:nat -> nat, a 0 = a 1.
+intro a.
+destruct a.
+change (0 = a 1).
+Abort.