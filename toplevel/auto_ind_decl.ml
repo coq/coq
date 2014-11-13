coq/coq

commit bf018569405c0ae1c5d08dfa27600102b9d77977

This commit introduces changes in induction and destruct.
The main change is that selection of subterm is made similar whether
the given term is fully applied or not.
- The selection of subterm now works as follows depending on whether
  the "at" is given, of whether the subterm is fully applied or not,
  and whether there are incompatible subterms matching the pattern. In
  particular, we have:
  "at" given
  | subterm fully applied
  | | incompatible subterms
  | | |
  Y Y - it works like in 8.4
  Y N - this was broken in 8.4 ("at" was ineffective and it was finding
        all subterms syntactically equal to the first one which matches)
  N Y Y it now finds all subterms like the first one which matches
        while in 8.4 it used to fail (I hope it is not a too risky in-draft
        for a semantics we would regret...) (e.g. "destruct (S _)" on
        goal "S x = S y + S x" now selects the two occurrences of "S x"
        while it was failing before)
  N Y N it works like in 8.4
  N N - it works like in 8.4, selecting all subterms like the
        first one which matches
- Note that the "historical" semantics, when looking for a subterm, to
  select all subterms that syntactically match the first subterm to
  match the pattern (looking from left to right) is now internally called
  "like first".
- Selection of subterms can now find the type by pattern-matching (useful e.g.
  for "induction (nat_rect _ _ _ _)")
- A version of Unification.w_unify w/o any conversion is used for
  finding the subterm: it could be easily replaced by an other
  matching algorithm.

In particular, "destruct H" now works on a goal such as "H:True -> x<=y |- P y".

Secondary change is in the interpretation of terms with existential
variables:
- When several arguments are given, interpretation is delayed at the
  time of execution
- Because we aim at eventually accepting "edestruct c" with unresolved
  holes in c, we need the sigma obtained from c to be an extension of

toplevel/auto_ind_decl.ml
@@ -77,23 +77,15 @@ let sumbool = Coqlib.build_coq_sumbool
 
 let andb = fun _ -> (Coqlib.build_bool_type()).Coqlib.andb
 
-let induct_on c =
-  induction false
-    [None,Tacexpr.ElimOnConstr (Evd.empty,(c,NoBindings))]
-    None (None,None) None
+let induct_on c = induction false None c None None
+
+let destruct_on c = destruct false None c None None
 
 let destruct_on_using c id =
-  destruct false
-    [None,Tacexpr.ElimOnConstr (Evd.empty,(c,NoBindings))]
-    None
-    (None,Some (dl,[[dl,IntroNaming IntroAnonymous];
-                    [dl,IntroNaming (IntroIdentifier id)]]))
-    None
+  destruct false None c (Some (dl,[[dl,IntroNaming IntroAnonymous]])) None
 
-let destruct_on c =
-  destruct false
-    [None,Tacexpr.ElimOnConstr (Evd.empty,(c,NoBindings))]
-    None (None,None) None
+let destruct_on_as c l =
+  destruct false None c (Some (dl,l)) None
 
 (* reconstruct the inductive with the correct deBruijn indexes *)
 let mkFullInd (ind,u) n =
@@ -591,12 +583,9 @@ repeat ( apply andb_prop in z;let z1:= fresh "Z" in destruct z as [z1 z]).
                          Simple.apply_in freshz (andb_prop());
                          Proofview.Goal.nf_enter begin fun gl ->
                            let fresht = fresh_id (Id.of_string "Z") gl in
-                            (destruct false [None,Tacexpr.ElimOnConstr
-                                      (Evd.empty,((mkVar freshz,NoBindings)))]
-                                  None
-                                  (None, Some (dl,[[
-                                    dl,IntroNaming (IntroIdentifier fresht);
-                                    dl,IntroNaming (IntroIdentifier freshz)]])) None)
+                            (destruct_on_as (mkVar freshz)
+                                  [[dl,IntroNaming (IntroIdentifier fresht);
+                                    dl,IntroNaming (IntroIdentifier freshz)]])
                          end
                         ]);
 (*

grammar/q_coqast.ml4
@@ -367,17 +367,18 @@ let rec mlexpr_of_atomic_tactic = function
   (* Derived basic tactics *)
   | Tacexpr.TacInductionDestruct (isrec,ev,l) ->
       <:expr< Tacexpr.TacInductionDestruct $mlexpr_of_bool isrec$ $mlexpr_of_bool ev$
-	$mlexpr_of_triple
+	$mlexpr_of_pair
 	(mlexpr_of_list
-	   (mlexpr_of_pair
-              (mlexpr_of_pair
+	   (mlexpr_of_triple
+	      (mlexpr_of_pair
                  (mlexpr_of_option mlexpr_of_bool)
-	         mlexpr_of_induction_arg)
+                 mlexpr_of_induction_arg)
 	      (mlexpr_of_pair
 		 (mlexpr_of_option (mlexpr_of_located mlexpr_of_intro_pattern_naming))
-		 (mlexpr_of_option (mlexpr_of_intro_pattern_disjunctive)))))
+		 (mlexpr_of_option (mlexpr_of_intro_pattern_disjunctive)))
+	      (mlexpr_of_option mlexpr_of_clause)))
 	(mlexpr_of_option mlexpr_of_constr_with_binding)
-	(mlexpr_of_option mlexpr_of_clause) l$ >>
+ l$ >>
 
   (* Context management *)
   | Tacexpr.TacClear (b,l) ->

intf/locus.mli
@@ -86,3 +86,9 @@ type goal_location = hyp_location option
    the conclusion (conclusion is represented by None) *)
 
 type simple_clause = Id.t option list
+
+(** {6 A notion of occurrences allowing to express "all occurrences
+       convertible to the first which matches"} *)
+
+type 'a or_like_first = AtOccs of 'a | LikeFirst
+

intf/tacexpr.mli
@@ -56,15 +56,15 @@ type 'id message_token =
   | MsgInt of int
   | MsgIdent of 'id
 
-type 'constr induction_clause =
+type ('constr,'id) induction_clause =
     'constr with_bindings induction_arg *
     (intro_pattern_naming_expr located option (* eqn:... *)
     * 'constr or_and_intro_pattern_expr located or_var option) (* as ... *)
+    * 'id clause_expr option (* in ... *)
 
 type ('constr,'id) induction_clause_list =
-    'constr induction_clause list
+    ('constr,'id) induction_clause list
     * 'constr with_bindings option (* using ... *)
-    * 'id clause_expr option (* in ... *)
 
 type 'a with_bindings_arg = clear_flag * 'a with_bindings
 

parsing/g_tactic.ml4
@@ -136,19 +136,19 @@ let induction_arg_of_constr (c,lbind as clbind) = match lbind with
   | _ -> ElimOnConstr clbind
 
 let mkTacCase with_evar = function
-  | [(clear,ElimOnConstr cl),(None,None)],None,None ->
+  | [(clear,ElimOnConstr cl),(None,None),None],None ->
       TacCase (with_evar,(clear,cl))
   (* Reinterpret numbers as a notation for terms *)
-  | [(clear,ElimOnAnonHyp n),(None,None)],None,None ->
+  | [(clear,ElimOnAnonHyp n),(None,None),None],None ->
       TacCase (with_evar,
         (clear,(CPrim (Loc.ghost, Numeral (Bigint.of_int n)),
 	 NoBindings)))
   (* Reinterpret ident as notations for variables in the context *)
   (* because we don't know if they are quantified or not *)
-  | [(clear,ElimOnIdent id),(None,None)],None,None ->
+  | [(clear,ElimOnIdent id),(None,None),None],None ->
       TacCase (with_evar,(clear,(CRef (Ident id,None),NoBindings)))
   | ic ->
-      if List.exists (function ((_, ElimOnAnonHyp _),_) -> true | _ -> false) (pi1 ic)
+      if List.exists (function ((_, ElimOnAnonHyp _),_,_) -> true | _ -> false) (fst ic)
       then
 	error "Use of numbers as direct arguments of 'case' is not supported.";
       TacInductionDestruct (false,with_evar,ic)
@@ -520,11 +520,17 @@ GEXTEND Gram
     [ [ b = orient; p = rewriter -> let (m,c) = p in (b,m,c) ] ]
   ;
   induction_clause:
-    [ [ c = induction_arg; pat = as_or_and_ipat; eq = eqn_ipat -> (c,(eq,pat)) ] ]
+    [ [ c = induction_arg; pat = as_or_and_ipat; eq = eqn_ipat; cl = opt_clause
+        -> (c,(eq,pat),cl) ] ]
   ;
   induction_clause_list:
-    [ [ ic = LIST1 induction_clause SEP ",";
-	el = OPT eliminator; cl = opt_clause -> (ic,el,cl) ] ]
+    [ [ ic = LIST1 induction_clause SEP ","; el = OPT eliminator;
+        cl_tolerance = opt_clause ->
+        (* Condition for accepting "in" at the end by compatibility *)
+        match ic,el,cl_tolerance with
+        | [c,pat,None],Some _,Some _ -> ([c,pat,cl_tolerance],el)
+        | _,_,Some _ -> err ()
+        | _,_,None -> (ic,el) ]]
   ;
   move_location:
     [ [ IDENT "after"; id = id_or_meta -> MoveAfter id
@@ -613,6 +619,13 @@ GEXTEND Gram
           l = LIST0 [","; c = pattern_occ; na = as_name -> (c,na)] ->
           TacAtom (!@loc, TacGeneralize (((nl,c),na)::l))
       | IDENT "generalize"; IDENT "dependent"; c = constr -> TacAtom (!@loc, TacGeneralizeDep c)
+(* Towards a "generalize in" which generalize in place: problem: this is somehow inconsistent with "generalize at" (from 8.2) which is not in place.
+      | IDENT "generalize"; c = constr; "in"; cl = in_clause;
+          na = as_name;
+          l = LIST0 [","; c = constr; "in"; cl = in_clause; na = as_name ->
+            ((cl,c),na)] ->
+          TacGeneralize (true,((cl,c),na)::l)
+*)
 
       (* Derived basic tactics *)
       | IDENT "induction"; ic = induction_clause_list ->

plugins/funind/indfun.ml
@@ -25,9 +25,7 @@ let is_rec_info scheme_info =
   List.fold_left_i test_branche 1 false (List.rev scheme_info.Tactics.branches)
 
 let choose_dest_or_ind scheme_info =
-    if is_rec_info scheme_info
-    then Tactics.induction false
-    else Tactics.destruct false
+  Tactics.induction_destruct (is_rec_info scheme_info) false
 
 let functional_induction with_clean c princl pat =
   Dumpglob.pause ();
@@ -80,7 +78,10 @@ let functional_induction with_clean c princl pat =
 	if princ_infos.Tactics.farg_in_concl
 	then [c] else []
       in
-      List.map (fun c -> None,Tacexpr.ElimOnConstr (Evd.empty,(c,NoBindings))) (args@c_list)
+      let encoded_pat_as_patlist =
+        List.make (List.length args + List.length c_list - 1) None @ [pat] in
+      List.map2 (fun c pat -> ((None,Tacexpr.ElimOnConstr (fun env sigma -> sigma,(c,NoBindings))),(None,pat),None))
+        (args@c_list) encoded_pat_as_patlist
     in
     let princ' = Some (princ,bindings) in
     let princ_vars =
@@ -113,10 +114,7 @@ let functional_induction with_clean c princl pat =
     Tacticals.tclTHEN
       (Proofview.V82.of_tactic (choose_dest_or_ind
 	 princ_infos
-	 args_as_induction_constr
-	 princ'
-	 (None,pat)
-         None))
+	 (args_as_induction_constr,princ')))
       subst_and_reduce
       g
   in

pretyping/evd.ml
@@ -1600,6 +1600,11 @@ let dependent_evar_ident ev evd =
   | _ -> anomaly (str "Not an evar resulting of a dependent binding")
 
 (*******************************************************************)
+
+type pending = (* before: *) evar_map * (* after: *) evar_map
+
+type pending_constr = pending * constr
+
 type open_constr = evar_map * constr
 
 (*******************************************************************)

pretyping/evd.mli
@@ -575,10 +575,16 @@ val eq_constr_univs_test : evar_map -> constr -> constr -> bool
 (** Syntactic equality up to universes, throwing away the (consistent) constraints
     in case of success. *)
 
-(********************************************************************
-   constr with holes *)
+(********************************************************************)
+(* constr with holes and pending resolution of classes, conversion  *)
+(* problems, candidates, etc.                                       *)
 
-type open_constr = evar_map * constr
+type pending = (* before: *) evar_map * (* after: *) evar_map
+
+type pending_constr = pending * constr
+
+type open_constr = evar_map * constr (* Special case when before is empty *)
+
 (** Partially constructed constrs. *)
 
 type unsolvability_explanation = SeveralInstancesFound of int

commit 1414ab30f9d030a4421da766121accb03276d827

Fixing Scheme Equality, after bug introduced in bf018569405c.

toplevel/auto_ind_decl.ml
@@ -82,7 +82,10 @@ let induct_on c = induction false None c None None
 let destruct_on c = destruct false None c None None
 
 let destruct_on_using c id =
-  destruct false None c (Some (dl,[[dl,IntroNaming IntroAnonymous]])) None
+  destruct false None c
+    (Some (dl,[[dl,IntroNaming IntroAnonymous];
+               [dl,IntroNaming (IntroIdentifier id)]]))
+    None
 
 let destruct_on_as c l =
   destruct false None c (Some (dl,l)) None