337800bee39698c888a5d32e6724efc1c12b3f3d
Matthieu Sozeau <matthieu.sozeau@inria.fr>
2014-11-21 00:46:45 +0100

    Add test-suite file for bug #3804.

---
+++ test-suite/bugs/closed/3804.v
@@ -0,0 +1,12 @@
+Set Universe Polymorphism.
+Module Foo.
+  Definition T : sigT (fun x => x).
+  Proof.
+    exists Set.
+    abstract exact nat.
+  Defined.
+End Foo.
+Module Bar.
+  Include Foo.
+End Bar.
+Definition foo := eq_refl : Foo.T = Bar.T. (* Error: Conversion test raised an anomaly *)
