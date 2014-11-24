c96bde02138e00ba8800733e4ca2f1915b4c0c6f
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2014-11-24 11:51:52 +0100

    Adding test for bug #3248.

---
+++ test-suite/bugs/opened/3248.v
@@ -0,0 +1,17 @@
+Ltac ret_and_left f :=
+  let tac := ret_and_left in
+  let T := type of f in
+  lazymatch eval hnf in T with
+    | ?T' -> _ =>
+      let ret := constr:(fun x' : T' => $(tac (f x'))$) in
+      exact ret
+    | ?T' => exact f
+  end.
+
+Goal forall A B : Prop, forall x y : A, True.
+Proof.
+  intros A B x y.
+  pose (f := fun (x y : A) => conj x y).
+  pose (a := $(ret_and_left f)$).
+  unify (a x y) (conj x y).
+Abort.
