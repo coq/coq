c854f859115add2ed95e2e4df36bce2eb2e6f22a
Pierre Boutillier <pierre.boutillier@pps.univ-paris-diderot.fr>
2014-12-17 11:11:43 +0100

    Revert and correctly fix "#4843 part 2 : The .cmxs files for plugins must have x permission"
    This reverts commit 607503b28fca50f4b76b2237d5ca13802b8252fa.

--- install.sh
+++ install.sh
@@ -7,5 +7,10 @@ for f; do
   bn=`basename $f`
   dn=`dirname $f`
   install -d "$dest/$dn"
-  install -m 644 $f "$dest/$dn/$bn"
+  case $bn in
+      *.cmxs) install -m 755 $f "$dest/$dn/$bn"
+	      ;;
+      *) install -m 644 $f "$dest/$dn/$bn"
+	 ;;
+  esac
 done
