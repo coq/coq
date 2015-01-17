e13f0bb3391177bb614d26d07383c94b96d0bf4c
Maxime Dénès <mail@maximedenes.fr>
2015-01-17 09:43:54 +0100

    Revert "Update test for #3363 now that Require is forbidden inside modules."
    This reverts commit 1c6e7d3744d101124ed0152c2aac1e71c9f9d40d.

--- test-suite/bugs/opened/3363.v
+++ test-suite/bugs/opened/3363.v
@@ -1,4 +1,19 @@
-(** In this file, either both [Check]s should fail, or both should succeed. *)
+(** In this file, either all four [Check]s should fail, or all four should succeed. *)
+Module A.
+  Section HexStrings.
+    Require Import String.
+  End HexStrings.
+  Fail Check string.
+End A.
+
+Module B.
+  Section HexStrings.
+    Require String.
+    Import String.
+  End HexStrings.
+  Fail Check string.
+End B.
+
 Section HexStrings.
   Require String.
   Import String.
