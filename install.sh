4daf9d821d000f9b78d4f8f187c6b95a3fd6b20a
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2015-01-29 09:44:05 +0100

    v8.2: backporting Pierre Boutillier's fix to #3843 part 2: "The .cmxs files for plugins must have x permission".

--- install.sh
+++ install.sh
@@ -7,7 +7,12 @@ for f; do
   bn=`basename $f`
   dn=`dirname $f`
   install -d "$dest/$dn"
-  install -m 644 $f "$dest/$dn/$bn"
+  case $bn in
+      *.cmxs) install -m 755 $f "$dest/$dn/$bn"
+	      ;;
+      *) install -m 644 $f "$dest/$dn/$bn"
+	 ;;
+  esac
 done
 
 
