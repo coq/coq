d7c8d18cca8a41db7ba12c8f6131e8a42491e962
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2015-02-27 20:36:19 +0100

    Test for bug #3249.

---
+++ test-suite/bugs/closed/3249.v
@@ -0,0 +1,11 @@
+Set Implicit Arguments.
+
+Ltac ret_and_left T :=
+  let t := type of T in
+  lazymatch eval hnf in t with
+    | ?a /\ ?b => constr:(proj1 T)
+    | forall x : ?T', @?f x =>
+      constr:(fun x : T' => $(let fx := constr:(T x) in
+                              let t := ret_and_left fx in
+                              exact t)$)
+  end.
