95f1e6467be41bfcdbac291a88f7c9a04474d585
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2015-03-07 13:22:11 +0100

    Continuing a8ad3abc15a2 and 60810aaec on freshness of name in tactic "evar". Not inventing a new fresh name if a non-fresh name is explicitly given, as in v8.4.

--- tactics/evar_tactics.ml
+++ tactics/evar_tactics.ml
@@ -71,8 +71,11 @@ let let_evar name typ =
   Proofview.Goal.enter begin fun gl ->
     let sigma = Proofview.Goal.sigma gl in
     let env = Proofview.Goal.env gl in
-    let id = Namegen.id_of_name_using_hdchar env typ name in
-    let id = Namegen.next_ident_away_in_goal id (Termops.ids_of_named_context (Environ.named_context env)) in
+    let id = match name with
+    | Names.Anonymous -> 
+      let id = Namegen.id_of_name_using_hdchar env typ name in
+      Namegen.next_ident_away_in_goal id (Termops.ids_of_named_context (Environ.named_context env))
+    | Names.Name id -> id in
     let sigma',evar = Evarutil.new_evar env sigma ~src ~naming:(Misctypes.IntroFresh id) typ in
     Tacticals.New.tclTHEN (Proofview.V82.tactic (Refiner.tclEVARS sigma'))
       (Tactics.letin_tac None (Names.Name id) evar None Locusops.nowhere)
