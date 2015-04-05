8936cf26ad1e1606a0e67c9d5406135df13d5007
Nickolai Zeldovich <nickolai@csail.mit.edu>
2015-04-05 07:09:28 -0400

    Update JSON extraction to match recent changes in trunk

--- plugins/extraction/json.ml
+++ plugins/extraction/json.ml
@@ -59,7 +59,7 @@ let preamble mod_name comment used_modules usf =
     ("need_magic", json_bool (usf.magic));
     ("need_dummy", json_bool (usf.mldummy));
     ("used_modules", json_list
-      (List.map (fun mf -> json_str (string_of_modfile mf)) used_modules))
+      (List.map (fun mf -> json_str (file_of_modfile mf)) used_modules))
   ]
 
 
@@ -265,6 +265,7 @@ let pp_struct mls =
 let json_descr = {
   keywords = Id.Set.empty;
   file_suffix = ".json";
+  file_naming = file_of_modfile;
   preamble = preamble;
   pp_struct = pp_struct;
   sig_suffix = None;
