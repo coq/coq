968c8af13deaa3871aca7fc7086f1a5dc5769fde
Matthieu Sozeau <mattam@mattam.org>
2015-04-09 17:34:09 +0200

    Add test-suite file for bug #4120.

---
+++ test-suite/bugs/closed/4120.v
@@ -0,0 +1,6 @@
+Definition id {T} (x : T) := x.
+Goal sigT (fun x => id x)%type.
+  change (fun x => ?f x) with f.
+  Unshelve.
+  exists Type. exact Set.
+Defined. (* Error: Attempt to save a proof with shelved goals (in proof Unnamed_thm) *)
