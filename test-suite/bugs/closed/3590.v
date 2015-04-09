33650e275a4b3f00541ea87ee4b39892be5fdb2f
Matthieu Sozeau <mattam@mattam.org>
2015-04-09 18:33:07 +0200

    Better test-suite files, removing reliance on admit.

--- test-suite/bugs/closed/3590.v
+++ test-suite/bugs/closed/3590.v
@@ -1,13 +1,10 @@
-Require Import TestSuite.admit.
-(* Set Primitive Projections. *)
 Set Implicit Arguments.
 Record prod A B := pair { fst : A ; snd : B }.
 Definition idS := Set.
-Goal forall x y : prod Set Set, fst x = fst y.
+Goal forall x y : prod Set Set, forall H : fst x = fst y, fst x = fst y.
   intros.
   change (@fst _ _ ?z) with (@fst Set idS z) at 2.
-  Unshelve.
-  admit.
+  apply H.
 Qed.
   
 (* Toplevel input, characters 20-58:
