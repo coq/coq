94afd8996251c30d2188a75934487009538e1303
Matthieu Sozeau <mattam@mattam.org>
2015-04-20 12:44:25 +0200

    Change magic numbers.

--- configure.ml
+++ configure.ml
@@ -14,8 +14,8 @@ open Printf
 let coq_version = "8.5beta2"
 let coq_macos_version = "8.4.92" (** "[...] should be a string comprised of
 three non-negative, period-separed integers [...]" *)
-let vo_magic = 8591
-let state_magic = 58501
+let vo_magic = 8492
+let state_magic = 58502
 let distributed_exec = ["coqtop";"coqc";"coqchk";"coqdoc";"coqmktop";"coqworkmgr";
 "coqdoc";"coq_makefile";"coq-tex";"gallina";"coqwc";"csdpcert";"coqdep"]
 
