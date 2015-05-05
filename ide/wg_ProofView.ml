a72b7db875780ccc84b9359ffa66c170cfe6ebfd
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2015-05-05 19:19:08 +0200

    Compatibility ocaml 3.12.

--- ide/wg_ProofView.ml
+++ ide/wg_ProofView.ml
@@ -6,6 +6,8 @@
 (*         *       GNU Lesser General Public License Version 2.1        *)
 (************************************************************************)
 
+open Util
+
 class type proof_view =
   object
     inherit GObj.widget
