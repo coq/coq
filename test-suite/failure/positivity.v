9420069e4d0bf3bf377c950bbc41ea762fbcfce8
Arnaud Spiwack <arnaud@spiwack.net>
2015-06-24 17:38:09 +0200

    Extend test-suite for the positivity checker.
    I wasn't very creative: I just added a single test by failure case in the positivity checker (plus one success).
    
    There should probably be tests with mutually inductives and co-inductives as well.

--- test-suite/failure/positivity.v
+++ test-suite/failure/positivity.v
@@ -5,5 +5,47 @@
 (*    //   *      This file is distributed under the terms of the       *)
 (*         *       GNU Lesser General Public License Version 2.1        *)
 (************************************************************************)
-Fail Inductive t : Set :=
-    c : (t -> nat) -> t.
+
+(* Negative occurrence *)
+Fail Inductive t : Type :=
+  c : (t -> nat) -> t.
+
+(* Non-strictely positive occurrence *)
+Fail Inductive t : Type :=
+  c : ((t -> nat) -> nat) -> t.
+
+(* Self-nested type (no proof of
+   soundness yet *)
+Fail Inductive t (A:Type) : Type :=
+  c : t (t A) -> t A.
+
+(* Nested inductive types *)
+
+Inductive pos (A:Type) :=
+  p : pos A -> pos A.
+
+Inductive nnpos (A:Type) :=
+  nnp : ((A -> nat) -> nat) -> nnpos A.
+
+Inductive neg (A:Type) :=
+  n : (A->neg A) -> neg A.
+
+Inductive arg : Type -> Prop :=
+  a : forall A, arg A -> arg A.
+
+(* Strictly covariant parameter: accepted. *)
+Fail Fail Inductive t :=
+  c : pos t -> t.
+
+(* Non-strictly covariant parameter: not
+   strictly positive. *)
+Fail Inductive t :=
+  c : nnpos t -> t.
+
+(* Contravariant parameter: not positive. *)
+Fail Inductive t :=
+  c : neg t -> t.
+
+(* Strict index: not positive. *)
+Fail Inductive t :=
+  c : arg t -> t.
