e53bb542a5215c2cda88b3249a64c9d7455234b8
Frédéric Chyzak <frederic.chyzak@inria.fr>
2015-06-24 15:52:25 +0200

    Complete round-trip (using only-parsing notation) + More tests
    The last test actually does not behave as I would like it too.

--- test-suite/output/Coercions.v
+++ test-suite/output/Coercions.v
@@ -27,49 +27,65 @@ Check ("1" 0).
 
 Section testQuotedCoercions.
 
+Require Import List.
+
 Axiom A : Type.
 Axiom B : Type.
 Axiom C : Type.
 Axiom a : A.
 Axiom f : A -> B.
 Axiom g : B -> C.
+Axiom h : forall A, option A -> list A.
 Axiom id : C -> C.
 Coercion f : A >-> B.
 Coercion g : B >-> C.
+Coercion h : option >-> list.
 
 Definition x := a : C.
 Definition x' := id a.
 
+Check "No Printing option".
 Check a : C.
 Print x.
 Print x'.
 
 Set Printing Coercions Quoted.
 
+Check "Printing Coercions Quoted".
 Check a : C.
 Print x.  (* This seems to be a bug, but this more of a misfeature: the would-be
   rationale is that the external coercion cannot be inferred with no context. *)
 Print x'.  (* This shows that within the context of id, both levels of coercion
   are printing as quotes. *)
 
+Check a.
+Check ““a”” : C.
+Check ““a” : B” : C.
+
+Check List.app (Some a) nil.
+
 Set Printing Coercions.
 
+Check "Printing Coercions Quoted + Printing Coercions".
 Check a : C.
 Print x.
 Print x'.
 
 Unset Printing Coercions Quoted.
 
+Check "Printing Coercions".
 Check a : C.
 Print x.
 Print x'.
 
 Unset Printing Coercions.
 
+Check "No Printing option again".
 Check a : C.
 Print x.
 Print x'.
 
+Check "Printing All".
 Set Printing All.
 
 Check QuotedCoercion g (QuotedCoercion f a).
@@ -81,10 +97,25 @@ Notation "“ x ↑ T ”" := (@QuotedCoercion _ T _ x) (at level 0,
 
 Set Printing Coercions Quoted.
 
+Check "Printing Coercions Quoted + uparrow".
 Check a : C.
 Print x.
 Print x'.
 
+Check QuotedCoercion g (QuotedCoercion f a).
+
+Notation "“f x ”" := (@QuotedCoercion _ _ (@f) x) (at level 0,
+  format "“f  x ”").
+
+Notation "“g x ”" := (@QuotedCoercion _ _ (@g) x) (at level 0,
+  format "“g  x ”").
+
+Check "Printing Coercions Quoted + notations for specific coercions".
+Check a : C.
+Print x.
+Print x'.
+
+(* I would like this to display with “f _” as above, not with “_ ↑ _”. *)
 Check QuotedCoercion g (QuotedCoercion f a).
 
 End testQuotedCoercions.
--- theories/Init/Notations.v
+++ theories/Init/Notations.v
@@ -96,3 +96,4 @@ Definition QuotedCoercion (T T' : Type) (f : forall (_ : T), T') (x : T) := f x.
 Arguments QuotedCoercion {T T'} f x.
 Notation "“ x ”" := (@QuotedCoercion _ _ _ x) (at level 0,
   format "“ x ”").
+Notation "“ x ”" := (x) (at level 0, only parsing).
