3bdadb4020c1d3e51957a06c1e3a52744f09148d
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2015-07-28 13:47:39 +0200

    Tests for bugs #3509 and #3510.

---
+++ test-suite/bugs/closed/3509.v
@@ -0,0 +1,6 @@
+Inductive T := Foo : T.
+Axiom (b : T) (R : forall x : T, Prop) (f : forall x : T, R x).
+Axiom a1 : match b with Foo => f end = f.
+Axiom a2 : match b with Foo => f b end = f b.
+Hint Rewrite a1 : bar.
+Hint Rewrite a2 : bar.
