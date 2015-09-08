coq/coq

commit 76f27140e6e3465b2d4086653bccae5206b3cfb6

Fixing clearing of temporary hypotheses with intro pattern pat/constr.

tactics/tactics.ml
@@ -2172,7 +2172,7 @@ and intro_pattern_action loc b style pat thin tac id = match pat with
           (Tacticals.New.tclTHENFIRST
              (* Skip the side conditions of the apply *)
              (apply_in_once false true true true naming id
-                (None,(sigma,(c,NoBindings))) tac_ipat) (tac thin None []))
+                (None,(sigma,(c,NoBindings))) tac_ipat) (tac ((dloc,id)::thin) None []))
           sigma
       end
 

commit 7e00e8d602e67810700a7071c419ffd7ef8806c5

Fixing incomplete bugfix in 76f27140e6e34 (unfortunately 5 commits ago) which broke compilation of theories/Logic/WKL.v (collision between a temporary name and a user name).

tactics/tactics.ml
@@ -2122,7 +2122,7 @@ let make_tmp_naming avoid l = function
      case of IntroFresh, we should use check_thin_clash_then anyway to
      prevent the case of an IntroFresh precisely using the wild_id *)
   | IntroWildcard -> NamingBasedOn (wild_id,avoid@explicit_intro_names l)
-  | _ -> NamingAvoid(avoid@explicit_intro_names l)
+  | pat -> NamingAvoid(avoid@explicit_intro_names ((dloc,IntroAction pat)::l))
 
 let fit_bound n = function
   | None -> true

test-suite/success/intros.v
@@ -51,3 +51,13 @@ Fail clear H0.
 match goal with H:_ |- _ => clear H end. (* clear H1:False *)
 match goal with H:_ |- _ => exact H end. (* check that next hyp shows 0=0 *)
 Qed.
+
+Goal (True -> 0=0) -> True -> 0=0.
+intros H H1/H.
+exact H1.
+Qed.
+
+Goal forall n, n = S n -> 0=0.
+intros n H/n_Sn.
+destruct H.
+Qed.