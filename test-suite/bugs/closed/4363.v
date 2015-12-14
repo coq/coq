9329cb508f336b48a2bf2e699886546158b6b4d8
Maxime Dénès <mail@maximedenes.fr>
2015-12-14 11:14:45 +0100

    Test file for #4363 was not complete.

--- test-suite/bugs/closed/4363.v
+++ test-suite/bugs/closed/4363.v
@@ -5,3 +5,5 @@ Proof.
   exact bar.
 Defined. (* Toplevel input, characters 0-8:
 Error:
+The term "(fun _ : Set => bar) foo_subproof" has type
+"Type@{Top.2}" while it is expected to have type "Type@{Top.1}". *)
