709621bb3f72711dda945fdc22f86181361eabe5
Jason Gross <jasongross9@gmail.com>
2016-01-15 21:21:20 -0500

    Update Tactics.v

--- theories/Init/Tactics.v
+++ theories/Init/Tactics.v
@@ -247,14 +247,14 @@ Ltac replace_with_at_by x y set_tac tac :=
   let H := fresh in
   let x' := fresh in
   set_tac x' x;
-  assert (H : x' = y) by (subst x'; tac);
-  clearbody x'; subst x'.
+  assert (H : y = x') by (subst x'; tac);
+  clearbody x'; induction H.
 Ltac replace_with_at x y set_tac :=
   let H := fresh in
   let x' := fresh in
   set_tac x' x;
-  cut (x' = y);
-  [ intro H; clearbody x'; subst x'
+  cut (y = x');
+  [ intro H; induction H
   | subst x' ].
 Tactic Notation "replace" constr(x) "with" constr(y) := 
   replace_with_at x y ltac:(fun x' x => set (x' := x) ).
