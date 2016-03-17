coq/coq

commit efa1c32a4d17870794dbc6f0301c3c0d46637a55

Fixing unification of subterms identified by patterns.

pretyping/unification.ml
@@ -1228,12 +1228,8 @@ let make_pattern_test inf_flags env sigma0 (sigma,c) =
     | e when Errors.noncritical e -> raise (NotUnifiable None) in
   let merge_fun c1 c2 =
     match c1, c2 with
-    | Some (evd,c1), Some (_,c2) ->
-      (try let evd = w_typed_unify env evd Reduction.CONV flags c1 c2 in
-	     Some (evd, c1)
-       with
-       | PretypeError (_,_,CannotUnify (c1,c2,Some e)) -> raise (NotUnifiable (Some (c1,c2,e)))
-       | e when Errors.noncritical e -> raise (NotUnifiable None))
+    | Some (evd,c1) as x, Some (_,c2) ->
+      if is_conv env sigma c1 c2 then x else raise (NotUnifiable None)
     | Some _, None -> c1
     | None, Some _ -> c2
     | None, None -> None in

test-suite/success/destruct.v
@@ -93,3 +93,20 @@ Goal let T:=nat in forall (x:nat) (g:T -> nat), g x = 0.
 intros.
 destruct (g _). (* This was failing in at least r14571 *)
 Abort.
+
+(* Check that subterm selection does not solve existing evars *)
+
+Goal exists x, S x = S 0.
+eexists.
+Fail destruct (S _). (* Incompatible occurrences *)
+Abort.
+
+Goal exists x, S 0 = S x.
+eexists.
+Fail destruct (S _). (* Incompatible occurrences *)
+Abort.
+
+Goal exists n p:nat, (S n,S n) = (S p,S p) /\ p = n.
+do 2 eexists.
+Fail destruct (_, S _). (* Was succeeding at some time in trunk *)
+Show Proof.

commit 82b371aceb1ef6b1e15bdace2cf142e65724a3c6

Fix #4623: set tactic too weak with universes (regression)
The regression was introduced by efa1c32a4d178, which replaced
unification by conversion when looking for more occurrences of a
subterm. The conversion function called was not the right one, as it
was not inferring constraints.

pretyping/unification.ml
@@ -1532,8 +1532,9 @@ let make_pattern_test from_prefix_of_ind is_correct_type env sigma (pending,c) =
     | e when Errors.noncritical e -> raise (NotUnifiable None) in
   let merge_fun c1 c2 =
     match c1, c2 with
-    | Some (evd,c1,_) as x, Some (_,c2,_) ->
-      if is_conv env sigma c1 c2 then x else raise (NotUnifiable None)
+    | Some (evd,c1,x), Some (_,c2,_) ->
+      let (evd,b) = infer_conv ~pb:CONV env evd c1 c2 in
+      if b then Some (evd, c1, x) else raise (NotUnifiable None)
     | Some _, None -> c1
     | None, Some _ -> c2
     | None, None -> None in