ab1f43defa72e93617c3e3b09abb1876ff5a1b46
Maxime Dénès <mail@maximedenes.fr>
2016-06-13 17:09:47 +0200

    STM classification: VernacTimeout may contain an "internal command".

--- stm/stm.ml
+++ stm/stm.ml
@@ -99,7 +99,7 @@ let vernac_interp ?proof id ?route { verbose; loc; expr } =
     | VernacResetName _ | VernacResetInitial | VernacBack _
     | VernacBackTo _ | VernacRestart | VernacUndo _ | VernacUndoTo _
     | VernacBacktrack _ | VernacAbortAll | VernacAbort _ -> true
-    | VernacTime (_,e) | VernacRedirect (_,(_,e)) -> internal_command e
+    | VernacTime (_,e) | VernacTimeout (_,e) | VernacRedirect (_,(_,e)) -> internal_command e
     | _ -> false in
   if internal_command expr then begin
     prerr_endline (fun () -> "ignoring " ^ Pp.string_of_ppcmds(pr_vernac expr))
