e1661dc9a43b34526437e9bc3029e6320e09f899
Maxime Dénès <mail@maximedenes.fr>
2016-07-08 13:03:16 +0200

    Fix test file for #4858.

--- test-suite/bugs/closed/4858.v
+++ test-suite/bugs/closed/4858.v
@@ -1,6 +1,6 @@
 Require Import Nsatz.
 Goal True.
-nsatz_compute
+try nsatz_compute
  (PEc 0%Z :: PEc (-1)%Z
     :: PEpow (PEsub (PEX Z 2) (PEX Z 3)) 1
        :: PEsub (PEX Z 1) (PEX Z 1) :: nil).
