coq/coq

commit 8e07227c5853de78eaed4577eefe908fb84507c0

A new infrastructure for warnings.
On the user side, coqtop and coqc take a list of warning names or categories
after -w. No prefix means activate the warning, a "-" prefix means deactivate
it, and "+" means turn the warning into an error. Special categories include
"all", and "default" which contains the warnings enabled by default.

We also provide a vernacular Set Warnings which takes the same flags as argument.

Note that coqc now prints warnings.

The name and category of a warning are printed with the warning itself.

On the developer side, Feedback.msg_warning is still accessible, but the
recommended way to print a warning is in two steps:

1) create it by:

let warn_my_warning =
  CWarnings.create ~name:"my-warning" ~category:"my-category"
    (fun args -> Pp.strbrk ...)

2) print it by:

warn_my_warning args

grammar/tacextend.mlp
@@ -111,14 +111,8 @@ let declare_tactic loc s c cl = match cl with
   declare_str_items loc
     [ <:str_item< do {
       let obj () = Tacenv.register_ltac True False $name$ $body$ in
-      try do {
-        Tacenv.register_ml_tactic $se$ [|$tac$|];
-        Mltop.declare_cache_obj obj $plugin_name$; }
-      with [ e when Errors.noncritical e ->
-        Feedback.msg_warning
-          (Pp.app
-            (Pp.str ("Exception in tactic extend " ^ $entry$ ^": "))
-            (Errors.print e)) ]; } >>
+      Tacenv.register_ml_tactic $se$ [|$tac$|];
+      Mltop.declare_cache_obj obj $plugin_name$; } >>
     ]
 | _ ->
   (** Otherwise we add parsing and printing rules to generate a call to a
@@ -129,14 +123,8 @@ let declare_tactic loc s c cl = match cl with
   let obj = <:expr< fun () -> Tacentries.add_ml_tactic_notation $se$ $gl$ >> in
   declare_str_items loc
     [ <:str_item< do {
-      try do {
         Tacenv.register_ml_tactic $se$ (Array.of_list $make_fun_clauses loc s cl$);
-        Mltop.declare_cache_obj $obj$ $plugin_name$; }
-      with [ e when Errors.noncritical e ->
-        Feedback.msg_warning
-          (Pp.app
-            (Pp.str ("Exception in tactic extend " ^ $entry$ ^": "))
-            (Errors.print e)) ]; } >>
+        Mltop.declare_cache_obj $obj$ $plugin_name$; } >>
     ]
 
 open Pcaml

parsing/g_vernac.ml4
@@ -123,16 +123,18 @@ GEXTEND Gram
   ;
 END
 
-let test_plural_form = function
+let warn_plural_command =
+  CWarnings.create ~name:"plural-command" ~category:"pedantic" ~default:CWarnings.Disabled
+         (fun kwd -> strbrk (Printf.sprintf "Command \"%s\" expects more than one assumption." kwd))
+
+let test_plural_form loc kwd = function
   | [(_,([_],_))] ->
-      Flags.if_verbose Feedback.msg_warning
-   (strbrk "Keywords Variables/Hypotheses/Parameters expect more than one assumption")
+     warn_plural_command ~loc:!@loc kwd
   | _ -> ()
 
-let test_plural_form_types = function
+let test_plural_form_types loc kwd = function
   | [([_],_)] ->
-      Flags.if_verbose Feedback.msg_warning
-   (strbrk "Keywords Implicit Types expect more than one type")
+     warn_plural_command ~loc:!@loc kwd
   | _ -> ()
 
 let fresh_var env c =
@@ -155,8 +157,8 @@ GEXTEND Gram
           VernacStartTheoremProof (thm, (Some id,(bl,c,None))::l, false)
       | stre = assumption_token; nl = inline; bl = assum_list ->
 	  VernacAssumption (stre, nl, bl)
-      | stre = assumptions_token; nl = inline; bl = assum_list ->
-	  test_plural_form bl;
+      | (kwd,stre) = assumptions_token; nl = inline; bl = assum_list ->
+	  test_plural_form loc kwd bl;
 	  VernacAssumption (stre, nl, bl)
       | d = def_token; id = pidentref; b = def_body ->
           VernacDefinition (d, id, b)
@@ -209,11 +211,11 @@ GEXTEND Gram
       | IDENT "Conjecture" -> (None, Conjectural) ] ]
   ;
   assumptions_token:
-    [ [ IDENT "Hypotheses" -> (Some Discharge, Logical)
-      | IDENT "Variables" -> (Some Discharge, Definitional)
-      | IDENT "Axioms" -> (None, Logical)
-      | IDENT "Parameters" -> (None, Definitional)
-      | IDENT "Conjectures" -> (None, Conjectural) ] ]
+    [ [ kwd = IDENT "Hypotheses" -> (kwd, (Some Discharge, Logical))
+      | kwd = IDENT "Variables" -> (kwd, (Some Discharge, Definitional))
+      | kwd = IDENT "Axioms" -> (kwd, (None, Logical))
+      | kwd = IDENT "Parameters" -> (kwd, (None, Definitional))
+      | kwd = IDENT "Conjectures" -> (kwd, (None, Conjectural)) ] ]
   ;
   inline:
     [ [ IDENT "Inline"; "("; i = INT; ")" -> InlineAt (int_of_string i)
@@ -422,6 +424,10 @@ let starredidentreflist_to_expr l =
   | [] -> SsEmpty
   | x :: xs -> List.fold_right (fun i acc -> SsUnion(i,acc)) xs x
 
+let warn_deprecated_include_type =
+  CWarnings.create ~name:"deprecated-include-type" ~category:"deprecated"
+         (fun () -> strbrk "Include Type is deprecated; use Include instead")
+
 (* Modules and Sections *)
 GEXTEND Gram
   GLOBAL: gallina_ext module_expr module_type section_subset_expr;
@@ -461,9 +467,8 @@ GEXTEND Gram
       | IDENT "Include"; e = module_type_inl; l = LIST0 ext_module_expr ->
 	  VernacInclude(e::l)
       | IDENT "Include"; "Type"; e = module_type_inl; l = LIST0 ext_module_type ->
-	  Flags.if_verbose
-            Feedback.msg_warning (strbrk "Include Type is deprecated; use Include instead");
-          VernacInclude(e::l) ] ]
+	 warn_deprecated_include_type ~loc:!@loc ();
+	 VernacInclude(e::l) ] ]
   ;
   export_token:
     [ [ IDENT "Import" -> Some false
@@ -566,6 +571,14 @@ GEXTEND Gram
       | "("; e = ssexpr; ")"; "*" -> SsFwdClose e ] ]
   ;
 END
+
+let warn_deprecated_arguments_scope =
+  CWarnings.create ~name:"deprecated-arguments-scope" ~category:"deprecated"
+         (fun () -> strbrk "Arguments Scope is deprecated; use Arguments instead")
+
+let warn_deprecated_implicit_arguments =
+  CWarnings.create ~name:"deprecated-implicit-arguments" ~category:"deprecated"
+         (fun () -> strbrk "Implicit Arguments is deprecated; use Arguments instead")
 
 (* Extensions: implicits, coercions, etc. *)
 GEXTEND Gram
@@ -681,22 +694,21 @@ GEXTEND Gram
      (* moved there so that camlp5 factors it with the previous rule *)
      | IDENT "Arguments"; IDENT "Scope"; qid = smart_global;
        "["; scl = LIST0 [ "_" -> None | sc = IDENT -> Some sc ]; "]" ->
-       Feedback. msg_warning (strbrk "Arguments Scope is deprecated; use Arguments instead");
+	warn_deprecated_arguments_scope ~loc:!@loc ();
         VernacArgumentsScope (qid,scl)
 
       (* Implicit *)
       | IDENT "Implicit"; IDENT "Arguments"; qid = smart_global;
 	   pos = LIST0 [ "["; l = LIST0 implicit_name; "]" ->
 	     List.map (fun (id,b,f) -> (ExplByName id,b,f)) l ] ->
-	   Flags.if_verbose
-            Feedback.msg_warning (strbrk "Implicit Arguments is deprecated; use Arguments instead");
-	   VernacDeclareImplicits (qid,pos)
+	 warn_deprecated_implicit_arguments ~loc:!@loc ();
+	 VernacDeclareImplicits (qid,pos)
 
       | IDENT "Implicit"; "Type"; bl = reserv_list ->
 	   VernacReserve bl
 
       | IDENT "Implicit"; IDENT "Types"; bl = reserv_list ->
-          test_plural_form_types bl;
+          test_plural_form_types loc "Implicit Types" bl;
            VernacReserve bl
 
       | IDENT "Generalizable"; 

commit 1ca19082cf506c304b3c7945e72c0238f2aa9d1a

restore compatibility with gallium's camlp4 (broken by commit 8e07227c)
 Apparently, in camlp4 (unlike camlp5) :

 - Something like "[ kwd = IDENT "foobar" -> .... kwd ... ]"
   produces a kwd of type token instead of string (which sounds reasonable ?).
   For now, I've replaced kwd by the explicit strings. Not so nice, but works
   with both camlp4 and camlp5

 - A quotation of the form "let obj = ... in bar; baz" is not
   interpreted in the usual OCaml way, but rather as
   "(let obj = ... in bar); baz".
   Let's use instead "let obj = ... in let () = bar in baz", which works fine.

grammar/tacextend.mlp
@@ -111,8 +111,8 @@ let declare_tactic loc s c cl = match cl with
   declare_str_items loc
     [ <:str_item< do {
       let obj () = Tacenv.register_ltac True False $name$ $body$ in
-      Tacenv.register_ml_tactic $se$ [|$tac$|];
-      Mltop.declare_cache_obj obj $plugin_name$; } >>
+      let () = Tacenv.register_ml_tactic $se$ [|$tac$|] in
+      Mltop.declare_cache_obj obj $plugin_name$ } >>
     ]
 | _ ->
   (** Otherwise we add parsing and printing rules to generate a call to a

parsing/g_vernac.ml4
@@ -211,11 +211,11 @@ GEXTEND Gram
       | IDENT "Conjecture" -> (None, Conjectural) ] ]
   ;
   assumptions_token:
-    [ [ kwd = IDENT "Hypotheses" -> (kwd, (Some Discharge, Logical))
-      | kwd = IDENT "Variables" -> (kwd, (Some Discharge, Definitional))
-      | kwd = IDENT "Axioms" -> (kwd, (None, Logical))
-      | kwd = IDENT "Parameters" -> (kwd, (None, Definitional))
-      | kwd = IDENT "Conjectures" -> (kwd, (None, Conjectural)) ] ]
+    [ [ IDENT "Hypotheses" -> ("Hypotheses", (Some Discharge, Logical))
+      | IDENT "Variables" -> ("Variables", (Some Discharge, Definitional))
+      | IDENT "Axioms" -> ("Axioms", (None, Logical))
+      | IDENT "Parameters" -> ("Parameters", (None, Definitional))
+      | IDENT "Conjectures" -> ("Conjectures", (None, Conjectural)) ] ]
   ;
   inline:
     [ [ IDENT "Inline"; "("; i = INT; ")" -> InlineAt (int_of_string i)