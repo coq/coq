4e85cc6e9792da116f1b20e484b2ce629c6f6865
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2016-10-02 20:27:59 +0200

    Adding explicitly a file to work in the context of propositional extensionality.

---
+++ theories/Logic/PropExtensionality.v
@@ -0,0 +1,22 @@
+(************************************************************************)
+(*  v      *   The Coq Proof Assistant  /  The Coq Development Team     *)
+(* <O___,, *   INRIA - CNRS - LIX - LRI - PPS - Copyright 1999-2016     *)
+(*   \VV/  **************************************************************)
+(*    //   *      This file is distributed under the terms of the       *)
+(*         *       GNU Lesser General Public License Version 2.1        *)
+(************************************************************************)
+
+(** This module states propositional extensionality and draws
+    consequences of it *)
+
+Axiom propositional_extensionality :
+  forall (P Q : Prop), (P <-> Q) -> P = Q.
+
+Require Import ClassicalFacts.
+
+Theorem proof_irrelevance : forall (P:Prop) (p1 p2:P), p1 = p2.
+Proof.
+  apply ext_prop_dep_proof_irrel_cic.
+  exact propositional_extensionality.
+Qed.
+
