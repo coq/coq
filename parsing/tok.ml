9f8714f4cd3fb59ce38afee48caf081db1919c0c
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2016-10-05 08:22:57 +0200

    Fix incorrect token description for bullets.

--- parsing/tok.ml
+++ parsing/tok.ml
@@ -55,7 +55,7 @@ let to_string = function
   | INT s -> Format.sprintf "INT %s" s
   | STRING s -> Format.sprintf "STRING %S" s
   | LEFTQMARK -> "LEFTQMARK"
-  | BULLET s -> Format.sprintf "STRING %S" s
+  | BULLET s -> Format.sprintf "BULLET %S" s
   | EOI -> "EOI"
 
 let match_keyword kwd = function
