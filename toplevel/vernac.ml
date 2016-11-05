d6a18782919367006ab1cee0a5577ed9b3028682
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2016-11-05 12:02:43 +0100

    Not using style tags when translating/beautifying a file.

--- toplevel/vernac.ml
+++ toplevel/vernac.ml
@@ -143,7 +143,7 @@ let pr_new_syntax_in_context loc chan_beautify ocom =
       | None -> mt() in
     let after = comment (CLexer.extract_comments (snd loc)) in
     if !beautify_file then
-      Pp.msg_with ~pp_tag:Ppstyle.pp_tag !Pp_control.std_ft (hov 0 (before ++ com ++ after))
+      Pp.msg_with !Pp_control.std_ft (hov 0 (before ++ com ++ after))
     else
       Feedback.msg_info (hov 4 (str"New Syntax:" ++ fnl() ++ (hov 0 com)));
     States.unfreeze fs;
