3b57395029447119eea1fd399636cd9cfe3e673e
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2016-12-06 12:12:48 +0100

    Generalizing the refine primitive so as to accept tactic arguments.

--- proofs/refine.mli
+++ proofs/refine.mli
@@ -12,6 +12,7 @@
     in Ltac which is actually based on the one below. *)
 
 open Proofview
+open Proofview.Notations
 
 (** {6 The refine tactic} *)
 
@@ -31,7 +32,11 @@ val refine : ?unsafe:bool -> EConstr.t Sigma.run -> unit tactic
     type-checked beforehand. *)
 
 val refine_one : ?unsafe:bool -> ('a * EConstr.t) Sigma.run -> 'a tactic
-(** A generalization of [refine] which assumes exactly one goal under focus *)
+(** A variant of [refine] which assumes exactly one goal under focus *)
+
+val generic_refine : ?unsafe:bool -> ('a * EConstr.t) tactic ->
+  ([ `NF ], 'r) Proofview.Goal.t -> 'a tactic
+(** The general version of refine. *)
 
 (** {7 Helper functions} *)
 
