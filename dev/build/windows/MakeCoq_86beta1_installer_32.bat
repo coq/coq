fb637d483d3637f0cf81eed92467f1ff491add2a
Maxime Dénès <mail@maximedenes.fr>
2016-12-08 10:30:26 +0100

    Fix paths in 32-bit windows build scripts.

--- dev/build/windows/MakeCoq_86beta1_installer_32.bat
+++ dev/build/windows/MakeCoq_86beta1_installer_32.bat
@@ -4,5 +4,5 @@ call MakeCoq_MinGW.bat ^
   -arch=32 ^
   -installer=Y ^
   -coqver=8.6beta1 ^
-  -destcyg=%ROOTPATH%\cygwin_coq64_86beta1_inst ^
-  -destcoq=%ROOTPATH%\coq64_86beta1_inst
+  -destcyg=%ROOTPATH%\cygwin_coq32_86beta1_inst ^
+  -destcoq=%ROOTPATH%\coq32_86beta1_inst
--- dev/build/windows/MakeCoq_86rc1_installer_32.bat
+++ dev/build/windows/MakeCoq_86rc1_installer_32.bat
@@ -4,5 +4,5 @@ call MakeCoq_MinGW.bat ^
   -arch=32 ^
   -installer=Y ^
   -coqver=8.6rc1 ^
-  -destcyg=%ROOTPATH%\cygwin_coq64_86rc1_inst ^
-  -destcoq=%ROOTPATH%\coq64_86rc1_inst
+  -destcyg=%ROOTPATH%\cygwin_coq32_86rc1_inst ^
+  -destcoq=%ROOTPATH%\coq32_86rc1_inst
