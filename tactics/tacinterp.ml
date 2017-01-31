f5923c2174fbb419397f127af31ab1cd0b223e0a
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2017-01-31 06:36:25 +0100

    Fixing #5311 (anomaly on unexpected intro pattern).
    This was introduced in 8.5 while reorganizing the structure of
    intro-patterns.

--- tactics/tacinterp.ml
+++ tactics/tacinterp.ml
@@ -894,7 +894,7 @@ let interp_or_and_intro_pattern_option ist env sigma = function
       (match coerce_to_intro_pattern env (Id.Map.find id ist.lfun) with
       | IntroAction (IntroOrAndPattern l) -> sigma, Some (loc,l)
       | _ ->
-        raise (CannotCoerceTo "a disjunctive/conjunctive introduction pattern"))
+        user_err_loc (loc,"", str "Cannot coerce to a disjunctive/conjunctive pattern."))
   | Some (ArgArg (loc,l)) ->
       let sigma,l = interp_or_and_intro_pattern ist env sigma l in
       sigma, Some (loc,l)
