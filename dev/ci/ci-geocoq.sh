fedf877e14ed75fd7156a97f5bd6dcdc9c6ffcef
Emilio Jesus Gallego Arias <e+git@x80.org>
2017-02-07 15:58:02 +0100

    [travis] [External CI] [geocoq] don't build slow file
    Unfortunately `Ch16_coordinates_with_functions.v` takes alone ~15
    minutes which is too much for Travis. Pity, because it was a nice use case.

--- dev/ci/ci-geocoq.sh
+++ dev/ci/ci-geocoq.sh
@@ -9,4 +9,8 @@ GeoCoq_CI_GITURL=https://github.com/GeoCoq/GeoCoq.git
 
 git clone --depth 1 -b ${GeoCoq_CI_BRANCH} ${GeoCoq_CI_GITURL}
 
-( cd GeoCoq && ./configure.sh && make -j ${NJOBS} )
+( cd GeoCoq                                               && \
+  ./configure.sh                                          && \
+  sed -i.bak '/Ch16_coordinates_with_functions\.v/d' Make && \
+  coq_makefile -f Make -o Makefile                        && \
+  make -j ${NJOBS} )
