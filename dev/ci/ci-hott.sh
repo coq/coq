d9d8977cf213f0d4b2e8d324c759c23df58ba457
Maxime Dénès <mail@maximedenes.fr>
2017-02-21 09:46:34 +0100

    [travis] track an 8.7 specific branch of HoTT.
    This is required since we merged PR#309: Ltac as a plugin.

--- dev/ci/ci-hott.sh
+++ dev/ci/ci-hott.sh
@@ -3,6 +3,6 @@
 ci_dir="$(dirname "$0")"
 source ${ci_dir}/ci-common.sh
 
-git_checkout mz-8.6 https://github.com/ejgallego/HoTT.git HoTT
+git_checkout mz-8.7 https://github.com/ejgallego/HoTT.git HoTT
 
 ( cd HoTT && ./autogen.sh && ./configure && make -j ${NJOBS} )
