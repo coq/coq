21a161ebf2fa5563f421c9aef0c9b0b57006c976
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2017-03-13 10:45:37 +0100

    Disable C-CoRN testing as it is incompatible with the new definition of IZR.

--- dev/ci/ci-math-classes.sh
+++ dev/ci/ci-math-classes.sh
@@ -7,6 +7,7 @@ source ${ci_dir}/ci-common.sh
 git_checkout v8.6 https://github.com/math-classes/math-classes.git math-classes
 ( cd math-classes && make -j ${NJOBS} && make install )
 
-git_checkout v8.6 https://github.com/c-corn/corn.git corn
-( cd corn         && make -j ${NJOBS} )
+# Disabled until https://github.com/c-corn/corn/pull/40 is merged
+#git_checkout v8.6 https://github.com/c-corn/corn.git corn
+#( cd corn         && make -j ${NJOBS} )
 
