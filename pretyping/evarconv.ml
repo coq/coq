2794b3c91bbbef115303b40f2e494ad97467dc9e
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2017-04-05 14:05:42 +0200

    Removing a normalization hotspot from EConstr.
    It was not necessary to normalize a term just to check whether it was a
    global reference. The hotspot appeared in mathcomp.

--- pretyping/evarconv.ml
+++ pretyping/evarconv.ml
@@ -154,7 +154,7 @@ let check_conv_record env sigma (t1,sk1) (t2,sk2) =
 	lookup_canonical_conversion
 	  (proji, Sort_cs (family_of_sort s)),[]
       | _ ->
-	let c2 = global_of_constr (EConstr.to_constr sigma t2) in
+	let (c2, _) = Termops.global_of_constr sigma t2 in
 	  lookup_canonical_conversion (proji, Const_cs c2),sk2
     with Not_found ->
       let (c, cs) = lookup_canonical_conversion (proji,Default_cs) in 
