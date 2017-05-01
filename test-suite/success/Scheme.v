coq/coq

commit 31dbba5f5c16f81c6dac2adaba46087da787ac12

Monomorphization (tactics)
git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@16003 85f007b7-540e-0410-9357-904b9bb8a0f7

tactics/eqschemes.ml
@@ -94,14 +94,15 @@ let get_coq_eq () =
 
 let get_sym_eq_data env ind =
   let (mib,mip as specif) = lookup_mind_specif env ind in
-  if Array.length mib.mind_packets <> 1 or Array.length mip.mind_nf_lc <> 1 then
+  if not (Int.equal (Array.length mib.mind_packets) 1) ||
+    not (Int.equal (Array.length mip.mind_nf_lc) 1) then
     error "Not an inductive type with a single constructor.";
   let realsign,_ = List.chop mip.mind_nrealargs_ctxt mip.mind_arity_ctxt in
-  if List.exists (fun (_,b,_) -> b <> None) realsign then
+  if List.exists (fun (_,b,_) -> not (Option.is_empty b)) realsign then
     error "Inductive equalities with local definitions in arity not supported.";
   let constrsign,ccl = decompose_prod_assum mip.mind_nf_lc.(0) in
   let _,constrargs = decompose_app ccl in
-  if rel_context_length constrsign<>rel_context_length mib.mind_params_ctxt then
+  if not (Int.equal (rel_context_length constrsign) (rel_context_length mib.mind_params_ctxt)) then
     error "Constructor must have no arguments"; (* This can be relaxed... *)
   let params,constrargs = List.chop mib.mind_nparams constrargs in
   if mip.mind_nrealargs > mib.mind_nparams then
@@ -126,14 +127,15 @@ let get_sym_eq_data env ind =
 
 let get_non_sym_eq_data env ind =
   let (mib,mip as specif) = lookup_mind_specif env ind in
-  if Array.length mib.mind_packets <> 1 or Array.length mip.mind_nf_lc <> 1 then
+  if not (Int.equal (Array.length mib.mind_packets) 1) ||
+    not (Int.equal (Array.length mip.mind_nf_lc) 1) then
     error "Not an inductive type with a single constructor.";
   let realsign,_ = List.chop mip.mind_nrealargs_ctxt mip.mind_arity_ctxt in
-  if List.exists (fun (_,b,_) -> b <> None) realsign then
+  if List.exists (fun (_,b,_) -> not (Option.is_empty b)) realsign then
     error "Inductive equalities with local definitions in arity not supported";
   let constrsign,ccl = decompose_prod_assum mip.mind_nf_lc.(0) in
   let _,constrargs = decompose_app ccl in
-  if rel_context_length constrsign<>rel_context_length mib.mind_params_ctxt then
+  if not (Int.equal (rel_context_length constrsign) (rel_context_length mib.mind_params_ctxt)) then
     error "Constructor must have no arguments";
   let _,constrargs = List.chop mib.mind_nparams constrargs in
   (specif,constrargs,realsign,mip.mind_nrealargs)
@@ -674,19 +676,19 @@ let rew_r2l_scheme_kind =
 
 let build_congr env (eq,refl) ind =
   let (mib,mip) = lookup_mind_specif env ind in
-  if Array.length mib.mind_packets <> 1 or Array.length mip.mind_nf_lc <> 1 then
+  if not (Int.equal (Array.length mib.mind_packets) 1) || not (Int.equal (Array.length mip.mind_nf_lc) 1) then
     error "Not an inductive type with a single constructor.";
-  if mip.mind_nrealargs <> 1 then
+  if not (Int.equal mip.mind_nrealargs 1) then
     error "Expect an inductive type with one predicate parameter.";
   let i = 1 in
   let realsign,_ = List.chop mip.mind_nrealargs_ctxt mip.mind_arity_ctxt in
-  if List.exists (fun (_,b,_) -> b <> None) realsign then
+  if List.exists (fun (_,b,_) -> not (Option.is_empty b)) realsign then
     error "Inductive equalities with local definitions in arity not supported.";
   let env_with_arity = push_rel_context mip.mind_arity_ctxt env in
   let (_,_,ty) = lookup_rel (mip.mind_nrealargs - i + 1) env_with_arity in
   let constrsign,ccl = decompose_prod_assum mip.mind_nf_lc.(0) in
   let _,constrargs = decompose_app ccl in
-  if rel_context_length constrsign<>rel_context_length mib.mind_params_ctxt then
+  if Int.equal (rel_context_length constrsign) (rel_context_length mib.mind_params_ctxt) then
     error "Constructor must have no arguments";
   let b = List.nth constrargs (i + mib.mind_nparams - 1) in
   let varB = fresh env (id_of_string "B") in

tactics/auto.ml
@@ -117,7 +117,7 @@ type search_entry = stored_data list * stored_data list * Bounded_net.t
 let empty_se = ([],[],Bounded_net.create ())
 
 let eq_pri_auto_tactic (_, x) (_, y) =
-  if x.pri = y.pri && x.pat = y.pat then
+  if Int.equal x.pri y.pri && Option.Misc.compare constr_pattern_eq x.pat y.pat then
     match x.code,y.code with
       | Res_pf(cstr,_),Res_pf(cstr1,_) -> 
 	   eq_constr cstr cstr1
@@ -173,13 +173,29 @@ let translate_hint (go,p) =
   in
   (go,{ p with code = code })
 
+let hints_path_atom_eq h1 h2 = match h1, h2 with
+| PathHints l1, PathHints l2 -> List.equal eq_gr l1 l2
+| PathAny, PathAny -> true
+| _ -> false
+
+let rec hints_path_eq h1 h2 = match h1, h2 with
+| PathAtom h1, PathAtom h2 -> hints_path_atom_eq h1 h2
+| PathStar h1, PathStar h2 -> hints_path_eq h1 h2
+| PathSeq (l1, r1), PathSeq (l2, r2) ->
+  hints_path_eq l1 l2 && hints_path_eq r1 r2
+| PathOr (l1, r1), PathOr (l2, r2) ->
+  hints_path_eq l1 l2 && hints_path_eq r1 r2
+| PathEmpty, PathEmpty -> true
+| PathEpsilon, PathEpsilon -> true
+| _ -> false
+
 let path_matches hp hints =
   let rec aux hp hints k =
     match hp, hints with
     | PathAtom _, [] -> false
     | PathAtom PathAny, (_ :: hints') -> k hints'
     | PathAtom p, (h :: hints') -> 
-      if p = h then k hints' else false
+      if hints_path_atom_eq p h then k hints' else false
     | PathStar hp', hints -> 
       k hints || aux hp' hints (fun hints' -> aux hp hints' k)
     | PathSeq (hp, hp'), hints -> 
@@ -209,7 +225,7 @@ let rec is_empty = function
 let rec path_derivate hp hint =
   let rec derivate_atoms hints hints' =
     match hints, hints' with
-    | gr :: grs, gr' :: grs' when gr = gr' -> derivate_atoms grs grs'
+    | gr :: grs, gr' :: grs' when eq_gr gr gr' -> derivate_atoms grs grs'
     | [], [] -> PathEpsilon
     | [], hints -> PathEmpty
     | grs, [] -> PathAtom (PathHints grs)
@@ -242,11 +258,11 @@ let rec normalize_path h =
   | PathOr (PathEmpty, p) | PathOr (p, PathEmpty) -> normalize_path p
   | PathOr (p, q) -> 
     let p', q' = normalize_path p, normalize_path q in
-      if p = p' && q = q' then h
+      if hints_path_eq p p' && hints_path_eq q q' then h
       else normalize_path (PathOr (p', q'))
   | PathSeq (p, q) -> 
     let p', q' = normalize_path p, normalize_path q in
-      if p = p' && q = q' then h
+      if hints_path_eq p p' && hints_path_eq q q' then h
       else normalize_path (PathSeq (p', q'))
   | _ -> h
 
@@ -347,7 +363,7 @@ module Hint_db = struct
     let pat = if not db.use_dn && is_exact v.code then None else v.pat in
       match k with
       | None ->
-	  if not (List.exists (fun (_, (_, v')) -> v = v') db.hintdb_nopat) then
+	  if not (List.exists (fun (_, (_, v')) -> Pervasives.(=) v v') db.hintdb_nopat) then (** FIXME *)
 	    { db with hintdb_nopat = (gr,idv) :: db.hintdb_nopat }
 	  else db
       | Some gr ->
@@ -496,7 +512,7 @@ let make_apply_entry env sigma (eapply,hnf,verbose) pri ?(name=PathAny) (c,cty) 
 	  try head_pattern_bound pat
           with BoundPattern -> failwith "make_apply_entry" in
         let nmiss = List.length (clenv_missing ce) in
-	if nmiss = 0 then
+	if Int.equal nmiss 0 then
 	  (Some hd,
           { pri = (match pri with None -> nb_hyp cty | Some p -> p);
             pat = Some pat;
@@ -526,7 +542,7 @@ let make_resolves env sigma flags pri ?name c =
   let ents = List.map_filter try_apply
       [make_exact_entry sigma pri ?name; make_apply_entry env sigma flags pri ?name]
   in
-  if ents = [] then
+  if List.is_empty ents then
     errorlabstrm "Hint"
       (pr_lconstr c ++ spc() ++
         (if pi1 flags then str"cannot be used as a hint."
@@ -689,7 +705,9 @@ let subst_autohint (subst,(local,name,hintlist as obj)) =
 	if path' == path then obj else (local, name, AddCut path')
 
 let classify_autohint ((local,name,hintlist) as obj) =
-  if local or hintlist = (AddHints []) then Dispose else Substitute obj
+  match hintlist with
+  | AddHints [] -> Dispose
+  | _ -> if local then Dispose else Substitute obj
 
 let inAutoHint : hint_obj -> obj =
   declare_object {(default_object "AUTOHINT") with
@@ -702,7 +720,7 @@ let create_hint_db l n st b =
   Lib.add_anonymous_leaf (inAutoHint (l,n,CreateDB (b, st)))
 
 let remove_hints local dbnames grs =
-  let dbnames = if dbnames = [] then ["core"] else dbnames in
+  let dbnames = if List.is_empty dbnames then ["core"] else dbnames in
     List.iter
       (fun dbname ->
 	 Lib.add_anonymous_leaf (inAutoHint(local, dbname, RemoveHints grs)))
@@ -799,7 +817,7 @@ let prepare_hint env (sigma,c) =
       (* We skip the test whether args is the identity or not *)
       let t = Evarutil.nf_evar sigma (existential_type sigma ev) in
       let t = List.fold_right (fun (e,id) c -> replace_term e id c) !subst t in
-      if free_rels t <> Intset.empty then
+      if not (Intset.is_empty (free_rels t)) then
 	error "Hints with holes dependent on a bound variable not supported.";
       if occur_existential t then
 	(* Not clever enough to construct dependency graph of evars *)
@@ -870,7 +888,7 @@ let interp_hints =
 let add_hints local dbnames0 h =
   if List.mem "nocore" dbnames0 then
     error "The hint database \"nocore\" is meant to stay empty.";
-  let dbnames = if dbnames0 = [] then ["core"] else dbnames0 in
+  let dbnames = if List.is_empty dbnames0 then ["core"] else dbnames0 in
   let env = Global.env() and sigma = Evd.empty in
   match h with
   | HintsResolveEntry lhints -> add_resolves env sigma lhints local dbnames
@@ -905,7 +923,7 @@ let pr_hint_list hintlist =
 
 let pr_hints_db (name,db,hintlist) =
   (str "In the database " ++ str name ++ str ":" ++
-     if hintlist = [] then (str " nothing" ++ fnl ())
+     if List.is_empty hintlist then (str " nothing" ++ fnl ())
      else (fnl () ++ pr_hint_list hintlist))
 
 (* Print all hints associated to head c in any database *)
@@ -916,7 +934,7 @@ let pr_hint_list_for_head c =
     (name, db, hints)
   in
   let valid_dbs = List.map validate dbs in
-  if valid_dbs = [] then
+  if List.is_empty valid_dbs then
     (str "No hint declared for :" ++ pr_global c)
   else
     hov 0
@@ -942,7 +960,7 @@ let pr_hint_term cl =
       let fn db = List.map (fun x -> 0, x) (fn db) in
       List.map (fun (name, db) -> (name, db, fn db)) dbs
     in
-      if valid_dbs = [] then
+      if List.is_empty valid_dbs then
 	(str "No hint applicable for current goal")
       else
 	(str "Applicable Hints :" ++ fnl () ++
@@ -1007,7 +1025,7 @@ let pr_searchtable () =
 (*          tactics with a trace mechanism for automatic search           *)
 (**************************************************************************)
 
-let priority l = List.filter (fun (_, hint) -> hint.pri = 0) l
+let priority l = List.filter (fun (_, hint) -> Int.equal hint.pri 0) l
 
 (* tell auto not to reuse already instantiated metas in unification (for
    compatibility, since otherwise, apply succeeds oftener) *)
@@ -1134,8 +1152,8 @@ let no_dbg () = (Off,0,ref [])
 
 let mk_trivial_dbg debug =
   let d =
-    if debug = Debug || !global_debug_trivial then Debug
-    else if debug = Info || !global_info_trivial then Info
+    if debug == Debug || !global_debug_trivial then Debug
+    else if debug == Info || !global_info_trivial then Info
     else Off
   in (d,0,ref [])
 
@@ -1144,8 +1162,8 @@ let mk_trivial_dbg debug =
 
 let mk_auto_dbg debug =
   let d =
-    if debug = Debug || !global_debug_auto then Debug
-    else if debug = Info || !global_info_auto then Info
+    if debug == Debug || !global_debug_auto then Debug
+    else if debug == Info || !global_info_auto then Info
     else Off
   in (d,1,ref [])
 
@@ -1194,7 +1212,7 @@ let rec cleanup_info_trace depth acc = function
 
 and erase_subtree depth = function
   | [] -> []
-  | (d,_) :: l -> if d = depth then l else erase_subtree depth l
+  | (d,_) :: l -> if Int.equal d depth then l else erase_subtree depth l
 
 let pr_info_atom (d,pp) =
   str (String.make d ' ') ++ pp () ++ str "."
@@ -1220,10 +1238,10 @@ let tclTRY_dbg d tac =
   tclORELSE0
     (fun gl ->
       let out = tac gl in
-      if level <> Off then msg_debug (pr_dbg_header d ++ fnl () ++ pr_info_trace d);
+      if level != Off then msg_debug (pr_dbg_header d ++ fnl () ++ pr_info_trace d);
       out)
     (fun gl ->
-      if level = Info then msg_debug (pr_info_nop d);
+      if level == Info then msg_debug (pr_info_nop d);
       tclIDTAC gl)
 
 (**************************************************************************)
@@ -1314,7 +1332,7 @@ and tac_of_hint dbg db_list local_db concl (flags, ({pat=p; code=t})) =
         (unify_resolve_gen flags (c,cl))
 	(* With "(debug) trivial", we shouldn't end here, and
 	   with "debug auto" we don't display the details of inner trivial *)
-        (trivial_fail_db (no_dbg ()) (flags <> None) db_list local_db)
+        (trivial_fail_db (no_dbg ()) (not (Option.is_empty flags)) db_list local_db)
     | Unfold_nth c ->
       (fun gl ->
        if exists_evaluable_reference (pf_env gl) c then
@@ -1401,7 +1419,7 @@ let intro_register dbg kont db =
 
 let search d n mod_delta db_list local_db =
   let rec search d n local_db =
-    if n=0 then (fun gl -> error "BOUND 2") else
+    if Int.equal n 0 then (fun gl -> error "BOUND 2") else
       tclORELSE0 (dbg_assumption d)
 	(tclORELSE0 (intro_register d (search d n) local_db)
 	   (fun gl ->

tactics/autorewrite.ml
@@ -137,7 +137,7 @@ let autorewrite_multi_in ?(conds=Naive) idl tac_main lbas : tactic =
        g::_ ->
         (match Environ.named_context_of_val (Goal.V82.hyps gl'.Evd.sigma g) with
             (lastid,_,_)::_ ->
-              if last_hyp_id <> lastid then
+              if not (id_eq last_hyp_id lastid) then
                begin
                 let gl'' =
                   if !to_be_cleared then
@@ -167,8 +167,8 @@ let gen_auto_multi_rewrite conds tac_main lbas cl =
   let try_do_hyps treat_id l =
     autorewrite_multi_in ~conds (List.map treat_id l) tac_main lbas
   in
-  if cl.concl_occs <> AllOccurrences &
-     cl.concl_occs <> NoOccurrences
+  if cl.concl_occs != AllOccurrences &&
+     cl.concl_occs != NoOccurrences
   then
     error "The \"at\" syntax isn't available yet for the autorewrite tactic."
   else
@@ -178,7 +178,7 @@ let gen_auto_multi_rewrite conds tac_main lbas cl =
 	| _ ->      tclTHENFIRST t1 t2
     in
     compose_tac
-	(if cl.concl_occs <> NoOccurrences then autorewrite ~conds tac_main lbas else tclIDTAC)
+	(if cl.concl_occs != NoOccurrences then autorewrite ~conds tac_main lbas else tclIDTAC)
 	(match cl.onhyps with
 	   | Some l -> try_do_hyps (fun ((_,id),_) -> id) l
 	   | None ->
@@ -191,7 +191,7 @@ let gen_auto_multi_rewrite conds tac_main lbas cl =
 let auto_multi_rewrite ?(conds=Naive) = gen_auto_multi_rewrite conds Refiner.tclIDTAC
 
 let auto_multi_rewrite_with ?(conds=Naive) tac_main lbas cl gl =
-  let onconcl = cl.Locus.concl_occs <> NoOccurrences in
+  let onconcl = match cl.Locus.concl_occs with NoOccurrences -> false | _ -> true in
   match onconcl,cl.Locus.onhyps with
     | false,Some [_] | true,Some [] | false,Some [] ->
 	(* autorewrite with .... in clause using tac n'est sur que

tactics/btermdn.ml
@@ -6,6 +6,7 @@
 (*         *       GNU Lesser General Public License Version 2.1        *)
 (************************************************************************)
 
+open Util
 open Term
 open Names
 open Pattern

tactics/class_tactics.ml4
@@ -50,7 +50,7 @@ let evars_to_goals p evm =
 	(gls', Evd.add evm' ev evi'))
       evm ([], Evd.defined_evars evm)
   in
-  if goals = [] then None else Some (List.rev goals, evm')
+  if List.is_empty goals then None else Some (List.rev goals, evm')
 
 (** Typeclasses instance search tactic / eauto *)
 
@@ -80,7 +80,7 @@ let auto_unif_flags = {
 
 let rec eq_constr_mod_evars x y =
   match kind_of_term x, kind_of_term y with
-  | Evar (e1, l1), Evar (e2, l2) when e1 <> e2 -> true
+  | Evar (e1, l1), Evar (e2, l2) when not (Int.equal e1 e2) -> true
   | _, _ -> compare_constr eq_constr_mod_evars x y
 
 let progress_evars t gl =
@@ -107,7 +107,7 @@ let unify_resolve flags (c,clenv) gls =
     Clenvtac.clenv_refine false ~with_classes:false clenv' gls
 
 let clenv_of_prods nprods (c, clenv) gls =
-  if nprods = 0 then Some clenv
+  if Int.equal nprods 0 then Some clenv
   else 
     let ty = pf_type_of gls c in
     let diff = nb_prod ty - nprods in
@@ -328,7 +328,7 @@ let compare (pri, _, _, res) (pri', _, _, res') =
     List.length (sig_it s) + nb_empty_evars (sig_sig s)
   in
   let pri = pri - pri' in
-    if pri <> 0 then pri
+    if not (Int.equal pri 0) then pri
     else nbgoals res - nbgoals res'
 
 let or_tac (x : 'a tac) (y : 'a tac) : 'a tac =
@@ -397,10 +397,12 @@ let hints_tac hints =
 
 let isProp env sigma concl = 
   let ty = Retyping.get_type_of env sigma concl in
-    kind_of_term ty = Sort (Prop Null)
+  match kind_of_term ty with
+  | Sort (Prop Null) -> true
+  | _ -> false
 
 let needs_backtrack only_classes env evd oev concl =
-  if oev = None || isProp env evd concl then
+  if Option.is_empty oev || isProp env evd concl then
     not (Intset.is_empty (Evarutil.evars_of_term concl))
   else true
 
@@ -413,7 +415,7 @@ let then_list (second : atac) (sk : (auto_result, 'a) sk) : (auto_result, 'a) sk
 	     second.skft
 	       (fun {it=gls';sigma=s'} fk' ->
 		  let needs_backtrack = 
-		    if gls' = [] then
+		    if List.is_empty gls' then
  		      needs_backtrack info.only_classes
 		        (Goal.V82.env s gl) s' info.is_evar (Goal.V82.concl s gl)
 		    else true
@@ -456,7 +458,7 @@ let rec fix (t : 'a tac) : 'a tac =
   then_tac t { skft = fun sk fk -> (fix t).skft sk fk }
 
 let rec fix_limit limit (t : 'a tac) : 'a tac =
-  if limit = 0 then fail_tac 
+  if Int.equal limit 0 then fail_tac 
   else then_tac t { skft = fun sk fk -> (fix_limit (pred limit) t).skft sk fk }
     
 let make_autogoal ?(only_classes=true) ?(st=full_transparent_state) cut ev g =
@@ -603,7 +605,7 @@ let error_unresolvable env comp do_split evd =
   let _, ev = Evd.fold_undefined
     (fun ev evi (b,acc) ->
       (* focus on one instance if only one was searched for *)
-      if class_of_constr evi.evar_concl <> None then
+      if not (Option.is_empty (class_of_constr evi.evar_concl)) then
 	if not b (* || do_split *) then
 	  true, Some ev
 	else b, None
@@ -619,7 +621,7 @@ let error_unresolvable env comp do_split evd =
     and return undefined evar_info *)
 
 let select_and_update_evars p oevd in_comp evd ev evi =
-  assert (evi.evar_body = Evar_empty);
+  assert (evi.evar_body == Evar_empty);
   try
     let oevi = Evd.find_undefined oevd ev in
     if Typeclasses.is_resolvable oevi then

tactics/dn.ml
@@ -1,5 +1,4 @@
-
-
+open Util
 
 
 

tactics/eauto.ml4
@@ -90,7 +90,7 @@ open Unification
 (* A tactic similar to Auto, but using EApply, Assumption and e_give_exact *)
 (***************************************************************************)
 
-let priority l = List.map snd (List.filter (fun (pr,_) -> pr = 0) l)
+let priority l = List.map snd (List.filter (fun (pr,_) -> Int.equal pr 0) l)
 
 let unify_e_resolve flags (c,clenv) gls =
   let clenv' = connect_clenv gls clenv in
@@ -177,7 +177,7 @@ module SearchProblem = struct
 
   type state = search_state
 
-  let success s = (sig_it s.tacres) = []
+  let success s = List.is_empty (sig_it s.tacres)
 
   let pr_ev evs ev = Printer.pr_constr_env (Evd.evar_env ev) (Evarutil.nf_evar evs ev.Evd.evar_concl)
 
@@ -205,13 +205,13 @@ module SearchProblem = struct
   let compare s s' =
     let d = s'.depth - s.depth in
     let nbgoals s = List.length (sig_it s.tacres) in
-    if d <> 0 then d else nbgoals s - nbgoals s'
+    if not (Int.equal d 0) then d else nbgoals s - nbgoals s'
 
   let branching s =
-    if s.depth = 0 then
+    if Int.equal s.depth 0 then
       []
     else
-      let ps = if s.prev = Unknown then Unknown else State s in
+      let ps = if s.prev == Unknown then Unknown else State s in
       let lg = s.tacres in
       let nbgl = List.length (sig_it lg) in
       assert (nbgl > 0);
@@ -292,8 +292,8 @@ let _ =
       Goptions.optwrite = (:=) global_info_eauto }
 
 let mk_eauto_dbg d =
-  if d = Debug || !global_debug_eauto then Debug
-  else if d = Info || !global_info_eauto then Info
+  if d == Debug || !global_debug_eauto then Debug
+  else if d == Info || !global_info_eauto then Info
   else Off
 
 let pr_info_nop = function
@@ -306,7 +306,7 @@ let pr_dbg_header = function
   | Info -> msg_debug (str "(* info eauto : *)")
 
 let pr_info dbg s =
-  if dbg <> Info then ()
+  if dbg != Info then ()
   else
     let rec loop s =
       match s.prev with
@@ -327,7 +327,7 @@ let make_initial_state dbg n gl dblist localdb =
     last_tactic = lazy (mt());
     dblist = dblist;
     localdb = [localdb];
-    prev = if dbg=Info then Init else Unknown;
+    prev = if dbg == Info then Init else Unknown;
   }
 
 let e_search_auto debug (in_depth,p) lems db_list gl =

commit cea40f37ab638031b9d5c6434ee5651a16ea1f3e

Fixing Set Rewriting Schemes bugs introduced in v8.5.
- Fixing a typo introduced in 31dbba5f.
- Adapting to computation of universe constraints in pretyping.
- Adding a regression test.

tactics/eqschemes.ml
@@ -89,6 +89,11 @@ let get_coq_eq ctx =
   with Not_found ->
     error "eq not found."
 
+let univ_of_eq env eq =
+  match kind_of_term (Retyping.get_type_of env Evd.empty eq) with
+  | Prod (_,t,_) -> (match kind_of_term t with Sort (Type u) -> u | _ -> assert false)
+  | _ -> assert false
+
 (**********************************************************************)
 (* Check if an inductive type [ind] has the form                      *)
 (*                                                                    *)
@@ -744,7 +749,7 @@ let build_congr env (eq,refl,ctx) ind =
   let ty = get_type (lookup_rel (mip.mind_nrealargs - i + 1) env_with_arity) in
   let constrsign,ccl = decompose_prod_assum mip.mind_nf_lc.(0) in
   let _,constrargs = decompose_app ccl in
-  if Int.equal (Context.Rel.length constrsign) (Context.Rel.length mib.mind_params_ctxt) then
+  if not (Int.equal (Context.Rel.length constrsign) (Context.Rel.length mib.mind_params_ctxt)) then
     error "Constructor must have no arguments";
   let b = List.nth constrargs (i + mib.mind_nparams - 1) in
   let varB = fresh env (Id.of_string "B") in
@@ -752,6 +757,7 @@ let build_congr env (eq,refl,ctx) ind =
   let varf = fresh env (Id.of_string "f") in
   let ci = make_case_info (Global.env()) ind RegularStyle in
   let uni, ctx = Universes.extend_context (Universes.new_global_univ ()) ctx in
+  let ctx = (fst ctx, Univ.enforce_leq uni (univ_of_eq env eq) (snd ctx)) in
   let c = 
   my_it_mkLambda_or_LetIn paramsctxt
      (mkNamedLambda varB (mkSort (Type uni))

test-suite/success/Scheme.v
@@ -2,3 +2,26 @@
 
 Scheme Induction for eq Sort Prop.
 Check eq_ind_dep.
+
+(* This was broken in v8.5 *)
+
+Set Rewriting Schemes.
+Inductive myeq A (a:A) : A -> Prop := myrefl : myeq A a a.
+Unset Rewriting Schemes.
+
+Check myeq_rect.
+Check myeq_ind.
+Check myeq_rec.
+Check myeq_congr.
+Check myeq_sym_internal.
+Check myeq_rew.
+Check myeq_rew_dep.
+Check myeq_rew_fwd_dep.
+Check myeq_rew_r.
+Check internal_myeq_sym_involutive.
+Check myeq_rew_r_dep.
+Check myeq_rew_fwd_r_dep.
+
+Set Rewriting Schemes.
+Inductive myeq_true : bool -> Prop := myrefl_true : myeq_true true.
+Unset Rewriting Schemes.