ccd8ab4721406991ad63c1e82a880a1f42bf065f
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2017-05-12 19:38:06 +0200

    Adding overlay for math-comp.

--- dev/ci/ci-user-overlay.sh
+++ dev/ci/ci-user-overlay.sh
@@ -25,10 +25,8 @@ echo $TRAVIS_PULL_REQUEST
 echo $TRAVIS_BRANCH
 echo $TRAVIS_COMMIT
 
-if [ $TRAVIS_PULL_REQUEST == "678" ] || [ $TRAVIS_BRANCH == "coqlib-part-02" ]; then
-
-    mathcomp_CI_BRANCH=coqlib-part-02
-    mathcomp_CI_GITURL=https://github.com/ejgallego/math-comp.git
-
+if [ $TRAVIS_PULL_REQUEST == "590" ] || [ $TRAVIS_BRANCH == "trunk+algebraic-matchingvar" ]; then
+    mathcomp_CI_BRANCH=trunk+pr590-patvar
+    mathcomp_CI_GITURL=https://github.com/herbelin/math-comp.git
 fi
 
