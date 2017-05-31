24fc0587498c5e9d2f1788d6d906346104ca465a
Matthieu Sozeau <mattam@mattam.org>
2017-05-31 15:21:12 +0200

    test-suite: eauto: working better now using refine

--- test-suite/bugs/closed/2713.v
+++ test-suite/bugs/closed/2713.v
@@ -13,5 +13,4 @@ Lemma test :
   forall (P1 P2:nat->Prop), 
   (forall Q, pred_le (fun a => P1 a /\ P2 a) Q -> True) ->
    True.
-Proof. intros. eapply H. eauto. (* used to work *)
-       apply pred_le_refl. Qed.
+Proof. intros. eapply H. eauto. Qed.
