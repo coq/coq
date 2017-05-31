3d3867b4ba32f2bf705d2f8c7d38c9a934dbdb6a
Maxime Dénès <mail@maximedenes.fr>
2017-05-31 17:43:55 +0200

    Overlay.

--- dev/ci/ci-user-overlay.sh
+++ dev/ci/ci-user-overlay.sh
@@ -25,8 +25,8 @@ echo $TRAVIS_PULL_REQUEST
 echo $TRAVIS_BRANCH
 echo $TRAVIS_COMMIT
 
-if [ $TRAVIS_PULL_REQUEST == "590" ] || [ $TRAVIS_BRANCH == "trunk+algebraic-matchingvar" ]; then
-    mathcomp_CI_BRANCH=trunk+pr590-patvar
-    mathcomp_CI_GITURL=https://github.com/herbelin/math-comp.git
+if [ $TRAVIS_PULL_REQUEST == "669" ] || [ $TRAVIS_BRANCH == "ssr-merge" ]; then
+    mathcomp_CI_BRANCH=ssr-merge
+    mathcomp_CI_GITURL=https://github.com/maximedenes/math-comp.git
 fi
 
