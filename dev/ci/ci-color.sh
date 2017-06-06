fe972a369adf533e2f4ec89eafb63b08a26e2ec7
Pierre Letouzey <pierre.letouzey@inria.fr>
2017-06-06 19:45:48 +0200

    [travis] adapt CoLoR compilation to depend on the bignum package

--- dev/ci/ci-color.sh
+++ dev/ci/ci-color.sh
@@ -5,6 +5,19 @@ source ${ci_dir}/ci-common.sh
 
 Color_CI_DIR=${CI_BUILD_DIR}/color
 
+# Setup Bignums
+
+git_checkout master https://github.com/coq/bignums.git bignums
+
+( cd bignums && make -j ${NJOBS} && make install )
+
+# Compiles CoLoR
+
 svn checkout ${Color_CI_SVNURL} ${Color_CI_DIR}
+
+sed -i -e "s/From Coq Require Import BigN/From Bignums Require Import BigN/" ${Color_CI_DIR}/Util/*/*.v
+sed -i -e "s/From Coq Require Export BigN/From Bignums Require Export BigN/" ${Color_CI_DIR}/Util/*/*.v
+sed -i -e "s/From Coq Require Import BigZ/From Bignums Require Import BigZ/" ${Color_CI_DIR}/Util/*/*.v
+sed -i -e "s/From Coq Require Export BigZ/From Bignums Require Export BigZ/" ${Color_CI_DIR}/Util/*/*.v
 
 ( cd ${Color_CI_DIR} && make -j ${NJOBS} )
