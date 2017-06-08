fd44a40f9d426a7b65f167bc30d320a0f7dd2bbd
Maxime Dénès <mail@maximedenes.fr>
2017-06-08 14:16:10 +0200

    Make Travis generate OSX packages.
    The packages will be built only for main branches (not pull requests),
    and are accessible via bintray: https://bintray.com/coq/coq

--- dev/build/osx/make-macos-dmg.sh
+++ dev/build/osx/make-macos-dmg.sh
@@ -4,19 +4,13 @@
 set -e
 
 # Configuration setup
-eval `opam config env`
-make distclean
 OUTDIR=$PWD/_install
 DMGDIR=$PWD/_dmg
-./configure -debug -prefix $OUTDIR -native-compiler no
 VERSION=$(sed -n -e '/^let coq_version/ s/^[^"]*"\([^"]*\)"$/\1/p' configure.ml)
 APP=bin/CoqIDE_${VERSION}.app
 
 # Create a .app file with CoqIDE
-~/.local/bin/jhbuild run make -j -l2 $APP
-
-# Build Coq and run test-suite
-make && make check
+make -j $NJOBS -l2 $APP
 
 # Add Coq to the .app file
 make OLDROOT=$OUTDIR COQINSTALLPREFIX=$APP/Contents/Resources/ install-coq install-ide-toploop
@@ -29,7 +23,9 @@ mkdir -p $DMGDIR
 ln -sf /Applications $DMGDIR/Applications
 cp -r $APP $DMGDIR
 
-# Temporary countermeasure to hdiutil error 5341
-head -c9703424 /dev/urandom > $DMGDIR/.padding
+mkdir -p _build
 
-hdiutil create -imagekey zlib-level=9 -volname CoqIDE_$VERSION -srcfolder $DMGDIR -ov -format UDZO CoqIDE_$VERSION.dmg
+# Temporary countermeasure to hdiutil error 5341
+# head -c9703424 /dev/urandom > $DMGDIR/.padding
+
+hdiutil create -imagekey zlib-level=9 -volname CoqIDE_$VERSION -srcfolder $DMGDIR -ov -format UDZO _build/CoqIDE_$VERSION.dmg
