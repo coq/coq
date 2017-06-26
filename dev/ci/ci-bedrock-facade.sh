aadadc0cb290a521c73caa381ffd0a62300c6dd4
Jason Gross <jgross@mit.edu>
2017-06-26 11:01:55 -0400

    [v8.6 only] Display timing data on the ci
    This is most useful for fiat-crypto and fiat-parsers; it allows us to
    see what the slowest files are, so that if and when the build
    sporadically takes too long on trvais, we can see which files need to be
    removed from the ci.

--- dev/ci/ci-bedrock-facade.sh
+++ dev/ci/ci-bedrock-facade.sh
@@ -6,5 +6,6 @@ source ${ci_dir}/ci-common.sh
 bedrock_facade_CI_DIR=${CI_BUILD_DIR}/bedrock-facade
 
 git_checkout ${bedrock_facade_CI_BRANCH} ${bedrock_facade_CI_GITURL} ${bedrock_facade_CI_DIR}
+( cd ${bedrock_facade_CI_DIR} && git submodule update --init --recursive )
 
-( cd ${bedrock_facade_CI_DIR} && make facade )
+( cd ${bedrock_facade_CI_DIR} && (make facade TIMED=1 2>&1 | tee time-of-build.log; exit ${PIPESTATUS[0]}) && python ./etc/coq-scripts/timing/make-one-time-file.py time-of-build.log time-of-build-pretty.log && cat time-of-build-pretty.log )
--- dev/ci/ci-fiat-crypto.sh
+++ dev/ci/ci-fiat-crypto.sh
@@ -8,4 +8,4 @@ fiat_crypto_CI_DIR=${CI_BUILD_DIR}/fiat-crypto
 git_checkout ${fiat_crypto_CI_BRANCH} ${fiat_crypto_CI_GITURL} ${fiat_crypto_CI_DIR}
 ( cd ${fiat_crypto_CI_DIR} && git submodule update --init --recursive )
 
-( cd ${fiat_crypto_CI_DIR} && make lite )
+( cd ${fiat_crypto_CI_DIR} && (make lite TIMED=1 2>&1 | tee time-of-build.log; exit ${PIPESTATUS[0]}) && python ./etc/coq-scripts/timing/make-one-time-file.py time-of-build.log time-of-build-pretty.log && cat time-of-build-pretty.log )
