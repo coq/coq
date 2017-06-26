d210fe355b41a658fac8c388658bfdb9202b9a18
Jason Gross <jgross@mit.edu>
2017-06-26 11:09:06 -0400

    Separate make from python script for HoTT
    HoTT still needs to use the submodule, but this will allow us to more easily see where the build fails, if it does
    
    (cherry picked from commit 987655c3bc8a8ebd60856356f465b3c34eb4e252)

--- dev/ci/ci-hott.sh
+++ dev/ci/ci-hott.sh
@@ -6,5 +6,6 @@ source ${ci_dir}/ci-common.sh
 HoTT_CI_DIR=${CI_BUILD_DIR}/HoTT
 
 git_checkout ${HoTT_CI_BRANCH} ${HoTT_CI_GITURL} ${HoTT_CI_DIR}
+( cd ${HoTT_CI_DIR} && git submodule update --init --recursive )
 
-( cd ${HoTT_CI_DIR} && ./autogen.sh && ./configure && git submodule update --init --recursive && ./etc/coq-scripts/timing/make-pretty-timed-or-error.sh )
+( cd ${HoTT_CI_DIR} && ./autogen.sh && ./configure && (make TIMED=1 | tee time-of-build.log; exit ${PIPESTATUS[0]}) && python ./etc/coq-scripts/timing/make-one-time-file.py time-of-build.log time-of-build-pretty.log && cat time-of-build-pretty.log )
