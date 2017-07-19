470977939bc256ed18f2bb27d647411724154b80
Matthieu Sozeau <matthieu.sozeau@inria.fr>
2017-07-19 16:06:08 +0200

    test-suite: apply: fix scripts 3258 and 3188 after change of behavior
    This otherwise results in divergence as the higher-order apply finds a
    useless predicate for apply reflexivity.

--- test-suite/bugs/closed/bug_3258.v
+++ test-suite/bugs/closed/bug_3258.v
@@ -1,9 +1,8 @@
 Require Import TestSuite.admit.
 Require Import Coq.Classes.Morphisms Coq.Classes.RelationClasses Coq.Program.Program Coq.Setoids.Setoid.
-
 Global Set Implicit Arguments.
 
-Hint Extern 0 => apply reflexivity : typeclass_instances.
+Hint Extern 0 => simple apply reflexivity : typeclass_instances.
 
 Inductive Comp : Type -> Type :=
 | Pick : forall A, (A -> Prop) -> Comp A.
