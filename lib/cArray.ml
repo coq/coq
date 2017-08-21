4c43d8c6e0298f9d5f27ebca170e7e42478ed46c
Paul Steckler <steck@stecksoft.com>
2017-08-21 18:28:42 -0400

    read, write from same array

--- lib/cArray.ml
+++ lib/cArray.ml
@@ -331,7 +331,7 @@ let smartmap f (ar : 'a array) =
     Array.unsafe_set ans !i v;
     incr i;
     while !i < len do
-      let v = Array.unsafe_get ar !i in
+      let v = Array.unsafe_get ans !i in
       let v' = f v in
       if v != v' then Array.unsafe_set ans !i v';
       incr i
