90028a4b4b1fbce689cd4e0258da931b3f492c65
Enrico Tassi <Enrico.Tassi@inria.fr>
2017-09-01 10:00:07 +0200

    fix test-suite/coq-makefile/findlib-package on windows
    (cherry picked from commit c2c1f7e74d13bf614a8704de1437d4c789b2c2c9)

--- test-suite/coq-makefile/findlib-package/run.sh
+++ test-suite/coq-makefile/findlib-package/run.sh
@@ -4,6 +4,11 @@
 
 echo "let () = Foolib.foo ();;" >> src/test_aux.ml
 export OCAMLPATH=$OCAMLPATH:$PWD/findlib
+if which cygpath 2>/dev/null; then
+  # the only way I found to pass OCAMLPATH on win is to have it contain
+  # only one entry
+  export OCAMLPATH=`cygpath -w $PWD/findlib`
+fi
 make -C findlib/foo clean
 coq_makefile -f _CoqProject -o Makefile
 cat Makefile.conf
