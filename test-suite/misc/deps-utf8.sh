cfdd9141bfeb4a53b00f720e1ee77c2469702a2a
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2017-09-11 16:32:39 +0200

    Adding a test for utf8 characters in directory names.
    (cherry picked from commit 98e2b048aa5419ac3d075e1d3f2499cb816fe92e)

---
+++ test-suite/misc/deps-utf8.sh
@@ -0,0 +1,17 @@
+# Check reading directories matching non pure ascii idents
+# See bug #5715 (utf-8 working on macos X and linux)
+# Windows is still not compliant
+a=`uname`
+if [ "$a" = "Darwin" -o "$a" = "Linux" ]; then
+rm -f misc/deps/théorèmes/*.v
+tmpoutput=`mktemp /tmp/coqcheck.XXXXXX`
+$coqc -R misc/deps AlphaBêta misc/deps/αβ/γδ.v
+R=$?
+$coqtop -R misc/deps AlphaBêta -load-vernac-source misc/deps/αβ/εζ.v
+S=$?
+if [ $R = 0 -a $S = 0 ]; then
+    exit 0
+else
+    exit 1
+fi
+fi
