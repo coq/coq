63d582c6cd12bc3f8134a5aa9e3bdbca0dd2e9ca
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2017-09-23 12:25:35 +0200

    Fixing a bug of Print for inductive types with let-ins in parameters.
    Adding a "let-in"-sensitive function hnf_prod_applist_assum to
    instantiate parameters and using it for printing.
    
    Thanks to PMP for reporting.

--- test-suite/output/Inductive.v
+++ test-suite/output/Inductive.v
@@ -1,3 +1,7 @@
 Fail Inductive list' (A:Set) : Set :=
 | nil' : list' A
 | cons' : A -> list' A -> list' (A*A).
+
+(* Check printing of let-ins *)
+Inductive foo (A : Type) (x : A) (y := x) := Foo.
+Print foo.
