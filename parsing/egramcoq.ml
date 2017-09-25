coq/coq

commit 3e70ea9c0967725bd320a6387d19cfb9d5a9b7fe

"A -> B" is a notation for "forall _ : A, B".
No good reason for that except uniformity so revert this commit if you find a
reason against it.

git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@15146 85f007b7-540e-0410-9357-904b9bb8a0f7

parsing/g_constr.ml4
@@ -170,9 +170,7 @@ GEXTEND Gram
       | c1 = operconstr; ":>" ->
                  CCast(loc,c1, CastCoerce) ]
     | "99" RIGHTA [ ]
-    | "90" RIGHTA
-      [ c1 = operconstr; "->"; c2 = binder_constr -> CArrow(loc,c1,c2)
-      | c1 = operconstr; "->"; c2 = SELF -> CArrow(loc,c1,c2)]
+    | "90" RIGHTA [ ]
     | "10" LEFTA
       [ f=operconstr; args=LIST1 appl_arg -> CApp(loc,(None,f),args)
       | "@"; f=global; args=LIST0 NEXT -> CAppExpl(loc,(None,f),args)

interp/constrextern.ml
@@ -194,9 +194,6 @@ let rec check_same_type ty1 ty2 =
         check_same_type a1 a2;
         check_same_type b1 b2)
         fl1 fl2
-  | CArrow(_,a1,b1), CArrow(_,a2,b2) ->
-      check_same_type a1 a2;
-      check_same_type b1 b2
   | CProdN(_,bl1,a1), CProdN(_,bl2,a2) ->
       List.iter2 check_same_binder bl1 bl2;
       check_same_type a1 a2
@@ -694,10 +691,6 @@ let rec extern inctx scopes vars r =
 	 | _       ->
 	     explicitize loc inctx [] (None,sub_extern false scopes vars f)
                (List.map (sub_extern true scopes vars) args))
-
-  | GProd (loc,Anonymous,_,t,c) ->
-      (* Anonymous product are never factorized *)
-      CArrow (loc,extern_typ scopes vars t, extern_typ scopes vars c)
 
   | GLetIn (loc,na,t,c) ->
       CLetIn (loc,(loc,na),sub_extern false scopes vars t,

interp/constrintern.ml
@@ -1385,8 +1385,6 @@ let internalize sigma globalenv env allow_patvar lvar c =
               Array.map (fun (bl,_,_) -> List.map snd bl) idl,
               Array.map (fun (_,ty,_) -> ty) idl,
               Array.map (fun (_,_,bd) -> bd) idl)
-    | CArrow (loc,c1,c2) ->
-        GProd (loc, Anonymous, Explicit, intern_type env c1, intern_type env c2)
     | CProdN (loc,[],c2) ->
         intern_type env c2
     | CProdN (loc,(nal,bk,ty)::bll,c2) ->

interp/topconstr.ml
@@ -869,7 +869,6 @@ type constr_expr =
   | CRef of reference
   | CFix of loc * identifier located * fix_expr list
   | CCoFix of loc * identifier located * cofix_expr list
-  | CArrow of loc * constr_expr * constr_expr
   | CProdN of loc * (name located list * binder_kind * constr_expr) list * constr_expr
   | CLambdaN of loc * (name located list * binder_kind * constr_expr) list * constr_expr
   | CLetIn of loc * name located * constr_expr * constr_expr
@@ -955,7 +954,6 @@ let constr_loc = function
   | CRef (Qualid (loc,_)) -> loc
   | CFix (loc,_,_) -> loc
   | CCoFix (loc,_,_) -> loc
-  | CArrow (loc,_,_) -> loc
   | CProdN (loc,_,_) -> loc
   | CLambdaN (loc,_,_) -> loc
   | CLetIn (loc,_,_,_) -> loc
@@ -1053,7 +1051,6 @@ let rec fold_local_binders g f n acc b = function
       f n acc b
 
 let fold_constr_expr_with_binders g f n acc = function
-  | CArrow (loc,a,b) -> f n (f n acc a) b
   | CAppExpl (loc,(_,_),l) -> List.fold_left (f n) acc l
   | CApp (loc,(_,t),l) -> List.fold_left (f n) (f n acc t) (List.map fst l)
   | CProdN (_,l,b) | CLambdaN (_,l,b) -> fold_constr_expr_binders g f n acc b l
@@ -1207,7 +1204,6 @@ let map_local_binders f g e bl =
   (e, List.rev rbl)
 
 let map_constr_expr_with_binders g f e = function
-  | CArrow (loc,a,b) -> CArrow (loc,f e a,f e b)
   | CAppExpl (loc,r,l) -> CAppExpl (loc,r,List.map (f e) l)
   | CApp (loc,(p,a),l) ->
       CApp (loc,(p,f e a),List.map (fun (a,i) -> (f e a,i)) l)

interp/topconstr.mli
@@ -141,7 +141,6 @@ type constr_expr =
   | CRef of reference
   | CFix of loc * identifier located * fix_expr list
   | CCoFix of loc * identifier located * cofix_expr list
-  | CArrow of loc * constr_expr * constr_expr
   | CProdN of loc * (name located list * binder_kind * constr_expr) list * constr_expr
   | CLambdaN of loc * (name located list * binder_kind * constr_expr) list * constr_expr
   | CLetIn of loc * name located * constr_expr * constr_expr

parsing/ppconstr.ml
@@ -309,7 +309,6 @@ let rename na na' t c =
     | _ -> (na',t,c)
 
 let split_product na' = function
-  | CArrow (loc,t,c) -> (na',t,c)
   | CProdN (loc,[[na],bk,t],c) -> rename na na' t c
   | CProdN (loc,([na],bk,t)::bl,c) -> rename na na' t (CProdN(loc,bl,c))
   | CProdN (loc,(na::nal,bk,t)::bl,c) ->
@@ -428,10 +427,6 @@ let pr pr sep inherited a =
              pr_recursive
               (pr_cofixdecl (pr mt) (pr_dangling_with_for mt pr)) (snd id) cofix),
       lfix
-  | CArrow (_,a,b) ->
-      hov 0 (pr mt (larrow,L) a ++ str " ->" ++
-             pr (fun () ->brk(1,0)) (-larrow,E) b),
-      larrow
   | CProdN _ ->
       let (bl,a) = extract_prod_binders a in
       hov 0 (

parsing/pptactic.ml
@@ -296,10 +296,6 @@ let strip_prod_binders_expr n ty =
 	  let bll = List.map (fun (x, _, y) -> x, y) bll in
             if nb >= n then (List.rev (bll@acc)), a
             else strip_ty (bll@acc) (n-nb) a
-      | Topconstr.CArrow(_,a,b) ->
-          if n=1 then
-            (List.rev (([(dummy_loc,Anonymous)],a)::acc), b)
-          else strip_ty (([(dummy_loc,Anonymous)],a)::acc) (n-1) b
       | _ -> error "Cannot translate fix tactic: not enough products" in
   strip_ty [] n ty
 

parsing/q_coqast.ml4
@@ -135,8 +135,6 @@ let rec mlexpr_of_constr = function
   | Topconstr.CRef r -> <:expr< Topconstr.CRef $mlexpr_of_reference r$ >>
   | Topconstr.CFix (loc,_,_) -> failwith "mlexpr_of_constr: TODO"
   | Topconstr.CCoFix (loc,_,_) -> failwith "mlexpr_of_constr: TODO"
-  | Topconstr.CArrow (loc,a,b) ->
-  <:expr< Topconstr.CArrow $dloc$ $mlexpr_of_constr a$ $mlexpr_of_constr b$ >>
   | Topconstr.CProdN (loc,l,a) -> <:expr< Topconstr.CProdN $dloc$ $mlexpr_of_list
       (mlexpr_of_triple (mlexpr_of_list (mlexpr_of_pair (fun _ -> dloc) mlexpr_of_name)) mlexpr_of_binder_kind mlexpr_of_constr) l$ $mlexpr_of_constr a$ >>
   | Topconstr.CLambdaN (loc,l,a) -> <:expr< Topconstr.CLambdaN $dloc$ $mlexpr_of_list (mlexpr_of_triple (mlexpr_of_list (mlexpr_of_pair (fun _ -> dloc) mlexpr_of_name)) mlexpr_of_binder_kind mlexpr_of_constr) l$ $mlexpr_of_constr a$ >>

commit b789106117653fec8340ecbd88866c254fe1201d

Fixing a little parsing bug with level 90 introduced in 3e70ea9c.
Unfortunately, some manual synchronization is needed between the
constr parser and the table of constr/pattern levels.

We do this synchronization which was missing in the commit moving
"x -> y" to a user-level notation.

parsing/egramcoq.ml
@@ -34,6 +34,7 @@ let default_levels =
   [200,Extend.RightA,false;
    100,Extend.RightA,false;
    99,Extend.RightA,true;
+   90,Extend.RightA,true;
    10,Extend.RightA,false;
    9,Extend.RightA,false;
    8,Extend.RightA,true;
@@ -44,6 +45,7 @@ let default_pattern_levels =
   [200,Extend.RightA,true;
    100,Extend.RightA,false;
    99,Extend.RightA,true;
+   90,Extend.RightA,true;
    11,Extend.LeftA,false;
    10,Extend.RightA,false;
    1,Extend.LeftA,false;

parsing/g_constr.ml4
@@ -376,6 +376,7 @@ GEXTEND Gram
     | "100" RIGHTA
       [ p = pattern; "|"; pl = LIST1 pattern SEP "|" -> CAst.make ~loc:!@loc @@ CPatOr (p::pl) ]
     | "99" RIGHTA [ ]
+    | "90" RIGHTA [ ]
     | "11" LEFTA
       [ p = pattern; "as"; id = ident ->
         CAst.make ~loc:!@loc @@ CPatAlias (p, id) ]