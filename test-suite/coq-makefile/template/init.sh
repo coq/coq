4a9295baa83c1922d9defd601ed410d0a8241135
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2017-11-24 14:48:34 +0100

    coq_makefile tests: build in easily removed temporary subdirectory.
    This allows us to avoid doing git clean.

--- test-suite/coq-makefile/template/init.sh
+++ test-suite/coq-makefile/template/init.sh
@@ -4,16 +4,18 @@ set -o pipefail
 export PATH=$COQBIN:$PATH
 export LC_ALL=C
 
-rm -rf theories src Makefile Makefile.conf tmp
-git clean -dfx || true
+rm -rf _test
+mkdir _test
+find . -maxdepth 1 -not -name . -not -name _test -exec cp -r '{}' -t _test ';'
 
+cd _test
 mkdir -p src
 mkdir -p theories/sub
 
-cp ../template/theories/sub/testsub.v theories/sub
-cp ../template/theories/test.v theories
-cp ../template/src/test.ml4 src
-cp ../template/src/test_aux.mli src
-cp ../template/src/test.mli src
-cp ../template/src/test_plugin.mlpack src
-cp ../template/src/test_aux.ml src
+cp ../../template/theories/sub/testsub.v theories/sub
+cp ../../template/theories/test.v theories
+cp ../../template/src/test.ml4 src
+cp ../../template/src/test_aux.mli src
+cp ../../template/src/test.mli src
+cp ../../template/src/test_plugin.mlpack src
+cp ../../template/src/test_aux.ml src
