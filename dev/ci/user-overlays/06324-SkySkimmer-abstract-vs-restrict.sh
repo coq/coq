ea87cce3f81e9b73047c1695ea716162aeb09ede
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2017-12-06 14:39:00 +0100

    Overlay for Equations.

---
+++ dev/ci/user-overlays/06324-SkySkimmer-abstract-vs-restrict.sh
@@ -0,0 +1,4 @@
+if [ "$TRAVIS_PULL_REQUEST" = "6324" ] || [ "$TRAVIS_BRANCH" = "fix-6323-restrict+abstract" ]; then
+    Equations_CI_BRANCH=fix-coq-6324
+    Equations_CI_GITURL=https://github.com/SkySkimmer/Coq-Equations.git
+fi
