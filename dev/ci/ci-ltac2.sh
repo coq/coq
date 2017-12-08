2434e85e1f2006e66efd74bd6cd7224e301e3bff
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2017-12-08 10:50:09 +0100

    Fix a copy-paste error in ci-ltac2.

--- dev/ci/ci-ltac2.sh
+++ dev/ci/ci-ltac2.sh
@@ -3,7 +3,7 @@
 ci_dir="$(dirname "$0")"
 source ${ci_dir}/ci-common.sh
 
-ltac2_CI_DIR=${CI_BUILD_DIR}/coq-dpdgraph
+ltac2_CI_DIR=${CI_BUILD_DIR}/ltac2
 
 git_checkout ${ltac2_CI_BRANCH} ${ltac2_CI_GITURL} ${ltac2_CI_DIR}
 
