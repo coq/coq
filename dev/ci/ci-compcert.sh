5d4cf69a3d7d472b54b5decc8400164b87e9a73f
Emilio Jesus Gallego Arias <e+git@x80.org>
2017-12-10 18:21:38 +0100

    [ci] Temporal workaround for checker non-backwards compatible change.

--- dev/ci/ci-compcert.sh
+++ dev/ci/ci-compcert.sh
@@ -8,4 +8,5 @@ CompCert_CI_DIR=${CI_BUILD_DIR}/CompCert
 opam install -j ${NJOBS} -y menhir
 git_checkout ${CompCert_CI_BRANCH} ${CompCert_CI_GITURL} ${CompCert_CI_DIR}
 
-( cd ${CompCert_CI_DIR} && ./configure -ignore-coq-version x86_32-linux && make && make check-proof )
+#( cd ${CompCert_CI_DIR} && ./configure -ignore-coq-version x86_32-linux && make && make check-proof )
+( cd ${CompCert_CI_DIR} && ./configure -ignore-coq-version x86_32-linux && make )
