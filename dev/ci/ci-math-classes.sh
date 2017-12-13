05b62ec91f3eb868031843c8823d817c2af1a064
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2017-12-13 15:13:17 +0100

    Put bignums, math-classes and corn dependencies in Makefile
    (cherry picked from commit f10d9fefdaf694253495c1ecf2e57ce5099b9375)

--- dev/ci/ci-math-classes.sh
+++ dev/ci/ci-math-classes.sh
@@ -5,20 +5,6 @@ source ${ci_dir}/ci-common.sh
 
 math_classes_CI_DIR=${CI_BUILD_DIR}/math-classes
 
-Corn_CI_DIR=${CI_BUILD_DIR}/corn
-
-# Setup Bignums
-
-source ${ci_dir}/ci-bignums.sh
-
-# Setup Math-Classes
-
 git_checkout ${math_classes_CI_BRANCH} ${math_classes_CI_GITURL} ${math_classes_CI_DIR}
 
 ( cd ${math_classes_CI_DIR} && make && make install )
-
-# Setup Corn
-
-git_checkout ${Corn_CI_BRANCH} ${Corn_CI_GITURL} ${Corn_CI_DIR}
-
-( cd ${Corn_CI_DIR} && make )
