391bb5e196901a3a9426295125b8d1c700ab6992
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2017-12-15 11:23:11 +0100

    Update Coq version number.

--- configure.ml
+++ configure.ml
@@ -11,8 +11,8 @@
 #load "str.cma"
 open Printf
 
-let coq_version = "8.7.0"
-let coq_macos_version = "8.7.0" (** "[...] should be a string comprised of
+let coq_version = "8.7.1"
+let coq_macos_version = "8.7.1" (** "[...] should be a string comprised of
 three non-negative, period-separated integers [...]" *)
 let vo_magic = 8700
 let state_magic = 58700
