4a72f3bfe23edaa87307449b6033e7a296a93b04
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2017-12-21 00:35:36 +0100

    Adding a custom Travis overlay for HoTT.

---
+++ dev/ci/user-overlays/06482-ppedrot-check-poly-effects.sh
@@ -0,0 +1,4 @@
+if [ "$TRAVIS_PULL_REQUEST" = "6483" ] || [ "$TRAVIS_BRANCH" = "check-poly-effects" ]; then
+    HoTT_CI_BRANCH=check-poly-effects
+    HoTT_CI_GITURL=https://github.com/ppedrot/HoTT.git
+fi
