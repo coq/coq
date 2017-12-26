b9f72bf53c831bd93d28760eefdab9ad8ffdac03
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2017-12-26 12:51:27 +0100

    Delete old overlays (leaving example)

--- dev/ci/user-overlays/06392-ejgallego-econstr+fix_class.sh
+++
@@ -1,4 +0,0 @@
-if [ "$TRAVIS_PULL_REQUEST" = "6392" ] || [ "$TRAVIS_BRANCH" = "econstr+fix_class" ]; then
-    Equations_CI_BRANCH=econstr+fix_class
-    Equations_CI_GITURL=https://github.com/ejgallego/Coq-Equations.git
-fi
