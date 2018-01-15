1856d60057ac9096c791d71d4282c0cdfef85913
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2018-01-15 13:54:01 +0100

    More tests on brackets with goal selectors (including failures).

--- test-suite/success/BracketsWithGoalSelector.v
+++ test-suite/success/BracketsWithGoalSelector.v
@@ -4,7 +4,13 @@ Proof.
   2: {
     left.
     exact HA.
+    Fail right. (* No such goal. Try unfocusing with "}". *)
   }
-  right.
-  exact HB.
+  Fail 2: { (* Non-existent goal. *)
+    idtac. (* The idtac is to get a dot, so that IDEs know to stop there. *)
+  1:{ (* Syntactic test: no space before bracket. *)
+    right.
+    exact HB.
+Fail Qed.
+  }
 Qed.
