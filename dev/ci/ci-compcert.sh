fface0eef97f9a9d9c4dbf193598909b0a62e6ca
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2018-01-23 15:44:07 +0100

    Put default value for NJOBS in ci-common.

--- dev/ci/ci-compcert.sh
+++ dev/ci/ci-compcert.sh
@@ -5,7 +5,7 @@ source ${ci_dir}/ci-common.sh
 
 CompCert_CI_DIR=${CI_BUILD_DIR}/CompCert
 
-opam install -j ${NJOBS} -y menhir
+opam install -j "$NJOBS" -y menhir
 git_checkout ${CompCert_CI_BRANCH} ${CompCert_CI_GITURL} ${CompCert_CI_DIR}
 
 ( cd ${CompCert_CI_DIR} && ./configure -ignore-coq-version x86_32-linux && make && make check-proof )
