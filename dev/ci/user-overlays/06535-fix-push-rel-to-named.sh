aa0418ded1084794354a2e34c409c7bbca9fe091
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2018-01-30 11:08:53 +0100

    Adding an overlay for Equations.

---
+++ dev/ci/user-overlays/06535-fix-push-rel-to-named.sh
@@ -0,0 +1,4 @@
+if [ "$CI_PULL_REQUEST" = "6535" ] || [ "$CI_BRANCH" = "fix-push-rel-to-named" ]; then
+    Equations_CI_BRANCH=fix-6535
+    Equations_CI_GITURL=https://github.com/ppedrot/Coq-Equations
+fi
