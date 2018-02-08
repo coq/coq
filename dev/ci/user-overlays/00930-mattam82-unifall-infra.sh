76b072743e74cb80af9da7fd1ca78d89c5f1b6f6
Matthieu Sozeau <matthieu.sozeau@inria.fr>
2018-02-08 16:36:53 +0100

    Add overlay for Equations

---
+++ dev/ci/user-overlays/00930-mattam82-unifall-infra.sh
@@ -0,0 +1,4 @@
+if [ "$CI_PULL_REQUEST" = "930" ] || [ "$CI_BRANCH" = "unifall-infra" ]; then
+    Equations_CI_BRANCH=coqpr930
+    Equations_CI_GITURL=https://github.com/mattam82/Coq-Equations.git
+fi
