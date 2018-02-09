999a7059840bdfbdc46ce198e1a16b8621f93588
Ralf Jung <post@ralfj.de>
2018-02-09 10:16:55 +0100

    disable tests: vio2vo is broken in Windows

--- test-suite/coq-makefile/quick2vo/run.sh
+++ test-suite/coq-makefile/quick2vo/run.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
+a=`uname`
 
 . ../template/init.sh
 
 coq_makefile -f _CoqProject -o Makefile
-cat Makefile.conf
-make quick2vo J=2
-test -f theories/test.vo
-make validate
+# vio2vo is broken on Windows (#6720)
+if [ "$a" = "Darwin" -o "$a" = "Linux" ]; then
+    make quick2vo J=2
+    test -f theories/test.vo
+    make validate
+fi
