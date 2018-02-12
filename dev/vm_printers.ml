1c01b93f0f4ec4f2c5e1219e6dff0c64044166c4
Maxime Dénès <mail@maximedenes.fr>
2018-02-12 09:54:57 +0100

    Add test case for #6677.
    (cherry picked from commit 07e861c1792fcc3bde091640ee5e42b398cfa6da)

--- dev/vm_printers.ml
+++ dev/vm_printers.ml
@@ -61,7 +61,7 @@ and ppstack s =
 and ppatom a =
   match a with
   | Aid idk -> print_idkey idk
-  | Atype (u,l) -> print_string "Type(...)"
+  | Atype u -> print_string "Type(...)"
   | Aind(sp,i) ->  print_string "Ind(";
       print_string (string_of_mind sp);
       print_string ","; print_int i;
