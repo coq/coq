1dcf5516ab772a6d5325d42c2ceaf727fe88bb86
Enrico Tassi <Enrico.Tassi@inria.fr>
2018-02-14 17:09:45 +0100

    ci: add elpi

---
+++ dev/ci/ci-elpi.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+ci_dir="$(dirname "$0")"
+source ${ci_dir}/ci-common.sh
+
+Elpi_CI_DIR=${CI_BUILD_DIR}/elpi
+
+git_checkout ${Elpi_CI_BRANCH} ${Elpi_CI_GITURL} ${Elpi_CI_DIR}
+
+( cd ${Elpi_CI_DIR} && make && make install )
