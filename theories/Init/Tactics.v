ccb7bd2948e9bd84997f3461257b2ce1c7ad3e6a
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2018-02-23 11:14:54 +0100

    Added tacticals assert_succeeds/assert_fails (courtesy of Jason Gross).

--- theories/Init/Tactics.v
+++ theories/Init/Tactics.v
@@ -313,3 +313,14 @@ Ltac time_constr tac :=
   let ret := tac () in
   let eval_early := match goal with _ => finish_timing ( "Tactic evaluation" ) end in
   ret.
+
+(** Useful combinators *)
+
+Ltac assert_fails tac :=
+  tryif tac then fail 0 tac "succeeds" else idtac.
+Ltac assert_succeeds tac :=
+  tryif (assert_fails tac) then fail 0 tac "fails" else idtac.
+Tactic Notation "assert_succeeds" tactic3(tac) :=
+  assert_succeeds tac.
+Tactic Notation "assert_fails" tactic3(tac) :=
+  assert_fails tac.
