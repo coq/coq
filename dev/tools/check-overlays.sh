d4def32456c0fadb7fc9814af7e7b2b21a37f0a6
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2018-03-09 13:59:17 +0100

    pre-commit: verify user overlay extensions (must be .sh).
    This has come up a couple times.

---
+++ dev/tools/check-overlays.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+for f in dev/ci/user-overlays/*
+do
+    if ! ([[ $f = dev/ci/user-overlays/README.md ]] || [[ $f == *.sh ]])
+    then
+        >&2 echo "Bad overlay '$f'."
+        >&2 echo "User overlays need to have extension .sh to be picked up!"
+        exit 1
+    fi
+done
