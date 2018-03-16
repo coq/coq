87d0b11b2fc0c64a171b05c9675f4c2f0a78a22b
Maxime Dénès <mail@maximedenes.fr>
2018-03-16 16:38:47 +0100

    Bump version number to 8.8+beta1
    I also fixed the macOS version string which was not following the
    convention.

--- configure.ml
+++ configure.ml
@@ -11,8 +11,8 @@
 #load "str.cma"
 open Printf
 
-let coq_version = "8.8+alpha"
-let coq_macos_version = "8.7.90" (** "[...] should be a string comprised of
+let coq_version = "8.8+beta1"
+let coq_macos_version = "8.7.91" (** "[...] should be a string comprised of
 three non-negative, period-separated integers [...]" *)
 let vo_magic = 8791
 let state_magic = 58791
