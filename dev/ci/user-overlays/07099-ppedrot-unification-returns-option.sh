ac962d957562e89a7786e7fcd868008c9ffa09c1
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2018-04-02 19:11:58 +0200

    Adding an overlay for the Equations plugin.

---
+++ dev/ci/user-overlays/07099-ppedrot-unification-returns-option.sh
@@ -0,0 +1,4 @@
+if [ "$CI_PULL_REQUEST" = "7099" ] || [ "$CI_BRANCH" = "unification-returns-option" ]; then
+    Equations_CI_BRANCH=unification-returns-option
+    Equations_CI_GITURL=https://github.com/ppedrot/Coq-Equations
+fi
