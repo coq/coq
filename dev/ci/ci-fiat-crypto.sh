e81e281754d7f8c76b4e7767edcbcbc5b41afd6f
Jason Gross <jgross@mit.edu>
2018-04-20 16:17:36 -0400

    [ci] Also make some display targets for fiat-crypto
    This will catch things like
    https://github.com/coq/coq/pull/7025#issuecomment-381424489

--- dev/ci/ci-fiat-crypto.sh
+++ dev/ci/ci-fiat-crypto.sh
@@ -8,4 +8,4 @@ fiat_crypto_CI_DIR="${CI_BUILD_DIR}/fiat-crypto"
 git_checkout "${fiat_crypto_CI_BRANCH}" "${fiat_crypto_CI_GITURL}" "${fiat_crypto_CI_DIR}"
 ( cd "${fiat_crypto_CI_DIR}" && git submodule update --init --recursive )
 
-( cd "${fiat_crypto_CI_DIR}" && make lite )
+( cd "${fiat_crypto_CI_DIR}" && make lite lite-display )
