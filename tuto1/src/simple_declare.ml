93273e29708ff5c24024bde0e4b9955cb94a0dc0
Yves Bertot <Yves.Bertot@inria.fr>
2018-05-03 22:57:55 +0200

    little cleanup on the defining command, and question in comments

--- tuto1/src/simple_declare.ml
+++ tuto1/src/simple_declare.ml
@@ -4,14 +4,16 @@ let packed_declare_definition ident
     let evalue, st = value_with_constraints in
     let evd = Evd.from_ctx st in
     let value = EConstr.to_constr evd evalue in
+(* Question to developers: could the next 3 lines be removed:
+   similar stuff present in comDefinition and elpi, but not in Mtac2 *)
     let evd = Evd.minimize_universes evd in
     let used = Univops.universes_of_constr (Global.env()) value in
-    let norm_used = Evd.restrict_universe_context evd used in
+    let evd = Evd.restrict_universe_context evd used in
     let decl = Univdecls.default_univ_decl in
-    let uctx = Evd.check_univ_decl ~poly:true norm_used decl in
+    let uctx = Evd.check_univ_decl ~poly:true evd decl in
     let ce = Declare.definition_entry ~univs:uctx value in
     let _  =
-      Pretyping.check_evars_are_solved (Global.env ()) norm_used Evd.empty in
+      Pretyping.check_evars_are_solved (Global.env ()) evd Evd.empty in
     let k = (Decl_kinds.Global,
              true (* polymorphic *), Decl_kinds.Definition) in
     let binders = Evd.universe_binders evd in
@@ -19,3 +21,5 @@ let packed_declare_definition ident
     let hook = Lemmas.mk_hook (fun _ x -> x) in
     ignore(DeclareDef.declare_definition ident k ce binders implicits hook)
   end
+
+(* But this definition cannot be undone by Reset ident *)
