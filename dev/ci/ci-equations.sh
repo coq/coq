8ad05c47ab6720596aba248845f3db1fd1511fd7
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2018-05-17 21:40:46 +0200

    Backport all recent changes in CI to v8.8.
    We use a different Docker image because the dependencies have changed
    slightly in master compared to what was the case in v8.8.

--- dev/ci/ci-equations.sh
+++ dev/ci/ci-equations.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 ci_dir="$(dirname "$0")"
-source ${ci_dir}/ci-common.sh
+. "${ci_dir}/ci-common.sh"
 
-Equations_CI_DIR=${CI_BUILD_DIR}/Equations
+Equations_CI_DIR="${CI_BUILD_DIR}/Equations"
 
-git_checkout ${Equations_CI_BRANCH} ${Equations_CI_GITURL} ${Equations_CI_DIR}
+git_checkout "${Equations_CI_BRANCH}" "${Equations_CI_GITURL}" "${Equations_CI_DIR}"
 
-( cd ${Equations_CI_DIR} && coq_makefile -f _CoqProject -o Makefile && make && make test-suite && make examples && make install)
+( cd "${Equations_CI_DIR}" && coq_makefile -f _CoqProject -o Makefile && make && make test-suite && make examples && make install)
