bffdf86298c451f83c3e4a359ca710f8107b24d7
Emilio Jesus Gallego Arias <e+git@x80.org>
2018-06-11 13:38:37 +0200

    [build] Fix checks and notes noting 4.02.1 instead of 4.02.3
    Bumping to 4.02.3 was decided some time ago in the WG, however a
    couple of places escaped updating.
    
    (cherry picked from commit ec741e8bb1bdf69f0be4bb6b594560af57f8811c)

--- configure.ml
+++ configure.ml
@@ -593,14 +593,14 @@ let caml_version_nums =
          "Is it installed properly?")
 
 let check_caml_version () =
-  if caml_version_nums >= [4;2;1] then
+  if caml_version_nums >= [4;2;3] then
     cprintf "You have OCaml %s. Good!" caml_version
   else
     let () = cprintf "Your version of OCaml is %s." caml_version in
     if !prefs.force_caml_version then
       cprintf "*Warning* Your version of OCaml is outdated."
     else
-      die "You need OCaml 4.02.1 or later."
+      die "You need OCaml 4.02.3 or later."
 
 let _ = check_caml_version ()
 
