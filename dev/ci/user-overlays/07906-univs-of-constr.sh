ffdf5c256cbe4be1743e17eb17d516fe987e3b8d
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2018-06-29 21:08:33 +0200

    Document that GITURL variables shouldn't have a trailing .git anymore.
    This allows to append /archive at the end.

--- dev/ci/user-overlays/07906-univs-of-constr.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ "$CI_PULL_REQUEST" = "7906" ] || [ "$CI_BRANCH" = "univs-of-constr-noseff" ]; then
-    Equations_CI_BRANCH=fix-univs-of-constr
-    Equations_CI_GITURL=https://github.com/SkySkimmer/Coq-Equations.git
-
-    Elpi_CI_BRANCH=fix-universes-of-constr
-    Elpi_CI_GITURL=https://github.com/SkySkimmer/coq-elpi.git
-fi
--- dev/ci/user-overlays/7080-herbelin-master+swapping-modules-constr-context.sh
+++
@@ -1,7 +0,0 @@
-if [ "$CI_PULL_REQUEST" = "7080" ] || [ "$CI_BRANCH" = "master+swapping-modules-constr-context" ]; then
-    Equations_CI_BRANCH=master+adapting-coq-pr7080
-    Equations_CI_GITURL=https://github.com/herbelin/Coq-Equations.git
-
-    Elpi_CI_BRANCH=coq-master+adapting-coq-pr7080
-    Elpi_CI_GITURL=https://github.com/herbelin/coq-elpi.git
-fi
