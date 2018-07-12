2856c290f09092dbf4a4f11c9aaf219360bdc1e4
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2018-07-12 16:30:23 +0200

    Clean-up user-overlays folder.

--- dev/ci/user-overlays/07820-mattam82-hints-constants.sh
+++
@@ -1,6 +0,0 @@
-_OVERLAY_BRANCH=hints-variables-overlay
-
-if [ "$CI_PULL_REQUEST" = "7820" ] || [ "$CI_BRANCH" = "_OVERLAY_BRANCH" ]; then
-
-    Equations_CI_BRANCH="$_OVERLAY_BRANCH"
-fi
--- dev/ci/user-overlays/07902-ppedrot-camlp5-parser.sh
+++
@@ -1,8 +0,0 @@
-_OVERLAY_BRANCH=camlp5-parser
-
-if [ "$CI_PULL_REQUEST" = "7902" ] || [ "$CI_BRANCH" = "$_OVERLAY_BRANCH" ]; then
-
-    ltac2_CI_BRANCH="$_OVERLAY_BRANCH"
-    ltac2_CI_GITURL=https://github.com/ppedrot/ltac2
-
-fi
