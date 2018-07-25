28a8ea1bc466dc44e233d2674a29bbb63badae4b
Maxime Dénès <mail@maximedenes.fr>
2018-07-25 17:59:47 +0200

    Update overlay

--- dev/ci/user-overlays/07878-omega-lia.sh
+++ dev/ci/user-overlays/07878-omega-lia.sh
@@ -8,4 +8,13 @@ if [ "$CI_PULL_REQUEST" = "7878" ] || [ "$CI_BRANCH" = "_OVERLAY_BRANCH" ]; then
     bignums_CI_BRANCH="$_OVERLAY_BRANCH"
     bignums_CI_GITURL=https://github.com/maximedenes/bignums.git
 
+    bedrock2_CI_BRANCH="$_OVERLAY_BRANCH"
+    bedrock2_CI_GITURL=https://github.com/maximedenes/bedrock2.git
+
+    CoLoR_CI_BRANCH="$_OVERLAY_BRANCH"
+    CoLoR_CI_GITURL=https://github.com/maximedenes/color.git
+
+    Flocq_CI_BRANCH="$_OVERLAY_BRANCH"
+    Flocq_CI_GITURL=https://gitlab.inria.fr/mdenes/flocq.git
+
 fi
