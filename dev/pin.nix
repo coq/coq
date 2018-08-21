2f864bac7efed257eb8c3e6b151ca64c14c59221
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2018-08-21 17:50:22 +0200

    Cachix can now be installed from nixpkgs.
    So we use the same version of nixpkgs to install Cachix and the dependencies of Coq.

---
+++ dev/pin.nix
@@ -0,0 +1,4 @@
+import (fetchTarball {
+  url = "https://github.com/NixOS/nixpkgs/tarball/87ae21cd122d2d74ea4ea55d4db6e2166d126ab6";
+  sha256 = "1vm5m97vqrp98ggnvrwbbbbmx3clb7riiigmmnb3r699x11n17za";
+}) {}
