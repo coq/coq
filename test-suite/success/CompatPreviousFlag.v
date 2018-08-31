68d153dcb516f55475a7b861835203446fdab85f
Jason Gross <jgross@mit.edu>
2018-08-31 17:36:39 -0400

    Make -compat 8.8 import Coq.Compat.Coq88
    Fixes #8311
    
    (cherry picked from commit 81ed24a15d78415e9524fd81323e728bae0450f5)

---
+++ test-suite/success/CompatPreviousFlag.v
@@ -0,0 +1,4 @@
+(* -*- coq-prog-args: ("-compat" "8.7") -*- *)
+(** Check that the current-minus-one compatibility flag actually requires the relevant modules. *)
+Import Coq.Compat.Coq88.
+Import Coq.Compat.Coq87.
