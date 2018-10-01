letouzey/coq-wip

Notation: remove support for prim tokens denoting inductive types in "return"
 This is prim token notations for inductive *types*, not values.
 So we're speaking of a scope where 0 is the type nat, 1 is the type bool, etc.

 To my knowledge, this feature hasn't ever been used, and is very unlikely
 to be used ever, so let's clean the code a bit by removing it.

interp/constrextern.ml
@@ -508,15 +508,7 @@ let extern_ind_pattern_in_scope (scopes:local_scopes) vars ind args =
   else
     try
       if !Flags.raw_print || !print_no_symbol then raise No_match;
-      let (sc,p) = uninterp_prim_token_ind_pattern ind args in
-      match availability_of_prim_token p sc scopes with
-	| None -> raise No_match
-	| Some key ->
-	  insert_pat_delimiters (CAst.make @@ CPatPrim p) key
-    with No_match ->
-      try
-	if !Flags.raw_print || !print_no_symbol then raise No_match;
-	extern_notation_ind_pattern scopes vars ind args
+      extern_notation_ind_pattern scopes vars ind args
           (uninterp_ind_pattern_notations ind)
     with No_match ->
       let c = extern_reference vars (IndRef ind) in

coq/coq

(cherry picked from commit 3099be05553dab10b41d864f4981860eb105f145)

interp/constrextern.ml
@@ -531,18 +531,7 @@ let extern_ind_pattern_in_scope (custom,scopes as allscopes) vars ind args =
   else
     try
       if !Flags.raw_print || !print_no_symbol then raise No_match;
-      let (sc,p) = uninterp_prim_token_ind_pattern ind args in
-      match availability_of_entry_coercion custom InConstrEntrySomeLevel with
-        | None -> raise No_match
-        | Some coercion ->
-      match availability_of_prim_token p sc scopes with
-	| None -> raise No_match
-	| Some key ->
-          insert_pat_coercion coercion (insert_pat_delimiters (CAst.make @@ CPatPrim p) key)
-    with No_match ->
-      try
-	if !Flags.raw_print || !print_no_symbol then raise No_match;
-        extern_notation_ind_pattern allscopes vars ind args
+      extern_notation_ind_pattern allscopes vars ind args
           (uninterp_ind_pattern_notations ind)
     with No_match ->
       let c = extern_reference vars (IndRef ind) in