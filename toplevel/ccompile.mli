90f15edf585f533cc47da545cd1fff011aca821f
Emilio Jesus Gallego Arias <e+git@x80.org>
2018-10-09 20:27:32 +0200

    [toplevel] Move compilation-related functions to their own module.
    We move compilation-specific functions to their own module.
    
    This helps isolating `.vo` compile-time functionality from
    interactive, toplevel-like processing.
    
    cc: #8683

---
+++ toplevel/ccompile.mli
@@ -0,0 +1,19 @@
+(************************************************************************)
+(*         *   The Coq Proof Assistant / The Coq Development Team       *)
+(*  v      *   INRIA, CNRS and contributors - Copyright 1999-2018       *)
+(* <O___,, *       (see CREDITS file for the list of authors)           *)
+(*   \VV/  **************************************************************)
+(*    //   *    This file is distributed under the terms of the         *)
+(*         *     GNU Lesser General Public License Version 2.1          *)
+(*         *     (see LICENSE file for the text of the license)         *)
+(************************************************************************)
+
+(** [load_init_vernaculars opts ~state] Load vernaculars from
+   the init (rc) file *)
+val load_init_vernaculars : Coqargs.coq_cmdopts -> state:Vernac.State.t-> Vernac.State.t
+
+(** [compile_files opts] compile files specified in [opts] *)
+val compile_files : Coqargs.coq_cmdopts -> unit
+
+(** [do_vio opts] process [.vio] files in [opts] *)
+val do_vio : Coqargs.coq_cmdopts -> unit
