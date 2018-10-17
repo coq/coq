a836f1b6f6877946393a988c49a82732d8995ee3
llee454@gmail.com <llee454@gmail.com>
2018-10-17 16:51:29 -0400

    Revised the comments and definitions for the Exists_impl and Forall_inv_tail theorems following code review comments by SkySimmer.

--- theories/Lists/List.v
+++ theories/Lists/List.v
@@ -2251,19 +2251,15 @@ Section Exists_Forall.
   End One_predicate.
 
   (*
-    Accepts a predicate, [P], and a list, [x0 ::
-    xs], and proves that if [P] is true for every
-    element in [x0 :: xs], then [P] is true for
-    every element in [xs].
+    Proves that if [P] is true for every element
+    in [x0 :: xs], then [P] is true for every
+    element in [xs].
   *)
-  Local Definition Forall_inv_tail
+  Definition Forall_inv_tail
     :  forall (P : A -> Prop) (x0 : A) (xs : list A), Forall P (x0 :: xs) -> Forall P xs
     := fun P x0 xs H
-         => let H0
-              :  forall x, In x (x0 :: xs) -> P x
-              := proj1 (Forall_forall P (x0 :: xs)) H in
+         => let H0 := proj1 (Forall_forall P (x0 :: xs)) H in
             let H1
-              :  forall x, In x xs -> P x
               := fun x H2
                    => H0 x (or_intror (x0 = x) H2) in
             proj2 (Forall_forall P xs) H1.
@@ -2275,7 +2271,7 @@ Section Exists_Forall.
     [P] is true, then there exists an element in
     [xs] for which [Q] is true.
   *)
-  Local Definition Exists_impl
+  Definition Exists_impl
     :  forall (P Q : A -> Prop),
        (forall x : A, P x -> Q x) ->
        forall xs : list A,
@@ -2287,15 +2283,15 @@ Section Exists_Forall.
               := proj1 (Exists_exists P xs) H0 in
             let H2
               :  exists x, In x xs /\ Q x
-              := ex_ind
-                   (fun x H2
+              := match H1 with
+                   | ex_intro _ x H2
                      => ex_intro
                           (fun x => In x xs /\ Q x)
                           x
                           (conj
                             (proj1 H2)
-                            (H x (proj2 H2))))
-                   H1 in
+                            (H x (proj2 H2)))
+                   end in
             (proj2 (Exists_exists Q xs)) H2.
 
   Lemma Forall_Exists_neg (P:A->Prop)(l:list A) :
