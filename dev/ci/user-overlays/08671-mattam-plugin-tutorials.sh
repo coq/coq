bc238a835ab705d97b37fd74441caaedc639a1f7
Matthieu Sozeau <matthieu.sozeau@inria.fr>
2018-10-17 12:57:18 +0200

    Fix overlay for this extension of the PR. To be removed.

--- dev/ci/user-overlays/08671-mattam-plugin-tutorials.sh
+++ dev/ci/user-overlays/08671-mattam-plugin-tutorials.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ "$CI_PULL_REQUEST" = "8671" ] || [ "$CI_BRANCH" = "evar_info-flags" ]; then
+if [ "$CI_PULL_REQUEST" = "8741" ] || [ "$CI_BRANCH" = "typeclasses-functional-evar_map" ]; then
     plugin_tutorial_CI_REF=pr8671-fix
     plugin_tutorial_CI_GITURL=https://github.com/mattam82/plugin_tutorials
 
