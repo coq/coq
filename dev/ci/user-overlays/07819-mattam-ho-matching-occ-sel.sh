62cf1ad380772628ed6e547de3fb875275610b4d
Matthieu Sozeau <matthieu.sozeau@inria.fr>
2018-10-18 16:56:17 +0200

    Add overlay for Equations

--- dev/ci/user-overlays/07819-mattam-ho-matching-occ-sel.sh
+++ dev/ci/user-overlays/07819-mattam-ho-matching-occ-sel.sh
@@ -7,4 +7,6 @@ if [ "$CI_PULL_REQUEST" = "7819" ] || [ "$CI_BRANCH" = "$_OVERLAY_BRANCH" ]; the
     mtac2_CI_REF="PR7819-overlay"
     mtac2_CI_GITURL=https://github.com/mattam82/Mtac2
 
+    Equations_CI_REF="PR7819-overlay"
+
 fi
