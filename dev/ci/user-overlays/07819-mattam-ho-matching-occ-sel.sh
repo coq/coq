2265d20c48dff6e1ea9a5cb9be9640603f3be3cf
Matthieu Sozeau <matthieu.sozeau@inria.fr>
2018-10-18 14:46:37 +0200

    Add overlays for unicoq and mtac2

---
+++ dev/ci/user-overlays/07819-mattam-ho-matching-occ-sel.sh
@@ -0,0 +1,10 @@
+_OVERLAY_BRANCH=ho-matching-occ-sel
+
+if [ "$CI_PULL_REQUEST" = "7819" ] || [ "$CI_BRANCH" = "$_OVERLAY_BRANCH" ]; then
+
+    unicoq_CI_REF="PR7819-overlay"
+
+    mtac2_CI_REF="PR7819-overlay"
+    mtac2_CI_GITURL=https://github.com/mattam82/Mtac2
+
+fi
