4db7296d629f08c5a71905d8f6d29e2c4e4c0517
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2018-11-12 12:58:44 +0100

    Add test for #8951.
    Close #8951.

---
+++ test-suite/bugs/closed/bug_8951.v
@@ -0,0 +1,14 @@
+Module Type T.
+  Polymorphic Parameter Inline t@{i} : Type@{i}.
+End T.
+
+Module M.
+  Polymorphic Definition t@{i} := nat.
+End M.
+
+Module Make (X:T).
+  Include X.
+
+End Make.
+
+Module P := Make M.
