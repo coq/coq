c9e177577ebabbc6cf61bada77a0d36d19570544
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2018-11-16 16:54:02 +0100

    Print logs in appveyor test suite run
    It seems we forgot to export when moving the script to a separate file.

--- test-suite/report.sh
+++ test-suite/report.sh
@@ -24,7 +24,7 @@ cp summary.log "$SAVEDIR"/
 rm "$FAILED"
 
 # print info
-if [ -n "$TRAVIS" ] || [ -n "$PRINT_LOGS" ]; then
+if [ -n "$TRAVIS" ] || [ -n "$APPVEYOR" ] || [ -n "$PRINT_LOGS" ]; then
     find logs/ -name '*.log' -not -name 'summary.log' -print0 | while IFS= read -r -d '' file; do
         if [ -n "$TRAVIS" ]; then
             # ${foo////.} replaces every / by . in $foo
@@ -40,12 +40,13 @@ if [ -n "$TRAVIS" ] || [ -n "$PRINT_LOGS" ]; then
         else printf '\n'
         fi
     done
+    printed_logs=1
 fi
 
 if grep -q -F 'Error!' summary.log ; then
     echo FAILURES;
     grep -F 'Error!' summary.log;
-    if [ -z "$TRAVIS" ] && [ -z "$PRINT_LOGS" ]; then
+    if [ -z "$printed_logs" ]; then
         echo 'To print details of failed tests, rerun with environment variable PRINT_LOGS=1'
         echo 'eg "make report PRINT_LOGS=1" from the test suite directory"'
         echo 'See README.md in the test suite directory for more information.'
