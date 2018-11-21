51ce6bf10be48a479d80978d6c1a517336aa3458
Vincent Laporte <Vincent.Laporte@fondation-inria.fr>
2018-11-21 11:30:55 +0000

    [nix-ci] Use master version of Unicoq

--- dev/ci/nix/unicoq.nix
+++ dev/ci/nix/unicoq.nix
@@ -1,11 +1,8 @@
-{ stdenv, fetchzip, coq }:
+{ stdenv, coq }:
 
 stdenv.mkDerivation {
   name = "coq${coq.coq-version}-unicoq-0.0-git";
-  src = fetchzip {
-    url = "https://github.com/vbgl/unicoq/archive/8b33e37700e92bfd404bf8bf9fe03f1be8928d97.tar.gz";
-    sha256 = "0s4z0wjxlp56ccgzxgk04z7skw90rdnz39v730ffkgrjl38rr9il";
-  };
+  src = fetchTarball https://github.com/unicoq/unicoq/archive/master.tar.gz;
 
   buildInputs = [ coq ] ++ (with coq.ocamlPackages; [ ocaml findlib camlp5 num ]);
 
