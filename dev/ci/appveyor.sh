e6c98655e3c98ea6b9fed5e906edf860da097707
Emilio Jesus Gallego Arias <e+git@x80.org>
2018-11-22 16:09:46 +0100

    [ci] [appveyor] Move Appveyor to OPAM 2.
    We update the Appveyor configuration so it uses OPAM 2.0, and thus it
    can install newer packages.

--- dev/ci/appveyor.sh
+++ dev/ci/appveyor.sh
@@ -2,14 +2,15 @@
 
 set -e -x
 
-APPVEYOR_OPAM_SWITCH=4.07.0+mingw64c
+APPVEYOR_OPAM_VARIANT=ocaml-variants.4.07.1+mingw64c
 
-wget https://github.com/fdopen/opam-repository-mingw/releases/download/0.0.0.1/opam64.tar.xz
+wget https://github.com/fdopen/opam-repository-mingw/releases/download/0.0.0.2/opam64.tar.xz -O opam64.tar.xz
 tar -xf opam64.tar.xz
 bash opam64/install.sh
 
-opam init -a mingw https://github.com/fdopen/opam-repository-mingw.git --comp $APPVEYOR_OPAM_SWITCH --switch $APPVEYOR_OPAM_SWITCH
-eval "$(opam config env)"
+opam init default -a -y "https://github.com/fdopen/opam-repository-mingw.git#opam2" -c $APPVEYOR_OPAM_VARIANT --disable-sandboxing
+eval "$(opam env)"
 opam install -y num ocamlfind ounit
 
+# Full regular Coq Build
 cd "$APPVEYOR_BUILD_FOLDER" && ./configure -local && make && make byte && make -C test-suite all INTERACTIVE= # && make validate
