6e34168a3513ace5beda5b8bd32ea85aecf0b15a
Maxime Dénès <maxime.denes@fondation-inria.fr>
2018-12-13 16:41:55 +0100

    Add overlay

---
+++ dev/ci/user-overlays/09220-maximedenes-stm-shallow-logic.sh
@@ -0,0 +1,6 @@
+if [ "$CI_PULL_REQUEST" = "9220" ] || [ "$CI_BRANCH" = "stm-shallow-logic" ]; then
+
+    paramcoq_CI_REF=stm-shallow-logic
+    paramcoq_CI_GITURL=https://github.com/maximedenes/paramcoq
+
+fi
