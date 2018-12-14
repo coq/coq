b8151738e219ec54c730b10da411e61635385ffb
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2018-12-14 17:16:17 +0100

    Bump version number to 8.9.

--- configure.ml
+++ configure.ml
@@ -11,11 +11,11 @@
 #load "str.cma"
 open Printf
 
-let coq_version = "8.9+beta1"
-let coq_macos_version = "8.8.91" (** "[...] should be a string comprised of
+let coq_version = "8.9.0"
+let coq_macos_version = "8.9.0" (** "[...] should be a string comprised of
 three non-negative, period-separated integers [...]" *)
-let vo_magic = 8891
-let state_magic = 58891
+let vo_magic = 8900
+let state_magic = 58900
 let distributed_exec =
   ["coqtop.opt"; "coqidetop.opt"; "coqqueryworker.opt"; "coqproofworker.opt"; "coqtacticworker.opt";
    "coqc";"coqchk";"coqdoc";"coqworkmgr";"coq_makefile";"coq-tex";"coqwc";"csdpcert";"coqdep"]
