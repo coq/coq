506eccce8f455b94a6f0862cd7f665846425e8d2
Maxime Dénès <maxime.denes@inria.fr>
2019-01-17 12:38:41 +0100

    Add Overlays

---
+++ dev/ci/user-overlays/09263-maximedenes-parsing-state.sh
@@ -0,0 +1,12 @@
+if [ "$CI_PULL_REQUEST" = "9263" ] || [ "$CI_BRANCH" = "parsing-state" ]; then
+
+    mtac2_CI_REF=proof-mode
+    mtac2_CI_GITURL=https://github.com/maximedenes/Mtac2
+
+    ltac2_CI_REF=proof-mode
+    ltac2_CI_GITURL=https://github.com/maximedenes/ltac2
+
+    equations_CI_REF=proof-mode
+    equations_CI_GITURL=https://github.com/maximedenes/Coq-Equations
+
+fi
