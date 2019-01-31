5f183420fc0439833051caa004f89988411afe92
Emilio Jesus Gallego Arias <e+git@x80.org>
2019-01-31 02:11:31 +0100

    [plugin tutorial] Adapt to removal of imperative state.

--- doc/plugin_tutorial/tuto1/src/simple_declare.ml
+++ doc/plugin_tutorial/tuto1/src/simple_declare.ml
@@ -1,5 +1,5 @@
 (* Ideally coq/coq#8811 would get merged and then this function could be much simpler. *)
-let edeclare ident (_, poly, _ as k) ~opaque sigma udecl body tyopt imps hook =
+let edeclare ~ontop ident (_, poly, _ as k) ~opaque sigma udecl body tyopt imps hook =
   let sigma = Evd.minimize_universes sigma in
   let body = EConstr.to_constr sigma body in
   let tyopt = Option.map (EConstr.to_constr sigma) tyopt in
@@ -11,7 +11,7 @@ let edeclare ident (_, poly, _ as k) ~opaque sigma udecl body tyopt imps hook =
   let univs = Evd.check_univ_decl ~poly sigma udecl in
   let ubinders = Evd.universe_binders sigma in
   let ce = Declare.definition_entry ?types:tyopt ~univs body in
-  DeclareDef.declare_definition ident k ce ubinders imps ~hook
+  DeclareDef.declare_definition ~ontop ident k ce ubinders imps ~hook
 
 let packed_declare_definition ~poly ident value_with_constraints =
   let body, ctx = value_with_constraints in
@@ -19,6 +19,6 @@ let packed_declare_definition ~poly ident value_with_constraints =
   let k = (Decl_kinds.Global, poly, Decl_kinds.Definition) in
   let udecl = UState.default_univ_decl in
   let nohook = Lemmas.mk_hook (fun _ x -> ()) in
-  ignore (edeclare ident k ~opaque:false sigma udecl body None [] nohook)
+  ignore (edeclare ~ontop:None ident k ~opaque:false sigma udecl body None [] nohook)
 
 (* But this definition cannot be undone by Reset ident *)
