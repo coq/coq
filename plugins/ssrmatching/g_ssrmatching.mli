b7b938a6878b754089814cbfdd478d6b9380c6bb
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2019-02-06 09:31:42 +0100

    Fix #9281: forgotten CeCILL-B headers in ssrmatching plugin.

--- plugins/ssrmatching/g_ssrmatching.mli
+++ plugins/ssrmatching/g_ssrmatching.mli
@@ -1,5 +1,14 @@
-(* (c) Copyright 2006-2015 Microsoft Corporation and Inria.                  *)
-(* Distributed under the terms of CeCILL-B.                                  *)
+(************************************************************************)
+(*         *   The Coq Proof Assistant / The Coq Development Team       *)
+(*  v      *   INRIA, CNRS and contributors - Copyright 1999-2018       *)
+(* <O___,, *       (see CREDITS file for the list of authors)           *)
+(*   \VV/  **************************************************************)
+(*    //   *    This file is distributed under the terms of the         *)
+(*         *     GNU Lesser General Public License Version 2.1          *)
+(*         *     (see LICENSE file for the text of the license)         *)
+(************************************************************************)
+
+(* (c) Copyright 2006-2015 Microsoft Corporation and Inria.             *)
 
 open Genarg
 open Ssrmatching
