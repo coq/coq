1123165d3186124bd0b72b382d39383aefe347d8
Enrico Tassi <Enrico.Tassi@inria.fr>
2019-02-13 13:48:05 +0100

    more tests

--- test-suite/bugs/closed/bug_9432.v
+++ test-suite/bugs/closed/bug_9432.v
@@ -7,4 +7,6 @@ Canonical Structure xx@{i} := {| f := Type@{i} |}.
 
 Fail Coercion cc@{} := fun x : Type => Build_foo x.
 
-Coercion cc@{i} := fun x : Type@{i} => Build_foo x.
+Polymorphic Coercion cc@{i} := fun x : Type@{i} => Build_foo x.
+
+Coercion cc1@{i} := (cc@{i}).
