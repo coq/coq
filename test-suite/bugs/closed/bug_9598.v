5ee6581330ac77596d31dddc1bf4fc09e585b1f6
Enrico Tassi <Enrico.Tassi@inria.fr>
2019-02-22 14:09:43 +0100

    add testcase for fix

--- test-suite/bugs/closed/bug_9598.v
+++ test-suite/bugs/closed/bug_9598.v
@@ -9,3 +9,15 @@ Module case.
   Fixpoint rec_ko (x : nat) : nat := fst (alias_K 0 (rec_ko x)).
 
 End case.
+
+Module fixpoint.
+
+  Inductive pair := K (n1 : nat) (n2 : nat).
+  Fixpoint fst (p : pair) : nat := match p with K n _ => n end.
+
+  Definition alias_K a b := K a b.
+
+  Fixpoint rec (x : nat) : nat := fst (K 0 (rec x)).
+  Fixpoint rec_ko (x : nat) : nat := fst (alias_K 0 (rec_ko x)).
+
+End fixpoint.
