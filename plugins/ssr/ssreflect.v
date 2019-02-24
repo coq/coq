360fc00c08b2f8637fcaca7b56b6385e4c5ae0c3
Erik Martin-Dorel <erik.martin-dorel@irit.fr>
2019-02-24 16:08:36 +0100

    [ssr] over: also works on universally quantified goals
    (cherry picked from commit 527e6a97d1429a58273ddc6b4ce2826df0dba1c6)

--- plugins/ssr/ssreflect.v
+++ plugins/ssr/ssreflect.v
@@ -571,6 +571,14 @@ Register Under as plugins.ssreflect.Under.
 Register Under_from_eq as plugins.ssreflect.Under_from_eq.
 
 Ltac over :=
-  by apply Under.under_done.
-(* [exact: Under.under_done] woud be more satisfying,
-   but issue with 2-var test-suite (test_over_2_2) *)
+  solve [ apply Under.under_done | by rewrite over ].
+
+(* The 2 variants below wouldn't work for the [test_over_2_1] test
+   (2-var case with evars)
+
+Ltac over :=
+  exact: Under.under_done.
+
+Ltac over :=
+  by rewrite over.
+ *)
