ad3d2b42e1996bfd87f322d59e7e926443bbecf8
Matthieu Sozeau <matthieu.sozeau@inria.fr>
2019-03-07 17:39:23 -0300

    Allow projections to be declared as classes.
    Fixes #12975.

---
+++ doc/changelog/02-specification-language/9711-primitive-projection-classes.rst
@@ -0,0 +1,4 @@
+- **Added:**
+  Ability to declare primitive projections as class, for dependent typeclass resolutions
+  (fixes `#12975 <https://github.com/coq/coq/issues/12975>`_, `#9711 <https://github.com/coq/coq/pull/9711>`_,
+  by Matthieu Sozeau).
---
+++ test-suite/bugs/closed/bug_12975.v
@@ -0,0 +1,32 @@
+Record foo :=
+{
+  myprop : Type;
+}.
+
+Set Primitive Projections.
+
+Record bar :=
+{
+  myotherprop : Type ;
+  someprop : myotherprop;
+  somefn : myotherprop -> myotherprop
+}.
+
+
+Existing Class myprop.
+Existing Class myotherprop.
+
+Instance : forall O, myprop O. Abort.
+Instance : forall (O : bar), myotherprop O.
+Proof.
+  intros O. destruct O; cbn. exact someprop0.
+Defined.
+
+Definition bar_nat : bar := {| myotherprop := nat; someprop := 0; somefn x := x |}.
+
+Definition overloaded {b : bar} (o : myotherprop b) : myotherprop b :=
+  somefn _ (someprop _).
+
+Global Instance defaultbar_nat : myotherprop bar_nat := 0.
+
+Type (_ : myotherprop bar_nat).
