2a4d0627e61ce9261d891b5199c2463c5b62ee4f
Vincent Laporte <Vincent.Laporte@fondation-inria.fr>
2019-03-15 13:00:17 +0000

    [nix] Store the reference to nixpkgs in a dedicated file

--- default.nix
+++ default.nix
@@ -21,11 +21,7 @@
 # Once the build is finished, you will find, in the current directory,
 # a symlink to where Coq was installed.
 
-{ pkgs ?
-    (import (fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/11cf7d6e1ffd5fbc09a51b76d668ad0858a772ed.tar.gz";
-      sha256 = "0zcg4mgfdk3ryiqj1j5iv5bljjvsgi6q6j9z1vkq383c4g4clc72";
-    }) {})
+{ pkgs ? import ./nixpkgs.nix {}
 , ocamlPackages ? pkgs.ocamlPackages
 , buildIde ? true
 , buildDoc ? true
---
+++ nixpkgs.nix
@@ -0,0 +1,4 @@
+import (fetchTarball {
+  url = "https://github.com/NixOS/nixpkgs/archive/11cf7d6e1ffd5fbc09a51b76d668ad0858a772ed.tar.gz";
+  sha256 = "0zcg4mgfdk3ryiqj1j5iv5bljjvsgi6q6j9z1vkq383c4g4clc72";
+})
