7de1df96a7e431e02709039e0b79683b1371b68b
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2019-03-20 12:13:36 +0100

    Add overlays.

---
+++ dev/ci/user-overlays/09710-ppedrot-compact-case-repr.sh
@@ -0,0 +1,15 @@
+if [ "$CI_PULL_REQUEST" = "9710" ] || [ "$CI_BRANCH" = "compact-case-repr" ]; then
+
+    color_CI_REF=compact-case-repr
+    color_CI_GITURL=https://github.com/ppedrot/color
+
+    coq_dpdgraph_CI_REF=compact-case-repr
+    coq_dpdgraph_CI_GITURL=https://github.com/ppedrot/coq-dpdgraph
+
+    relation_algebra_CI_REF=compact-case-repr
+    relation_algebra_CI_GITURL=https://github.com/ppedrot/relation-algebra
+
+    unicoq_CI_REF=compact-case-repr
+    unicoq_CI_GITURL=https://github.com/ppedrot/unicoq
+
+fi
