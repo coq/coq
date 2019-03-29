f35966aaeddec9e07be0fd20716aea880b5666f4
Emilio Jesus Gallego Arias <e+git@x80.org>
2019-03-29 18:01:33 +0100

    [ci] Overlays for move_termination_routine_out

---
+++ dev/ci/user-overlays/09566-ejgallego-proof_global+move_termination_routine_out.sh
@@ -0,0 +1,21 @@
+if [ "$CI_PULL_REQUEST" = "9566" ] || [ "$CI_BRANCH" = "proof_global+move_termination_routine_out" ]; then
+
+    elpi_CI_REF=proof_global+move_termination_routine_out
+    elpi_CI_GITURL=https://github.com/ejgallego/coq-elpi
+
+    equations_CI_REF=proof_global+move_termination_routine_out
+    equations_CI_GITURL=https://github.com/ejgallego/Coq-Equations
+
+    ltac2_CI_REF=proof_global+move_termination_routine_out
+    ltac2_CI_GITURL=https://github.com/ejgallego/ltac2
+
+    mtac2_CI_REF=proof_global+move_termination_routine_out
+    mtac2_CI_GITURL=https://github.com/ejgallego/Mtac2
+
+    paramcoq_CI_REF=proof_global+move_termination_routine_out
+    paramcoq_CI_GITURL=https://github.com/ejgallego/paramcoq
+
+    quickchick_CI_REF=proof_global+move_termination_routine_out
+    quickchick_CI_GITURL=https://github.com/ejgallego/QuickChick
+
+fi
