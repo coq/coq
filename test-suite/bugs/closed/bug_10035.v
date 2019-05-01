df8635e664bb72672d37e49fd1385765e4d40272
Jasper Hugunin <jasperh@cs.washington.edu>
2019-04-30 21:37:00 -0700

    Refresh Set when defining evars up to cumulativity, flexibly.
    Before, we solved a problem `Set <= ?X` by `X := Set`, which is less
    general than `X := Type@{i}, Set <= i` for a flexible `i` which can
    be collapsed to `Set`.

---
+++ test-suite/bugs/closed/bug_10035.v
@@ -0,0 +1,13 @@
+Check (fun b : bool => match b with true => Set  | false => unit end).
+Check (fun b : bool => match b with true => unit | false => Set  end).
+Check (
+  let X : Type@{_} := _ in
+  let a : X := unit in
+  (* already at this point, X := Set *)
+  let b : X := Set in 3).
+Definition foo (b : bool) : Set :=
+  match b return _ with true => unit | false => nat end.
+Definition foo' (b : bool) : Set :=
+  match b with true => unit | false => nat end.
+Definition foo'' (b : bool) : Prop :=
+  match b return _ with true => True | false => False end.
