b20f9553570a555ece25a6dd1ff705849cbc121f
Vincent Laporte <Vincent.Laporte@fondation-inria.fr>
2019-05-03 10:06:29 +0000

    Add overlays for CompCert, VST, and coquelicot
    (cherry picked from commit b50bd0f9fd2fedb7dd14edd39baabf2fc3be8e3b)

---
+++ dev/ci/user-overlays/09854-vbgl-field_simplify_int.sh
@@ -0,0 +1,12 @@
+if [ "$CI_PULL_REQUEST" = "9854" ] || [ "$CI_BRANCH" = "field_simplify_int" ]; then
+
+    compcert_CI_REF=field_simplify_int
+    compcert_CI_GITURL=https://github.com/vbgl/CompCert
+
+    coquelicot_CI_REF=field_simplify_int
+    coquelicot_CI_GITURL=https://gitlab.com/vbgl/coquelicot
+
+    vst_CI_REF=field_simplify_int
+    vst_CI_GITURL=https://github.com/vbgl/VST
+
+fi
