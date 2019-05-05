c633a348b97f951d9374e4adcd3e1ebb5bb2fa0f
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2019-05-05 17:11:06 +0200

    Stop using pstate in global print queries
    Using pstate makes no sense for printing global stuff
    
    This incidentally removes all use of ![proof_opt_query], but that's
    because non-global queries are all in vernacentries (eg Check)

--- tactics/autorewrite.mli
+++ tactics/autorewrite.mli
@@ -42,7 +42,7 @@ val auto_multi_rewrite : ?conds:conditions -> string list -> Locus.clause -> uni
 
 val auto_multi_rewrite_with : ?conds:conditions -> unit Proofview.tactic -> string list -> Locus.clause -> unit Proofview.tactic
 
-val print_rewrite_hintdb : Environ.env -> Evd.evar_map -> string -> Pp.t
+val print_rewrite_hintdb : string -> Pp.t
 
 open Clenv
 
