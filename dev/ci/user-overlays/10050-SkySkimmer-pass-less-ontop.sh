aac26daf5f3d81aa53a5310f07fb744a93d826c1
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2019-05-06 14:02:53 +0200

    Overlays for coq/coq#10050 (proof_global API changes)

---
+++ dev/ci/user-overlays/10050-SkySkimmer-pass-less-ontop.sh
@@ -0,0 +1,18 @@
+if [ "$CI_PULL_REQUEST" = "10050" ] || [ "$CI_BRANCH" = "pass-less-ontop" ]; then
+
+    elpi_CI_REF=pass-less-ontop
+    elpi_CI_GITURL=https://github.com/SkySkimmer/coq-elpi
+
+    equations_CI_REF=pass-less-ontop
+    equations_CI_GITURL=https://github.com/SkySkimmer/Coq-Equations
+
+    mtac2_CI_REF=pass-less-ontop
+    mtac2_CI_GITURL=https://github.com/SkySkimmer/Mtac2
+
+    paramcoq_CI_REF=pass-less-ontop
+    paramcoq_CI_GITURL=https://github.com/SkySkimmer/paramcoq
+
+    quickchick_CI_REF=pass-less-ontop
+    quickchick_CI_GITURL=https://github.com/SkySkimmer/QuickChick
+
+fi
