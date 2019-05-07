65c3e2985edaa73d827dc368e7cb437a7dbd0307
Jasper Hugunin <jasperh@cs.washington.edu>
2019-05-07 15:04:04 -0700

    Use Print Custom Grammar to inspect custom entries
    (cherry picked from commit beb5bdec79ff371f48a478df3c24f2cf9d68aa1f)

--- test-suite/output/Notations4.v
+++ test-suite/output/Notations4.v
@@ -9,6 +9,7 @@ Notation "x + y" := (Nat.add x y) (in custom myconstr at level 5).
 Notation "x * y" := (Nat.mul x y) (in custom myconstr at level 4).
 Notation "< x >" := x (in custom myconstr at level 3, x constr at level 10).
 Check [ < 0 > + < 1 > * < 2 >].
+Print Custom Grammar myconstr.
 
 Axiom a : nat.
 Notation b := a.
