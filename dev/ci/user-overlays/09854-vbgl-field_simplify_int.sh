22a808d3e046de0525046a9c3851a781d79ecfad
Michael Soegtrop <7895506+MSoegtropIMC@users.noreply.github.com>
2019-05-09 10:44:15 +0200

    Switched Coquelicot CI URLs from INRIA gforge to INRIA gitlab
    (cherry picked from commit 281e6657c7fe5033a13c7a2fd2b6cc6f51cb6911)

--- dev/ci/user-overlays/09854-vbgl-field_simplify_int.sh
+++
@@ -1,6 +0,0 @@
-if [ "$CI_PULL_REQUEST" = "9854" ] || [ "$CI_BRANCH" = "field_simplify_int" ]; then
-
-    coquelicot_CI_REF=field_simplify_int
-    coquelicot_CI_GITURL=https://gitlab.com/vbgl/coquelicot
-
-fi
