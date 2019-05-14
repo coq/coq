7b41d102e90b08c008c54fe0da1f2dedc5ec43da
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2019-05-14 14:28:43 +0200

    Overlay for definition-not-visible overhaul

---
+++ dev/ci/user-overlays/10157-SkySkimmer-def-not-visible-generic-warning.sh
@@ -0,0 +1,6 @@
+if [ "$CI_PULL_REQUEST" = "10157" ] || [ "$CI_BRANCH" = "def-not-visible-generic-warning" ]; then
+
+    elpi_CI_REF=def-not-visible-generic-warning
+    elpi_CI_GITURL=https://github.com/SkySkimmer/coq-elpi
+
+fi
