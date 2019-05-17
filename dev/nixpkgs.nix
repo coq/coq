5c1e7ae5e16803fd7bd19fbb343b57877b646119
Vincent Laporte <Vincent.Laporte@fondation-inria.fr>
2019-05-17 08:35:41 +0000

    [nix] Update reference to nixpkgs

--- dev/nixpkgs.nix
+++ dev/nixpkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/8471ab76242987b11afd4486b82888e1588f8307.tar.gz";
-  sha256 = "06pp6b6x78jlinxifnphkbp79dx58jr990fkm4qziq0ay5klpxd7";
+  url = "https://github.com/NixOS/nixpkgs/archive/bc9df0f66110039e495b6debe3a6cda4a1bb0fed.tar.gz";
+  sha256 = "0y2w259j0vqiwjhjvlbsaqnp1nl2zwz6sbwwhkrqn7k7fmhmxnq1";
 })
