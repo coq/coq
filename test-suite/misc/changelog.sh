66eff2d8fad35c782e62f7a86f39edf1dac44f53
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2019-05-22 17:07:07 +0200

    Fix changelog test file on macOS: do not use ls + wc.
    Co-authored-by: Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
    (cherry picked from commit 4d8a64fd4445e6a1d6a76243199f674624fa9a75)

--- test-suite/misc/changelog.sh
+++ test-suite/misc/changelog.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 if grep -q -F "is_a_released_version = False" ../config/coq_config.py; then
     echo "This is not a released version: nothing to test."
@@ -7,7 +7,8 @@ fi
 
 for d in ../doc/changelog/*; do
   if [ -d "$d" ]; then
-    if [ "$(ls $d/*.rst | wc -l)" != "1" ]; then
+    files=("$d"/*.rst)
+    if [ "${#files[@]}" != 1 ]; then
       echo "Fatal: unreleased changelog entries remain in ${d#../}/"
       echo "Include them in doc/sphinx/changes.rst and remove them from doc/changelog/"
       exit 1
