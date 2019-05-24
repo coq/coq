99e7872a75b7611d117bccfe2b755b0397602d5e
Maxime Dénès <maxime.denes@inria.fr>
2019-05-24 11:38:23 +0200

    Fix test case for 4202, simple apply was instantiating an evar (TO BE CHECKED)

--- test-suite/bugs/closed/bug_4202.v
+++ test-suite/bugs/closed/bug_4202.v
@@ -4,7 +4,7 @@ Lemma foo (H : True) : exists n, g n /\ g n.
 eexists.
 clear H.
 split.
-simple apply a.
+simple eapply a.
 (* goal is "g (S ?Goal0@ {H:=H})" while H has long ceased to exist *)
 simpl.
 Abort.
