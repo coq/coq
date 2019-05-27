1e3ca892b208c22956d6c8f89a1d5863711d0cd9
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2019-05-27 22:04:27 +0200

    Overlays for Mtac2 and Equations.

---
+++ dev/ci/user-overlays/10231-herbelin-master+locating-warning-different-implicit-term-type.sh
@@ -0,0 +1,9 @@
+if [ "$CI_PULL_REQUEST" = "10231" ] || [ "$CI_BRANCH" = "master+locating-warning-different-implicit-term-type" ]; then
+
+    equations_CI_REF=master+fix-manual-implicit-pr10231
+    equations_CI_GITURL=https://github.com/herbelin/Coq-Equations
+
+    mtac2_CI_REF=master+fix-manual-implicit-pr10231
+    mtac2_CI_GITURL=https://github.com/herbelin/Mtac2
+
+fi
