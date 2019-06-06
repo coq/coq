2e371e35b420e2f86f83cdac8b7a2c1816c40ef9
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2019-06-06 13:37:45 +0200

    Overlays for 10319

---
+++ dev/ci/user-overlays/10319-SkySkimmer-vernac-when-sideff.sh
@@ -0,0 +1,9 @@
+if [ "$CI_PULL_REQUEST" = "10319" ] || [ "$CI_BRANCH" = "vernac-when-sideff" ]; then
+
+    mtac2_CI_REF=vernac-when-sideff
+    mtac2_CI_GITURL=https://github.com/SkySkimmer/Mtac2
+
+    equations_CI_REF=vernac-when-sideff
+    equations_CI_GITURL=https://github.com/SkySkimmer/Coq-Equations
+
+fi
