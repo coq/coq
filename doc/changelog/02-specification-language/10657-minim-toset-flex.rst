dfc40122eb765ded59454dcf88cf91a68fe5bcd6
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2019-06-07 10:54:14 +0200

    restrict minimization to set to flexibles
    Split from #10331
    Fix part of #8196
    Replaces #9343
    
    (cherry picked from commit ccae6b2053970a0bd587bb48c2d30fc235de4118)

---
+++ doc/changelog/02-specification-language/10657-minim-toset-flex.rst
@@ -0,0 +1,3 @@
+- Changed heuristics for universe minimization to :g:`Set`: only
+  minimize flexible universes (`#10657 <https://github.com/coq/coq/pull/10657>`_,
+  by Gaëtan Gilbert with help from Maxime Dénès and Matthieu Sozeau).
--- engine/univMinim.ml
+++ engine/univMinim.ml
@@ -276,7 +276,7 @@ let normalize_context_set ~lbound g ctx us algs weak =
     Constraint.partition (fun (l,d,r) -> d == Le && (Level.equal l lbound || Level.is_sprop l)) csts
   in
   let smallles = if get_set_minimization ()
-    then Constraint.filter (fun (l,d,r) -> LSet.mem r ctx && not (Level.is_sprop l)) smallles
+    then Constraint.filter (fun (l,d,r) -> LMap.mem r us && not (Level.is_sprop l)) smallles
     else Constraint.empty
   in
   let csts, partition =
