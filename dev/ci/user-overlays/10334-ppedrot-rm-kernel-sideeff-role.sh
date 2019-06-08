ee270f7d111e5e71e90a4f3eb5e4edf5c13ea7e3
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2019-06-08 16:58:25 +0200

    Adding an overlay for Equations.

---
+++ dev/ci/user-overlays/10334-ppedrot-rm-kernel-sideeff-role.sh
@@ -0,0 +1,6 @@
+if [ "$CI_PULL_REQUEST" = "10334" ] || [ "$CI_BRANCH" = "rm-kernel-sideeff-role" ]; then
+
+    equations_CI_REF=rm-kernel-sideeff-role
+    equations_CI_GITURL=https://github.com/ppedrot/Coq-Equations
+
+fi
