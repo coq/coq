0c6c495b92186ee357eb6b6a5ff62826040f549c
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2019-06-18 16:36:58 +0200

    Overlays for UIP in SProp

---
+++ dev/ci/user-overlays/10390-SkySkimmer-uip.sh
@@ -0,0 +1,30 @@
+if [ "$CI_PULL_REQUEST" = "10390" ] || [ "$CI_BRANCH" = "uip" ]; then
+
+    unicoq_CI_REF=uip
+    unicoq_CI_GITURL=https://github.com/SkySkimmer/unicoq
+
+    mtac2_CI_REF=uip
+    mtac2_CI_GITURL=https://github.com/SkySkimmer/Mtac2
+
+    elpi_CI_REF=uip
+    elpi_CI_GITURL=https://github.com/SkySkimmer/coq-elpi
+
+    equations_CI_REF=uip
+    equations_CI_GITURL=https://github.com/SkySkimmer/Coq-Equations
+
+    paramcoq_CI_REF=uip
+    paramcoq_CI_GITURL=https://github.com/SkySkimmer/paramcoq
+
+    relation_algebra_CI_REF=uip
+    relation_algebra_CI_GITURL=https://github.com/SkySkimmer/relation-algebra
+
+    coq_dpdgraph_CI_REF=uip
+    coq_dpdgraph_CI_GITURL=https://github.com/SkySkimmer/coq-dpdgraph
+
+    coqhammer_CI_REF=uip
+    coqhammer_CI_GITURL=https://github.com/SkySkimmer/coqhammer
+
+    metacoq_CI_REF=uip
+    metacoq_CI_GITURL=https://github.com/SkySkimmer/metacoq
+
+fi
