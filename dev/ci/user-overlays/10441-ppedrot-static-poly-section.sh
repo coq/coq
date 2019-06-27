eabe207bc6159f1349f7e6b8e63a55984ea9aa32
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2019-06-27 22:42:06 +0200

    Adding overlays.

---
+++ dev/ci/user-overlays/10441-ppedrot-static-poly-section.sh
@@ -0,0 +1,6 @@
+if [ "$CI_PULL_REQUEST" = "10441" ] || [ "$CI_BRANCH" = "static-poly-section" ]; then
+
+    ext_lib_CI_REF=static-poly-section
+    ext_lib_CI_GITURL=https://github.com/ppedrot/coq-ext-lib
+
+fi
