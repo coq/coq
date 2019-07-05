c548dfa45d8bfe64822a62a7189bdca11978045a
Jonathan Chan <jon@alumni.ubc.ca>
2019-07-05 16:27:50 -0700

    Incorporated rec_check into type inference for Fix. CoFix is missing the recursive argument index array so it doesn't do anything for CoFix.

--- kernel/stages.mli
+++ kernel/stages.mli
@@ -96,6 +96,7 @@ sig
   val remove_pos_vars : SVars.t -> t -> t
   val next : ?s:Annot.t -> t -> Annot.t * t
   val pr : t -> Pp.t
+  val pr_vars : string -> vars -> Pp.t
 end
 
 module Constraints :
