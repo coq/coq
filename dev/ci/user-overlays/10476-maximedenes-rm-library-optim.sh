ebae43ef801ae282592285f5800289c1d6398657
Maxime Dénès <maxime.denes@inria.fr>
2019-09-05 17:22:40 +0200

    Add SF overlay

---
+++ dev/ci/user-overlays/10476-maximedenes-rm-library-optim.sh
@@ -0,0 +1,7 @@
+if [ "$CI_PULL_REQUEST" = "10476" ] || [ "$CI_BRANCH" = "rm-library-optim" ]; then
+
+    sf_lf_CI_TARURL=https://www.maximedenes.fr/download/lf.tgz
+    sf_plf_CI_TARURL=https://www.maximedenes.fr/download/plf.tgz
+    sf_vfa_CI_TARURL=https://www.maximedenes.fr/download/vfa.tgz
+
+fi
