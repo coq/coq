a5fde0dec78e7d9f1747a43ac3805a32490e57e5
Maxime Dénès <maxime.denes@inria.fr>
2019-09-17 08:00:16 +0200

    Overlay for VST

--- dev/ci/user-overlays/10476-maximedenes-rm-library-optim.sh
+++ dev/ci/user-overlays/10476-maximedenes-rm-library-optim.sh
@@ -4,4 +4,7 @@ if [ "$CI_PULL_REQUEST" = "10476" ] || [ "$CI_BRANCH" = "rm-library-optim" ]; th
     sf_plf_CI_TARURL=https://www.maximedenes.fr/download/plf.tgz
     sf_vfa_CI_TARURL=https://www.maximedenes.fr/download/vfa.tgz
 
+    vst_CI_REF=fix-export
+    vst_CI_GITURL=https://github.com/maximedenes/VST
+
 fi
