11231a495cc3dc2b8a754dac77d7739c5b54ac30
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2019-10-30 13:09:29 +0100

    add test for #4502 (fixed by unknown commit)
    Close #4502

---
+++ test-suite/bugs/closed/bug_4502.v
@@ -0,0 +1,17 @@
+Require Import FunInd.
+
+Set Universe Polymorphism.
+Set Primitive Projections.
+Set Implicit Arguments.
+Set Strongly Strict Implicit.
+
+Function first_false (n : nat) (f : nat -> bool) : option nat :=
+  match n with
+  | O => None
+  | S m =>
+    match first_false m f with
+    | (Some _) as s => s
+    | None => if f m then None else Some m
+    end
+  end.
+(* undefined universe *)
