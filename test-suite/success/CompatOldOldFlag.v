d6c45c99a9fd0dae4932f375ac663c368b1bdaf3
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2019-11-02 22:06:07 +0100

    Run update-compat script with --release option.
    This should ideally have been done before the 8.11 branching.

--- test-suite/success/CompatOldOldFlag.v
+++
@@ -1,6 +0,0 @@
-(* -*- coq-prog-args: ("-compat" "8.8") -*- *)
-(** Check that the current-minus-three compatibility flag actually requires the relevant modules. *)
-Import Coq.Compat.Coq811.
-Import Coq.Compat.Coq810.
-Import Coq.Compat.Coq89.
-Import Coq.Compat.Coq88.
