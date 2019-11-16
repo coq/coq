e1e97e171258afed4b750d0827fca4639a0e5a6f
Guillaume Munch-Maccagnoni <Guillaume.Munch-Maccagnoni@Inria.fr>
2019-11-16 22:50:35 +0100

    failwith -> caml_failwith
    (cherry picked from commit 93f45c7349e64b3acfddbb2c4d2767980a3a5b44)

--- kernel/byterun/coq_values.c
+++ kernel/byterun/coq_values.c
@@ -39,8 +39,8 @@ value coq_closure_arity(value clos) {
   if (Is_instruction(c,RESTART)) {
     c++;
     if (Is_instruction(c,GRAB)) return Val_int(3 + c[1] - Wosize_val(clos));
-    else { 
-      if (Wosize_val(clos) != 2) failwith("Coq Values : coq_closure_arity");
+    else {
+      if (Wosize_val(clos) != 2) caml_failwith("Coq Values : coq_closure_arity");
       return Val_int(1);
     }
   }
