f437655f3afbfd89e7abba7d9d53b49a2334b8ae
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2019-12-04 14:15:42 +0100

    Overlay for ELPI

---
+++ dev/ci/user-overlays/11172-herbelin-master+coercion-notation-interleaved-printing.sh
@@ -0,0 +1,6 @@
+if [ "$CI_PULL_REQUEST" = "11172" ] || [ "$CI_BRANCH" = "master+coercion-notation-interleaved-printing" ]; then
+
+    elpi_CI_REF=coq-master+mini-fix-mkGApp
+    elpi_CI_GITURL=https://github.com/herbelin/coq-elpi
+
+fi
