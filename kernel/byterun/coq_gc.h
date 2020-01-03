4af1f7ec60b442ef815c056ce95519cfe1621ca3
Kate <kit.ty.kate@disroot.org>
2020-01-03 13:00:27 +0000

    Add support for OCaml 4.10

--- kernel/byterun/coq_gc.h
+++ kernel/byterun/coq_gc.h
@@ -13,12 +13,15 @@
 #include <caml/mlvalues.h>
 #include <caml/alloc.h>
 #include <caml/memory.h>
+#include <caml/version.h>
 
 typedef void (*scanning_action) (value, value *);
 
 
+#if OCAML_VERSION < 41000
 CAMLextern char *young_ptr;
 CAMLextern char *young_limit;
+#endif
 CAMLextern void (*scan_roots_hook) (scanning_action);
 CAMLextern void minor_collection (void);
 
@@ -43,16 +46,16 @@ CAMLextern void minor_collection (void);
 #endif
 
 #define Alloc_small(result, wosize, tag) do{                            \
-  young_ptr -= Bhsize_wosize (wosize);                                  \
-  if (young_ptr < young_limit){                                         \
-    young_ptr += Bhsize_wosize (wosize);                                \
+  caml_young_ptr -= Bhsize_wosize (wosize);                             \
+  if (caml_young_ptr < caml_young_limit){                               \
+    caml_young_ptr += Bhsize_wosize (wosize);                           \
     Setup_for_gc;                                                       \
     minor_collection ();                                                \
     Restore_after_gc;                                                   \
-    young_ptr -= Bhsize_wosize (wosize);                                \
+    caml_young_ptr -= Bhsize_wosize (wosize);                           \
   }                                                                     \
-  Hd_hp (young_ptr) = Make_header ((wosize), (tag), Caml_black);        \
-  (result) = Val_hp (young_ptr);                                        \
+  Hd_hp (caml_young_ptr) = Make_header ((wosize), (tag), Caml_black);   \
+  (result) = Val_hp (caml_young_ptr);                                   \
   }while(0) 
 
 
