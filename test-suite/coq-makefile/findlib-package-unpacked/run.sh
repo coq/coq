665ddac62b7f028f582fbf8fca842e920ed2268c
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2020-01-03 16:01:12 +0100

    coq_makefile: test with CAMLPKGS and mllib

---
+++ test-suite/coq-makefile/findlib-package-unpacked/run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+. ../template/init.sh
+mv src/test_plugin.mlpack src/test_plugin.mllib
+
+echo "let () = Foolib.foo ();;" >> src/test_aux.ml
+export OCAMLPATH=$OCAMLPATH:$PWD/findlib
+if which cygpath 2>/dev/null; then
+  # the only way I found to pass OCAMLPATH on win is to have it contain
+  # only one entry
+  OCAMLPATH=$(cygpath -w "$PWD"/findlib)
+  export OCAMLPATH
+fi
+make -C findlib/foo clean
+coq_makefile -f _CoqProject -o Makefile
+cat Makefile.conf
+cat Makefile.local
+make -C findlib/foo
+make
+make byte
