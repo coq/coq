c9d4954d710af441c3bbde392ef7d2ae524d9cec
Karl Palmskog <palmskog@gmail.com>
2020-01-13 14:59:22 -0600

    [coqdoc] Fix #11353: coqdoc -g omits all sentences with decorations
    (cherry picked from commit 6b05ae1c447680cd4ed1332c0c8b4f0e24b33f03)

---
+++ test-suite/coqdoc/bug11353.v
@@ -0,0 +1,7 @@
+(* -*- coq-prog-args: ("-g") -*- *)
+Definition a := 0. #[ (* templatize *)  universes( template) ]
+Inductive mysum (A B:Type) : Type :=
+  | myinl : A -> mysum A B
+  | myinr : B -> mysum A B.
+
+#[local]Definition b := 1.
