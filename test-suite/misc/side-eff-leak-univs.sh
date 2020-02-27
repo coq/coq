fe4d324e9e64fd34b2b377d29944eaac3c18e37f
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2020-02-27 14:44:10 +0100

    Adding a test to the test-suite.
    We take inspiration and code from the Evil plugin.

---
+++ test-suite/misc/side-eff-leak-univs.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+export COQBIN=$BIN
+export PATH=$COQBIN:$PATH
+
+cd misc/side-eff-leak-univs/
+
+coq_makefile -f _CoqProject -o Makefile
+
+make clean
+
+make src/evil_plugin.cma
+
+if make; then
+    >&2 echo 'Should have failed!'
+    exit 1
+fi
