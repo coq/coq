f157dcd938c9ec4530d74cbf414fd18c41e56df6
Enrico Tassi <Enrico.Tassi@Inria.fr>
2020-02-28 10:42:48 +0100

    [ci] [docker] overlay for elpi 1.10

---
+++ dev/ci/user-overlays/11708-gares-elpi-1.10.sh
@@ -0,0 +1,6 @@
+if [ "$CI_PULL_REQUEST" = "11708" ] || [ "$CI_BRANCH" = " elpi-1.10+coq-elpi-1.3" ]; then
+
+    elpi_CI_REF="coq-master+coq-elpi-1.3"
+    elpi_CI_GITURL=https://github.com/LPCIC/coq-elpi
+
+fi
