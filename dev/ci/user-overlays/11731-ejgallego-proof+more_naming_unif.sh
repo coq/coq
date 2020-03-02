0e329b81794c582ca845d2fc547bfdefad89a972
Emilio Jesus Gallego Arias <e+git@x80.org>
2020-03-02 02:56:32 -0500

    [ci] Overlays for declare interface refactoring.

---
+++ dev/ci/user-overlays/11731-ejgallego-proof+more_naming_unif.sh
@@ -0,0 +1,9 @@
+if [ "$CI_PULL_REQUEST" = "11731" ] || [ "$CI_BRANCH" = "proof+more_naming_unif" ]; then
+
+    equations_CI_REF=proof+more_naming_unif
+    equations_CI_GITURL=https://github.com/ejgallego/Coq-Equations
+
+    rewriter_CI_REF=proof+more_naming_unif
+    rewriter_CI_GITURL=https://github.com/ejgallego/rewriter
+
+fi
