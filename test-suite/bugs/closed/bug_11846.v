7abbfae117a7adea4b627ff66f63e80fb0fa05e4
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2020-03-17 18:33:49 +0100

    Properly thread let-bindings in Funind principle construction.
    Fixes #11846: Funind fails to generate principles for terms with let bindings.
    
    (cherry picked from commit ca91aa115378ec24a5be34ba506b071b4662df4b)

---
+++ test-suite/bugs/closed/bug_11846.v
@@ -0,0 +1,16 @@
+Require Import FunInd.
+
+Inductive tree : Type :=
+| Node : unit -> tree.
+
+Definition height (s : tree) : unit :=
+match s with
+| Node h => h
+end.
+
+Definition bal : forall l, let h := height l in tree := fun l =>
+  let h := height l in
+  Node h.
+
+Set Warnings "+all".
+Functional Scheme bal_ind := Induction for bal Sort Prop.
