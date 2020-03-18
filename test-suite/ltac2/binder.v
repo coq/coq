de9f75fcca7247abadbc02176a1fe06033cd6e38
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2020-03-18 18:34:19 +0100

    Adding a round-trip test for binders.

---
+++ test-suite/ltac2/binder.v
@@ -0,0 +1,37 @@
+Require Import Ltac2.Ltac2.
+
+Ltac2 check () :=
+let t := Control.goal () in
+match Constr.Unsafe.kind t with
+| Constr.Unsafe.Prod b t =>
+  let na := Constr.Binder.name b in
+  let u := Constr.Binder.type b in
+  let b := Constr.Binder.make na u in
+  let c := Constr.Unsafe.make (Constr.Unsafe.Prod b t) in
+  pose (v := $c);
+  refine '(_ : &v);
+  unfold &v
+| _ => fail
+end.
+
+Goal forall x : nat, x = x.
+Proof.
+check (); intros; reflexivity.
+Qed.
+
+Goal forall x : Type, x = x.
+Proof.
+check (); intros; reflexivity.
+Qed.
+
+Goal forall x : Set, x = x.
+Proof.
+check (); intros; reflexivity.
+Qed.
+
+Inductive True : SProp := I.
+
+Goal forall x : True, True.
+Proof.
+check (); intros; constructor.
+Qed.
