c3641353b05d9a6d31f52d07127d0626ff370750
Emilio Jesus Gallego Arias <e+git@x80.org>
2020-03-31 06:22:04 -0400

    [nit] [plugin_tuto] Remove empty function and use new API directly

--- doc/plugin_tutorial/tuto1/src/simple_declare.ml
+++ doc/plugin_tutorial/tuto1/src/simple_declare.ml
@@ -1,8 +1,6 @@
-let edeclare ?hook ~name ~poly ~scope ~kind ~opaque ~udecl ~impargs sigma body tyopt  =
-  DeclareDef.declare_definition ~name ~scope ~kind ~impargs ?hook
-    ~opaque ~poly ~udecl ~types:tyopt ~body sigma
-
 let declare_definition ~poly name sigma body =
   let udecl = UState.default_univ_decl in
-  edeclare ~name ~poly ~scope:(DeclareDef.Global Declare.ImportDefaultBehavior)
-    ~kind:Decls.(IsDefinition Definition) ~opaque:false ~impargs:[] ~udecl sigma body None
+  let scope = DeclareDef.Global Declare.ImportDefaultBehavior in
+  let kind = Decls.(IsDefinition Definition) in
+  DeclareDef.declare_definition ~name ~scope ~kind ~impargs:[] ~udecl
+    ~opaque:false ~poly ~types:None ~body sigma
