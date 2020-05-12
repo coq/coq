833d7672a4cc1dbdd4ab5a861362824b03f72d57
Emilio Jesus Gallego Arias <e+git@x80.org>
2020-05-12 17:35:16 +0200

    [ci] Old overlay cleanup.

--- dev/ci/user-overlays/10185-SkySkimmer-instance-no-bang.sh
+++
@@ -1,6 +0,0 @@
-if [ "$CI_PULL_REQUEST" = "10185" ] || [ "$CI_BRANCH" = "instance-no-bang" ]; then
-
-    quickchick_CI_REF=instance-no-bang
-    quickchick_CI_GITURL=https://github.com/SkySkimmer/QuickChick
-
-fi
--- dev/ci/user-overlays/12107-SkySkimmer-no-mod-univs.sh
+++
@@ -1,6 +0,0 @@
-if [ "$CI_PULL_REQUEST" = "12107" ] || [ "$CI_BRANCH" = "no-mod-univs" ]; then
-
-    elpi_CI_REF=no-mod-univs
-    elpi_CI_GITURL=https://github.com/SkySkimmer/coq-elpi
-
-fi
