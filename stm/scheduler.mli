bd324f10abf31659d10a29822f34d6c2a3eb2eb3
Enrico Tassi <Enrico.Tassi@Inria.fr>
2020-06-08 17:38:40 +0200

    comment scheduler.mli

--- stm/scheduler.mli
+++ stm/scheduler.mli
@@ -30,9 +30,18 @@ type task =
   | DelegateQuery of sentence_id * ast
 
 type schedule
+(** Holds the dependencies among sentences and a schdule to evaluate all
+    sentences *)
 
 val initial_schedule : schedule
+
 val schedule_sentence : sentence_id * ast option -> state -> schedule -> state * schedule
+(** Identifies the structure of the document and dependencies between sentences
+    in order to easily compute the tasks to interpret the a sentence.
+    Input sentence is None on parsing error. *)
 
 val task_for_sentence : schedule -> sentence_id -> sentence_id option * task
+(** Finds the task to be performed and the state on which the task should run *)
+
 val dependents : schedule -> sentence_id -> Stateid.Set.t
+(** Computes what should be invalidated *)
