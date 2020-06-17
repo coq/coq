c68e580377c49435f50f08544c6e9c135f1783a6
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2020-06-17 17:47:03 +0200

    Adding test for #12525 (Search of lemmas in Include failing when in Module).

--- test-suite/output/Search.v
+++ test-suite/output/Search.v
@@ -66,3 +66,26 @@ Reset Initial.
 Require Import Morphisms.
 
 Search is:Instance [ Reflexive | Symmetric ].
+
+Module Bug12525.
+  (* This was revealing a kernel bug with delta-resolution *)
+  Module A. Axiom a:Prop. Axiom b:a. End A.
+  Module B. Include A. End B.
+  Module M.
+    Search B.a.
+  End M.
+End Bug12525.
+
+From Coq Require Lia.
+
+Module Bug12647.
+  (* Similar to #12525 *)
+  Module Type Foo.
+  Axiom P : nat -> Prop.
+  Axiom L : P 0.
+  End Foo.
+
+  Module Bar (F : Foo).
+  Search F.P.
+  End Bar.
+End Bug12647.
