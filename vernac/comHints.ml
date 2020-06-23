37ba76b1ba6f4f5edcf05d69e6a21a46fef026d9
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2020-06-23 14:17:30 +0200

    Hint Opaque/Transparent/Unfold: don't error on opaque constants
    Helps with #12566

--- vernac/comHints.ml
+++ vernac/comHints.ml
@@ -9,6 +9,7 @@
 (************************************************************************)
 
 open Util
+open Names
 
 (** (Partial) implementation of the [Hint] command; some more
    functionality still lives in tactics/hints.ml *)
@@ -61,7 +62,7 @@ let project_hint ~poly pri l2r r =
       cb
   in
   let info = {Typeclasses.hint_priority = pri; hint_pattern = None} in
-  (info, false, true, Hints.PathAny, Hints.IsGlobRef (Names.GlobRef.ConstRef c))
+  (info, false, true, Hints.PathAny, Hints.IsGlobRef (GlobRef.ConstRef c))
 
 let warn_deprecated_hint_constr =
   CWarnings.create ~name:"deprecated-hint-constr" ~category:"deprecated"
@@ -69,6 +70,16 @@ let warn_deprecated_hint_constr =
       Pp.strbrk
         "Declaring arbitrary terms as hints is deprecated; declare a global \
          reference instead")
+
+(* Only error when we have to (axioms may be instantiated if from functors)
+   XXX maybe error if not from a functor argument?
+ *)
+let soft_evaluable =
+  let open GlobRef in
+  function
+  | ConstRef c -> EvalConstRef c
+  | VarRef id -> EvalVarRef id
+  | (IndRef _ | ConstructRef _) as r -> Tacred.error_not_evaluable r
 
 let interp_hints ~poly h =
   let env = Global.env () in
@@ -88,7 +99,7 @@ let interp_hints ~poly h =
     Dumpglob.add_glob ?loc:r.CAst.loc gr;
     gr
   in
-  let fr r = Tacred.evaluable_of_global_reference env (fref r) in
+  let fr r = soft_evaluable (fref r) in
   let fi c =
     let open Hints in
     let open Vernacexpr in
@@ -135,7 +146,7 @@ let interp_hints ~poly h =
         "ind";
       List.init (Inductiveops.nconstructors env ind) (fun i ->
           let c = (ind, i + 1) in
-          let gr = Names.GlobRef.ConstructRef c in
+          let gr = GlobRef.ConstructRef c in
           ( empty_hint_info
           , Declareops.inductive_is_polymorphic mib
           , true
@@ -147,9 +158,7 @@ let interp_hints ~poly h =
     let pat = Option.map (fp sigma) patcom in
     let l = match pat with None -> [] | Some (l, _) -> l in
     let ltacvars =
-      List.fold_left
-        (fun accu x -> Names.Id.Set.add x accu)
-        Names.Id.Set.empty l
+      List.fold_left (fun accu x -> Id.Set.add x accu) Id.Set.empty l
     in
     let env = Genintern.{(empty_glob_sign env) with ltacvars} in
     let _, tacexp = Genintern.generic_intern env tacexp in
