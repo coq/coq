6bb0c6df0dd331b8acb78a720eaf076aea5fce47
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2020-06-27 14:58:17 +0200

    Adding overlay.

---
+++ dev/ci/user-overlays/12599-ppedrot-rm-deprecated-refiner.sh
@@ -0,0 +1,6 @@
+if [ "$CI_PULL_REQUEST" = "12599" ] || [ "$CI_BRANCH" = "rm-deprecated-refiner" ]; then
+
+    equations_CI_REF=rm-deprecated-refiner
+    equations_CI_GITURL=https://github.com/ppedrot/Coq-Equations
+
+fi
