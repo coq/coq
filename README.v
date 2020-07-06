d646373c858f3d048b04cf3f80c1215795feadf1
Jonathan Chan <jcxz@cs.ubc.ca>
2020-07-06 12:50:10 -0700

    Cleaned up README.v.

--- README.v
+++ README.v
@@ -1,51 +1,45 @@
 (** Practical Sized Typing for Coq
 
-(Commands below are enclosed within backticks.)
-
 You will need your environment set up to build Coq. Instructions are provided
-in the Coq repository, but they are generally as follows (for Ubuntu):
-  1. `sudo apt install opam` (OCaml package manager)
+in the Coq repository, but they are generally as follows:
+  1. `sudo apt install opam` (OCaml package manager) (for Ubuntu)
   2. `opam init && opam switch create ocaml-base-compiler` (OCaml compiler)
   3. `opam install num ocamlfind` (OCaml libraries needed for compilation)
 
-To build the Coq toplevel (REPL):
-  1. `./configure -profile devel` (N.B. this turns warnings into errors)
-  2. `make coqbinaries` (for native code) or `make byte` (for bytecode; faster)
-  3. `make COQUSERFLAGS="-set 'Sized Typing'" coqlib` (libraries; slow)
-    Note that currently (as of 18 October 2019), not all Coq libraries are able
-    to be compiled. Allow the compilation to proceed as far as it can. To
-    compile specific libraries, run `make <path>.vo`. For instance, the Strings
-    library can be built using `make theories/Strings/String.vo`.
+To build the Coq toplevel locally (REPL):
+  1. `./configure -local` (N.B. this turns warnings into errors)
+  2. `make coqbinaries` (for native code) or `make byte` (for bytecode)
+  3. `make coqlib` (stdlib) or `make theories/Init/Prelude.vo` (core library)
   4. `bin/coqtop` (if native code) or `bin/coqtop.byte` (if bytecode)
 
-In the toplevel, before running any Coq code, first turn off guard checking
-using
-  `Unset Guard Checking.`
-then turn on sized typing using
-  `Set Sized Typing.`
-Flags can be printed using
-  `Print Typing Flags.`
-Example programs can be found in coq/test-suite/success/sized_typing.v.
-We have included programs mentioned in the paper below.
-*)
+The `Guard Checking` flag toggles the existing guard-predicate-based checking,
+while the `Sized Typing` flag toggles our new CIC^* size inference/checking.
+
+More example programs can be found in coq/test-suite/success/sized_typing.v.
+Programs mentioned in the paper are included below. *)
 
 Unset Guard Checking.
 Set Sized Typing.
 
-(* Coinductives *)
+(** Coinductives *)
 
 CoInductive Stream (A : Type) :=
   Cons : A -> Stream A -> Stream A.
 
 CoFixpoint const A a : Stream A := Cons A a (const A a).
 
-(* Arithmetic *)
-Fixpoint add n m : nat :=
+(** Arithmetic *)
+
+Reserved Infix "+" (at level 50, left associativity).
+Reserved Infix "-" (at level 50, left associativity).
+Reserved Infix "/" (at level 40, left associativity).
+
+Fixpoint plus n m : nat :=
   match n with
   | O => m
-  | S p => S (add p m)
-  end.
-
+  | S p => S (plus p m)
+  end
+where "n + m" := (plus n m).
 
 Fixpoint minus n m :=
   match n, m with
@@ -53,15 +47,17 @@ Fixpoint minus n m :=
   | _, O => n
   | S n', S m' =>
     minus n' m'
-  end.
+  end
+where "n - m" := (minus n m).
 
 Fixpoint div n m :=
   match n with
   | O => O
   | S n' => S (div (minus n' m) m)
-  end.
+  end
+where "n / m" := (div n m).
 
-(* Quicksort *)
+(** Quicksort *)
 
 Fixpoint leb n m :=
   match n, m with
@@ -70,46 +66,45 @@ Fixpoint leb n m :=
     | S n', S m' => leb n' m'
   end.
 
-Fixpoint filter T (f: T -> bool) (l: list T) :=
+Definition gtb n m :=
+  negb (leb n m).
+
+Fixpoint filter {T} (p: T -> bool) (l: list T) :=
   match l with
   | nil => nil
-  | cons x l' =>
-    if (f x) then
-      cons x (filter T f l')
+  | cons hd tl =>
+    if (p hd) then
+      cons hd (filter p tl)
     else
-      filter T f l'
+      filter p tl
   end.
 
-Fixpoint append T (l1 l2: list T) :=
+Fixpoint append {T} (l1 l2: list T) :=
   match l1 with
   | nil => l2
-  | cons x l => cons x (append T l l2)
+  | cons hd tl =>
+    cons hd (append tl l2)
   end.
 
-Fixpoint quicksort l :=
+Fixpoint quicksort (l: list nat) :=
   match l with
   | nil => nil
-  | cons hd tl => append nat
-    (quicksort (filter nat (fun x => (leb x hd)) tl))
-    (cons hd (quicksort (filter nat (fun x => negb (leb x hd)) tl)))
+  | cons hd tl => append
+    (quicksort (filter (gtb hd) tl))
+    (cons hd (quicksort (filter (leb hd) tl)))
   end.
 
-(* GCD *)
+(** GCD
+  These are lifted directly from Nat in the standard library. *)
 
 Fixpoint divmod x y q u :=
   match x with
-    | 0 => (q,u)
-    | S x' =>
-      match u with
-      | 0 => divmod x' y (S q) y
-      | S u' => divmod x' y q u'
-      end
-  end.
-
-Definition div' x y :=
-  match y with
-    | 0 => y
-    | S y' => fst (divmod x y' 0 y')
+  | 0 => (q, u)
+  | S x' =>
+    match u with
+    | 0 => divmod x' y (S q) y
+    | S u' => divmod x' y q u'
+    end
   end.
 
 Definition modulo x y :=
@@ -118,8 +113,7 @@ Definition modulo x y :=
     | S y' => y' - snd (divmod x y' 0 y')
   end.
 
-Infix "/" := div' : nat_scope.
-Infix "mod" := modulo (at level 40, no associativity) : nat_scope.
+Infix "mod" := modulo (at level 40, no associativity).
 
 Fail Fixpoint gcd a b :=
   match a with
@@ -137,9 +131,13 @@ Fixpoint gcd a b :=
 
 Unset Guard Checking.
 
-(* The following is a model of capture-avoiding substitution
-  in simply-typed lambda calculus.
-  You will need to run `make theories/Strings/String.vo` first. *)
+(** STLC
+  A model of capture-avoiding substitution in simply-typed lambda calculus.
+  You may need to run `make theories/Strings/String.vo` first.
+
+  Notice that `freshen` is size-preserving, which is why it can be used in
+  the recursive argument of `subst`, but `subst` itself is not size-preserving,
+  since `val` may be a "larger" term than `exp`. *)
 
 Require Import Strings.String.
 
@@ -148,38 +146,32 @@ Module stlc.
 Parameter names: list string.
 Parameter fresh: True -> string.
 
-Inductive STLCA: Type :=
-  | unit: STLCA
-  | arr (A b: STLCA): STLCA.
+Inductive Typ: Type :=
+  | unit: Typ
+  | arr (T U: Typ): Typ.
 
-Inductive STLCE: Type :=
-  | vare (v: string): STLCE
-  | lambdae (v: string) (A: STLCA) (body: STLCE): STLCE
-  | appe (e1: STLCE) (e2: STLCE): STLCE.
+Inductive Exp: Type :=
+  | var (x: string): Exp
+  | lam (x: string) (T: Typ) (body: Exp): Exp
+  | app (e1: Exp) (e2: Exp): Exp.
 
-Fixpoint size (e: STLCE): nat :=
+(* freshen : string^∞ → string^∞ → Exp^ι → Exp^ι *)
+Fixpoint freshen (old: string) (new: string) (e: Exp) :=
   match e with
-  | vare _ => 1
-  | lambdae _ _ body => 1 + (size body)
-  | appe e1 e2 => 1 + (size e1) + (size e2)
+  | var x => if (x =? old)%string then var new else e
+  | app e1 e2 => app (freshen old new e1) (freshen old new e2)
+  | lam x T body => lam x T (freshen old new body)
   end.
 
-(* We assume [new] to be unbound in e. *)
-Fixpoint freshen (old: string) (new: string) (e: STLCE) :=
-  match e with
-  | vare n => if (n =? old)%string then vare new else e
-  | appe e1 e2 => appe (freshen old new e1) (freshen old new e2)
-  | lambdae n A body => lambdae n A (freshen old new body)
-  end.
-
-Fixpoint subst (name: string) (v: STLCE) (exp: STLCE) {struct exp} :=
+(* subst : string^∞ → Exp^∞ → Exp^ι → Exp^∞ *)
+Fixpoint subst (name: string) (val exp: Exp) {struct exp} :=
   match exp with
-  | vare n => if (n =? name)%string then v else exp
-  | appe e1 e2 => appe (subst name v e1) (subst name v e2)
-  | lambdae n A body =>
-    if (n =? name)%string then exp else
-    let n' := fresh I in
-    lambdae n' A (subst name v (freshen n n' body))
+  | var x => if (x =? name)%string then val else exp
+  | app e1 e2 => app (subst name val e1) (subst name val e2)
+  | lam x T body =>
+    if (x =? name)%string then exp else
+    let y := fresh I in
+    lam y T (subst name val (freshen x y body))
   end.
 
 End stlc.
