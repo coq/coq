0824cb85077c6884937fde58ab156e9d3afb2e3b
Abhishek Anand <abhishek.anand.iitg@gmail.com>
2020-07-17 12:26:46 -0700

    fixed explanation of the testcase

--- test-suite/misc/deps-missing-vo/run.sh
+++ test-suite/misc/deps-missing-vo/run.sh
@@ -1,2 +1,2 @@
 coq_makefile -f _CoqProject -o Makefile
-COQDEP="../../../bin/coqdep -compute_missing -coqlib ../../../" make A.vo # building F.vo would require building error.v which cannot be currently built, e.g. because of errors in its source error.cpp. Also, if someone wants to quickly interactively work on F.v they can use this to not autogenerate all the .v files
+COQDEP="../../../bin/coqdep -compute_missing -coqlib ../../../" make A.vo # building F.vo would require building error.v which cannot be currently built, e.g. because of errors in its source error.cpp. Also, if someone wants to quickly interactively work on A.v they can use this to not waste time in autogenerating  irrelevant .v files from .cpp files
