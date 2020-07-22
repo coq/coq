239b1d166e2b31201a89f9d879adc46e9e68c7a8
Emilio Jesus Gallego Arias <e+git@x80.org>
2020-07-22 23:07:28 +0200

    [release] Bump magic numbers after 8.12.0 tag

--- configure.ml
+++ configure.ml
@@ -12,12 +12,12 @@
 #load "str.cma"
 open Printf
 
-let coq_version = "8.12.0"
-let coq_macos_version = "8.12.0" (** "[...] should be a string comprised of
+let coq_version = "8.12.0+beta_8.12.1"
+let coq_macos_version = "8.12.1" (** "[...] should be a string comprised of
 three non-negative, period-separated integers [...]" *)
-let vo_magic = 81200
-let state_magic = 581200
-let is_a_released_version = true
+let vo_magic = 81201
+let state_magic = 581201
+let is_a_released_version = false
 let distributed_exec =
   ["coqtop.opt"; "coqidetop.opt"; "coqqueryworker.opt"; "coqproofworker.opt"; "coqtacticworker.opt";
    "coqc.opt";"coqchk";"coqdoc";"coqworkmgr";"coq_makefile";"coq-tex";"coqwc";"csdpcert";"coqdep";"votour"]
