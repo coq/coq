64ea7c0dcb2b46eb3994e3ee48970129d8ab3423
Enrico Tassi <Enrico.Tassi@Inria.fr>
2020-07-28 14:52:16 +0200

    [vernac] refine check for unresolved evars
    (cherry picked from commit d269b70616cbc67aca0e7fe1c6571c486d334532)

--- test-suite/output/RecordMissingField.v
+++ test-suite/output/RecordMissingField.v
@@ -3,6 +3,10 @@ should contain missing field, and the inferred type of the record **)
 
 Record point2d := mkPoint { x2p: nat; y2p: nat }.
 
-
-Definition increment_x (p: point2d) : point2d :=
+Fail Definition increment_x (p: point2d) : point2d :=
   {| x2p := x2p p + 1; |}.
+
+(* Here there is also an unresolved implicit, which should give an
+   understadable error as well *)
+Fail Definition increment_x (p: point2d) : point2d :=
+  {| x2p := x2p p + (fun n => _) 1; |}.
