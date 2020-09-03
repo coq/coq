e6f7517be65e9f5d2127a86e2213eb717d37e43f
Pierre Roux <pierre@roux01.fr>
2020-09-03 13:28:00 +0200

    Add overlays

---
+++ dev/ci/user-overlays/12218-proux01-numeral-notations-non-inductive.sh
@@ -0,0 +1,18 @@
+if [ "$CI_PULL_REQUEST" = "12218" ] || [ "$CI_BRANCH" = "numeral-notations-non-inductive" ]; then
+
+    stdlib2_CI_REF=numeral-notations-non-inductive
+    stdlib2_CI_GITURL=https://github.com/proux01/stdlib2
+
+    hott_CI_REF=numeral-notations-non-inductive
+    hott_CI_GITURL=https://github.com/proux01/HoTT
+
+    paramcoq_CI_REF=numeral-notations-non-inductive
+    paramcoq_CI_GITURL=https://github.com/proux01/paramcoq
+
+    quickchick_CI_REF=numeral-notations-non-inductive
+    quickchick_CI_GITURL=https://github.com/proux01/QuickChick
+
+    metacoq_CI_REF=numeral-notations-non-inductive
+    metacoq_CI_GITURL=https://github.com/proux01/metacoq
+
+fi
