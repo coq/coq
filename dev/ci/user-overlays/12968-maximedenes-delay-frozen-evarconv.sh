6f3d15a9edc2e04e973c47ee20093a1f79a990cf
Maxime Dénès <maxime.denes@inria.fr>
2020-09-03 09:40:13 +0200

    Add Equations overlay

---
+++ dev/ci/user-overlays/12968-maximedenes-delay-frozen-evarconv.sh
@@ -0,0 +1,6 @@
+if [ "$CI_PULL_REQUEST" = "12968" ] || [ "$CI_BRANCH" = "delay-frozen-evarconv" ]; then
+
+    equations_CI_REF=delay-frozen-evarconv
+    equations_CI_GITURL=https://github.com/maximedenes/Coq-Equations
+
+fi
