a57d49d6f2f4bc262c8457066678a154f11fd0f6
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2020-09-16 07:03:04 +0200

    Add overlay for coq-elpi

---
+++ dev/ci/user-overlays/13025-master+fix-printing-custom-no-level.sh
@@ -0,0 +1,6 @@
+if [ "$CI_PULL_REQUEST" = "13025" ] || [ "$CI_BRANCH" = "master+fix-printing-custom-no-level" ]; then
+
+    elpi_CI_REF=coq-master+adapt-coq-pr13025-fix-custom-entry-no-level-printing
+    elpi_CI_GITURL=https://github.com/herbelin/coq-elpi
+
+fi
