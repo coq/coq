585a97c62f7e80adbc60f283d4d864401383a729
Théo Zimmermann <theo.zimmermann@inria.fr>
2020-09-23 12:57:57 +0200

    Overlays for paramcoq and tlc.

---
+++ dev/ci/user-overlays/13069-Zimmi48-maximal-implicit-length.sh
@@ -0,0 +1,8 @@
+if [ "$CI_PULL_REQUEST" = "13069" ] || [ "$CI_BRANCH" = "maximal-implicit-length" ]; then
+
+    paramcoq_CI_REF=overlay-13069
+    paramcoq_CI_GITURL=https://github.com/Zimmi48/paramcoq
+
+    tlc_CI_REF=overlay-13069
+    tlc_CI_GITURL=https://github.com/Zimmi48/tlc
+fi
