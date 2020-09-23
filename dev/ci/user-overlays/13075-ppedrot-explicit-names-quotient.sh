375fc707b402b855770ec32c57ad1362f2a89e5c
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2020-09-23 17:53:23 +0200

    Add overlays.

---
+++ dev/ci/user-overlays/13075-ppedrot-explicit-names-quotient.sh
@@ -0,0 +1,9 @@
+if [ "$CI_PULL_REQUEST" = "13075" ] || [ "$CI_BRANCH" = "explicit-names-quotient" ]; then
+
+    elpi_CI_REF=explicit-names-quotient
+    elpi_CI_GITURL=https://github.com/ppedrot/coq-elpi
+
+    coq_dpdgraph_CI_REF=explicit-names-quotient
+    coq_dpdgraph_CI_GITURL=https://github.com/ppedrot/coq-dpdgraph
+
+fi
