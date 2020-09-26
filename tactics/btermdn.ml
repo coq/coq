c4105005e4770e11b3c760c502e53a189fcffb4b
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2020-09-26 13:37:16 +0200

    Factorize the constr_pat_discr_{*} functions.

--- tactics/btermdn.ml
+++ tactics/btermdn.ml
@@ -88,8 +88,10 @@ let constr_val_discr env sigma st t =
     | Rel _ | Meta _ | Cast _ | LetIn _ | App _ | Case _ | Fix _ | CoFix _
     | Proj _ | Int _ | Float _ | Array _ -> Nothing
 
-let constr_pat_discr env t =
-  if not (Patternops.occur_meta_pattern t) then
+let constr_pat_discr env st t =
+  if Option.is_empty st && not (Patternops.occur_meta_pattern t) then
+    (* Extremely fishy, and breaks very quickly when fiddled with. Why should
+       **ground** terms match **everything** when non-discriminated??? *)
     None
   else
     let open GlobRef in
@@ -97,44 +99,31 @@ let constr_pat_discr env t =
     | PRef ((IndRef _) as ref), args
     | PRef ((ConstructRef _ ) as ref), args -> Some (GRLabel ref,args)
     | PRef ((VarRef v) as ref), args ->
-      if Environ.evaluable_named v env then None
+      if is_evaluable_named v env st then None
       else Some(GRLabel ref,args)
     | PRef ((ConstRef c) as ref), args ->
-      if evaluable_constant c env then None
+      if is_evaluable_constant c env st then None
       else Some (GRLabel ref, args)
+    | PVar v, args ->
+      begin match st with
+      | None -> None
+      | Some st ->
+        if TransparentState.is_transparent_variable st v then None
+        else Some(GRLabel (VarRef v),args)
+      end
     | PProd (_, d, c), [] -> Some (ProdLabel, [d ; c])
+    | PLambda (_, d, c), [] ->
+      (* This is fishy but seems needed for backwards compat *)
+      if Option.is_empty st then None
+      else Some (LambdaLabel, [d ; c])
     | PSort s, [] -> Some (SortLabel, [])
     | _ -> None
 
-let constr_pat_discr_st env ts t =
-  let open GlobRef in
-  match decomp_pat t with
-  | PRef ((IndRef _) as ref), args
-  | PRef ((ConstructRef _ ) as ref), args -> Some (GRLabel ref,args)
-  | PRef ((VarRef v) as ref), args ->
-    if Environ.evaluable_named v env && (TransparentState.is_transparent_variable ts v) then None
-    else Some(GRLabel ref,args)
-  | PRef ((ConstRef c) as ref), args ->
-    if evaluable_constant c env && TransparentState.is_transparent_constant ts c then None
-    else Some (GRLabel ref, args)
-  | PVar v, args when not (TransparentState.is_transparent_variable ts v) ->
-      Some(GRLabel (VarRef v),args)
-  | PProd (_, d, c), [] -> Some (ProdLabel, [d ; c])
-  | PLambda (_, d, c), [] -> Some (LambdaLabel, [d ; c])
-  | PSort s, [] -> Some (SortLabel, [])
-  | _ -> None
-
 let bounded_constr_pat_discr env st (t,depth) =
-  if Int.equal depth 0 then
-    None
-  else
-    let ans = match st with
-    | None -> constr_pat_discr env t
-    | Some st -> constr_pat_discr_st env st t
-    in
-    match ans with
-      | None -> None
-      | Some (c,l) -> Some(c,List.map (fun c -> (c,depth-1)) l)
+  if Int.equal depth 0 then None
+  else match constr_pat_discr env st t with
+  | None -> None
+  | Some (c,l) -> Some(c,List.map (fun c -> (c,depth-1)) l)
 
 let bounded_constr_val_discr env st sigma (t,depth) =
   if Int.equal depth 0 then Nothing
