124135344d6c9ef1f7622b8340c16cc3d5ac8e06
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2020-10-05 16:01:22 +0200

    Add overlays.

---
+++ dev/ci/user-overlays/13139-ppedrot-clean-hint-constr.sh
@@ -0,0 +1,9 @@
+if [ "$CI_PULL_REQUEST" = "13139" ] || [ "$CI_BRANCH" = "clean-hint-constr" ]; then
+
+    equations_CI_REF=clean-hint-constr
+    equations_CI_GITURL=https://github.com/ppedrot/Coq-Equations
+
+    fiat_parsers_CI_REF=clean-hint-constr
+    fiat_parsers_CI_GITURL=https://github.com/ppedrot/fiat
+
+fi
