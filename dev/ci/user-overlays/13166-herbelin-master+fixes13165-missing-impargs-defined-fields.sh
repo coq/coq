2b8a11101a1f152f78f0f8c924701e5f3915b4f7
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2020-10-16 08:57:28 +0200

    Overlay for elpi.

---
+++ dev/ci/user-overlays/13166-herbelin-master+fixes13165-missing-impargs-defined-fields.sh
@@ -0,0 +1,6 @@
+if [ "$CI_PULL_REQUEST" = "13166" ] || [ "$CI_BRANCH" = "master+fixes13165-missing-impargs-defined-fields" ]; then
+
+    elpi_CI_REF=coq-master+adapt-coq-pr13166-impargs-record-fields
+    elpi_CI_GITURL=https://github.com/herbelin/coq-elpi
+
+fi
