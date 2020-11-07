7bd93b0d6000766d766da4a73dbe44e4189b2085
Cyril Cohen <cohen@crans.org>
2020-11-08 00:22:53 +0100

    fixup

--- test-suite/ssr/ipat_swap.v
+++ test-suite/ssr/ipat_swap.v
@@ -2,7 +2,7 @@ Require Import ssreflect.
 
 Section Swap.
 
-Variable P : nat -> bool.
+Definition P n := match n with 1 => true | _ => false end.
 
 Lemma test_swap1 : forall (n : nat) (b : bool), P n = b.
 Proof. move=> /[swap] b n; suff: P n = b by []. Abort.
