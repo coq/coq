a028e70817e7fd43b35643a8faa9bc7f59897ddb
Enrico Tassi <Enrico.Tassi@Inria.fr>
2020-11-09 14:55:23 +0100

    [compat] remove 8.10

--- test-suite/success/CompatOldOldFlag.v
+++
@@ -1,6 +0,0 @@
-(* -*- coq-prog-args: ("-compat" "8.10") -*- *)
-(** Check that the current-minus-three compatibility flag actually requires the relevant modules. *)
-Import Coq.Compat.Coq813.
-Import Coq.Compat.Coq812.
-Import Coq.Compat.Coq811.
-Import Coq.Compat.Coq810.
