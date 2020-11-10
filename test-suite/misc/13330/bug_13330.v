332bb8c5199eb264d09d2810546170e0654f4527
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2020-11-10 13:35:35 +0100

    Add the test as a misc script.
    I left the other test as a v file since it might become relevant when the
    corresponding Qed bug is fixed.

---
+++ test-suite/misc/13330/bug_13330.v
@@ -0,0 +1,16 @@
+Polymorphic Inductive path {A : Type} (x : A) : A -> Type :=
+  refl : path x x.
+
+Goal False.
+Proof.
+simple refine (let H : False := _ in _).
++ exact_no_check I.
++ assert (path true false -> path false true).
+  (** Create a dummy polymorphic side-effect *)
+  {
+    intro IHn.
+    rewrite IHn.
+    reflexivity.
+  }
+  exact H.
+Qed.
