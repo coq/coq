146415fb624c182493d46413d738a3c2c3f47e02
Pierre Roux <pierre@roux01.fr>
2020-11-13 12:03:19 +0100

    [CI] Update coq_makefile

--- test-suite/coq-makefile/mlpack1/run.sh
+++ test-suite/coq-makefile/mlpack1/run.sh
@@ -12,6 +12,10 @@ make install DSTROOT="$PWD/tmp"
 sort > desired <<EOT
 .
 ./test
+./test/.coq-native
+./test/.coq-native/Ntest_test.cmi
+./test/.coq-native/Ntest_test.cmx
+./test/.coq-native/Ntest_test.cmxs
 ./test/test.glob
 ./test/test_plugin.cmi
 ./test/test_plugin.cmx
@@ -20,4 +24,5 @@ sort > desired <<EOT
 ./test/test.v
 ./test/test.vo
 EOT
+(coqc -config | grep -q "NATIVE_COMPILER_DEFAULT=yes") || sed -i.bak '/\.coq-native/d' desired
 exec diff -u desired actual
