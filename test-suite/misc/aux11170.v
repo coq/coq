735fcd3d1bba5b848e3673e1e0cb245077e7505d
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2020-11-21 11:44:49 +0100

    Remove dependency on BinNat.
    (cherry picked from commit 8e152ab7156c6c642bb4665d4610cc8c49242141)

--- test-suite/misc/aux11170.v
+++ test-suite/misc/aux11170.v
@@ -1,7 +1,6 @@
-Require Import BinNat.
 Fixpoint T n := match n with O => nat | S n => nat -> T n end.
 Fixpoint app n : T n -> nat :=
   match n with O => fun x => x | S n => fun f => app n (f 0) end.
-Definition n := N.to_nat 5000.
+Definition n := (fix aux n := match n with S n => aux n + aux n | O => 1 end) 13.
 Axiom f : T n.
 Eval vm_compute in let t := (app n f, 0) in snd t.
