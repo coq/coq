5510629f8b2aa7bd32edc955d6ce0baae8b00f45
Jim Fehrle <jim.fehrle@gmail.com>
2020-11-30 11:31:57 -0800

    Revert removal of eoi_entry in #13447

--- parsing/pcoq.mli
+++ parsing/pcoq.mli
@@ -120,6 +120,7 @@ end
 (** Parse a string *)
 
 val parse_string : 'a Entry.t -> ?loc:Loc.t -> string -> 'a
+val eoi_entry : 'a Entry.t -> 'a Entry.t
 
 type gram_universe [@@deprecated "Deprecated in 8.13"]
 [@@@ocaml.warning "-3"]
@@ -180,6 +181,7 @@ module Prim :
 module Constr :
   sig
     val constr : constr_expr Entry.t
+    val constr_eoi : constr_expr Entry.t
     val lconstr : constr_expr Entry.t
     val binder_constr : constr_expr Entry.t
     val term : constr_expr Entry.t
