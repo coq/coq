0de3acedfb4da3f5bbb804101b9998e3c6d785cd
Fabian Kunze <fkunze@fkunze.de>
2020-12-03 17:02:48 +0100

    fix #9319 by checking that files in VFILES realy exist
    also added tests: make should mention the file name of missing files, and v files should be generatable in Makefile.local during make itself

---
+++ test-suite/coq-makefile/missing-included/run.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+. ../template/init.sh
+
+cat > _CoqProject <<EOF
+-R ./theories/ ""
+theories/b.v
+theories/a.v
+theories/noSuchFile.v
+EOF
+touch theories/a.v
+cat > theories/b.v <<EOF
+Require a.
+EOF
+
+coq_makefile -f _CoqProject -o Makefile
+
+if make 2> stdErr.log; then exit 1; fi
+
+# on osx it says "No rule to make target `.Makefile.d', needed by `theories/b.vo'.  Stop."
+#grep -q noSuchFile stdErr.log
