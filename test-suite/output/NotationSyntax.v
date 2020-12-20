1ca6daf280492909c362ff7b4112616f64ba1423
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2020-12-20 11:51:01 +0100

    Notations for numerals are treated natively and should not modify the parser.
    For instance:
    
      Notation "0" := tt.
      Check 0%nat.
    
    should not return "tt".

--- test-suite/output/NotationSyntax.v
+++ test-suite/output/NotationSyntax.v
@@ -1,6 +1,10 @@
 (* Various meaningless notations *)
-Notation "1" := 0 (at level 3).
 Fail Notation "#" := 0 (only parsing, only parsing).
 Fail Notation "#" := 0 (only parsing, only printing).
 Fail Notation "#" := 0 (only printing, only printing).
 Notation "#" := 0 (only parsing, format "#").
+
+(* Alerting about primitive syntax *)
+Notation "1" := tt (at level 3).
+Check 1%nat.
+Fail Reserved Notation "1".
