b5e989cd9865f1f89014a4a0edafc2fd41fdc719
Yves Bertot <Yves.Bertot@inria.fr>
2021-02-04 19:24:33 +0100

    repair ci-overlay

--- dev/ci/user-overlays/13448-ybertot-fix_simpl_never_wrt_delta.sh
+++ dev/ci/user-overlays/13448-ybertot-fix_simpl_never_wrt_delta.sh
@@ -1,6 +1,2 @@
-if [ "$CI_PULL_REQUEST" = "13448" ] || [ "$CI_BRANCH" = "fix_simpl_never_wrt_delta" ]; then
-
-    overlay hott https://github.com/ybertot/HoTT respect_simpl_never_flags
-    overlay stdpp https://gitlab.inria.fr/bertot/stdpp respect_simpl_never_flags
-
-fi
+overlay hott https://github.com/ybertot/HoTT respect_simpl_never_flags 13448
+overlay stdpp https://gitlab.inria.fr/bertot/stdpp respect_simpl_never_flags 13448
