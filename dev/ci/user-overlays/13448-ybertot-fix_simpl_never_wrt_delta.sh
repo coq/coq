96c1ed00c710dfc2dc233f9cb198ecaebacfe918
Yves Bertot <Yves.Bertot@inria.fr>
2021-02-05 20:31:40 +0100

    attempt to solve the ci-perennial problem

--- dev/ci/user-overlays/13448-ybertot-fix_simpl_never_wrt_delta.sh
+++ dev/ci/user-overlays/13448-ybertot-fix_simpl_never_wrt_delta.sh
@@ -1,2 +1,3 @@
 overlay hott https://github.com/ybertot/HoTT respect_simpl_never_flags 13448
 overlay stdpp https://gitlab.inria.fr/bertot/stdpp respect_simpl_never_flags 13448
+overlay perennial https://github.com/ybertot/perennial respect_simpl_never_flags 13448
