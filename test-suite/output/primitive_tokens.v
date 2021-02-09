b6fb8c0f87652463c3269f97c8d0ad4f33e89617
Enrico Tassi <Enrico.Tassi@Inria.fr>
2021-02-09 18:26:49 +0100

    [test-suite] test for primitive tokens in patterns

--- test-suite/output/primitive_tokens.v
+++ test-suite/output/primitive_tokens.v
@@ -1,5 +1,7 @@
 Require Import String.
 
+Record test := { field : nat }.
+
 Open Scope string_scope.
 
 Unset Printing Notations.
@@ -7,9 +9,15 @@ Unset Printing Notations.
 Check "foo".
 Check 1234.
 Check 1 + 2.
+Check match "a" with "a" => true | _ => false end.
+Check match 1 with 1 => true | _ => false end.
+Check {| field := 7 |}.
 
-Unset Printing Primitive Tokens.
+Set Printing Raw Literals.
 
 Check "foo".
 Check 1234.
 Check 1 + 2.
+Check match "a" with "a" => true | _ => false end.
+Check match 1 with 1 => true | _ => false end.
+Check {| field := 7 |}.
