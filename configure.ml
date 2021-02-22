de250a5f4931d4cbdcd660b17538cabd939478c1
Enrico Tassi <Enrico.Tassi@Inria.fr>
2021-02-22 10:41:02 +0100

    update version number to 8.13.1

--- configure.ml
+++ configure.ml
@@ -12,8 +12,8 @@
 #load "str.cma"
 open Printf
 
-let coq_version = "8.13.0"
-let coq_macos_version = "8.13.0" (** "[...] should be a string comprised of
+let coq_version = "8.13.1"
+let coq_macos_version = "8.13.1" (** "[...] should be a string comprised of
 three non-negative, period-separated integers [...]" *)
 let vo_magic = 81300
 let state_magic = 581300
