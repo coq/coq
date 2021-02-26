1574adb511414eece17c1a90b726d70da68a7386
Emilio Jesus Gallego Arias <e+git@x80.org>
2021-02-26 19:30:18 +0100

    [ocaml] [ci] Bump Ocaml edge to 4.12.0

--- tools/configure/configure.ml
+++ tools/configure/configure.ml
@@ -614,8 +614,9 @@ let camltag = match caml_version_list with
     48: implicit elimination of optional arguments: too common
     58: "no cmx file was found in path": See https://github.com/ocaml/num/issues/9
     67: "unused functor parameter" seems totally bogus
+    68: "This pattern depends on mutable state" no idea what it means, dune builds don't display it
 *)
-let coq_warnings = "-w +a-4-9-27-41-42-44-45-48-58-67"
+let coq_warnings = "-w +a-4-9-27-41-42-44-45-48-58-67-68"
 let coq_warn_error =
     if !prefs.warn_error
     then "-warn-error +a"
