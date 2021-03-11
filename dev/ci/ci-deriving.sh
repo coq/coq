f4579a0bcdbdba6304a9970fc6ea6190a68f9c8c
Emilio Jesus Gallego Arias <e+git@x80.org>
2021-03-11 17:19:07 +0100

    [ci] [gitlab] Remove ad-hoc mathcomp install macros
    They should not be necessary today as they date from the shareable
    pre-artifact epoch, an incur in an slowdown.

--- dev/ci/ci-deriving.sh
+++ dev/ci/ci-deriving.sh
@@ -3,8 +3,6 @@
 ci_dir="$(dirname "$0")"
 . "${ci_dir}/ci-common.sh"
 
-install_ssreflect
-
 git_download deriving
 
 ( cd "${CI_BUILD_DIR}/deriving" && make && make tests && make install )
--- dev/ci/ci-quickchick.sh
+++ dev/ci/ci-quickchick.sh
@@ -3,8 +3,6 @@
 ci_dir="$(dirname "$0")"
 . "${ci_dir}/ci-common.sh"
 
-install_ssreflect
-
 git_download quickchick
 
 ( cd "${CI_BUILD_DIR}/quickchick" && make && make install)
