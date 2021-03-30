c76dea14aed728ef48756f0cb82cae19152f0f0b
Anton Trunov <anton.a.trunov@gmail.com>
2021-03-30 18:47:59 +0300

    Update overlays to new format

--- dev/ci/user-overlays/13069-Zimmi48-maximal-implicit-length.sh
+++ dev/ci/user-overlays/13069-Zimmi48-maximal-implicit-length.sh
@@ -1,8 +1,2 @@
-if [ "$CI_PULL_REQUEST" = "13069" ] || [ "$CI_BRANCH" = "maximal-implicit-length" ]; then
-
-    paramcoq_CI_REF=overlay-13069
-    paramcoq_CI_GITURL=https://github.com/Zimmi48/paramcoq
-
-    tlc_CI_REF=overlay-13069
-    tlc_CI_GITURL=https://github.com/Zimmi48/tlc
-fi
+overlay paramcoq https://github.com/Zimmi48/paramcoq overlay-13069 13069
+overlay tlc https://github.com/Zimmi48/tlc overlay-13069 13069
