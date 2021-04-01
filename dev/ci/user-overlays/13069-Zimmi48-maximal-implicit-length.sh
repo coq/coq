d1f26b9d70d1d3e30e316812b1c3d66ebccf8465
Anton Trunov <anton.a.trunov@gmail.com>
2021-04-01 11:36:30 +0300

    Update overlay for tlc

--- dev/ci/user-overlays/13069-Zimmi48-maximal-implicit-length.sh
+++ dev/ci/user-overlays/13069-Zimmi48-maximal-implicit-length.sh
@@ -1,2 +1,2 @@
 overlay paramcoq https://github.com/Zimmi48/paramcoq overlay-13069 13069
-overlay tlc https://github.com/Zimmi48/tlc overlay-13069 13069
+overlay tlc https://github.com/anton-trunov/tlc overlay-13069 13069
