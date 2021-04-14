3721746580a0d53809d02c5c77580a06ef29ef84
Enrico Tassi <Enrico.Tassi@inria.fr>
2021-04-14 19:41:01 +0200

    Update dev/ci/user-overlays/14111-gares-update-elpi.sh
    Co-authored-by: Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>

--- dev/ci/user-overlays/14111-gares-update-elpi.sh
+++ dev/ci/user-overlays/14111-gares-update-elpi.sh
@@ -1,2 +1,2 @@
 overlay elpi https://github.com/LPCIC/coq-elpi coq-master+1.9.5 14111
-overlay hierarchy-builder https://github.com/math-comp/hierarchy-builder coq-master+1.1.0 14111
+overlay hierarchy_builder https://github.com/math-comp/hierarchy-builder coq-master+1.1.0 14111
