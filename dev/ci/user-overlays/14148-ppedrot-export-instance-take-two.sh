810283a3b006675b450e45d026c224e0f6ab3a7a
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2021-04-22 11:37:25 +0200

    Adding user overlays.

---
+++ dev/ci/user-overlays/14148-ppedrot-export-instance-take-two.sh
@@ -0,0 +1,9 @@
+overlay elpi https://github.com/ppedrot/coq-elpi export-instance-take-two 14148
+
+overlay equations https://github.com/ppedrot/Coq-Equations export-instance-take-two 14148
+
+overlay metacoq https://github.com/ppedrot/metacoq export-instance-take-two 14148
+
+overlay mtac2 https://github.com/ppedrot/Mtac2 export-instance-take-two 14148
+
+overlay quickchick https://github.com/ppedrot/QuickChick export-instance-take-two 14148
