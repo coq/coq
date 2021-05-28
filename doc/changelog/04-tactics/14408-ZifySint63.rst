106bd8238e970f63acdebcaaf822294b27b78e42
Ana Borges <ana.agvb@gmail.com>
2021-05-28 16:06:51 +0100

    Add zify support for Sint63

---
+++ doc/changelog/04-tactics/14408-ZifySint63.rst
@@ -0,0 +1,4 @@
+- **Added:**
+  ``zify`` (``lia``/``nia``) support for ``Sint63``.
+  (`#14408 <https://github.com/coq/coq/pull/14408>`_,
+  by Ana Borges, with help from Frédéric Besson).
--- test-suite/micromega/int63.v
+++ test-suite/micromega/int63.v
@@ -4,7 +4,7 @@ Require ZifyInt63.
 
 Open Scope int63_scope.
 
-Goal forall (x:int), 0 <= x = true.
+Goal forall (x:int), 0 <=? x = true.
 Proof. lia. Qed.
 
 Goal max_int = 9223372036854775807.
@@ -16,7 +16,7 @@ Proof. lia. Qed.
 Goal wB = (2^63)%Z.
 Proof. lia. Qed.
 
-Goal forall x y, x + y <= max_int = true.
+Goal forall x y, x + y <=? max_int = true.
 Proof. lia. Qed.
 
 Goal forall x, x <> 0 -> x / x = 1.
