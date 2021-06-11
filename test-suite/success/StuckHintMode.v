8cfb13b5964335d80b7c8d5097d420c138b51926
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2021-06-11 12:20:16 +0200

    Add test for possible bug encountered in #14480

---
+++ test-suite/success/StuckHintMode.v
@@ -0,0 +1,24 @@
+
+Require Import Coq.Setoids.Setoid Coq.Classes.Morphisms.
+
+Class PartialOrder {A} (R : relation A) : Prop := {
+  partial_order_pre :> PreOrder R;
+}.
+Global Hint Mode PartialOrder - ! : typeclass_instances.
+
+Axiom Permutation : forall {A:Type}, list A -> list A -> Prop.
+
+Infix "≡ₚ" := Permutation (at level 70, no associativity).
+
+Global Declare Instance Permutation_cons A :
+ Proper (Logic.eq ==> @Permutation A ==> @Permutation A) (@cons A) | 7.
+(* priority < 7 does not trigger the bug *)
+
+Global Declare Instance Permutation_Equivalence A : Equivalence (@Permutation A).
+
+Lemma bla A (x:A) (lc lc' lac lbc : list A) (Hc: lac ++ lbc ≡ₚ lc') (HH:x :: lc' ≡ₚ lc)
+  : True /\ x :: lac ++ lbc ≡ₚ lc.
+Proof.
+  rewrite Hc.
+  auto.
+Qed.
