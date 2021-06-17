ef5abf9c41e36e14ba87d5297f2752aee051525a
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2021-06-17 14:42:30 +0200

    Fast pretyping of application.
    We use a fast path for types that are already in product form, and keep an
    explicit substitution around.
    
    Fixes #12118 (mostly). The algorithm is still quadratic outside of the fast
    path, and there is another stupid source of quadraticity even in the fast
    path, but the major source of slowdown has been removed.

--- pretyping/pretyping.ml
+++ pretyping/pretyping.ml
@@ -824,15 +824,32 @@ struct
         else sigma, resj
       | _ -> sigma, resj
     in
-    let rec apply_rec env sigma n resj resj_before_bidi candargs bidiargs = function
-      | [] -> sigma, resj, resj_before_bidi, List.rev bidiargs
+    let rec apply_rec env sigma n jval (subs, typ) val_before_bidi candargs bidiargs = function
+      | [] ->
+        let typ = Vars.esubst Vars.lift_substituend subs typ in
+        let resj = { uj_val = jval; uj_type = typ } in
+        sigma, resj, val_before_bidi, List.rev bidiargs
       | c::rest ->
         let bidi = n >= nargs_before_bidi in
         let argloc = loc_of_glob_constr c in
-        let sigma, resj, trace = Coercion.inh_app_fun ~program_mode resolve_tc !!env sigma resj in
-        let resty = whd_all !!env sigma resj.uj_type in
-        match EConstr.kind sigma resty with
-        | Prod (na,c1,c2) ->
+        let sigma, jval, na, c1, subs, c2, trace = match EConstr.kind sigma typ with
+        | Prod (na, c1, c2) ->
+          (* Fast path *)
+          let c1 = Vars.esubst Vars.lift_substituend subs c1 in
+          sigma, jval, na, c1, subs, c2, Coercion.empty_coercion_trace
+        | _ ->
+          let resj = { uj_val = jval; uj_type = Vars.esubst Vars.lift_substituend subs typ } in
+          let sigma, cresj, trace = Coercion.inh_app_fun ~program_mode resolve_tc !!env sigma resj in
+          let resty = whd_all !!env sigma cresj.uj_type in
+          let na, c1, c2 = match EConstr.kind sigma resty with
+          | Prod (na, c1, c2) -> (na, c1, c2)
+          | _ ->
+            let sigma, hj = pretype empty_tycon env sigma c in
+            error_cant_apply_not_functional
+              ?loc:(Loc.merge_opt floc argloc) !!env sigma resj [|hj|]
+          in
+          sigma, cresj.uj_val, na, c1, Esubst.subs_id 0, c2, trace
+        in
           let (sigma, hj), bidiargs =
             if bidi then
               (* We want to get some typing information from the context before
@@ -856,16 +873,13 @@ struct
               end
           in
           let sigma, ujval = adjust_evar_source sigma na.binder_name ujval in
-          let value, typ = app_f n (j_val resj) ujval, subst1 ujval c2 in
-          let resj = { uj_val = value; uj_type = typ } in
-          let resj_before_bidi = if bidi then resj_before_bidi else resj in
-          apply_rec env sigma (n+1) resj resj_before_bidi candargs bidiargs rest
-        | _ ->
-          let sigma, hj = pretype empty_tycon env sigma c in
-          error_cant_apply_not_functional
-            ?loc:(Loc.merge_opt floc argloc) !!env sigma resj [|hj|]
+          let subs = Esubst.subs_cons (Vars.make_substituend ujval) subs in
+          let jval = app_f n jval ujval in
+          let val_before_bidi = if bidi then val_before_bidi else jval in
+          apply_rec env sigma (n+1) jval (subs, c2) val_before_bidi candargs bidiargs rest
     in
-    let sigma, resj, resj_before_bidi, bidiargs = apply_rec env sigma 0 fj fj candargs [] args in
+    let typ = (Esubst.subs_id 0, fj.uj_type) in
+    let sigma, resj, val_before_bidi, bidiargs = apply_rec env sigma 0 fj.uj_val typ fj.uj_val candargs [] args in
     let sigma, resj = refresh_template env sigma resj in
     let sigma, resj, otrace = inh_conv_coerce_to_tycon ?loc ~program_mode resolve_tc env sigma resj tycon in
     let refine_arg n (sigma,t) (newarg,ty,origarg,trace) =
@@ -880,7 +894,7 @@ struct
     in
     (* We now refine any arguments whose typing was delayed for
        bidirectionality *)
-    let t = resj_before_bidi.uj_val in
+    let t = val_before_bidi in
     let sigma, t = List.fold_left_i refine_arg nargs_before_bidi (sigma,t) bidiargs in
     (* If we did not get a coercion trace (e.g. with `Program` coercions, we
     replaced user-provided arguments with inferred ones. Otherwise, we apply
