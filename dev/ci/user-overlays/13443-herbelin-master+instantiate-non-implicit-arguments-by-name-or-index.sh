79bc84982af04fab29072dadeb01b037d06cf0e8
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2021-07-03 21:03:08 +0200

    Adding overlays for equations and elpi.

---
+++ dev/ci/user-overlays/13443-herbelin-master+instantiate-non-implicit-arguments-by-name-or-index.sh
@@ -0,0 +1,2 @@
+overlay elpi https://github.com/herbelin/coq-elpi coq-master+adapt-coq-pr13443-non-implicit-args-by-name 13443
+overlay equations https://github.com/herbelin/Coq-Equations master+adapt-coq-pr13443-non-implicit-args-by-name 13443
