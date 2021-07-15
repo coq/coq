81f0ec5786528d88326eade97a576fa5f589b4ea
Ali Caglayan <alizter@gmail.com>
2021-07-15 23:05:29 +0100

    Add test for #14652
    (cherry picked from commit e503f6870864e5b3e285c2f2c3f93673801f436d)

---
+++ test-suite/bugs/closed/bug_14652.v
@@ -0,0 +1,33 @@
+Record myRecord := {
+  my_class : Type ;
+}.
+
+Fail Instance my_instance : forall x, my_class x.
+
+Existing Class my_class.
+
+Instance my_instance : forall x, my_class x. Abort.
+
+Definition my_existing_instance : forall x, my_class x. Admitted.
+
+#[export]
+Existing Instance my_existing_instance.
+
+Set Primitive Projections.
+
+Record myOtherRecord := {
+  my_other_class : Type ;
+}.
+
+Fail Instance my_other_instance : forall x, my_other_class x.
+
+Existing Class my_other_class.
+
+Instance my_other_instance : forall x, my_other_class x. Abort.
+
+Definition my_other_existing_instance : forall x, my_other_class x. Admitted.
+
+(* ??? *)
+(* Used to be: Constant does not build instances of a declared type class. *)
+#[export]
+Existing Instance my_other_existing_instance.
