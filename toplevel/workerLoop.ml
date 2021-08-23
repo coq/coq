910bdcdccc9592079798f49e5a52e13147c96b55
Enrico Tassi <Enrico.Tassi@Inria.fr>
2021-08-23 17:42:00 +0200

    [sysinit] fix oversight
    The old code was not passing all the arguments to init_toploop which
    in turn was never really called. This kind of worked because stm
    workers do receive a state by master, so generating the initial one
    locally is not necessary strictly speaking.

--- toplevel/workerLoop.ml
+++ toplevel/workerLoop.ml
@@ -12,10 +12,10 @@ let worker_parse_extra extra_args =
   let stm_opts, extra_args = Stmargs.parse_args ~init:Stm.AsyncOpts.default_opts extra_args in
   ((),stm_opts), extra_args
 
-let worker_init init ((),_) _injections ~opts =
+let worker_init init ((),stm_opts)  injections ~opts : Vernac.State.t =
   Flags.quiet := true;
   init ();
-  Coqtop.init_toploop opts
+  Coqtop.init_toploop opts stm_opts injections
 
 let worker_specific_usage name = Usage.{
   executable_name = name;
@@ -31,6 +31,8 @@ let start ~init ~loop name =
     usage = worker_specific_usage name;
     initial_args = Coqargs.default;
     init_extra = worker_init init;
-    run = (fun ((),_) ~opts:_ _state (* why is state not used *) -> loop ());
+    run = (fun ((),_) ~opts:_ (_state : Vernac.State.t) ->
+      (* the state is not used since the worker will receive one from master *)
+      loop ());
   } in
   start_coq custom
