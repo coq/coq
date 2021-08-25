3d2e4e4a86713de012ea04511c54b0b1e95843bd
Emilio Jesus Gallego Arias <e+git@x80.org>
2021-08-25 13:42:56 +0200

    [configure] Reworking of directory configuration
    Still more work to do.

--- tools/configure/configure.ml
+++ tools/configure/configure.ml
@@ -382,20 +382,34 @@ type path_style =
   | Absolute of string (* Should start with a "/" *)
   | Relative of string (* Should not start with a "/" *)
 
-let install prefs = [
-  "COQPREFIX", "Coq", prefs.prefix,
-    Relative "", Relative "";
-  "COQLIBINSTALL", "the Coq library", prefs.libdir,
-    Relative "lib", Relative "lib/coq";
-  "CONFIGDIR", "the Coqide configuration files", prefs.configdir,
-    Relative "config", Absolute "/etc/xdg/coq";
-  "DATADIR", "the Coqide data files", prefs.datadir,
-    Relative "share", Relative "share/coq";
-  "MANDIR", "the Coq man pages", prefs.mandir,
-    Relative "man", Relative "share/man";
-  "DOCDIR", "documentation prefix path for all Coq packages", prefs.docdir,
-    Relative "doc", Relative "share/doc";
- ]
+module InstallDir = struct
+
+  type t =
+    { var : string
+    (** Makefile variable to write *)
+    ; msg : string
+    (** Description of the directory  *)
+    ; uservalue : string option
+    (** Value given explictly by the user *)
+    ; selfcontainedlayout : path_style
+    (** Path style when layout is "local" *)
+    ; unixlayout : path_style
+    (** Path style for installation *)
+    }
+
+  let make var msg uservalue selfcontainedlayout unixlayout =
+    { var; msg; uservalue; selfcontainedlayout; unixlayout }
+
+end
+
+let install prefs =
+  [ InstallDir.make "COQPREFIX" "Coq" prefs.prefix (Relative "") (Relative "")
+  ; InstallDir.make "COQLIBINSTALL" "the Coq library" prefs.libdir (Relative "lib") (Relative "lib/coq")
+  ; InstallDir.make "CONFIGDIR" "the Coqide configuration files" prefs.configdir (Relative "config") (Absolute "/etc/xdg/coq")
+  ; InstallDir.make "DATADIR" "the Coqide data files" prefs.datadir (Relative "share") (Relative "share/coq")
+  ; InstallDir.make "MANDIR" "the Coq man pages" prefs.mandir (Relative "man") (Relative "share/man")
+  ; InstallDir.make "DOCDIR" "documentation prefix path for all Coq packages" prefs.docdir (Relative "doc") (Relative "share/doc")
+  ]
 
 let strip_trailing_slash_if_any p =
   if p.[String.length p - 1] = '/' then String.sub p 0 (String.length p - 1) else p
@@ -421,36 +435,39 @@ let find_suffix prefix path = match prefix with
      else
        Absolute path
 
-let do_one_instdir prefs arch (var,msg,uservalue,selfcontainedlayout,unixlayout) =
-  let dir,suffix =
-    let env_prefix =
-      match prefs.prefix with
-      | None ->
-        begin
-          try Some (Sys.getenv "COQ_CONFIGURE_PREFIX")
-          with
-          | Not_found when prefs.interactive -> None
-          | Not_found -> Some Sys.(getcwd () ^ "/../install/default")
-        end
-      | p -> p
-    in match uservalue, env_prefix with
-    | Some d, p -> d,find_suffix p d
-    | _, Some p ->
-      let suffix = if (arch_is_win32 arch) then selfcontainedlayout else relativize unixlayout in
-      use_suffix p suffix, suffix
-    | _, p ->
-      let suffix = if (unix arch) then unixlayout else selfcontainedlayout in
-      let base = if (unix arch) then "/usr/local" else "C:/coq" in
-      let dflt = use_suffix base suffix in
-      let () = printf "Where should I install %s [%s]? " msg dflt in
-      let line = read_line () in
-      if line = "" then (dflt,suffix) else (line,find_suffix None line)
+(* This computes the actual effective path for an install directory,
+   based on the given prefix; if prefix is absent, it is assumed that
+   the profile is "local" *)
+let do_one_instdir ~prefix ~arch InstallDir.{var; msg; uservalue; selfcontainedlayout; unixlayout} =
+  (var,msg),
+  match uservalue, prefix with
+  | Some d, p -> d, find_suffix p d
+  | None, Some p ->
+    let suffix = if (arch_is_win32 arch) then selfcontainedlayout else relativize unixlayout in
+    use_suffix p suffix, suffix
+  | None, None ->
+    let suffix = if (unix arch) then unixlayout else selfcontainedlayout in
+    let base = if (unix arch) then "/usr/local" else "C:/coq" in
+    let dflt = use_suffix base suffix in
+    let () = printf "Where should I install %s [%s]? " msg dflt in
+    let line = read_line () in
+    if line = "" then (dflt,suffix) else (line,find_suffix None line)
+
+let install_dirs prefs arch =
+  let prefix =
+    match prefs.prefix with
+    | None ->
+      begin
+        try Some (Sys.getenv "COQ_CONFIGURE_PREFIX")
+        with
+        | Not_found when prefs.interactive -> None
+        | Not_found -> Some Sys.(getcwd () ^ "/../install/default")
+      end
+    | p -> p
   in
-  (var,msg,dir,suffix)
+  List.map (do_one_instdir ~prefix ~arch) (install prefs)
 
-let install_dirs prefs arch = List.map (do_one_instdir prefs arch) (install prefs)
-
-let select var install_dirs = List.find (fun (v,_,_,_) -> v=var) install_dirs
+let select var install_dirs = List.find (fun ((v,_),_) -> v=var) install_dirs |> snd
 
 module CoqEnv = struct
   type t =
@@ -465,15 +482,12 @@ module CoqEnv = struct
 end
 
 let resolve_coqenv install_dirs =
-  let coqlib,coqlibsuffix =
-    let (_,_,d,s) = select "COQLIBINSTALL" install_dirs in d,s in
-  let docdir,docdirsuffix =
-    let (_,_,d,s) = select "DOCDIR" install_dirs in d,s in
-  let configdir,configdirsuffix =
-    let (_,_,d,s) = select "CONFIGDIR" install_dirs in d,s in
-  let datadir,datadirsuffix =
-    let (_,_,d,s) = select "DATADIR" install_dirs in d,s in
-  { CoqEnv.coqlib; coqlibsuffix; docdir; docdirsuffix; configdir; configdirsuffix; datadir; datadirsuffix }
+  let coqlib, coqlibsuffix = select "COQLIBINSTALL" install_dirs in
+  let docdir, docdirsuffix = select "DOCDIR" install_dirs in
+  let configdir, configdirsuffix = select "CONFIGDIR" install_dirs in
+  let datadir,datadirsuffix = select "DATADIR" install_dirs in
+  { CoqEnv.coqlib; coqlibsuffix; docdir; docdirsuffix
+  ; configdir; configdirsuffix; datadir; datadirsuffix }
 
 (** * CC runtime flags *)
 
@@ -578,7 +592,7 @@ let print_summary prefs arch operating_system camlenv vmbyteflags custom_flag be
   pr "  Native Compiler enabled     : %s\n\n" (pr_native prefs.nativecompiler);
   (pr "  Paths for true installation:\n";
    List.iter
-     (fun (_,msg,dir,_) -> pr "  - %s will be copied in %s\n" msg (esc dir))
+     (fun ((_,msg),(dir,_)) -> pr "  - %s will be copied in %s\n" msg (esc dir))
      install_dirs);
   pr "\n";
   pr "If anything is wrong above, please restart './configure'.\n\n";
@@ -706,8 +720,8 @@ let write_makefile prefs camlenv custom_flag vmbyteflags natdynlinkflag install_
   pr "VMBYTEFLAGS=%s\n" (String.concat " " vmbyteflags);
   pr "%s\n\n" !build_loadpath;
   pr "# Paths for true installation\n";
-  List.iter (fun (v,msg,_,_) -> pr "# %s: path for %s\n" v msg) install_dirs;
-  List.iter (fun (v,_,dir,_) -> pr "%s=%S\n" v dir) install_dirs;
+  List.iter (fun ((v,msg),_) -> pr "# %s: path for %s\n" v msg) install_dirs;
+  List.iter (fun ((v,_),(dir,_)) -> pr "%s=%S\n" v dir) install_dirs;
   pr "\n# Coq version\n";
   pr "VERSION=%s\n" coq_version;
   pr "# Objective-Caml compile command\n";
