herbelin/github-coq

Manipulating univ_entry and universe_binders together.
This allows in particular to centralize the calls to declare_univ_binders.

vernac/declareInd.ml
@@ -154,7 +154,7 @@ type one_inductive_impls =
   Impargs.manual_implicits (* for inds *) *
   Impargs.manual_implicits list (* for constrs *)
 
-let declare_mutual_inductive_with_eliminations ?(primitive_expected=false) ?typing_flags mie pl impls =
+let declare_mutual_inductive_with_eliminations ?(primitive_expected=false) ?typing_flags mie univs impls =
   (* spiwack: raises an error if the structure is supposed to be non-recursive,
         but isn't *)
   begin match mie.mind_entry_finite with
@@ -169,7 +169,7 @@ let declare_mutual_inductive_with_eliminations ?(primitive_expected=false) ?typi
   let (_, kn), prim = declare_mind ?typing_flags mie in
   let mind = Global.mind_of_delta_kn kn in
   if primitive_expected && not prim then warn_non_primitive_record (mind,0);
-  DeclareUniv.declare_univ_binders (GlobRef.IndRef (mind,0)) pl;
+  DeclareUniv.declare_univ_binders (GlobRef.IndRef (mind,0)) univs;
   List.iteri (fun i (indimpls, constrimpls) ->
       let ind = (mind,i) in
       let gr = GlobRef.IndRef ind in

coq/coq

vernac/declareInd.ml
@@ -154,7 +154,7 @@ type one_inductive_impls =
   Impargs.manual_implicits (* for inds *) *
   Impargs.manual_implicits list (* for constrs *)
 
-let declare_mutual_inductive_with_eliminations ?(primitive_expected=false) ?typing_flags mie pl impls =
+let declare_mutual_inductive_with_eliminations ?(primitive_expected=false) ?typing_flags mie binders impls =
   (* spiwack: raises an error if the structure is supposed to be non-recursive,
         but isn't *)
   begin match mie.mind_entry_finite with
@@ -169,7 +169,7 @@ let declare_mutual_inductive_with_eliminations ?(primitive_expected=false) ?typi
   let (_, kn), prim = declare_mind ?typing_flags mie in
   let mind = Global.mind_of_delta_kn kn in
   if primitive_expected && not prim then warn_non_primitive_record (mind,0);
-  DeclareUniv.declare_univ_binders (GlobRef.IndRef (mind,0)) pl;
+  DeclareUniv.declare_univ_binders (GlobRef.IndRef (mind,0)) binders;
   List.iteri (fun i (indimpls, constrimpls) ->
       let ind = (mind,i) in
       let gr = GlobRef.IndRef ind in