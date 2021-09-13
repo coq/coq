63ac734825ed5f78ced1ad2df60dc7cea31818e7
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2021-09-13 14:36:27 +0200

    Overlay for cast_kind changes

---
+++ dev/ci/user-overlays/14773-SkySkimmer-revertcast.sh
@@ -0,0 +1,13 @@
+overlay elpi https://github.com/SkySkimmer/coq-elpi revertcast 14773
+
+overlay quickchick https://github.com/SkySkimmer/QuickChick revertcast 14773
+
+overlay aac_tactics https://github.com/SkySkimmer/aac-tactics revertcast 14773
+
+overlay equations https://github.com/SkySkimmer/Coq-Equations revertcast 14773
+
+overlay metacoq https://github.com/SkySkimmer/metacoq revertcast 14773
+
+overlay gappa https://gitlab.inria.fr/ggilbert/gappa revertcast 14773
+
+overlay relation_algebra https://github.com/SkySkimmer/relation-algebra revertcast 14773
