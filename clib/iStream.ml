93792092b869d363a2f83653589e5cb8de8e7789
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2021-10-08 15:11:36 +0200

    Monomorphize IStream node type
    This means we remove [IStream.make], but that's not used anyway.

--- clib/iStream.ml
+++ clib/iStream.ml
@@ -8,27 +8,19 @@
 (*         *     (see LICENSE file for the text of the license)         *)
 (************************************************************************)
 
-type ('a,'r) u =
-| Nil
-| Cons of 'a * 'r
-
-type 'a node = Node of ('a,'a t) u [@@unboxed]
+type 'a node =
+  | Nil
+  | Cons of 'a * 'a t
 
 and 'a t = 'a node Lazy.t
 
-let empty = Lazy.from_val (Node Nil)
+let empty = Lazy.from_val Nil
 
-let cons x s = Lazy.from_val (Node (Cons (x, s)))
+let cons x s = Lazy.from_val (Cons (x, s))
 
-let peek x = let Node x = Lazy.force x  in x
+let peek x : _ node = Lazy.force x
 
-let thunk f = Lazy.from_fun (fun () -> Node (f ()))
-
-let rec make_node f s = match f s with
-| Nil -> Nil
-| Cons (x, s) -> (Cons (x, make f s))
-
-and make f s = lazy (Node (make_node f s))
+let thunk f : _ t = Lazy.from_fun f
 
 let rec force s = match peek s with
 | Nil -> ()
@@ -56,13 +48,13 @@ let rec map_node f = function
 | Nil -> Nil
 | Cons (x, s) -> Cons (f x, map f s)
 
-and map f s = lazy (Node (map_node f (peek s)))
+and map f s = lazy (map_node f (peek s))
 
 let rec app_node n1 s2 = match n1 with
 | Nil -> peek s2
 | Cons (x, s1) -> Cons (x, app s1 s2)
 
-and app s1 s2 = lazy (Node (app_node (peek s1) s2))
+and app s1 s2 = lazy (app_node (peek s1) s2)
 
 let rec fold f accu s = match peek s with
 | Nil -> accu
@@ -76,17 +68,17 @@ let rec map_filter_node f = function
   | Some y -> Cons (y, map_filter f s)
   end
 
-and map_filter f s = lazy (Node (map_filter_node f (peek s)))
+and map_filter f s = lazy (map_filter_node f (peek s))
 
 let rec concat_node = function
 | Nil -> Nil
 | Cons (s, sl) -> app_node (peek s) (concat sl)
 
 and concat (s : 'a t t) =
-  lazy (Node (concat_node (peek s)))
+  lazy (concat_node (peek s))
 
 let rec concat_map_node f = function
 | Nil -> Nil
 | Cons (x,s) -> app_node (peek (f x)) (concat_map f s)
 
-and concat_map f l = lazy (Node (concat_map_node f (peek l)))
+and concat_map f l = lazy (concat_map_node f (peek l))
