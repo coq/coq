acdad7968432088c619e4462667be6fa554889fa
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2021-10-08 14:55:44 +0200

    Stop using rectypes for iStream

--- clib/iStream.ml
+++ clib/iStream.ml
@@ -12,43 +12,43 @@ type ('a,'r) u =
 | Nil
 | Cons of 'a * 'r
 
-type 'a node = ('a,'a t) u
+type 'a node = Node of ('a,'a t) u [@@unboxed]
 
 and 'a t = 'a node Lazy.t
 
-let empty = Lazy.from_val Nil
+let empty = Lazy.from_val (Node Nil)
 
-let cons x s = Lazy.from_val (Cons (x, s))
+let cons x s = Lazy.from_val (Node (Cons (x, s)))
 
-let thunk = Lazy.from_fun
+let peek x = let Node x = Lazy.force x  in x
+
+let thunk f = Lazy.from_fun (fun () -> Node (f ()))
 
 let rec make_node f s = match f s with
 | Nil -> Nil
-| Cons (x, s) -> Cons (x, make f s)
+| Cons (x, s) -> (Cons (x, make f s))
 
-and make f s = lazy (make_node f s)
+and make f s = lazy (Node (make_node f s))
 
-let rec force s = match Lazy.force s with
+let rec force s = match peek s with
 | Nil -> ()
 | Cons (_, s) -> force s
 
 let force s = force s; s
 
-let is_empty s = match Lazy.force s with
+let is_empty s = match peek s with
 | Nil -> true
 | Cons (_, _) -> false
-
-let peek = Lazy.force
 
 let rec of_list = function
 | [] -> empty
 | x :: l -> cons x (of_list l)
 
-let rec to_list s = match Lazy.force s with
+let rec to_list s = match peek s with
 | Nil -> []
 | Cons (x, s) -> x :: (to_list s)
 
-let rec iter f s = match Lazy.force s with
+let rec iter f s = match peek s with
 | Nil -> ()
 | Cons (x, s) -> f x; iter f s
 
@@ -56,15 +56,15 @@ let rec map_node f = function
 | Nil -> Nil
 | Cons (x, s) -> Cons (f x, map f s)
 
-and map f s = lazy (map_node f (Lazy.force s))
+and map f s = lazy (Node (map_node f (peek s)))
 
 let rec app_node n1 s2 = match n1 with
-| Nil -> Lazy.force s2
+| Nil -> peek s2
 | Cons (x, s1) -> Cons (x, app s1 s2)
 
-and app s1 s2 = lazy (app_node (Lazy.force s1) s2)
+and app s1 s2 = lazy (Node (app_node (peek s1) s2))
 
-let rec fold f accu s = match Lazy.force s with
+let rec fold f accu s = match peek s with
 | Nil -> accu
 | Cons (x, s) -> fold f (f accu x) s
 
@@ -72,21 +72,21 @@ let rec map_filter_node f = function
 | Nil -> Nil
 | Cons (x, s) ->
   begin match f x with
-  | None -> map_filter_node f (Lazy.force s)
+  | None -> map_filter_node f (peek s)
   | Some y -> Cons (y, map_filter f s)
   end
 
-and map_filter f s = lazy (map_filter_node f (Lazy.force s))
+and map_filter f s = lazy (Node (map_filter_node f (peek s)))
 
 let rec concat_node = function
 | Nil -> Nil
-| Cons (s, sl) -> app_node (Lazy.force s) (concat sl)
+| Cons (s, sl) -> app_node (peek s) (concat sl)
 
 and concat (s : 'a t t) =
-  lazy (concat_node (Lazy.force s))
+  lazy (Node (concat_node (peek s)))
 
 let rec concat_map_node f = function
 | Nil -> Nil
-| Cons (x,s) -> app_node (Lazy.force (f x)) (concat_map f s)
+| Cons (x,s) -> app_node (peek (f x)) (concat_map f s)
 
-and concat_map f l = lazy (concat_map_node f (Lazy.force l))
+and concat_map f l = lazy (Node (concat_map_node f (peek l)))
