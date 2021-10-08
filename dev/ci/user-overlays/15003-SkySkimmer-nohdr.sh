fe1fc9eabf317db1a83879072389ec46559b73c0
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2021-10-08 17:45:33 +0200

    Overlays for remove of user_err hdr

---
+++ dev/ci/user-overlays/15003-SkySkimmer-nohdr.sh
@@ -0,0 +1,7 @@
+overlay coqhammer https://github.com/SkySkimmer/coqhammer nohdr 15003
+
+overlay equations https://github.com/SkySkimmer/Coq-Equations nohdr 15003
+
+overlay mtac2 https://github.com/SkySkimmer/Mtac2 nohdr 15003
+
+overlay vscoq https://github.com/SkySkimmer/vscoq nohdr 15003
