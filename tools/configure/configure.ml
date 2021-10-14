33df5f8d5b3b37b1f2cb0542443f57028ff0189f
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2021-10-14 10:08:48 +0200

    Upgrade version number to 8.14.0.

--- tools/configure/configure.ml
+++ tools/configure/configure.ml
@@ -13,9 +13,9 @@
 (**********************************)
 open Printf
 
-let coq_version = "8.14+rc1"
-let vo_magic = 81391
-let state_magic = 581391
+let coq_version = "8.14.0"
+let vo_magic = 81400
+let state_magic = 581400
 let is_a_released_version = true
 let verbose = ref false (* for debugging this script *)
 
