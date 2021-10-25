06791c09008d5aaeb4b544486fa389d41f75b64d
Théo Zimmermann <theo.zimmermann@inria.fr>
2021-10-25 18:04:01 +0200

    Update nixpkgs to use Dune 2.9.

--- dev/nixpkgs.nix
+++ dev/nixpkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/5c7a370a208d93d458193fc05ed84ced0ba7f387.tar.gz";
-  sha256 = "1jkn71xscsk4rb0agbp5saf06hy36qvy512zzh3881pkkn67i9js";
+  url = "https://github.com/NixOS/nixpkgs/archive/a6a0964eacef611f364ca22256d6882d8670721c.tar.gz";
+  sha256 = "0h2cv4zdlmf1di55i64vcavc0rfh7cvbnax4wll2vbjj7bahawyc";
 })
