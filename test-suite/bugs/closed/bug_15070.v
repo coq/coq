6c1a867083f034e44e90f7205a48c734f7191593
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2021-10-27 18:35:26 +0200

    Adding a test for the bug.
    (cherry picked from commit ff9862c9d313eb6028784fc5e5aebc874f2c4938)

---
+++ test-suite/bugs/closed/bug_15070.v
@@ -0,0 +1,22 @@
+Require Import PArray PrimFloat Int63.
+
+Goal False.
+Proof.
+set (f := fun x => get (set (make 2 one) 1 x) 1).
+generalize (eq_refl (f zero)).
+change (f zero = zero -> False).
+intros H0.
+generalize (eq_refl f) (eq_refl f).
+generalize f at 1 3.
+intros g H1.
+vm_compute.
+rewrite H1.
+intros H2.
+revert H0.
+rewrite H2.
+intros H3.
+apply (f_equal classify) in H3.
+revert H3.
+vm_compute.
+Fail discriminate.
+Abort.
