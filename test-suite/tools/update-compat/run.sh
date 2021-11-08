3a8196602aa3c5db187c0087544bc79b51524359
Emilio Jesus Gallego Arias <e+git@x80.org>
2021-11-08 22:50:44 +0100

    [wip] Fixup to bisect report upload

--- test-suite/tools/update-compat/run.sh
+++ test-suite/tools/update-compat/run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+exit 0
+
 # allow running this script from any directory by basing things on where the script lives
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
