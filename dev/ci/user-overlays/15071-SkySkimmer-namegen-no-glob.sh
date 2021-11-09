b627a66e5d1319e2849dd192bf6587c4d65480f0
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2021-11-09 17:34:53 +0100

    Overlays for namegen don't avoid imported defs

---
+++ dev/ci/user-overlays/15071-SkySkimmer-namegen-no-glob.sh
@@ -0,0 +1,5 @@
+overlay color https://github.com/SkySkimmer/color namegen-no-glob 15071
+
+overlay vst https://github.com/SkySkimmer/VST namegen-no-glob 15071
+
+overlay metacoq https://github.com/SkySkimmer/metacoq namegen-no-glob 15071
