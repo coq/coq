71e92bb24c1db7101c38a3c1454ee05177a3b12a
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2021-12-21 17:30:36 +0100

    Adding overlays.

---
+++ dev/ci/user-overlays/15393-ppedrot-deprecate-sigma-wrapper.sh
@@ -0,0 +1,5 @@
+overlay equations https://github.com/ppedrot/Coq-Equations deprecate-sigma-wrapper 15393
+
+overlay aac_tactics https://github.com/ppedrot/aac-tactics deprecate-sigma-wrapper 15393
+
+overlay itauto https://gitlab.inria.fr/pedrot/itauto deprecate-sigma-wrapper 15393
