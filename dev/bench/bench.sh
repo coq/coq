b26452a99833ee4efc50872af3f25914e8b9a44e
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2022-01-07 13:29:04 +0100

    rerun corn

--- dev/bench/bench.sh
+++ dev/bench/bench.sh
@@ -53,7 +53,7 @@ echo $PWD
 : "${old_coq_opam_archive_git_branch:=master}"
 : "${num_of_iterations:=1}"
 : "${timeout:=3h}"
-: "${coq_opam_packages:=coq-bignums coq-hott coq-performance-tests-lite coq-engine-bench-lite coq-mathcomp-ssreflect coq-mathcomp-fingroup coq-mathcomp-algebra coq-mathcomp-solvable coq-mathcomp-field coq-mathcomp-character coq-mathcomp-odd-order coq-math-classes coq-corn coq-flocq3 coq-compcert coq-geocoq coq-color coq-coqprime coq-coqutil coq-bedrock2 coq-rewriter coq-fiat-core coq-fiat-parsers coq-fiat-crypto coq-unimath coq-coquelicot coq-iris-examples coq-verdi coq-verdi-raft coq-fourcolor coq-rewriter-perf-SuperFast coq-perennial coq-vst}"
+: "${coq_opam_packages:=coq-bignums coq-math-classes coq-corn}"
 
 new_coq_commit=76e81f9a7d9dafb36d993aa217e8faa955efe690
 old_coq_commit=6015db928c4b4b769c6a442e986e4474fcef27d3
