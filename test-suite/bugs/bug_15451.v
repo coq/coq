850694d554828af111d785c072acd43163391a41
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2022-01-09 14:24:54 +0100

    Taking into account cofix redexes in stepwise check of fixpoint guard.
    (cherry picked from commit 34e06803847d37bf1247e53f05357cc0bc3b3020)

---
+++ test-suite/bugs/bug_15451.v
@@ -0,0 +1,31 @@
+(* Treat "match" on a "cofix" like "match" on a constructor in guard condition *)
+
+Module CofixRedex.
+
+CoInductive Stream := {hd : nat; tl : Stream}.
+Definition zeros := cofix zeros := {|hd := 0; tl := zeros|}.
+
+Fixpoint f n :=
+  match n with
+  | 0 => 0
+  | S n =>
+    match zeros with
+    | {|hd:=_|} => fun f => f n
+    end f
+  end.
+
+End CofixRedex.
+
+Module CofixRedexPrimProj.
+
+Set Primitive Projections.
+CoInductive Stream A := {hd : A; tl : Stream A}.
+Arguments hd {A} s.
+
+Fixpoint f n :=
+  match n with
+  | 0 => 0
+  | S n => (cofix cst := {|hd := (fun f => f n); tl := cst|}).(hd) f
+  end.
+
+End CofixRedexPrimProj.
