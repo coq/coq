herbelin/github-coq

Moving impargs.ml in pretyping for use in coercion.ml.

parsing/pcoq.mli
@@ -196,7 +196,7 @@ module Constr :
     val binders_fixannot : (local_binder_expr list * recursion_order_expr option) Entry.t
     val typeclass_constraint : (lname * bool * constr_expr) Entry.t
     val record_declaration : constr_expr Entry.t
-    val appl_arg : (constr_expr * explicitation CAst.t option) Entry.t
+    val appl_arg : (constr_expr * Nameops.explicitation CAst.t option) Entry.t
     val type_cstr : constr_expr Entry.t
   end
 

coq/coq

parsing/pcoq.mli
@@ -186,7 +186,7 @@ module Constr :
     val binders_fixannot : (local_binder_expr list * recursion_order_expr option) Entry.t
     val typeclass_constraint : (lname * bool * constr_expr) Entry.t
     val record_declaration : constr_expr Entry.t
-    val arg : (constr_expr * explicitation CAst.t option) Entry.t
+    val arg : (constr_expr * Nameops.explicitation CAst.t option) Entry.t
     val type_cstr : constr_expr Entry.t
   end
 