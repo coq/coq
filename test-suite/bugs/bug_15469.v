6a0844b8ab966091b2a551522f6cc778baa4f492
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2022-01-13 16:17:07 +0100

    Adding a test for the behaviour observed in #15469.

---
+++ test-suite/bugs/bug_15469.v
@@ -0,0 +1,14 @@
+Require Import Coq.ssr.ssreflect.
+
+Class Dist := dist0 : Prop.
+
+Definition dist {Dist : Dist} : Prop := Dist.
+
+Axiom later_dist : Dist.
+Axiom foo : later_dist.
+
+Goal @dist later_dist.
+Proof.
+rewrite /dist.
+match goal with [ |- later_dist ] => refine (foo) end.
+Qed.
