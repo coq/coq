a59c356a45180dd56b1ec86c8725a257bffe71fa
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2022-01-13 12:55:53 +0100

    Release 8.15.0

--- tools/configure/configure.ml
+++ tools/configure/configure.ml
@@ -19,9 +19,9 @@ open CmdArgs.Prefs
 
 let (/) = Filename.concat
 
-let coq_version = "8.15+rc1"
-let vo_magic = 81491
-let is_a_released_version = false
+let coq_version = "8.15.0"
+let vo_magic = 81500
+let is_a_released_version = true
 
 (** Default OCaml binaries *)
 
