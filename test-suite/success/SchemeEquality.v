fe9bc3e673db50bb1815b0c583fc7a92f6adc715
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2022-01-23 14:36:20 +0100

    New parametricity-based algo for generating Boolean equality on inductive types.
    At the current time, it adds support for:
    - higher-order parameters: e.g. Inductive I (F:nat->(Type->Type)->Type) := ...
    - non-Type parameters: e.g. Inductive I (f:nat->nat) := ...
    - local definitions in parameters and constructors
    - application of inductive types to non-variables:
      e.g. Inductive I := C : list nat -> I
    - not generating equality on SProp/Prop parameters
    - universe polymorphism (thanks to #15525)
    - recursively non-uniform parameters
    - reasoning under a "match"
    - primitive projections (w/o compatibility level)
    
    It still does not support:
    - indices (as in Vector.t) and dependencies in constructors (as in sigT):
      - for `Vector.t`, we could do it by using a generalization in pattern-matching
      - for sigT, this would require to manipulate heterogeneous Boolean
        equality (quite easy to do though)
    - parameters encapsulated in data types: e.g. Inductive I (T:Type*Type) := ...
    - no support for the hierarchy of universes: e.g.
      Inductive I (F:forall A, A -> A) (G:F Type Type) := ...
      could a priori be associated to "I_beq (F_eq:forall A, (A->A->bool) -> (A->A->bool))
      (F_eq_type:forall A, (A->A->Type) -> (A->A->Type))
      (G:F Type Type) (G_eq:F_eq_type Type (fun A B => A -> B -> bool))"
      where two equalities would be attached to each sort, one on bool to
      anticipate the application to a type, and the other on Type to
      prepare the possible application of a sort
    - reasoning under a "fix" or under projections
    
    It still has the peculiarity that equalities are added as a second
    context rather than glued to a Type, as in
    prod_beq : forall A B, (A->A->bool) -> (B->B->bool) -> A*B->A*B->bool;
    this is nicer in some sense but also less compositional (e.g. needs
    eta-expansion, or tricks with polymorphism).
    
    It still unfolds defined constants, while it should rely on Boolean
    equality associated to constants.
    
    Somehow, one would need attributes to parameterize the construction:
    - an attribute to choose between translation of arguments
      compositionally or by block
    - an attribute to choose homogeneous or heterogeneous equality
    - an attribute to support polymorphism (and then, Type would be
      interpreted as the pair of a A1 -> A2 -> Type and of A1 -> A2 -> bool,
      the former being degenerated for non-sorts and being
      \A1.\A2.((A1->A2->Type) * (A1->A2->bool)) for sorts.
    
    One would then also need a naming scheme to differentiate those variants.
    
    Thanks to Jim Fehrle <jim.fehrle@gmail.com>, Enrico Tassi, Jason
    Gross, Gaëtan Gilbert for suggestions and help.

--- test-suite/success/SchemeEquality.v
+++ test-suite/success/SchemeEquality.v
@@ -38,4 +38,203 @@ Module D.
   Inductive box A := c : A -> box A.
   Inductive prodbox (A B:Type) := pairbox : box A -> box B -> prodbox A B.
   Scheme Equality for prodbox.
+  Check eq_refl : prodbox_beq @{Set Set} =
+    fun (A B : Type@{Set}) eq_A eq_B =>
+    fix prodbox_eqrec (X Y : prodbox A B) {struct X} : bool :=
+      match X, Y with
+      | pairbox _ _ x x0, pairbox _ _ x1 x2 =>
+              (internal_box_beq A eq_A x x1 &&
+               internal_box_beq B eq_B x0 x2)%bool
+      end.
 End D.
+
+(* With hidden "X" and "Y" (was formerly cause of collisions) *)
+Module E.
+  Section S.
+  Variables X Y : Type.
+  Variable eq_X : X -> X -> bool.
+  Variable eq_Y : Y -> Y -> bool.
+  Inductive EI := EC : X -> Y -> EI.
+  Scheme Boolean Equality for EI.
+  End S.
+End E.
+
+(* With inductive parameters instantiated by non-variable types *)
+Module F.
+  Inductive FI := FC : list nat -> FI.
+  Scheme Boolean Equality for FI.
+
+  Inductive tree := node : list tree -> tree.
+  Scheme Boolean Equality for tree.
+
+  Inductive rose A := Leaf : A -> rose A | Node : list (rose A) -> rose A.
+  Scheme Boolean Equality for rose.
+  Print rose_beq.
+  Check eq_refl : rose_beq =
+    fun (A : Type) (eq_A : A -> A -> bool) =>
+    fix rose_eqrec (X Y : rose A) {struct X} : bool :=
+      match X with
+      | Leaf _ x => match Y with
+                    | Leaf _ x0 => eq_A x x0
+                    | Node _ _ => false
+                    end
+      | Node _ x =>
+          match Y with
+          | Leaf _ _ => false
+          | Node _ x0 => C.internal_list_beq (rose A) rose_eqrec x x0
+          end
+      end.
+End F.
+
+(* With higher-order parameters and non-Type parameters *)
+Module G.
+  Inductive GI (F:Type->Type) A := GC : F A -> F nat -> GI F A.
+  Scheme Boolean Equality for GI.
+
+  Inductive GJ (F:nat->(nat->Type->Type)->Type) (f:nat->nat) (n:nat) (A:nat->Type->Type) :=
+     GD : F 0 (fun n => list) -> F 1 A -> GJ F f n A.
+  Scheme Boolean Equality for GJ.
+End G.
+
+(* With local definitions in constructors *)
+Module H.
+  Inductive HJ A : Type := HD : let a := 0 in nat -> list A -> HJ A.
+  Scheme Boolean Equality for HJ.
+End H.
+
+(* With recursively non-uniform arguments *)
+Module I.
+  Inductive T A := C : A -> T (option A) -> T A.
+  Scheme Boolean Equality for T.
+  Check eq_refl : T_beq =
+    fix T_eqrec
+    (A : Type) (eq_A : A -> A -> bool)
+    (X Y : T A) {struct X} : bool :=
+      match X, Y with
+      | C _ x x0, C _ x1 x2 =>
+              (eq_A x x1 &&
+               T_eqrec (option A)
+                 (internal_option_beq A eq_A) x0 x2)%bool
+      end.
+End I.
+
+(* With mutual definitions *)
+Module J.
+  Inductive tree A := node : forest A -> tree A
+  with forest A := nil : forest A | cons : A -> tree A -> forest A.
+  Scheme Boolean Equality for tree.
+
+  Inductive K (F:nat->(nat->Type->Type)->Type) (A:nat->Type->Type) :=
+     D : L F A -> F 1 A -> K F A
+  with L (F:nat->(nat->Type->Type)->Type) (A:nat->Type->Type) :=
+     E : F 0 (fun n => list) -> K F A -> L F A.
+  Scheme Boolean Equality for K.
+End J.
+
+(* With "match" or "fix" *)
+Module K.
+  Inductive K1 (b:bool) (F : if b then Type else Type->Type) : Type :=
+  C1 : nat -> K1 b F.
+  Scheme Boolean Equality for K1.
+
+  Inductive K2 (b:bool) (F : if b then Type else Type->Type) : Type :=
+  C2 : (if b return (if b then Type else Type->Type) -> Type
+        then fun A => A else fun F => F nat) F ->
+       K2 b F.
+  Scheme Boolean Equality for K2.
+
+  (* Almost work *)
+  Inductive K3 (n:nat) (A : Type) : Type :=
+   C3 : (fix mkprod n := match n with 0 => A | S n => (mkprod n * A)%type end) n ->
+       K3 n A.
+  Fail Scheme Boolean Equality for K3.
+End K.
+
+Require Int63 Floats.
+
+Module ElpiTestSuite.
+
+Inductive empty := .
+Scheme Equality for empty.
+
+Inductive unit := tt.
+Scheme Equality for unit.
+
+Inductive peano := Zero | Succ (n : peano).
+Scheme Equality for peano.
+
+Inductive option A := None | Some (_ : A).
+Scheme Equality for option.
+
+Inductive pair A B := Comma (a : A) (b : B).
+Scheme Equality for pair.
+
+Inductive seq A := Nil | Cons (x : A) (xs : seq A).
+Scheme Equality for seq.
+
+Inductive nest A := NilN | ConsN (x : A) (xs : nest (pair A A)).
+Scheme Boolean Equality for nest.
+
+Inductive zeta Sender (Receiver := Sender) := Envelope (a : Sender) (ReplyTo := a) (c : Receiver).
+Scheme Boolean Equality for zeta.
+
+Inductive beta (A : (fun x : Type => x) Type) := Redex (a : (fun x : Type => x) A).
+Scheme Boolean Equality for beta.
+
+Inductive large :=
+| K1 (_ : bool)
+| K2 (_ : bool) (_ : bool)
+| K3 (_ : bool) (_ : bool) (_ : bool)
+| K4 (_ : bool) (_ : bool) (_ : bool) (_ : bool)
+| K5 (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool)
+| K6 (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool)
+| K7 (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool)
+| K8 (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool)
+| K9 (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool)
+| K10(_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool)
+| K11(_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool)
+| K12(_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool)
+| K13(_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool)
+| K14(_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool)
+| K15(_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool)
+| K16(_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool)
+| K17(_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool)
+| K18(_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool)
+| K19(_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool)
+| K20(_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool)
+| K21(_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool)
+| K22(_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool)
+| K23(_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool)
+| K24(_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool)
+| K25(_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool)
+| K26(_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool) (_ : bool).
+Scheme Equality for large.
+
+Record fo_record := { f1 : peano; f2 : bool; }.
+
+Scheme Equality for fo_record.
+
+Record pa_record A := { f3 : peano; f4 : A; }.
+Scheme Equality for pa_record.
+
+Set Primitive Projections.
+Record pr_record A := { pf3 : peano; pf4 : A; }.
+Unset Primitive Projections.
+Scheme Boolean Equality for pr_record.
+
+Variant enum := E1 | E2 | E3.
+Scheme Equality for enum.
+
+End ElpiTestSuite.
+
+(* Ignoring SProp/Prop subterms *)
+Module L.
+  Inductive seq {A} (x:A) : A -> SProp := seq_refl : seq x x.
+  Inductive A := C : forall n, seq n 0 -> A.
+  Scheme Boolean Equality for A.
+  Check eq_refl : A_beq =
+    fix A_eqrec (X Y : A) {struct X} : bool :=
+      match X, Y with
+      | C n _, C n0 _ => A.internal_nat_beq n n0
+      end.
+End L.
