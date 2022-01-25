eaf62c6cc82c7be7f69fc04eaddbe0d5fb9fa3c8
Enrico Tassi <Enrico.Tassi@Inria.fr>
2022-01-25 14:10:40 +0100

    [test-suite] expect -I to work for finding META

--- test-suite/coq-makefile/template/init.sh
+++ test-suite/coq-makefile/template/init.sh
@@ -24,4 +24,4 @@ cp_file src/test_aux.mli
 cp_file src/test.mli
 cp_file src/test_plugin.mlpack
 cp_file src/test_aux.ml
-cp_file META.coq-test-suite
+cp_file src/META.coq-test-suite
--- test-suite/coq-makefile/template/path-init.sh
+++ test-suite/coq-makefile/template/path-init.sh
@@ -4,5 +4,3 @@ set -o pipefail
 
 export PATH="$COQBIN:$PATH"
 export LC_ALL=C
-if which cygpath >/dev/null 2>&1; then OCAMLFINDSEP=\;; else OCAMLFINDSEP=:; fi
-export OCAMLPATH=$PWD/_test$OCAMLFINDSEP$OCAMLPATH
