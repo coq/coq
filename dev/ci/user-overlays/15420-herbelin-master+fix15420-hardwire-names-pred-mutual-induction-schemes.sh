c175a21c20628bc89c86e0e096206d520a8e51ef
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2022-01-26 08:17:38 +0100

    Overlays for Hott, stdlib2 and Mtac2

---
+++ dev/ci/user-overlays/15420-herbelin-master+fix15420-hardwire-names-pred-mutual-induction-schemes.sh
@@ -0,0 +1,3 @@
+overlay hott https://github.com/herbelin/HoTT master+adapt-coq-pr15420-fix-naming-arguments-ind-schemes 15537 master+fix15420-hardwire-names-pred-mutual-induction-schemes
+overlay stdlib2 https://github.com/herbelin/stdlib2 master+adapt-coq-pr15420-fix-naming-arguments-ind-schemes 15537 master+fix15420-hardwire-names-pred-mutual-induction-schemes
+overlay mtac2 https://github.com/herbelin/Mtac2 master+adapt-coq-pr15420-fix-naming-arguments-ind-schemes 15537 master+fix15420-hardwire-names-pred-mutual-induction-schemes
