9db859070c106267290ea4c10cce0dd99bf9ec48
Jim Fehrle <jim.fehrle@gmail.com>
2022-02-13 13:32:34 -0800

    Remove dead code

--- plugins/ltac/tactic_debug.mli
+++ plugins/ltac/tactic_debug.mli
@@ -9,7 +9,6 @@
 (************************************************************************)
 
 open Environ
-open Pattern
 open Names
 open Tacexpr
 open EConstr
@@ -39,30 +38,6 @@ val db_initialize : bool -> unit Proofview.NonLogical.t
 (** Prints a constr *)
 val db_constr : debug_info -> env -> evar_map -> constr -> unit Proofview.NonLogical.t
 
-(** Prints the pattern rule *)
-val db_pattern_rule :
-  debug_info -> env -> evar_map -> int -> (Genintern.glob_constr_and_expr * constr_pattern,glob_tactic_expr) match_rule -> unit Proofview.NonLogical.t
-
-(** Prints a matched hypothesis *)
-val db_matched_hyp :
-  debug_info -> env -> evar_map -> Id.t * constr option * constr -> Name.t -> unit Proofview.NonLogical.t
-
-(** Prints the matched conclusion *)
-val db_matched_concl : debug_info -> env -> evar_map -> constr -> unit Proofview.NonLogical.t
-
-(** Prints a success message when the goal has been matched *)
-val db_mc_pattern_success : debug_info -> unit Proofview.NonLogical.t
-
-(** Prints a failure message for an hypothesis pattern *)
-val db_hyp_pattern_failure :
-  debug_info -> env -> evar_map -> Name.t * constr_pattern match_pattern -> unit Proofview.NonLogical.t
-
-(** Prints a matching failure message for a rule *)
-val db_matching_failure : debug_info -> unit Proofview.NonLogical.t
-
-(** Prints an evaluation failure message for a rule *)
-val db_eval_failure : debug_info -> Pp.t -> unit Proofview.NonLogical.t
-
 (** An exception handler *)
 val explain_logic_error: exn -> Pp.t
 
@@ -72,15 +47,9 @@ val explain_logic_error: exn -> Pp.t
    from users that they report these anomalies. *)
 val explain_logic_error_no_anomaly : exn -> Pp.t
 
-(** Prints a logic failure message for a rule *)
-val db_logic_failure : debug_info -> exn -> unit Proofview.NonLogical.t
-
 (** Check for/process idtac breakpoint *)
 val db_breakpoint : debug_info ->
   lident message_token list -> unit Proofview.NonLogical.t
-
-val extract_ltac_trace :
-  ?loc:Loc.t -> Tacexpr.ltac_stack -> Pp.t Loc.located
 
 (** Prints a message only if debugger stops at the next step *)
 val defer_output : (unit -> Pp.t) -> unit Proofview.NonLogical.t
