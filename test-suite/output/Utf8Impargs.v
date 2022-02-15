8092fabffaaf195050a5b0a89d97cb0b78804cf6
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2022-02-15 16:36:18 +0100

    Insert implicit arguments in glob_constr at beginning of extern
    Fix #13392
    
    (cherry picked from commit 8fb633ac0daa65da1b1a9b6400e8da7b76cf1bbe)

---
+++ test-suite/output/Utf8Impargs.v
@@ -0,0 +1,7 @@
+Notation "∀ x .. y , P" := (forall x, .. (forall y, P) ..)
+  (at level 200, x binder, y binder, right associativity,
+  format "'[  ' '[  ' ∀  x  ..  y ']' ,  '/' P ']'") : type_scope.
+(* from Utf8_core.v *)
+
+Definition id {A} (a:A) := a.
+About id.
