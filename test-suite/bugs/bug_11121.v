1527772dd49b4ceb278de8a32f57cdef2d493812
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2022-02-27 13:58:31 +0100

    Replace a dummy fixpoint by a definition in test-suite.

--- test-suite/bugs/bug_11121.v
+++ test-suite/bugs/bug_11121.v
@@ -1,13 +1,13 @@
 Declare Custom Entry example.
 
 Module M1.
-Fixpoint stupid (x : nat) : nat := 1.
+Definition stupid (x : nat) : nat := 1.
 Reserved Notation " x '==' 1 " (in custom example at level 0, x constr).
 Notation " x '==' 1" := (stupid x) (in custom example).
 End M1.
 
 Module M2.
-Fixpoint stupid (x : nat) : nat := 1.
+Definition stupid (x : nat) : nat := 1.
 Notation " x '==' 1" := (stupid x) (in custom example at level 0).
 Fail Notation " x '==' 1" := (stupid x) (in custom example at level 1).
 End M2.
@@ -15,7 +15,9 @@ End M2.
 Module M3.
 Reserved Notation " x '==' 1 " (in custom example at level 55, x constr).
 
-Fixpoint stupid (x : nat) : nat := 1
+Notation "[[ x ]]" := x (x custom example).
+
+Fixpoint stupid (x : nat) : nat := match x with 0 => 1 | S x => [[ x == 1 ]] end
 where " x '==' 1" := (stupid x) (in custom example).
 
 End M3.
