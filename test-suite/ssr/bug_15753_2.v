3ea410ee502e965ceca69e05da0761dab205731c
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2022-03-03 18:23:50 +0100

    Adding tests for the behaviours encountered in the PR.

---
+++ test-suite/ssr/bug_15753_2.v
@@ -0,0 +1,10 @@
+Require Import Coq.ssr.ssreflect.
+
+Class FromPureT (φ : Type) :=
+  from_pureT : exists ψ : Prop, φ = ψ.
+
+Lemma into_forall_impl_pure φ : FromPureT φ -> φ -> True.
+Proof.
+  rewrite /FromPureT => -[φ' ->].
+  constructor.
+Qed.
