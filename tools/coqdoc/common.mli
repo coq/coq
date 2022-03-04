3049017484b238476498317b4af2e387053f096b
Ramkumar Ramachandra <r@artagnon.com>
2022-03-04 15:43:31 +0100

    coqdoc/{cmdArgs, common}: add copyright headers
    Signed-off-by: Ramkumar Ramachandra <r@artagnon.com>

--- tools/coqdoc/common.mli
+++ tools/coqdoc/common.mli
@@ -1,3 +1,13 @@
+(************************************************************************)
+(*         *   The Coq Proof Assistant / The Coq Development Team       *)
+(*  v      *         Copyright INRIA, CNRS and contributors             *)
+(* <O___,, * (see version control and CREDITS file for authors & dates) *)
+(*   \VV/  **************************************************************)
+(*    //   *    This file is distributed under the terms of the         *)
+(*         *     GNU Lesser General Public License Version 2.1          *)
+(*         *     (see LICENSE file for the text of the license)         *)
+(************************************************************************)
+
 (* Misc types **********************************************************************)
 type target_language = LaTeX | HTML | TeXmacs | Raw
 type output_t = StdOut | MultFiles | File of string
