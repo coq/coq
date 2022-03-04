b89822b2f27faf82acd3a5b42902e73c8cf7a6f3
Ramkumar Ramachandra <r@artagnon.com>
2022-03-04 15:53:27 +0100

    coqdoc/cmdArgs: polish --help output
    Signed-off-by: Ramkumar Ramachandra <r@artagnon.com>

--- tools/coqdoc/cmdArgs.ml
+++ tools/coqdoc/cmdArgs.ml
@@ -76,119 +76,124 @@ let arg_url_path f = Arg.Rest_all (fun l ->
 
 let args_options = Arg.align [
   "--html", arg_set (fun p -> { p with targetlang = HTML }),
-  "produce a HTML document (default)";
+  " Produce a HTML document (default)";
   "--latex", arg_set (fun p -> { p with targetlang = LaTeX }),
-  "produce a LaTeX document";
+  " Produce a LaTeX document";
   "--texmacs",arg_set (fun p -> { p with targetlang = TeXmacs }),
-  "produce a TeXmacs document";
+  " Produce a TeXmacs document";
   "--raw", arg_set (fun p -> { p with targetlang = Raw }),
-  "produce a text document";
+  " Produce a text document";
   "--dvi", arg_set (fun p -> { { p with targetlang = LaTeX }
                               with compile_targets = Dvi :: !prefs.compile_targets }),
-  "output the DVI";
+  " Output the DVI";
   "--ps", arg_set (fun p -> { { p with targetlang = LaTeX }
                                with compile_targets = Ps :: !prefs.compile_targets }),
-  "output the PostScript";
+  " Output the PostScript";
   "--pdf", arg_set (fun p -> { { p with targetlang = LaTeX }
                               with compile_targets = Pdf :: !prefs.compile_targets }),
-  "output the Pdf";
+  " Output the Pdf";
   "--stdout", arg_set (fun p -> { p with out_to = StdOut }),
-  "write output to stdout";
+  " Write output to stdout";
   "-o", arg_string (fun p f -> { { p with out_to = File (Filename.basename f) }
                             with output_dir = Filename.dirname f }),
-  "<file> write output to file <file>";
+  "<file> Write output to file <file>";
   "-d", arg_string (fun p f -> { p with output_dir = f }),
-  "<dir> output files into directory <dir>";
+  "<dir> Output files into directory <dir>";
+  "--directory", arg_string (fun p f -> { p with output_dir = f }),
+  "<dir> Output files into directory <dir>";
   "-g", arg_set (fun p -> { p with gallina = true }),
-  "(gallina) skip proofs";
+  " Skip proofs (gallina)";
   "-s", arg_set (fun p -> { p with short = true }),
-  "(short) no titles for files";
+  " No titles for files (short)";
+  "--short", arg_set (fun p -> { p with short = true }),
+  " No titles for files";
   "-l", arg_set (fun p -> { { p with gallina = true } with light = true }),
-  "light mode (only defs and statements)";
+  " Light mode (only defs and statements)";
   "-t", arg_string (fun p s -> { p with title = s }),
-  "<string> give a title to the document";
+  "<string> Give a title to the document";
   "--body-only", arg_set (fun p -> { p with header_trailer = false }),
-  "suppress LaTeX/HTML header and trailer";
+  " Suppress LaTeX/HTML header and trailer";
   "--with-header", arg_file (fun p f -> { p with header_trailer = true;
                                                  header_file_spec = true;
                                                  header_file = f }),
-  "<file> prepend <file> as html reader";
+  "<file> Prepend <file> as html reader";
   "--with-footer", arg_file (fun p f -> { p with header_trailer = true;
                                                  footer_file_spec = true;
                                                  footer_file = f }),
   "<file> append <file> as html footer";
   "--no-index", arg_set (fun p -> { p with index = false }),
-  "do not output the index";
+  " Do not output the index";
   "--multi-index", arg_set (fun p -> { p with multi_index = true }),
-  "index split in multiple files";
+  " Index split in multiple files";
   "--index <string>", arg_string (fun p s -> { p with index_name = s }),
-  "set index name (default is index)";
+  " Set index name (default is index)";
   "--toc", arg_set (fun p -> { p with toc = true }),
-  "output a table of contents";
+  " Output a table of contents";
   "--vernac-file",
   arg_file (fun p f -> { p with files = Vernac_file (f, coq_module f) :: !prefs.files }),
   "<file> consider <file> as a .v file";
   "--tex-file", arg_file (fun p f -> { p with files =  Latex_file f :: !prefs.files }),
-  "<file> consider <file> as a .tex file";
+  "<file> Consider <file> as a .tex file";
   "-p", Arg.String (fun f -> Output.push_in_preamble f),
-  "<string> insert <string> in LaTeX preamble";
+  "<string> Insert <string> in LaTeX preamble";
   "--files-from", arg_file (fun p f -> { p with
     files = List.append (List.map what_file (FileUtil.files_from_file f)) !prefs.files }),
-  "<file>  read file names to process in <file>";
+  "<file> Read file names to process in <file>";
   "--glob-from", arg_file (fun p f -> { p with glob_source = GlobFile f }),
-  "<file> read globalization information from <file>";
+  "<file> Read globalization information from <file>";
   "--no-glob", arg_set (fun p -> { p with glob_source = NoGlob }),
-  "don't use any globalization information (no links will be inserted at identifiers)";
-  "--quiet", arg_set (fun p -> { p with quiet = true }), "quiet mode (default)";
-  "--verbose", arg_set (fun p -> { p with quiet = false }), "verbose mode";
+  " Don't use any globalization information (no links will be inserted at identifiers)";
+  "-q", arg_set (fun p -> { p with quiet = true }), " Quiet mode (default)";
+  "--quiet", arg_set (fun p -> { p with quiet = true }), " Quiet mode (default)";
+  "--verbose", arg_set (fun p -> { p with quiet = false }), " Verbose mode";
   "--no-externals", arg_set (fun p -> { p with externals = false }),
-  "no links to Coq standard library";
+  " No links to Coq standard library";
   "--external", arg_url_path Index.add_external_library,
-  "<url> <d> set URL for external library <d>";
+  "<url>+<d> set URL for external library <d>";
   "--coqlib_url", arg_string (fun p u -> { p with coqlib_url = u }),
-  "<url> set URL for Coq standard library (default: " ^ Coq_config.wwwstdlib ^ ")";
+  "<url> Set URL for Coq standard library (default: " ^ Coq_config.wwwstdlib ^ ")";
   "--coqlib", Arg.String (fun d -> Boot.Env.set_coqlib d),
-  "<dir> set the path where Coq files are installed";
+  "<dir> Set the path where Coq files are installed";
   "-R", arg_path (fun p l -> { p with paths = l :: !prefs.paths }),
-  "<dir> <coqdir> map physical dir to Coq dir";
+  "<dir>+<coqdir> map physical dir to Coq dir";
   "-Q", arg_path (fun p l -> { p with paths = l :: !prefs.paths }),
-  "<dir> <coqdir> map physical dir to Coq dir";
+  "<dir>+<coqdir> Map physical dir to Coq dir";
   "--latin1", arg_set (fun p -> {p with encoding = { charset = "iso-8859-1";
                                                      inputenc = "latin1";
                                                      latin1 = true;
                                                      utf8 = false } }),
-  "set ISO-8859-1 mode";
+  " Set ISO-8859-1 mode";
   "--utf8", arg_set (fun p -> {p with encoding = { charset = "utf-8";
                                                    inputenc = "utf8x";
                                                    latin1 = false;
                                                    utf8 = true } }),
-  "set UTF-8 mode";
+  " Set UTF-8 mode";
   "--charset", arg_string (fun p s -> { p with encoding = { !prefs.encoding with charset = s } }),
-   "<string> set HTML charset";
+   "<string> Set HTML charset";
   "--inputenc", arg_string (fun p s -> { p with encoding = { !prefs.encoding with inputenc = s } }),
-  "<string> set LaTeX input encoding";
+  "<string> Set LaTeX input encoding";
   "--interpolate", arg_set (fun p -> {p with interpolate = true }),
-  "try to typeset identifiers in comments using definitions in the same module";
+  " Try to typeset identifiers in comments using definitions in the same module";
   "--raw-comments", arg_set (fun p -> {p with raw_comments = true }),
-  "raw comments";
+  " Raw comments";
   "--parse-comments", arg_set (fun p -> {p with parse_comments = true }),
-  "parse regular comments";
+  " Parse regular comments";
   "--plain-comments", arg_set (fun p -> {p with plain_comments = true }),
-  "consider comments as non-literate text";
+  " Consider comments as non-literate text";
   "--toc-depth", arg_int (fun p d -> { p with toc_depth = Some d }),
-  "<int> don't include TOC entries for sections below level <int>";
+  "<int> Don't include TOC entries for sections below level <int>";
   "--no-lib-name", arg_set (fun p -> { p with lib_name = "" }),
-  "don't display \"Library\" before library names in the toc";
+  " Don't display \"Library\" before library names in the toc";
   "--lib-name", arg_string (fun p s -> { p with lib_name = s }),
-  "<string> call top level toc entries <string> instead of \"Library\"";
+  "<string> Call top level toc entries <string> instead of \"Library\"";
   "--lib-subtitles", arg_set (fun p -> { p with lib_subtitles = true }),
-  "first line comments of the form (** * ModuleName : text *) will be interpreted as subtitles";
+  " First line comments of the form (** * ModuleName : text *) will be interpreted as subtitles";
   "--inline-notmono", arg_set (fun p -> { p with inline_notmono = true }),
-  "use a proportional width font for inline code (possibly with a different color)";
+  " Use a proportional width font for inline code (possibly with a different color)";
 ]
 
 let parse_args () =
   Arg.parse
     args_options
     (fun s -> raise (Arg.Bad ("Unknown option: " ^ s)))
-    "Available options for configure are: "
+    "Available options for coqdoc are: "
