coq/coq

commit 70fecff5d3ddbf4ebd5674cf2355ed7dbabf948a

Extrude the opaque table definition out of the kernel.
The proof-carrying map is now part of Library, and we fully inverted control
w.r.t. opaque proof verification. In particular, the kernel is now future-free.

toplevel/ccompile.ml
@@ -150,8 +150,7 @@ let compile opts stm_options injections copts ~echo ~f_in ~f_out =
       (* In .vo production, dump a complete .vo file.
          In .vok production, only dump an empty .vok file. *)
       if mode = BuildVo
-        then Library.save_library_to ~output_native_objects Library.ProofsTodoNone ldir long_f_dot_out (Global.opaque_tables ())
-        else create_empty_file long_f_dot_out;
+        then Library.save_library_to ~output_native_objects Library.ProofsTodoNone ldir long_f_dot_out;
       Aux_file.record_in_aux_at "vo_compile_time"
         (Printf.sprintf "%.3f" (wall_clock2 -. wall_clock1));
       Aux_file.stop_aux_file ();

checker/check.ml
@@ -51,7 +51,7 @@ let pr_path sp =
 type compilation_unit_name = DirPath.t
 
 type seg_univ = Univ.ContextSet.t * bool
-type seg_proofs = Opaqueproof.opaque_disk
+type seg_proofs = Opaqueproof.opaque_proofterm option array
 
 type library_t = {
   library_name : compilation_unit_name;
@@ -97,14 +97,21 @@ let access_opaque_table dp i =
     try LibraryMap.find dp !opaque_tables
     with Not_found -> assert false
   in
-  Opaqueproof.get_opaque_disk i t
+  let i = Opaqueproof.repr_handle i in
+  let () = assert (0 <= i) in
+  if i < Array.length t then t.(i)
+  else None (* FIXME: we have to work around #13324 *)
 
-let access_discharge = Cooking.cook_constr
-
-let indirect_accessor = {
-  Opaqueproof.access_proof = access_opaque_table;
-  Opaqueproof.access_discharge = access_discharge;
-}
+let indirect_accessor o =
+  let (sub, ci, dp, i) = Opaqueproof.repr o in
+  let c = access_opaque_table dp i in
+  let c = match c with
+  | None ->  CErrors.user_err Pp.(str "Cannot access opaque delayed proof.")
+  | Some c -> c
+  in
+  let (c, prv) = Cooking.cook_constr ci c in
+  let c = Mod_subst.(List.fold_right subst_mps sub c) in
+  (c, prv)
 
 let () = Mod_checking.set_indirect_accessor indirect_accessor
 

checker/mod_checking.ml
@@ -8,10 +8,8 @@ open Environ
 
 (** {6 Checking constants } *)
 
-let indirect_accessor = ref {
-  Opaqueproof.access_proof = (fun _ _ -> assert false);
-  Opaqueproof.access_discharge = (fun _ _ -> assert false);
-}
+let indirect_accessor : (cooking_info Opaqueproof.opaque -> Constr.t * unit Opaqueproof.delayed_universes) ref =
+  ref (fun _ -> assert false)
 
 let set_indirect_accessor f = indirect_accessor := f
 
@@ -50,12 +48,11 @@ let check_constant_declaration env opac kn cb opacify =
   in
   let ty = cb.const_type in
   let _ = infer_type env ty in
-  let otab = Environ.opaque_tables env in
   let body, env = match cb.const_body with
     | Undef _ | Primitive _ -> None, env
     | Def c -> Some c, env
     | OpaqueDef o ->
-      let c, u = Opaqueproof.force_proof !indirect_accessor otab o in
+      let c, u = !indirect_accessor o in
       let env = match u, cb.const_universes with
         | Opaqueproof.PrivateMonomorphic (), Monomorphic -> env
         | Opaqueproof.PrivatePolymorphic (_, local), Polymorphic _ ->

checker/mod_checking.mli
@@ -8,6 +8,6 @@
 (*         *     (see LICENSE file for the text of the license)         *)
 (************************************************************************)
 
-val set_indirect_accessor : Declarations.cooking_info Opaqueproof.indirect_accessor -> unit
+val set_indirect_accessor : (Declarations.cooking_info Opaqueproof.opaque -> Constr.t * unit Opaqueproof.delayed_universes) -> unit
 
 val check_module : Environ.env -> Names.Cset.t Names.Cmap.t -> Names.ModPath.t -> Declarations.module_body -> Names.Cset.t Names.Cmap.t

kernel/modops.ml
@@ -137,10 +137,6 @@ let rec functor_smart_map fty f0 funct = match funct with
   |NoFunctor a ->
     let a' = f0 a in if a==a' then funct else NoFunctor a'
 
-let rec functor_iter fty f0 = function
-  |MoreFunctor (_mbid,ty,e) -> fty ty; functor_iter fty f0 e
-  |NoFunctor a -> f0 a
-
 (** {6 Misc operations } *)
 
 let module_type_of_module mb =
@@ -163,11 +159,6 @@ let implem_smartmap fs fa impl = match impl with
   |Struct e -> let e' = fs e in if e==e' then impl else Struct e'
   |Algebraic a -> let a' = fa a in if a==a' then impl else Algebraic a'
   |Abstract|FullStruct -> impl
-
-let implem_iter fs fa impl = match impl with
-  |Struct e -> fs e
-  |Algebraic a -> fa a
-  |Abstract|FullStruct -> ()
 
 (** {6 Substitutions of modular structures } *)
 
@@ -601,27 +592,3 @@ let rec collect_mbid l sign =  match sign with
 
 let clean_bounded_mod_expr sign =
   if is_functor sign then collect_mbid MBIset.empty sign else sign
-
-(** {6 Stm machinery } *)
-let join_constant_body except otab cb =
-  match cb.const_body with
-  | OpaqueDef o -> Opaqueproof.join_opaque ~except otab o
-  | _ -> ()
-
-let join_structure except otab s =
-  let rec join_module : 'a. 'a generic_module_body -> unit = fun mb ->
-    Option.iter join_expression mb.mod_type_alg;
-    join_signature mb.mod_type
-  and join_field (_l,body) = match body with
-    |SFBconst sb -> join_constant_body except otab sb
-    |SFBmind _ -> ()
-    |SFBmodule m ->
-      implem_iter join_signature join_expression m.mod_expr;
-      join_module m
-    |SFBmodtype m -> join_module m
-  and join_structure struc = List.iter join_field struc
-  and join_signature sign =
-    functor_iter join_module join_structure sign
-  and join_expression me = functor_iter join_module (fun _ -> ()) me in
-  join_structure s
-

kernel/modops.mli
@@ -82,11 +82,6 @@ val subst_modtype_and_resolver : module_type_body -> ModPath.t ->
 
 val clean_bounded_mod_expr : module_signature -> module_signature
 
-(** {6 Stm machinery } *)
-
-val join_structure :
-  Future.UUIDSet.t -> Opaqueproof.opaquetab -> structure_body -> unit
-
 (** {6 Errors } *)
 
 type signature_mismatch_error =

kernel/opaqueproof.ml
@@ -18,54 +18,31 @@ type 'a delayed_universes =
 
 type opaque_proofterm = Constr.t * unit delayed_universes
 
-type 'cooking_info indirect_accessor = {
-  access_proof : DirPath.t -> int -> opaque_proofterm option;
-  access_discharge : 'cooking_info list -> opaque_proofterm -> opaque_proofterm;
-}
-
-let drop_mono = function
-| PrivateMonomorphic _ -> PrivateMonomorphic ()
-| PrivatePolymorphic _ as ctx -> ctx
-
 type proofterm = (constr * Univ.ContextSet.t delayed_universes) Future.computation
 
 type 'cooking_info opaque =
 | Indirect of substitution list * 'cooking_info list * DirPath.t * int (* subst, discharge, lib, index *)
 
 type opaquetab = {
-  opaque_val : proofterm Int.Map.t;
-  (** Actual proof terms *)
   opaque_len : int;
   (** Size of the above map *)
   opaque_dir : DirPath.t;
 }
 let empty_opaquetab = {
-  opaque_val = Int.Map.empty;
   opaque_len = 0;
   opaque_dir = DirPath.initial;
 }
 
-let not_here () =
-  CErrors.user_err Pp.(str "Cannot access opaque delayed proof")
+let repr (Indirect (s, ci, dp, i)) = (s, ci, dp, i)
 
-let create dp cu tab =
-  let hcons (c, u) =
-    let c = Constr.hcons c in
-    let u = match u with
-    | PrivateMonomorphic u -> PrivateMonomorphic (Univ.hcons_universe_context_set u)
-    | PrivatePolymorphic (n, u) -> PrivatePolymorphic (n, Univ.hcons_universe_context_set u)
-    in
-    (c, u)
-  in
-  let cu = Future.chain cu hcons in
+let create dp tab =
   let id = tab.opaque_len in
-  let opaque_val = Int.Map.add id cu tab.opaque_val in
   let opaque_dir =
     if DirPath.equal dp tab.opaque_dir then tab.opaque_dir
     else if DirPath.equal tab.opaque_dir DirPath.initial then dp
     else CErrors.anomaly
       (Pp.str "Using the same opaque table for multiple dirpaths.") in
-  let ntab = { opaque_val; opaque_dir; opaque_len = id + 1 } in
+  let ntab = { opaque_dir; opaque_len = id + 1 } in
   Indirect ([], [], dp, id), ntab
 
 let subst_opaque sub = function
@@ -76,82 +53,8 @@ let discharge_opaque info = function
   assert (CList.is_empty s);
   Indirect ([], info :: ci, dp, i)
 
-let join except cu = match except with
-| None -> ignore (Future.join cu)
-| Some except ->
-  if Future.UUIDSet.mem (Future.uuid cu) except then ()
-  else ignore (Future.join cu)
+module HandleMap = Int.Map
 
-let join_opaque ?except { opaque_val = prfs; opaque_dir = odp; _ } = function
-| Indirect (_,_,dp,i) ->
-    if DirPath.equal dp odp then
-      let fp = Int.Map.find i prfs in
-      join except fp
-
-let force_proof access { opaque_val = prfs; opaque_dir = odp; _ } = function
-  | Indirect (l,d,dp,i) ->
-      let c, u =
-        if DirPath.equal dp odp
-        then
-          let cu = Int.Map.find i prfs in
-          let (c, u) = Future.force cu in
-          access.access_discharge d (c, drop_mono u)
-        else
-          let cu = access.access_proof dp i in
-          match cu with
-          | None -> not_here ()
-          | Some (c, u) -> access.access_discharge d (c, u)
-      in
-      let l = List.fold_left Mod_subst.join Mod_subst.empty_subst (List.rev l) in
-      let c = subst_mps l c in
-      (c, u)
-
-let get_mono (_, u) = match u with
-| PrivateMonomorphic ctx -> ctx
-| PrivatePolymorphic _ -> Univ.ContextSet.empty
-
-let force_constraints _access { opaque_val = prfs; opaque_dir = odp; _ } = function
-| Indirect (_,_,dp,i) ->
-      if DirPath.equal dp odp
-      then
-        let cu = Int.Map.find i prfs in
-        get_mono (Future.force cu)
-      else Univ.ContextSet.empty
-
-module FMap = Future.UUIDMap
-
-type opaque_disk = opaque_proofterm option array
 type opaque_handle = int
-
-let dump ?(except = Future.UUIDSet.empty) { opaque_val = otab; opaque_len = n; _ } =
-  let opaque_table = Array.make n None in
-  let f2t_map = ref FMap.empty in
-  let iter n cu =
-    let uid = Future.uuid cu in
-    let () = f2t_map := FMap.add (Future.uuid cu) n !f2t_map in
-    let c =
-      if Future.is_val cu then
-        let (c, priv) = Future.force cu in
-        let priv = drop_mono priv in
-        Some (c, priv)
-      else if Future.UUIDSet.mem uid except then None
-      else
-        CErrors.anomaly
-          Pp.(str"Proof object "++int n++str" is not checked nor to be checked")
-    in
-    opaque_table.(n) <- c
-  in
-  let () = Int.Map.iter iter otab in
-  opaque_table, !f2t_map
-
-let get_opaque_disk i t =
-  let () = assert (0 <= i && i < Array.length t) in
-  t.(i)
-
-let set_opaque_disk i (c, priv) t =
-  let () = assert (0 <= i && i < Array.length t) in
-  let () = assert (Option.is_empty t.(i)) in
-  let c = Constr.hcons c in
-  t.(i) <- Some (c, priv)
 
 let repr_handle i = i

kernel/opaqueproof.mli
@@ -29,42 +29,25 @@ type 'cooking_info opaque
 
 val empty_opaquetab : opaquetab
 
-(** From a [proofterm] to some [opaque]. *)
-val create : DirPath.t -> proofterm -> opaquetab -> 'c opaque * opaquetab
+(** Create a fresh handle in the table. *)
+val create : DirPath.t -> opaquetab -> 'c opaque * opaquetab
 
 type opaque_proofterm = Constr.t * unit delayed_universes
 
 type opaque_handle
 
-type 'cooking_info indirect_accessor = {
-  access_proof : DirPath.t -> opaque_handle -> opaque_proofterm option;
-  access_discharge : 'cooking_info list -> opaque_proofterm -> opaque_proofterm;
-}
+module HandleMap : CSig.MapS with type key = opaque_handle
+
 (** Opaque terms are indexed by their library
     dirpath and an integer index. The two functions above activate
     this indirect storage, by telling how to retrieve terms.
 *)
-
-(** From a [opaque] back to a [constr]. This might use the
-    indirect opaque accessor given as an argument. *)
-val force_proof : 'c indirect_accessor -> opaquetab -> 'c opaque -> opaque_proofterm
-val force_constraints : 'c indirect_accessor -> opaquetab -> 'c opaque -> Univ.ContextSet.t
 
 val subst_opaque : substitution -> 'c opaque -> 'c opaque
 
 val discharge_opaque :
   'cooking_info -> 'cooking_info opaque -> 'cooking_info opaque
 
-val join_opaque : ?except:Future.UUIDSet.t -> opaquetab -> 'c opaque -> unit
-
-(** {5 Serialization} *)
-
-type opaque_disk
-
-val dump : ?except:Future.UUIDSet.t -> opaquetab -> opaque_disk * opaque_handle Future.UUIDMap.t
-
-val get_opaque_disk : opaque_handle -> opaque_disk -> opaque_proofterm option
-val set_opaque_disk : opaque_handle -> opaque_proofterm -> opaque_disk -> unit
-
-(** Only used for pretty-printing *)
 val repr_handle : opaque_handle -> int
+
+val repr : 'c opaque -> substitution list * 'c list * DirPath.t * opaque_handle

commit cabe77e11233fde8fe236493ce647095b70e9a11

toplevel: fix a bug in `coqc -vok` invocation
The commit 70fecff broke the production of empty .vok files in the
BuildVok mode. In this patch, we dump a complete .vo in BuildVo mode
along with empty .vos and .vok files, preserving the existing behavior,
and fix the bug of empty .vok production in BuildVok mode.

Reported-by: Paolo G. Giarrusso <p.giarrusso@gmail.com>
Reviewed-by: Paolo G. Giarrusso <p.giarrusso@gmail.com>
Reviewed-by: Ali Caglayan <alizter@gmail.com>
Signed-off-by: Ramkumar Ramachandra <r@artagnon.com>

toplevel/ccompile.ml
@@ -122,9 +122,11 @@ let compile opts stm_options injections copts ~echo ~f_in ~f_out =
   let long_f_dot_in, long_f_dot_out =
     ensure_exists_with_prefix f_in f_out ext_in ext_out in
   let dump_empty_vos () =
-    (* Produce an empty .vos file, as a way to ensure that a stale .vos can never be loaded *)
     let long_f_dot_vos = (chop_extension long_f_dot_out) ^ ".vos" in
     create_empty_file long_f_dot_vos in
+  let dump_empty_vok () =
+    let long_f_dot_vok = (chop_extension long_f_dot_out) ^ ".vok" in
+    create_empty_file long_f_dot_vok in
   match mode with
   | BuildVo | BuildVok ->
       let doc, sid = Topfmt.(in_phase ~phase:LoadingPrelude)
@@ -147,19 +149,19 @@ let compile opts stm_options injections copts ~echo ~f_in ~f_out =
       let _doc = Stm.join ~doc:state.doc in
       let wall_clock2 = Unix.gettimeofday () in
       check_pending_proofs long_f_dot_in;
-      (* In .vo production, dump a complete .vo file.
-         In .vok production, only dump an empty .vok file. *)
+      (* In .vo production, dump a complete .vo file. *)
       if mode = BuildVo
         then Library.save_library_to ~output_native_objects Library.ProofsTodoNone ldir long_f_dot_out;
       Aux_file.record_in_aux_at "vo_compile_time"
         (Printf.sprintf "%.3f" (wall_clock2 -. wall_clock1));
       Aux_file.stop_aux_file ();
-      (* In .vo production, dump an empty .vos file to indicate that the .vo should be loaded,
-         and dump an empty .vok file to indicate that proofs are ok. *)
-      if mode = BuildVo then begin
+      (* Additionally, dump an empty .vos file to make sure that
+        stale ones are never loaded *)
+      if mode = BuildVo then
         dump_empty_vos();
-        create_empty_file (long_f_dot_out ^ "k");
-      end;
+      (* In both .vo, and .vok production mode, dump an empty .vok file to
+         indicate that proofs are ok. *)
+      dump_empty_vok();
       Dumpglob.end_dump_glob ()
 
   | BuildVio | BuildVos ->