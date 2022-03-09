cca583de042f143df2b37595883eba85d514d590
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2022-03-09 08:43:23 +0100

    Add a test for the observed change in behaviour.

---
+++ test-suite/ssr/bug_15770.v
@@ -0,0 +1,14 @@
+Require Import Coq.ssr.ssreflect.
+
+Axiom xget : forall {T} (P : T -> Prop), T.
+
+Variant xget_spec {T} (P : T -> Prop) : T -> Prop -> Type :=
+| XGetSome x of P x : xget_spec P x True.
+
+Axiom xgetP : forall {T} (P : T -> Prop), xget_spec P (xget P) (P (xget P)).
+
+Lemma xgetPex {T} (P : T -> Prop) : P (xget P).
+Proof.
+case: xgetP.
+constructor.
+Qed.
