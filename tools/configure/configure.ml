1c9b5d9b25ba3dabada4a2b0ed37fbc2d885346c
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2022-03-22 17:25:53 +0100

    let is_a_released_version = false (post 8.15.1)

--- tools/configure/configure.ml
+++ tools/configure/configure.ml
@@ -21,7 +21,7 @@ let (/) = Filename.concat
 
 let coq_version = "8.15.1"
 let vo_magic = 81500
-let is_a_released_version = true
+let is_a_released_version = false
 
 (** Default OCaml binaries *)
 
