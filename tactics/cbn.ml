80015c0001f74e05119e8af43dba24d94cc86f27
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2022-03-24 17:17:49 +0100

    Remove a hackish use of univ_of_sort in cbn.
    This is still pretty hackish, but it is not more broken than it was before.

--- tactics/cbn.ml
+++ tactics/cbn.ml
@@ -454,7 +454,11 @@ let magically_constant_of_fixbody env sigma reference bd = function
                 let l, r = match cst with
                   | ULub (u, v) | UWeak (u, v) -> u, v
                   | UEq (u, v) | ULe (u, v) ->
-                    let get u = Option.get (Universe.level (Sorts.univ_of_sort u)) in
+                    let get u = match u with
+                    | Sorts.SProp | Sorts.Prop -> assert false
+                    | Sorts.Set -> Level.set
+                    | Sorts.Type u -> Option.get (Universe.level u)
+                    in
                     get u, get v
                 in
                 Univ.Level.Map.add l r acc)
