e9eb288d842bb5d0037f2639191ecdfbf8de3898
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2022-04-01 16:17:49 +0200

    Remove the universe-inferring constr equality functions from the API.

--- kernel/constr.mli
+++ kernel/constr.mli
@@ -417,14 +417,6 @@ val eq_constr_univs : constr UGraph.check_function
     alpha, casts, application grouping and the universe inequalities in [u]. *)
 val leq_constr_univs : constr UGraph.check_function
 
-(** [eq_constr_univs u a b] is [true] if [a] equals [b] modulo alpha, casts,
-   application grouping and the universe equalities in [u]. *)
-val eq_constr_univs_infer : UGraph.t -> constr -> constr -> bool Univ.constrained
-
-(** [leq_constr_univs u a b] is [true] if [a] is convertible to [b] modulo
-    alpha, casts, application grouping and the universe inequalities in [u]. *)
-val leq_constr_univs_infer : UGraph.t -> constr -> constr -> bool Univ.constrained
-
 (** [eq_constr_univs a b] [true, c] if [a] equals [b] modulo alpha, casts,
    application grouping and ignoring universe instances. *)
 val eq_constr_nounivs : constr -> constr -> bool
