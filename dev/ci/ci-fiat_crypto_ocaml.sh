a21e38c29ce668f80b5c2540ce9cf35ba70d3352
Jason Gross <jgross@mit.edu>
2022-04-22 15:03:41 +0100

    [ci] Fix fiat-crypto-ocaml build

--- dev/ci/ci-fiat_crypto_ocaml.sh
+++ dev/ci/ci-fiat_crypto_ocaml.sh
@@ -8,7 +8,9 @@ if [ "$DOWNLOAD_ONLY" ]; then exit 0; fi
 ci_dir="$(dirname "$0")"
 . "${ci_dir}/ci-common.sh"
 
-make_args=(EXTERNAL_REWRITER=1 EXTERNAL_COQPRIME=1)
+# we explicitly pass OCAMLFIND so that we pick up the opam
+# (non-flambda one) rather than the one used to build coq
+make_args=(EXTERNAL_REWRITER=1 EXTERNAL_COQPRIME=1 OCAMLFIND=ocamlfind)
 
 ( cd "${CI_BUILD_DIR}/fiat_crypto"
   make "${make_args[@]}" standalone-ocaml lite-generated-files
