528800bfc79b74388de752e8c8995b3b6e4fb312
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2022-05-04 21:36:40 +0200

    run unimath/bicategories with -j 1
    Hopefully this fixes CI without too much time cost

--- dev/ci/ci-unimath.sh
+++ dev/ci/ci-unimath.sh
@@ -11,7 +11,9 @@ if [ "$DOWNLOAD_ONLY" ]; then exit 0; fi
 
 export COQEXTRAFLAGS='-native-compiler no'
 ( cd "${CI_BUILD_DIR}/unimath"
-  # DisplayedInserter consumes too much memory for the shared workers
-  sed -i.bak 's|DisplayedBicats/Examples/DisplayedInserter.v||'  UniMath/Bicategories/.package/files
+  # Bicategories consumes too much memory for the shared workers when -j 2
+  sed -i.bak 's|Bicategories||' Makefile
   make BUILD_COQ=no
+  mv Makefile.bak Makefile
+  make BUILD_COQ=no -j 1
 )
