ac907d9ceac7be48506b09fd3c13ed6ee3f4e9fb
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2022-05-06 14:10:01 +0200

    CI unimath: skip BiadjunctionPreserveInserters

--- dev/ci/ci-unimath.sh
+++ dev/ci/ci-unimath.sh
@@ -11,7 +11,12 @@ if [ "$DOWNLOAD_ONLY" ]; then exit 0; fi
 
 export COQEXTRAFLAGS='-native-compiler no'
 ( cd "${CI_BUILD_DIR}/unimath"
-  # DisplayedInserter consumes too much memory for the shared workers
-  sed -i.bak 's|DisplayedBicats/Examples/DisplayedInserter.v||'  UniMath/Bicategories/.package/files
+  # these files consumes too much memory for the shared workers
+  # (at least with -j 2 when the scheduler runs them in parallel)
+  for f in DisplayedBicats/Examples/DisplayedInserter.v \
+               PseudoFunctors/Preservation/BiadjunctionPreserveInserters.v;
+  do
+      sed -i.bak "s|$f||"  UniMath/Bicategories/.package/files
+  done
   make BUILD_COQ=no
 )
