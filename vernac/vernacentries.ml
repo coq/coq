herbelin/github-coq

Use an indirection for vernac_context, like it is done for all commands.

vernac/vernacentries.ml
@@ -1312,6 +1312,9 @@ let vernac_declare_instance ~atts id bl inst pri =
   in
   Classes.declare_new_instance ~program_mode:program ~locality ~poly id bl inst pri
 
+let vernac_context ~atts sup =
+  ComAssumption.context ~poly:(only_polymorphism atts) sup
+
 let vernac_existing_instance ~atts insts =
   let locality = Attributes.(parse option_locality) atts in
   List.iter (fun (id, info) -> Classes.existing_instance locality id (Some info)) insts
@@ -2232,7 +2235,7 @@ let translate_vernac ?loc ~atts v = let open Vernacextend in match v with
   | VernacDeclareInstance (id, bl, inst, info) ->
     VtDefault(fun () -> vernac_declare_instance ~atts id bl inst info)
   | VernacContext sup ->
-    VtDefault(fun () -> ComAssumption.context ~poly:(only_polymorphism atts) sup)
+    VtDefault(fun () -> vernac_context ~atts sup)
   | VernacExistingInstance insts ->
     VtDefault(fun () -> vernac_existing_instance ~atts insts)
   | VernacExistingClass id ->

coq/coq

vernac/vernacentries.ml
@@ -1535,6 +1535,9 @@ let vernac_declare_instance ~atts id bl inst pri =
   in
   Classes.declare_new_instance ~program_mode:program ~locality ~poly id bl inst pri
 
+let vernac_context ~atts sup =
+  ComAssumption.do_context ~poly:(only_polymorphism atts) sup
+
 let vernac_existing_instance ~atts insts =
   let locality = Attributes.parse Classes.instance_locality atts in
   List.iter (fun (id, info) -> Classes.existing_instance locality id (Some info)) insts
@@ -2444,7 +2447,7 @@ let translate_vernac ?loc ~atts v = let open Vernacextend in match v with
   | VernacDeclareInstance (id, bl, inst, info) ->
     vtdefault(fun () -> vernac_declare_instance ~atts id bl inst info)
   | VernacContext sup ->
-    vtdefault(fun () -> ComAssumption.do_context ~poly:(only_polymorphism atts) sup)
+    vtdefault(fun () -> vernac_context ~atts sup)
   | VernacExistingInstance insts ->
     vtdefault(fun () -> vernac_existing_instance ~atts insts)
   | VernacExistingClass id ->