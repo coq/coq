66d15697eafc35a0fdcce83edf926327fedf3ff4
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2022-06-01 22:39:49 +0200

    Unset the released tag.

--- tools/configure/configure.ml
+++ tools/configure/configure.ml
@@ -21,7 +21,7 @@ let (/) = Filename.concat
 
 let coq_version = "8.16+rc1"
 let vo_magic = 81591
-let is_a_released_version = true
+let is_a_released_version = false
 
 (** Default OCaml binaries *)
 
