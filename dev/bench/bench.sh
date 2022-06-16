f7413f89c6dae529a4a828c6fba35e745c13c710
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2022-06-16 11:47:33 +0200

    Double stack limit in bench

--- dev/bench/bench.sh
+++ dev/bench/bench.sh
@@ -266,6 +266,9 @@ zulip_autofail() {
 }
 if [[ $zulip_post ]]; then trap zulip_autofail ERR; fi
 
+# see https://github.com/coq/coq/pull/15807
+ulimit -s $((2 * $(ulimit -s)))
+
 # Clone the indicated git-repository.
 
 coq_dir="$working_dir/coq"
