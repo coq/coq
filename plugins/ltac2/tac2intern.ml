b478846624103f797e17223d03c30b61bb520327
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2022-06-25 16:01:01 +0200

    Put Tac2intern.environment in its own module
    This will allow splitting tac2intern more, for instance have pattern
    analysis in its own file.

--- plugins/ltac2/tac2intern.ml
+++ plugins/ltac2/tac2intern.ml
@@ -18,6 +18,7 @@ open Locus
 open Tac2env
 open Tac2print
 open Tac2expr
+open Tac2typing_env
 
 (** Hardwired types and constants *)
 
@@ -31,166 +32,11 @@ let t_ltac1 = ltac1_type "t"
 let t_preterm = coq_type "preterm"
 let t_bool = coq_type "bool"
 
-(** Union find *)
-
-module UF :
-sig
-type elt
-type 'a t
-val equal : elt -> elt -> bool
-val create : unit -> 'a t
-val fresh : 'a t -> elt
-val find : elt -> 'a t -> (elt * 'a option)
-val union : elt -> elt -> 'a t -> unit
-val set : elt -> 'a -> 'a t -> unit
-module Map :
-sig
-  type key = elt
-  type +'a t
-  val empty : 'a t
-  val add : key -> 'a -> 'a t -> 'a t
-  val mem : key -> 'a t -> bool
-  val find : key -> 'a t -> 'a
-  val exists : (key -> 'a -> bool) -> 'a t -> bool
-end
-end
-=
-struct
-type elt = int
-let equal = Int.equal
-module Map = Int.Map
-
-type 'a node =
-| Canon of int * 'a option
-| Equiv of elt
-
-type 'a t = {
-  mutable uf_data : 'a node array;
-  mutable uf_size : int;
-}
-
-let resize p =
-  if Int.equal (Array.length p.uf_data) p.uf_size then begin
-    let nsize = 2 * p.uf_size + 1 in
-    let v = Array.make nsize (Equiv 0) in
-    Array.blit p.uf_data 0 v 0 (Array.length p.uf_data);
-    p.uf_data <- v;
-  end
-
-let create () = { uf_data = [||]; uf_size = 0 }
-
-let fresh p =
-  resize p;
-  let n = p.uf_size in
-  p.uf_data.(n) <- (Canon (1, None));
-  p.uf_size <- n + 1;
-  n
-
-let rec lookup n p =
-  let node = Array.get p.uf_data n in
-  match node with
-  | Canon (size, v) -> n, size, v
-  | Equiv y ->
-    let ((z, _, _) as res) = lookup y p in
-    if not (Int.equal z y) then Array.set p.uf_data n (Equiv z);
-    res
-
-let find n p =
-  let (x, _, v) = lookup n p in (x, v)
-
-let union x y p =
-  let ((x, size1, _) as xcan) = lookup x p in
-  let ((y, size2, _) as ycan) = lookup y p in
-  let xcan, ycan = if size1 < size2 then xcan, ycan else ycan, xcan in
-  let x, _, xnode = xcan in
-  let y, _, ynode = ycan in
-  assert (Option.is_empty xnode);
-  assert (Option.is_empty ynode);
-  p.uf_data.(x) <- Equiv y;
-  p.uf_data.(y) <- Canon (size1 + size2, None)
-
-let set x v p =
-  let (x, s, v') = lookup x p in
-  assert (Option.is_empty v');
-  p.uf_data.(x) <- Canon (s, Some v)
-
-end
-
-type mix_var =
-| GVar of UF.elt
-| LVar of int
-
-type mix_type_scheme = int * mix_var glb_typexpr
-
-type environment = {
-  env_var : mix_type_scheme Id.Map.t;
-  (** Type schemes of bound variables *)
-  env_cst : UF.elt glb_typexpr UF.t;
-  (** Unification state *)
-  env_als : UF.elt Id.Map.t ref;
-  (** Map user-facing type variables to unification variables *)
-  env_opn : bool;
-  (** Accept unbound type variables *)
-  env_rec : (KerName.t * int) Id.Map.t;
-  (** Recursive type definitions *)
-  env_str : bool;
-  (** True iff in strict mode *)
-}
-
-let empty_env () = {
-  env_var = Id.Map.empty;
-  env_cst = UF.create ();
-  env_als = ref Id.Map.empty;
-  env_opn = true;
-  env_rec = Id.Map.empty;
-  env_str = true;
-}
-
-let env_name env =
-  (* Generate names according to a provided environment *)
-  let mk num =
-    let base = num mod 26 in
-    let rem = num / 26 in
-    let name = String.make 1 (Char.chr (97 + base)) in
-    let suff = if Int.equal rem 0 then "" else string_of_int rem in
-    let name = name ^ suff in
-    name
-  in
-  let fold id elt acc = UF.Map.add elt (Id.to_string id) acc in
-  let vars = Id.Map.fold fold env.env_als.contents UF.Map.empty in
-  let vars = ref vars in
-  let rec fresh n =
-    let name = mk n in
-    if UF.Map.exists (fun _ name' -> String.equal name name') !vars then fresh (succ n)
-    else name
-  in
-  fun n ->
-    if UF.Map.mem n !vars then UF.Map.find n !vars
-    else
-      let ans = fresh 0 in
-      let () = vars := UF.Map.add n ans !vars in
-      ans
-
-let ltac2_env : environment Genintern.Store.field =
+let ltac2_env : Tac2typing_env.t Genintern.Store.field =
   Genintern.Store.field ()
 
 let drop_ltac2_env store =
   Genintern.Store.remove store ltac2_env
-
-let fresh_id env = UF.fresh env.env_cst
-
-let get_alias {loc;v=id} env =
-  try Id.Map.find id env.env_als.contents
-  with Not_found ->
-    if env.env_opn then
-      let n = fresh_id env in
-      let () = env.env_als := Id.Map.add id n env.env_als.contents in
-      n
-    else user_err ?loc (str "Unbound type parameter " ++ Id.print id)
-
-let push_name id t env = match id with
-| Anonymous -> env
-| Name id -> { env with env_var = Id.Map.add id t env.env_var }
 
 let error_nargs_mismatch ?loc kn nargs nfound =
   let cstr = Tac2env.shortest_qualid_of_constructor kn in
@@ -203,23 +49,16 @@ let error_nparams_mismatch ?loc nargs nfound =
     str " arguments, but is applied to " ++ int nfound ++
     str " arguments")
 
-let rec subst_type subst (t : 'a glb_typexpr) = match t with
-| GTypVar id -> subst id
-| GTypArrow (t1, t2) -> GTypArrow (subst_type subst t1, subst_type subst t2)
-| GTypRef (qid, args) ->
-  GTypRef (qid, List.map (fun t -> subst_type subst t) args)
-
-let rec intern_type env ({loc;v=t} : raw_typexpr) : UF.elt glb_typexpr = match t with
+let rec intern_type env ({loc;v=t} : raw_typexpr) : TVar.t glb_typexpr = match t with
 | CTypVar (Name id) -> GTypVar (get_alias (CAst.make ?loc id) env)
 | CTypVar Anonymous -> GTypVar (fresh_id env)
 | CTypRef (rel, args) ->
   let (kn, nparams) = match rel with
   | RelId qid ->
-    let id = qualid_basename qid in
-    if qualid_is_ident qid && Id.Map.mem id env.env_rec then
-      let (kn, n) = Id.Map.find id env.env_rec in
+    begin match (if qualid_is_ident qid then find_rec_var (qualid_basename qid) env else None) with
+    | Some (kn, n) ->
       (Other kn, n)
-    else
+    | None ->
       let kn =
         try Tac2env.locate_type qid
         with Not_found ->
@@ -227,6 +66,7 @@ let rec intern_type env ({loc;v=t} : raw_typexpr) : UF.elt glb_typexpr = match t
       in
       let (nparams, _) = Tac2env.interp_type kn in
       (Other kn, nparams)
+    end
   | AbsKn (Other kn) ->
     let (nparams, _) = Tac2env.interp_type kn in
     (Other kn, nparams)
@@ -248,13 +88,13 @@ let rec intern_type env ({loc;v=t} : raw_typexpr) : UF.elt glb_typexpr = match t
   GTypRef (kn, List.map (fun t -> intern_type env t) args)
 | CTypArrow (t1, t2) -> GTypArrow (intern_type env t1, intern_type env t2)
 
-let fresh_type_scheme env (t : type_scheme) : UF.elt glb_typexpr =
+let fresh_type_scheme env (t : type_scheme) : TVar.t glb_typexpr =
   let (n, t) = t in
   let subst = Array.init n (fun _ -> fresh_id env) in
   let substf i = GTypVar subst.(i) in
   subst_type substf t
 
-let fresh_mix_type_scheme env (t : mix_type_scheme) : UF.elt glb_typexpr =
+let fresh_mix_type_scheme env (t : mix_type_scheme) : TVar.t glb_typexpr =
   let (n, t) = t in
   let subst = Array.init n (fun _ -> fresh_id env) in
   let substf = function
@@ -271,119 +111,6 @@ let fresh_reftype env (kn : KerName.t or_tuple) =
   let subst = Array.init n (fun _ -> fresh_id env) in
   let t = GTypRef (kn, Array.map_to_list (fun i -> GTypVar i) subst) in
   (subst, t)
-
-(** First-order unification algorithm *)
-let is_unfoldable kn = match snd (Tac2env.interp_type kn) with
-| GTydDef (Some _) -> true
-| GTydDef None | GTydAlg _ | GTydRec _ | GTydOpn -> false
-
-let unfold env kn args =
-  let (nparams, def) = Tac2env.interp_type kn in
-  let def = match def with
-  | GTydDef (Some t) -> t
-  | _ -> assert false
-  in
-  let args = Array.of_list args in
-  let subst n = args.(n) in
-  subst_type subst def
-
-(** View function, allows to ensure head normal forms *)
-let rec kind env t = match t with
-| GTypVar id ->
-  let (id, v) = UF.find id env.env_cst in
-  begin match v with
-  | None -> GTypVar id
-  | Some t -> kind env t
-  end
-| GTypRef (Other kn, tl) ->
-  if is_unfoldable kn then kind env (unfold env kn tl) else t
-| GTypArrow _ | GTypRef (Tuple _, _) -> t
-
-(** Normalize unification variables without unfolding type aliases *)
-let rec nf env t = match t with
-| GTypVar id ->
-  let (id, v) = UF.find id env.env_cst in
-  begin match v with
-  | None -> GTypVar id
-  | Some t -> nf env t
-  end
-| GTypRef (kn, tl) ->
-  let tl = List.map (fun t -> nf env t) tl in
-  GTypRef (kn, tl)
-| GTypArrow (t, u) ->
-  let t = nf env t in
-  let u = nf env u in
-  GTypArrow (t, u)
-
-let pr_glbtype env t =
-  let t = nf env t in
-  let name = env_name env in
-  pr_glbtype name t
-
-exception Occur
-
-let rec occur_check env id t = match kind env t with
-| GTypVar id' -> if UF.equal id id' then raise Occur
-| GTypArrow (t1, t2) ->
-  let () = occur_check env id t1 in
-  occur_check env id t2
-| GTypRef (kn, tl) ->
-  List.iter (fun t -> occur_check env id t) tl
-
-exception CannotUnify of UF.elt glb_typexpr * UF.elt glb_typexpr
-
-let unify_var env id t = match kind env t with
-| GTypVar id' ->
-  if not (UF.equal id id') then UF.union id id' env.env_cst
-| GTypArrow _ | GTypRef _ ->
-  try
-    let () = occur_check env id t in
-    UF.set id t env.env_cst
-  with Occur -> raise (CannotUnify (GTypVar id, t))
-
-let eq_or_tuple eq t1 t2 = match t1, t2 with
-| Tuple n1, Tuple n2 -> Int.equal n1 n2
-| Other o1, Other o2 -> eq o1 o2
-| _ -> false
-
-let rec unify0 env t1 t2 = match kind env t1, kind env t2 with
-| GTypVar id, t | t, GTypVar id ->
-  unify_var env id t
-| GTypArrow (t1, u1), GTypArrow (t2, u2) ->
-  let () = unify0 env t1 t2 in
-  unify0 env u1 u2
-| GTypRef (kn1, tl1), GTypRef (kn2, tl2) ->
-  if eq_or_tuple KerName.equal kn1 kn2 then
-    List.iter2 (fun t1 t2 -> unify0 env t1 t2) tl1 tl2
-  else raise (CannotUnify (t1, t2))
-| _ -> raise (CannotUnify (t1, t2))
-
-let unify ?loc env t1 t2 =
-  try unify0 env t1 t2
-  with CannotUnify (u1, u2) ->
-    user_err ?loc (str "This expression has type" ++ spc () ++ pr_glbtype env t1 ++
-      spc () ++ str "but an expression was expected of type" ++ spc () ++ pr_glbtype env t2)
-
-let unify_arrow ?loc env ft args =
-  let ft0 = ft in
-  let rec iter ft args is_fun = match kind env ft, args with
-  | t, [] -> t
-  | GTypArrow (t1, ft), (loc, t2) :: args ->
-    let () = unify ?loc env t2 t1 in
-    iter ft args true
-  | GTypVar id, (_, t) :: args ->
-    let ft = GTypVar (fresh_id env) in
-    let () = unify ?loc env (GTypVar id) (GTypArrow (t, ft)) in
-    iter ft args true
-  | GTypRef _, _ :: _ ->
-    if is_fun then
-      user_err ?loc (str "This function has type" ++ spc () ++ pr_glbtype env ft0 ++
-        spc () ++ str "and is applied to too many arguments")
-    else
-      user_err ?loc (str "This expression has type" ++ spc () ++ pr_glbtype env ft0 ++
-        spc () ++ str "and is not a function")
-  in
-  iter ft args false
 
 (** Term typing *)
 
@@ -412,58 +139,6 @@ let is_rec_rhs = function
 | GTacAtm _ | GTacVar _ | GTacRef _ | GTacApp _ | GTacLet _ | GTacPrj _
 | GTacSet _ | GTacExt _ | GTacPrm _ | GTacCst _
 | GTacCse _ | GTacOpn _ | GTacWth _ | GTacFullMatch _-> false
-
-let rec fv_type f t accu = match t with
-| GTypVar id -> f id accu
-| GTypArrow (t1, t2) -> fv_type f t1 (fv_type f t2 accu)
-| GTypRef (kn, tl) -> List.fold_left (fun accu t -> fv_type f t accu) accu tl
-
-let fv_env env =
-  let rec f id accu = match UF.find id env.env_cst with
-  | id, None -> UF.Map.add id () accu
-  | _, Some t -> fv_type f t accu
-  in
-  let fold_var id (_, t) accu =
-    let fmix id accu = match id with
-    | LVar _ -> accu
-    | GVar id -> f id accu
-    in
-    fv_type fmix t accu
-  in
-  let fv_var = Id.Map.fold fold_var env.env_var UF.Map.empty in
-  let fold_als _ id accu = f id accu in
-  Id.Map.fold fold_als !(env.env_als) fv_var
-
-let abstract_var env (t : UF.elt glb_typexpr) : mix_type_scheme =
-  let fv = fv_env env in
-  let count = ref 0 in
-  let vars = ref UF.Map.empty in
-  let rec subst id =
-    let (id, t) = UF.find id env.env_cst in
-    match t with
-    | None ->
-      if UF.Map.mem id fv then GTypVar (GVar id)
-      else
-        begin try UF.Map.find id !vars
-        with Not_found ->
-          let n = !count in
-          let var = GTypVar (LVar n) in
-          let () = incr count in
-          let () = vars := UF.Map.add id var !vars in
-          var
-        end
-    | Some t -> subst_type subst t
-  in
-  let t = subst_type subst t in
-  (!count, t)
-
-let monomorphic (t : UF.elt glb_typexpr) : mix_type_scheme =
-  let subst id = GTypVar (GVar id) in
-  (0, subst_type subst t)
-
-let polymorphic ((n, t) : type_scheme) : mix_type_scheme =
-  let subst id = GTypVar (LVar id) in
-  (n, subst_type subst t)
 
 let warn_not_unit =
   CWarnings.create ~name:"not-unit" ~category:"ltac"
@@ -519,23 +194,6 @@ let check_unit ?loc t =
 let check_redundant_clause = function
 | [] -> ()
 | (p, _) :: _ -> warn_redundant_clause ?loc:p.loc ()
-
-let get_variable0 mem var = match var with
-| RelId qid ->
-  let id = qualid_basename qid in
-  if qualid_is_ident qid && mem id then ArgVar CAst.(make ?loc:qid.CAst.loc id)
-  else
-    let kn =
-      try Tac2env.locate_ltac qid
-      with Not_found ->
-        CErrors.user_err ?loc:qid.CAst.loc (str "Unbound value " ++ pr_qualid qid)
-    in
-    ArgArg kn
-| AbsKn kn -> ArgArg kn
-
-let get_variable env var =
-  let mem id = Id.Map.mem id env.env_var in
-  get_variable0 mem var
 
 let get_constructor env var = match var with
 | RelId qid ->
@@ -709,9 +367,6 @@ let check_deprecated_ltac2 ?loc qid def =
     | None -> ()
     | Some depr -> deprecated_ltac2_def ?loc (kn, depr)
     end
-
-let push_ids ids env =
-  { env with env_var = Id.Map.union (fun _ x _ -> Some x) ids env.env_var }
 
 let ctor_data_for_patterns kn data = {
   ctyp = data.cdata_type;
@@ -1138,7 +793,7 @@ let rec intern_rec env {loc;v=e} = match e with
 | CTacRef qid ->
   begin match get_variable env qid with
   | ArgVar {CAst.v=id} ->
-    let sch = Id.Map.find id env.env_var in
+    let sch = find_var id env in
     (GTacVar id, fresh_mix_type_scheme env sch)
   | ArgArg (TacConstant kn) ->
     let { Tac2env.gdata_type = sch; gdata_deprecation = depr } =
@@ -1167,7 +822,7 @@ let rec intern_rec env {loc;v=e} = match e with
   | Some t -> intern_type env t
   in
   let tl = List.map map bnd in
-  let (nas, exp) = expand_pattern (Id.Map.domain env.env_var) bnd in
+  let (nas, exp) = expand_pattern (bound_vars env) bnd in
   let env = List.fold_left2 (fun env na t -> push_name na (monomorphic t) env) env nas tl in
   let (e, t) = intern_rec env (exp e) in
   let t = List.fold_right (fun t accu -> GTypArrow (t, accu)) tl t in
@@ -1293,7 +948,7 @@ let rec intern_rec env {loc;v=e} = match e with
   let ist = empty_glob_sign genv in
   let ist = { ist with extra = Store.set ist.extra ltac2_env env } in
   let arg, tpe =
-    if env.env_str then
+    if env_strict env then
       let arg () = obj.ml_intern self ist arg in
       Flags.with_option Ltac_plugin.Tacintern.strict_check arg ()
     else
@@ -1311,7 +966,7 @@ and intern_rec_with_constraint env e exp =
   er
 
 and intern_let env loc ids el e =
-  let avoid = Id.Set.union ids (Id.Map.domain env.env_var) in
+  let avoid = Id.Set.union ids (bound_vars env) in
   let fold (pat, t, e) (avoid, accu) =
     let nas, exp = expand_pattern avoid [pat, t] in
     let na = match nas with [x] -> x | _ -> assert false in
@@ -1593,46 +1248,29 @@ and super_intern_case env loc e pl =
   let pl = List.map (fun (p,br) -> glb_of_wip_pat p, br) pl in
   (e,pl,rt)
 
-let normalize env (count, vars) (t : UF.elt glb_typexpr) =
-  let get_var id =
-    try UF.Map.find id !vars
-    with Not_found ->
-      let () = assert env.env_opn in
-      let n = GTypVar !count in
-      let () = incr count in
-      let () = vars := UF.Map.add id n !vars in
-      n
-  in
-  let rec subst id = match UF.find id env.env_cst with
-  | id, None -> get_var id
-  | _, Some t -> subst_type subst t
-  in
-  subst_type subst t
-
 type context = (Id.t * type_scheme) list
 
 let intern ~strict ctx e =
-  let env = empty_env () in
-  let env = if strict then env else { env with env_str = false } in
+  let env = empty_env ~strict () in
   let fold accu (id, t) = push_name (Name id) (polymorphic t) accu in
   let env = List.fold_left fold env ctx in
   let (e, t) = intern_rec env e in
   let count = ref 0 in
-  let vars = ref UF.Map.empty in
+  let vars = ref TVar.Map.empty in
   let t = normalize env (count, vars) t in
   (e, (!count, t))
 
 let intern_typedef self (ids, t) : glb_quant_typedef =
-  let env = { (empty_env ()) with env_rec = self } in
+  let env = set_rec self (empty_env ()) in
   (* Initialize type parameters *)
   let map id = get_alias id env in
   let ids = List.map map ids in
   let count = ref (List.length ids) in
-  let vars = ref UF.Map.empty in
-  let iter n id = vars := UF.Map.add id (GTypVar n) !vars in
+  let vars = ref TVar.Map.empty in
+  let iter n id = vars := TVar.Map.add id (GTypVar n) !vars in
   let () = List.iteri iter ids in
   (* Do not accept unbound type variables *)
-  let env = { env with env_opn = false } in
+  let env = reject_unbound_tvar env in
   let intern t =
     let t = intern_type env t in
     normalize env (count, vars) t
@@ -1665,7 +1303,7 @@ let intern_open_type t =
   let env = empty_env () in
   let t = intern_type env t in
   let count = ref 0 in
-  let vars = ref UF.Map.empty in
+  let vars = ref TVar.Map.empty in
   let t = normalize env (count, vars) t in
   (!count, t)
 
@@ -2059,9 +1697,8 @@ let () =
     let env = match Genintern.Store.get ist.extra ltac2_env with
     | None ->
       (* Only happens when Ltac2 is called from a toplevel ltac1 quotation *)
-      let env = empty_env () in
-      if !Ltac_plugin.Tacintern.strict_check then env
-      else { env with env_str = false }
+      let strict = !Ltac_plugin.Tacintern.strict_check in
+      empty_env ~strict ()
     | Some env -> env
     in
     let fold env id =
@@ -2081,14 +1718,13 @@ let () =
     let env = match Genintern.Store.get ist.extra ltac2_env with
     | None ->
       (* Only happens when Ltac2 is called from a constr quotation *)
-      let env = empty_env () in
-      if !Ltac_plugin.Tacintern.strict_check then env
-      else { env with env_str = false }
+      let strict = !Ltac_plugin.Tacintern.strict_check in
+      empty_env ~strict ()
     | Some env -> env
     in
     (* Special handling of notation variables *)
     let fold id _ (ids, env) =
-      let () = assert (not @@ Id.Map.mem id env.env_var) in
+      let () = assert (not @@ mem_var id env) in
       let t = monomorphic (GTypRef (Other t_preterm, [])) in
       let env = push_name (Name id) t env in
       (Id.Set.add id ids, env)
@@ -2111,9 +1747,8 @@ let () =
     let env = match Genintern.Store.get ist.extra ltac2_env with
     | None ->
       (* Only happens when Ltac2 is called from a constr or ltac1 quotation *)
-      let env = empty_env () in
-      if !Ltac_plugin.Tacintern.strict_check then env
-      else { env with env_str = false }
+      let strict = !Ltac_plugin.Tacintern.strict_check in
+      empty_env ~strict ()
     | Some env -> env
     in
     (* Special handling of notation variables *)
@@ -2123,7 +1758,7 @@ let () =
         unify ?loc env (GTypRef (Other t_preterm, [])) (GTypRef (Other t_constr, []))
     in
     let t =
-      try Id.Map.find id env.env_var
+      try find_var id env
       with Not_found ->
         CErrors.user_err ?loc (str "Unbound value " ++ Id.print id)
     in
---
+++ plugins/ltac2/tac2typing_env.mli
@@ -0,0 +1,91 @@
+(************************************************************************)
+(*         *   The Coq Proof Assistant / The Coq Development Team       *)
+(*  v      *         Copyright INRIA, CNRS and contributors             *)
+(* <O___,, * (see version control and CREDITS file for authors & dates) *)
+(*   \VV/  **************************************************************)
+(*    //   *    This file is distributed under the terms of the         *)
+(*         *     GNU Lesser General Public License Version 2.1          *)
+(*         *     (see LICENSE file for the text of the license)         *)
+(************************************************************************)
+
+open Names
+open Tac2expr
+
+module TVar : sig
+  type t
+
+  val equal : t -> t -> bool
+
+  module Map : CSig.MapS with type key = t
+end
+
+type t
+
+(** default strict:true *)
+val empty_env : ?strict:bool -> unit -> t
+
+val set_rec : (KerName.t * int) Id.Map.t -> t -> t
+
+val reject_unbound_tvar : t -> t
+
+val env_strict : t -> bool
+
+val env_name : t -> TVar.t -> string
+
+val fresh_id : t -> TVar.t
+
+val get_alias : lident -> t -> TVar.t
+
+val find_rec_var : Id.t -> t -> (KerName.t * int) option
+
+type mix_var =
+| GVar of TVar.t
+| LVar of int
+
+type mix_type_scheme = int * mix_var glb_typexpr
+
+val mem_var : Id.t -> t -> bool
+
+val find_var : Id.t -> t -> mix_type_scheme
+
+val bound_vars : t -> Id.Set.t
+
+val get_variable0 : (Id.t -> bool) -> tacref or_relid -> tacref Locus.or_var
+
+val get_variable : t -> tacref or_relid -> tacref Locus.or_var
+
+(** View function, allows to ensure head normal forms *)
+val kind : t -> TVar.t glb_typexpr -> TVar.t glb_typexpr
+
+val pr_glbtype : t -> TVar.t glb_typexpr -> Pp.t
+
+val eq_or_tuple : ('a -> 'b -> bool) -> 'a or_tuple -> 'b or_tuple -> bool
+
+exception CannotUnify of TVar.t glb_typexpr * TVar.t glb_typexpr
+
+(** unify0 raises CannotUnify, unify raises usererror *)
+val unify0 : t -> TVar.t glb_typexpr -> TVar.t glb_typexpr -> unit
+
+val unify : ?loc:Loc.t -> t -> TVar.t glb_typexpr -> TVar.t glb_typexpr -> unit
+
+val unify_arrow : ?loc:Loc.t -> t
+  -> TVar.t glb_typexpr
+  -> (Loc.t option * TVar.t glb_typexpr) list
+  -> TVar.t glb_typexpr
+
+val abstract_var : t -> TVar.t glb_typexpr -> mix_type_scheme
+
+val monomorphic : TVar.t glb_typexpr -> mix_type_scheme
+
+val polymorphic : type_scheme -> mix_type_scheme
+
+val push_name : Name.t -> mix_type_scheme -> t -> t
+
+val push_ids : mix_type_scheme Id.Map.t -> t -> t
+
+val subst_type : ('a -> 'b glb_typexpr) -> 'a glb_typexpr -> 'b glb_typexpr
+
+val normalize : t ->
+  int ref * int glb_typexpr TVar.Map.t ref ->
+  TVar.t glb_typexpr ->
+  int glb_typexpr
