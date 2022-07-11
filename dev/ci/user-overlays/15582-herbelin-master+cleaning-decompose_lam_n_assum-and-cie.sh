5aa39c5194cb81d9c52904aab205e3b7d8da97f1
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2022-07-11 19:01:28 +0200

    Overlay for Equations

---
+++ dev/ci/user-overlays/15582-herbelin-master+cleaning-decompose_lam_n_assum-and-cie.sh
@@ -0,0 +1,1 @@
+overlay equations https://github.com/herbelin/Coq-Equations master+adapt-coq-pr15582-cleaning-API-destruct-construct-abstractions-products 15582 master+cleaning-decompose_lam_n_assum-and-cie
