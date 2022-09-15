eee07d5b590e9c62e761d915f885666725c3e7c2
Gregory Malecha <gregory@bedrocksystems.com>
2022-09-15 15:38:45 -0400

    Avoid round-trip [fconstr] -> [constr] -> [fconstr].

--- kernel/typeops.ml
+++ kernel/typeops.ml
@@ -196,10 +196,10 @@ let type_of_apply env func funt argsv argstv =
       | FProd (_, c1, c2, e) ->
         let arg = argsv.(i) in
         let argt = argstv.(i) in
-        let c1 = term_of_fconstr c1 in
-        begin match conv_leq env argt c1 with
+        begin match conv_leq_fconstr env argt c1 with (* << checks that the type of the argument is compatible with the type of the binder *)
         | () -> apply_rec (i+1) (mk_clos (CClosure.usubs_cons (inject arg) e) c2)
         | exception NotConvertible ->
+          let c1 = term_of_fconstr c1 in
           error_cant_apply_bad_type env
             (i+1,c1,argt)
             (make_judge func funt)
