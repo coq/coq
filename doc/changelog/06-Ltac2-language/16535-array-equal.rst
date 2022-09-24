0f200f85758bb738791b83004f2a3feea2591659
Jason Gross <jgross@mit.edu>
2022-09-24 12:29:34 +0530

    Add Ltac2.Array.{for_all2,equal}
    We mirror the List functions.

---
+++ doc/changelog/06-Ltac2-language/16535-array-equal.rst
@@ -0,0 +1,4 @@
+- **Added:**
+  ``Ltac2.Array.for_all2`` and ``Ltac2.Array.equal``
+  (`#16535 <https://github.com/coq/coq/pull/16535>`_,
+  by Jason Gross).
--- user-contrib/Ltac2/Array.v
+++ user-contrib/Ltac2/Array.v
@@ -225,3 +225,24 @@ Ltac2 for_all (p : 'a -> bool) (a : 'a array) := for_all_aux p a 0 (length a).
 (* Note: we don't have (yet) a generic equality function in Ltac2 *)
 Ltac2 mem (eq : 'a -> 'a -> bool) (x : 'a) (a : 'a array) :=
   exist (eq x) a.
+
+Ltac2 rec for_all2_aux (p : 'a -> 'b -> bool) (a : 'a array) (b : 'b array) (pos : int) (len : int) :=
+  if Int.equal len 0
+  then true
+  else if p (get a pos) (get b pos)
+       then for_all2_aux p a b (Int.add pos 1) (Int.sub len 1)
+       else false.
+
+Ltac2 for_all2 p a b :=
+  let lena := length a in
+  let lenb := length b in
+  if Int.equal lena lenb
+  then for_all2_aux p a b 0 lena
+  else Control.throw_invalid_argument "Array.for_all2".
+
+Ltac2 equal p a b :=
+  let lena := length a in
+  let lenb := length b in
+  if Int.equal lena lenb
+  then for_all2_aux p a b 0 lena
+  else false.
