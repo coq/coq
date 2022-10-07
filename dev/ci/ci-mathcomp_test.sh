d2076f157d6d351740c517c4c597df8907f5ee81
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2022-10-07 14:03:00 +0200

    Split running the mathcomp test suite to its own job

--- dev/ci/ci-mathcomp_test.sh
+++ dev/ci/ci-mathcomp_test.sh
@@ -5,11 +5,8 @@ set -e
 ci_dir="$(dirname "$0")"
 . "${ci_dir}/ci-common.sh"
 
-git_download mathcomp
-
 if [ "$DOWNLOAD_ONLY" ]; then exit 0; fi
 
 ( cd "${CI_BUILD_DIR}/mathcomp/mathcomp"
-  make
-  make install
+  make test-suite
 )
