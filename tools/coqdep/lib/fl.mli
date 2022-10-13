ce9b6ddd97e1dc43e6827f14d43272b3324c7d27
Emilio Jesus Gallego Arias <e+git@x80.org>
2022-10-13 17:14:29 +0200

    [coqdep] Be more deterministic w.r.t. the plugin loading mode
    If the legacy mode is set, we don't even attempt to use findlib to
    resolve anything, we also won't emit a META dependency, even if this
    dependency is in scope.
    
    This is morally the right thing to do, should reduce non-determinism,
    and helps a bit to isolate the legacy code more.
    
    This should help with problems such as
    https://github.com/ocaml/dune/issues/5833

--- tools/coqdep/lib/fl.mli
+++ tools/coqdep/lib/fl.mli
@@ -8,22 +8,20 @@
 (*         *     (see LICENSE file for the text of the license)         *)
 (************************************************************************)
 
-(** [findlib_resolve meta_files file package legacy_name plugin_name]
-   tries to locate a [.cmxs] for a given [package.plugin_name]
+(** [findlib_resolve meta_files file package plugin_name] tries to
+   locate a [.cmxs] for a given [package.plugin_name]
 
     If a [META] file for [package] is found, it will try to use it to
-   resolve the path to the [.cmxs], and return both. If not, it will
-   return [None, legacy_name]. If [legacy_name] is absent, it errors.
+   resolve the path to the [.cmxs], and return both. If not, it
+   errors.
 
     The [META] file for [package] is search among the list of
    [meta_files] first, then using [Findlib.package_meta_file]. note
    that coqdep doesn't initialize findlib so that function performs
-   implicity initialization.
-*)
+   implicity initialization.  *)
 val findlib_resolve
   : meta_files:string list
   -> file:string
   -> package:string
-  -> legacy_name:string option
   -> plugin_name:string list
-  -> string option * string
+  -> string * string
