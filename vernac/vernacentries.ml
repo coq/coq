herbelin/github-coq

Adding support for deactivating/reactivating notations.
This was provided for parser notations but without user syntax.

We add syntax and provide it for abbreviations, including the removal
of the abbreviation from the nametab.

vernac/vernacentries.ml
@@ -533,6 +533,16 @@ let vernac_notation ~atts =
 let vernac_custom_entry ~module_local s =
   Metasyntax.declare_custom_entry module_local s
 
+let vernac_toggle_notation ~module_local ~on ~use rule =
+  let open Notation in
+  let rule = match rule with
+    | NotationRule (sc,s) -> NotationRule (sc,s)
+    | SynDefRule qid ->
+      match Nametab.locate_extended qid with
+      | Globnames.TrueGlobal _ -> user_err (str "Not a notation.")
+      | Globnames.SynDef kn -> SynDefRule kn in
+  Metasyntax.declare_notation_toggle module_local ~on ~use rule
+
 (***********)
 (* Gallina *)
 
@@ -2110,6 +2120,8 @@ let translate_vernac ?loc ~atts v = let open Vernacextend in match v with
         Metasyntax.add_notation_extra_printing_rule n k v)
   | VernacDeclareCustomEntry s ->
     VtDefault(fun () -> with_module_locality ~atts vernac_custom_entry s)
+  | VernacToggleNotation (on,use,rule) ->
+    VtDefault(fun () -> with_module_locality ~atts vernac_toggle_notation ~on ~use rule)
 
   (* Gallina *)
 

vernac/vernacexpr.ml
@@ -321,6 +321,7 @@ type nonrec vernac_expr =
       scope_name option
   | VernacNotationAddFormat of string * string * string
   | VernacDeclareCustomEntry of string
+  | VernacToggleNotation of bool * Notation.notation_use * qualid Notation.interp_rule_gen
 
   (* Gallina *)
   | VernacDefinition of (discharge * Decls.definition_object_kind) * name_decl * definition_expr

coq/coq

vernac/vernacentries.ml
@@ -538,6 +538,16 @@ let vernac_notation ~atts ~infix =
 let vernac_custom_entry ~module_local s =
   Metasyntax.declare_custom_entry module_local s
 
+let vernac_toggle_notation ~module_local ~on ~use rule =
+  let open Notationextern in
+  let rule = match rule with
+    | NotationRule (sc,s) -> NotationRule (sc,s)
+    | AbbrevRule qid ->
+      match Nametab.locate_extended qid with
+      | Globnames.TrueGlobal _ -> user_err (str "Not a notation.")
+      | Globnames.Abbrev kn -> AbbrevRule kn in
+  Metasyntax.declare_notation_toggle module_local ~on ~use rule
+
 (***********)
 (* Gallina *)
 
@@ -2310,6 +2320,8 @@ let translate_vernac ?loc ~atts v = let open Vernacextend in match v with
     vtdefault(fun () -> vernac_notation ~atts ~infix c infpl sc)
   | VernacDeclareCustomEntry s ->
     vtdefault(fun () -> with_module_locality ~atts vernac_custom_entry s)
+  | VernacToggleNotation (on,use,rule) ->
+    vtdefault(fun () -> with_module_locality ~atts vernac_toggle_notation ~on ~use rule)
 
   (* Gallina *)
 

vernac/vernacexpr.ml
@@ -351,6 +351,7 @@ type nonrec vernac_expr =
       infix_flag * constr_expr * (lstring * syntax_modifier CAst.t list) *
       scope_name option
   | VernacDeclareCustomEntry of string
+  | VernacToggleNotation of bool * Notationextern.notation_use * qualid Notationextern.interp_rule_gen
 
   (* Gallina *)
   | VernacDefinition of (discharge * Decls.definition_object_kind) * name_decl * definition_expr