77b6713c8fb716e49ad07337682abeda50b95381
Théo Zimmermann <theo.zimmermann@inria.fr>
2022-11-16 17:42:50 +0100

    Remove compatibility with Coq 8.14.
    Changes generated by running dev/tools/update-compat.py --release

--- test-suite/success/CompatOldOldFlag.v
+++
@@ -1,6 +0,0 @@
-(* -*- coq-prog-args: ("-compat" "8.14") -*- *)
-(** Check that the current-minus-three compatibility flag actually requires the relevant modules. *)
-Import Coq.Compat.Coq817.
-Import Coq.Compat.Coq816.
-Import Coq.Compat.Coq815.
-Import Coq.Compat.Coq814.
