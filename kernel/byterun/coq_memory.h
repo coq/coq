c4fae667f40d76d13d7a23b91015dc1c2b189f1a
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2022-12-08 16:45:36 +0100

    Remove signatures of non-existing functions.

--- kernel/byterun/coq_memory.h
+++ kernel/byterun/coq_memory.h
@@ -44,9 +44,7 @@ extern value * coq_sp;
 value coq_static_alloc(value size);  /* ML */
 
 value init_coq_vm(value unit); /* ML */
-value re_init_coq_vm(value unit); /* ML */
 
 void  realloc_coq_stack(asize_t required_space);
-value coq_set_transp_value(value transp); /* ML */
-value get_coq_transp_value(value unit); /* ML */
+
 #endif /* _COQ_MEMORY_ */
