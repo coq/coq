eede36cd6ce959832314e5389285b1bbb4cb7ec9
Théo Zimmermann <theo.zimmermann@inria.fr>
2022-12-09 18:22:22 +0100

    Extend list-contributors script to also list reviewers and assignees.

--- dev/tools/list-contributors.sh
+++ dev/tools/list-contributors.sh
@@ -7,9 +7,23 @@ if [ $# != 1 ]; then
   exit 1
 fi
 
-git shortlog -s -n --group=author --group=trailer:Co-authored-by $1 | cut -f2 | sort -k 2 | grep -v -e "coqbot" -e "^$" > contributors.tmp
+git shortlog -s -n --no-merges --group=author --group=trailer:Co-authored-by $1 | cut -f2 | sort -k 2 | grep -v -e "coqbot" -e "^$" > contributors.tmp
 
 cat contributors.tmp | wc -l | xargs echo "Contributors:"
 cat contributors.tmp | $SED -z "s/\n/, /g"
 echo
 rm contributors.tmp
+
+git shortlog -s -n --merges --group=author --group=trailer:Co-authored-by $1 | cut -f2 | sort -k 2 | grep -v -e "coqbot" -e "^$" > assignees.tmp
+
+cat assignees.tmp | wc -l | xargs echo "Assignees:"
+cat assignees.tmp | $SED -z "s/\n/, /g"
+echo
+rm assignees.tmp
+
+git shortlog -s -n --merges --group=trailer:reviewed-by --group=trailer:ack-by $1 | cut -f2 | sort -k 2 | grep -v -e "coqbot" -e "^$" > reviewers.tmp
+
+cat reviewers.tmp | wc -l | xargs echo "Reviewers:"
+cat reviewers.tmp | $SED -z "s/\n/, /g"
+echo
+rm reviewers.tmp
