f6a46015d893d61b43f3d5d8fc19c5b4bc54c4d4
Maxime Dénès <maxime.denes@inria.fr>
2022-12-09 11:08:34 +0100

    Add overlays

---
+++ dev/ci/user-overlays/16944-maximedenes-separate-notation-syntax.sh
@@ -0,0 +1,3 @@
+overlay equations https://github.com/maximedenes/Coq-Equations separate-notation-syntax 16944
+
+overlay serapi https://github.com/maximedenes/coq-serapi separate-notation-syntax 16944
