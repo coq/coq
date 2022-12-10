5b9ff23dfd013f519bf2b9dd591be1eb6c7c9931
Emilio Jesus Gallego Arias <e+git@x80.org>
2022-12-10 19:30:21 +0100

    [stream] Allow resumption directly at the stream level
    This is useful for parsing resumption, so we can avoid to have to call
    `Stream.njunk` often.
    
    Unit test improved to account for both methods.
    
    (cherry picked from commit dbfffa4035b4dea03d74b363ca83ea38a21cab2e)

--- ide/coqide/idetop.ml
+++ ide/coqide/idetop.ml
@@ -87,18 +87,13 @@ let ide_doc = ref None
 let get_doc () = Option.get !ide_doc
 let set_doc doc = ide_doc := Some doc
 
-(* Adds n spaces at the beggining of the string *)
-let wrap_string n s = String.make n ' ' ^ s
-
 let add ((((s,eid),(sid,verbose)),off),(line_nb,bol_pos)) =
   let doc = get_doc () in
   let open Loc in
   (* note: this won't yield correct values for bol_pos_last,
      but the debugger doesn't use that *)
   let loc = { (initial ToplevelInput) with bp=off; line_nb } in
-  let s_wrapped = wrap_string off s in
-  let r_stream = Gramlib.Stream.of_string s_wrapped in
-  let () = Gramlib.Stream.njunk off r_stream in
+  let r_stream = Gramlib.Stream.of_string ~offset:off s in
   let pa = Pcoq.Parsable.make ~loc r_stream in
   match Stm.parse_sentence ~doc sid ~entry:Pvernac.main_entry pa with
   | None -> assert false (* s may not be empty *)
