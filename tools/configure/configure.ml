5ac26a2955a4d10c7935e01caad4e31e45deb0df
Théo Zimmermann <theo.zimmermann@inria.fr>
2023-01-16 16:32:12 +0100

    Set is_a_released_version to false per release process.

--- tools/configure/configure.ml
+++ tools/configure/configure.ml
@@ -24,7 +24,7 @@ let (/) = Filename.concat
 
 let coq_version = "8.17+rc1"
 let vo_magic = 81699
-let is_a_released_version = true
+let is_a_released_version = false
 
 (** Default OCaml binaries *)
 
