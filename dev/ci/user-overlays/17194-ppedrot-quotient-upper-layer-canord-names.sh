6c0d35796497158aa02518a683d330a7d0cca664
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2023-02-03 12:55:27 +0100

    Adding overlays.

---
+++ dev/ci/user-overlays/17194-ppedrot-quotient-upper-layer-canord-names.sh
@@ -0,0 +1,3 @@
+overlay equations https://github.com/ppedrot/Coq-Equations quotient-upper-layer-canord-names 17194
+
+overlay mtac2 https://github.com/ppedrot/Mtac2 quotient-upper-layer-canord-names 17194
