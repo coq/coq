c814b7c0f4d5b4547464c55a48ed674fbd887654
Lasse Blaauwbroek <lasse@blaauwbroek.eu>
2023-02-26 02:22:54 +0100

    Add tests for already fixed issues
    Fix #10175
    Fix #10179

---
+++ test-suite/bugs/bug_10175.v
@@ -0,0 +1,20 @@
+From Coq Require Import ssreflect ssrbool ssrfun.
+From Coq Require Import List.
+
+Import Nat.
+
+Definition partition {X : Type}
+                     (test : X -> bool)
+                     (l : list X)
+                   : list X * list X
+  := (filter test l, filter (fun x => negb (test x)) l).
+
+Definition fold (A B: Type) f l b:= @fold_right B A f b l.
+
+Theorem partition_fst: forall (X : Type)  (test : X -> bool) (l : list X),
+                         fold_right andb true (map test (fst (partition test l))) = true.
+Proof.
+  move => ? test.
+  elim => [|x xs IHxs] //.
+  case H: eqb (test x) true.  (* <-- anomaly here! *)
+Abort.
