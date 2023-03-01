3440de2a73740fbef76296a6dcb3378f3da77f91
Enrico Tassi <Enrico.Tassi@Inria.fr>
2023-03-01 10:00:54 +0100

    validate env after initialization

--- boot/env.ml
+++ boot/env.ml
@@ -33,9 +33,13 @@ let fail_msg =
 let fail s = Format.eprintf "%s@\n%!" fail_msg; exit 1
 
 (* This code needs to be refactored, for now it is just what used to be in envvars  *)
+
+let theories_dir = "theories"
+let plugins_dir = "plugins"
+let prelude = Filename.concat theories_dir "Init/Prelude.vo"
+
 let guess_coqlib () =
   Util.getenv_else "COQLIB" (fun () ->
-  let prelude = "theories/Init/Prelude.vo" in
   Util.check_file_else
     ~dir:Coq_config.coqlibsuffix
     ~file:prelude
@@ -46,9 +50,32 @@ let guess_coqlib () =
 
 (* Build layout uses coqlib = coqcorelib *)
 let guess_coqcorelib lib =
-  if Sys.file_exists (Path.relative lib "plugins")
+  if Sys.file_exists (Path.relative lib plugins_dir)
   then lib
   else Path.relative lib "../coq-core"
+
+let fail_lib lib =
+  let open Printf in
+  eprintf "File not found: %s\n" lib;
+  eprintf "The path for Coq libraries is wrong.\n";
+  eprintf "Coq libraries are shipped in the coq-stdlib package.\n";
+  eprintf "Please check the COQLIB env variable or the -coqlib option.\n";
+  exit 1
+
+let fail_core plugin =
+  let open Printf in
+  eprintf "File not found: %s\n" plugin;
+  eprintf "The path for Coq plugins is wrong.\n";
+  eprintf "Coq plugins are shipped in the coq-core package.\n";
+  eprintf "Please check the COQCORELIB env variable.\n";
+  exit 1
+
+let validate_env ({ core; lib } as env) =
+  let lib = Filename.concat lib prelude in
+  if not (Sys.file_exists lib) then fail_lib lib;
+  let plugin = Filename.concat core plugins_dir in
+  if not (Sys.file_exists plugin) then fail_core plugin;
+  env
 
 (* Should we fail on double initialization? That seems a way to avoid
    mis-use for example when we pass command line arguments *)
@@ -56,13 +83,7 @@ let init () =
   let lib = guess_coqlib () in
   let core = Util.getenv_else "COQCORELIB"
       (fun () -> guess_coqcorelib lib) in
-  { core ; lib }
-
-let init () =
-  let { core; lib } = init () in
-  (* debug *)
-  if false then Format.eprintf "core = %s@\n lib = %s@\n%!" core lib;
-  { core; lib }
+  validate_env { core ; lib }
 
 let env_ref = ref None
 
@@ -74,13 +95,13 @@ let init () =
   | Some env -> env
 
 let set_coqlib lib =
-  let env = { lib; core = guess_coqcorelib lib } in
+  let env = validate_env { lib; core = guess_coqcorelib lib } in
   env_ref := Some env
 
 let coqlib { lib; _ } = lib
 let corelib { core; _ } = core
-let plugins { core; _ } = Path.relative core "plugins"
-let stdlib { lib; _ } = Path.relative lib "theories"
+let plugins { core; _ } = Path.relative core plugins_dir
+let stdlib { lib; _ } = Path.relative lib theories_dir
 let user_contrib { lib; _ } = Path.relative lib "user-contrib"
 let tool { core; _ } tool = Path.(relative (relative core "tools") tool)
 let revision { core; _ } = Path.relative core "revision"
