9c42fc0da7c656f80c328d5802a46621e877740c
Emilio Jesus Gallego Arias <e+git@x80.org>
2023-03-07 02:28:26 +0100

    Revert "Merge PR #16978: [parsing] Remove hack which hampered correct parsing resumption, fix CoqIDE tooltip display."
    This reverts commit 0b30c2161c7221e6269d1d8fa28dd92b75b118aa, reversing
    changes made to bd67ef1d617e68ea2e5fd89ed496ae5447a850ea.

--- gramlib/stream.mli
+++ gramlib/stream.mli
@@ -15,9 +15,8 @@
 
 (** Streams and parsers. *)
 
-type ('e,'a) t
-(** The type of streams holding values of type ['a].
-    Producing a new value needs an environment ['e]. *)
+type 'a t
+(** The type of streams holding values of type ['a]. *)
 
 exception Failure
 (** Raised by parsers when none of the first components of the stream
@@ -39,7 +38,7 @@ val from : ?offset:int -> ('e -> 'a option) -> ('e,'a) t
     useful for some uses cases such as parsing resumption.
 *)
 
-val empty : unit -> ('e,'a) t
+val empty : unit -> 'a t
 (** Return the stream holding the elements of the list in the same
    order. *)
 
@@ -47,41 +46,41 @@ val of_string : ?offset:int -> string -> (unit,char) t
 (** Return the stream of the characters of the string parameter. If
     set. [offset] parameter is similar to [from]. *)
 
-val of_channel : in_channel -> (unit,char) t
+val of_channel : in_channel -> char t
 (** Return the stream of the characters read from the input channel. *)
 
 (** {1 Predefined parsers} *)
 
-val next : 'e -> ('e,'a) t -> 'a
+val next : 'a t -> 'a
 (** Return the first element of the stream and remove it from the
    stream.
    @raise Stream.Failure if the stream is empty. *)
 
-val is_empty : 'e -> ('e,'a) t -> bool
+val is_empty : 'a t -> bool
 (** Return [true] if the stream is empty, else [false]. *)
 
 
 (** {1 Useful functions} *)
 
-val peek : 'e -> ('e,'a) t -> 'a option
+val peek : 'a t -> 'a option
 (** Return [Some] of "the first element" of the stream, or [None] if
    the stream is empty. *)
 
-val junk : 'e -> ('e,'a) t -> unit
+val junk : 'a t -> unit
 (** Remove the first element of the stream, possibly unfreezing
    it before. *)
 
-val count : ('e,'a) t -> int
+val count : 'a t -> int
 (** Return the current count of the stream elements, i.e. the number
    of the stream elements discarded. *)
 
-val npeek : 'e -> int -> ('e,'a) t -> 'a list
-(** [npeek e n] returns the list of the [n] first elements of
+val npeek : int -> 'a t -> 'a list
+(** [npeek n] returns the list of the [n] first elements of
    the stream, or all its remaining elements if less than [n]
    elements are available. *)
 
-val nth : 'e -> int -> ('e,'a) t -> 'a
+val nth : int -> 'a t -> 'a
 
-val njunk : 'e -> int -> ('e,'a) t -> unit
+val njunk : int -> 'a t -> unit
 
 (**/**)
