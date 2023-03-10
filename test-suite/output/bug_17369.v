51e48f29e0e0bd0e6d90c26a50169eb42a3423f1
Stefan Radziuk <stefan.radziuk19@imperial.ac.uk>
2023-03-10 00:18:17 +0000

    Haskell extraction: use block comments where line wrapping may occur

---
+++ test-suite/output/bug_17369.v
@@ -0,0 +1,13 @@
+(* Ensure that 'logical inductive' comments in extracted Haskell
+ * do not get uncommented by line wrapping *)
+
+From Coq Require Import Extraction.
+
+Set Printing Width 60.
+
+Inductive my_inductive_prop : Prop :=
+  constr_1 | constr_2 | constr_3 | constr_4 | constr_5 | constr_6 | constr_7
+.
+
+Extraction Language Haskell.
+Extraction my_inductive_prop.
