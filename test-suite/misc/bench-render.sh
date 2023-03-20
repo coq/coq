97679da372cdb7fb2f46defb40fcee66d9582da7
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2023-03-20 13:04:38 +0100

    Add test for timelog2html, make it public (as coqtimelog2html)
    This test displays the line number bug (`Definition a` should be at line 9
    but says line 8, `Definition b` should be at line 11 but says line 8).

---
+++ test-suite/misc/bench-render.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+export COQBIN=$BIN
+export PATH="$COQBIN:$PATH"
+export LC_ALL=C
+
+cd misc/bench-render
+
+coqtimelog2html foo.v foo.v.time1 foo.v.time2 > result.html.real
+
+diff result.html result.html.real
+
+if coqtimelog2html foo.v foo.v.time1 foo.v.time3 > bad1v3.html.real 2>stderr1v3.real
+then >&2 echo "Should have failed!"; exit 1
+fi
+
+diff /dev/null bad1v3.html.real
+diff stderr1v3 stderr1v3.real
+
+if coqtimelog2html foo.v foo.v.time1 foo.v.time4 > bad1v4.html.real 2>stderr1v4.real
+then >&2 echo "Should have failed!"; exit 1
+fi
+
+diff /dev/null bad1v4.html.real
+diff stderr1v4 stderr1v4.real
