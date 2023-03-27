103097b7a3037e341e8e9692760b758392f5d892
Maxime Dénès <maxime.denes@inria.fr>
2023-03-27 13:50:07 +0200

    Add overlays

---
+++ dev/ci/user-overlays/17331-separate-parsing-exec-vernac.sh
@@ -0,0 +1,11 @@
+overlay lean_importer https://github.com/maximedenes/coq-lean-import vernac-synterp 17331
+
+overlay elpi https://github.com/maximedenes/coq-elpi vernac-synterp 17331
+
+overlay serapi https://github.com/maximedenes/coq-serapi vernac-synterp 17331
+
+overlay coq_lsp https://github.com/maximedenes/coq-lsp.git vernac-synterp 17331
+
+overlay metacoq https://github.com/maximedenes/metacoq.git vernac-synterp 17331
+
+overlay tactician https://github.com/SkySkimmer/coq-tactician vernac-synterp 17331
