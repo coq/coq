4e6c163db96f89bc9cff0a9aceeb41970ce95387
Sylvain Chiron <chironsylvain@orange.fr>
2023-04-01 18:27:22 +0200

    coqide document tabs: Factorize code; make tabs reorderable

--- ide/coqide/wg_Notebook.ml
+++ ide/coqide/wg_Notebook.ml
@@ -11,56 +11,59 @@
 class ['a] typed_notebook make_page kill_page nb =
 object(self)
   inherit GPack.notebook nb as super
+
   val mutable term_list = []
 
-  method append_term (term:'a) =
-    let tab_label,menu_label,page = make_page term in
-      (* XXX - Temporary hack to compile with archaic lablgtk *)
-    ignore (super#append_page ?tab_label ?menu_label page);
+  method private get_nth_oid index =
+    (super#get_nth_page index)#misc#get_oid
+
+  method private create_term creator (term: 'a) =
+    let tab_label, menu_label, page = make_page term in
+    (* XXX - Temporary hack to compile with archaic lablgtk *)
+    let _ = creator ?tab_label ?menu_label page in
     let real_pos = super#page_num page in
-    let lower,higher = Util.List.chop real_pos term_list in
-      term_list <- lower@[term]@higher;
-      real_pos
-(* XXX - Temporary hack to compile with archaic lablgtk
-  method insert_term ?(build=default_build) ?pos (term:'a) =
-    let tab_label,menu_label,page = build term in
-    let real_pos = super#insert_page ?tab_label ?menu_label ?pos page in
-    let lower,higher = Util.List.chop real_pos term_list in
-      term_list <- lower@[term]@higher;
-      real_pos
- *)
-  method prepend_term (term:'a) =
-    let tab_label,menu_label,page = make_page term in
-      (* XXX - Temporary hack to compile with archaic lablgtk *)
-    ignore (super#prepend_page ?tab_label ?menu_label page);
-    let real_pos = super#page_num page in
-    let lower,higher = Util.List.chop real_pos term_list in
-      term_list <- lower@[term]@higher;
-      real_pos
+    term_list <- (page#misc#get_oid, term) :: term_list;
+    super#set_tab_reorderable page true;
+    real_pos
 
-  method set_term (term:'a) =
-    let tab_label,menu_label,page = make_page term in
-    let real_pos = super#current_page in
-      term_list <- Util.List.map_i (fun i x -> if i = real_pos then term else x) 0 term_list;
-      super#set_page ?tab_label ?menu_label page
+  method append_term = self#create_term super#append_page
+  method prepend_term = self#create_term super#prepend_page
+  method insert_term ?pos = self#create_term (super#insert_page ?pos)
 
-  method get_nth_term i =
-    List.nth term_list i
+  method set_term (term: 'a) =
+    let tab_label, menu_label, page = make_page term in
+    let orig_oid = self#get_nth_oid super#current_page in
+    term_list <- List.map
+        (fun (oid, sn) -> if oid = orig_oid then page#misc#get_oid, term else oid, sn) term_list;
+    super#set_page ?tab_label ?menu_label page
 
-  method term_num f p =
-    Util.List.index0 f p term_list
+  method get_nth_term index =
+    List.assoc (self#get_nth_oid index) term_list
 
-  method pages = term_list
+  method pages =
+    let terms_count = List.length term_list in
+    let oid_to_num = Hashtbl.create terms_count in
+    for index = 0 to terms_count - 1 do
+      Hashtbl.add oid_to_num (self#get_nth_oid index) index;
+    done;
+    let term_array = Array.make terms_count None in
+    List.iter (fun (oid, sn) -> term_array.(Hashtbl.find oid_to_num oid) <- Some sn) term_list;
+    Array.fold_right (fun t l -> Option.get t :: l) term_array []
 
   method! remove_page index =
-    term_list <- Util.List.filteri (fun i x -> if i = index then kill_page x; i <> index) term_list;
+    kill_page (self#get_nth_term index);
+    term_list <- List.remove_assoc (self#get_nth_oid index) term_list;
     super#remove_page index
 
   method current_term =
-    List.nth term_list super#current_page
+    if super#current_page < 0 then invalid_arg "current_term";
+    self#get_nth_term super#current_page
 
   method goto_term term =
-    List.iteri (fun i sn -> if sn == term then self#goto_page i) term_list
+    let rec oid_to_num ?(index = 0) oid =
+      if (self#get_nth_oid index) = oid then index else oid_to_num ~index:(index + 1) oid
+    in
+    List.iter (fun (oid, sn) -> if sn == term then super#goto_page (oid_to_num oid)) term_list
 
 end
 
