4151596d3a6f67c82da529a2ae47cf64e74d6357
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2023-04-05 15:56:45 +0200

    Ltac2: polymorphic tagged maps
    In the future this should let us provide a command-level mutable table
    API using this type, like
    
    ~~~coq
    Ltac2 Declare Table foo_table for constant_tag containing int.
    (* foo_table : (constant, int) table *)
    
    Ltac2 Table Bind eq_rect to 1 in foo_table.
    
    (* get_table : ('k, 'v) table -> ('k, 'v) FMap.t *)
    Ltac2 Eval let t : (constant, int) FMap.t := get_table foo_table in ...
    ~~~

--- test-suite/ltac2/map.v
+++ test-suite/ltac2/map.v
@@ -5,44 +5,43 @@ Ltac2 Type exn ::= [ Regression_Test_Failure ].
 Ltac2 ensure b := if b then () else Control.throw Regression_Test_Failure.
 
 Ltac2 sort_int_list l :=
-  Int.FSet.elements
-    (List.fold_left (fun acc x => Int.FSet.add x acc) l (Int.FSet.empty ())).
+  FSet.elements
+    (List.fold_left (fun acc x => FSet.add x acc) l (FSet.empty FSet.Tags.int_tag)).
 
 Ltac2 Eval
   ensure (List.equal Int.equal [1;2;5;8] (sort_int_list [2;5;1;8])).
 
 Ltac2 Eval
-  ensure (String.FSet.is_empty (String.FSet.empty ())).
+  ensure (FSet.is_empty (FSet.empty FSet.Tags.int_tag)).
 
 Ltac2 Eval
-  ensure (Bool.neg (String.FSet.is_empty (String.FSet.add "hello" (String.FSet.empty ())))).
+  ensure (Bool.neg (FSet.is_empty (FSet.add "hello" (FSet.empty FSet.Tags.string_tag)))).
 
 Ltac2 Eval
-  ensure (String.FSet.is_empty (String.FSet.remove "hello" (String.FSet.add "hello" (String.FSet.empty ())))).
+  ensure (FSet.is_empty (FSet.remove "hello" (FSet.add "hello" (FSet.empty FSet.Tags.string_tag)))).
 
 Ltac2 Eval
-  ensure (Bool.neg (String.FSet.is_empty (String.FSet.remove "hello" (String.FSet.add "other" (String.FSet.add "hello" (String.FSet.empty ())))))).
+  ensure (Bool.neg (FSet.is_empty (FSet.remove "hello" (FSet.add "other" (FSet.add "hello" (FSet.empty FSet.Tags.string_tag)))))).
 
 Ltac2 Eval
-  ensure (String.FSet.mem "other" (String.FSet.remove "hello" (String.FSet.add "other" (String.FSet.add "hello" (String.FSet.empty ()))))).
+  ensure (FSet.mem "other" (FSet.remove "hello" (FSet.add "other" (FSet.add "hello" (FSet.empty FSet.Tags.string_tag))))).
 
 Ltac2 Eval
-  ensure (Bool.neg (String.FSet.mem "hello" (String.FSet.remove "hello" (String.FSet.add "other" (String.FSet.add "hello" (String.FSet.empty ())))))).
+  ensure (Bool.neg (FSet.mem "hello" (FSet.remove "hello" (FSet.add "other" (FSet.add "hello" (FSet.empty FSet.Tags.string_tag)))))).
 
 Ltac2 Notation "sprintf" fmt(format) := Message.Format.kfprintf (fun m => Message.to_string m) fmt.
 
 Ltac2 Eval
-  let m := String.Map.empty () in
-  let m := String.Map.add "one" 1 m in
-  let m := String.Map.add "two" 2 m in
-  let m := String.Map.add "three" 3 m in
-  let m := String.Map.add "four" 4 m in
-  let m := String.Map.mapi (fun s i => sprintf "%s=%i" s i) m in
-  match String.Map.find_opt "two" m with
+  let m := FMap.empty FSet.Tags.string_tag in
+  let m := FMap.add "one" 1 m in
+  let m := FMap.add "two" 2 m in
+  let m := FMap.add "three" 3 m in
+  let m := FMap.add "four" 4 m in
+  let m := FMap.mapi (fun s i => sprintf "%s=%i" s i) m in
+  match FMap.find_opt "two" m with
   | None => ensure false
   | Some v => ensure (String.equal "two=2" v)
   end.
 
 (* types didn't collapse *)
-Fail Ltac2 Eval
-  ensure (Int.FSet.is_empty (String.FSet.empty ())).
+Fail Ltac2 Eval FSet.add 1 (FSet.empty FSet.Tags.string_tag).
---
+++ user-contrib/Ltac2/FSet.v
@@ -0,0 +1,50 @@
+(************************************************************************)
+(*         *   The Coq Proof Assistant / The Coq Development Team       *)
+(*  v      *         Copyright INRIA, CNRS and contributors             *)
+(* <O___,, * (see version control and CREDITS file for authors & dates) *)
+(*   \VV/  **************************************************************)
+(*    //   *    This file is distributed under the terms of the         *)
+(*         *     GNU Lesser General Public License Version 2.1          *)
+(*         *     (see LICENSE file for the text of the license)         *)
+(************************************************************************)
+
+From Ltac2 Require Import Init.
+
+Module Import Tags.
+  Ltac2 Type 'a tag.
+
+  Ltac2 @ external ident_tag : ident tag := "coq-core.plugins.ltac2" "fmap_ident_tag".
+  Ltac2 @ external int_tag : int tag := "coq-core.plugins.ltac2" "fmap_int_tag".
+  Ltac2 @ external inductive_tag : inductive tag := "coq-core.plugins.ltac2" "fmap_inductive_tag".
+  Ltac2 @ external constructor_tag : constructor tag := "coq-core.plugins.ltac2" "fmap_constructor_tag".
+  Ltac2 @ external constant_tag : constant tag := "coq-core.plugins.ltac2" "fmap_constant_tag".
+
+  (* NB: strings are copied when keys are added and read to prevent mutation *)
+  Ltac2 @ external string_tag : string tag := "coq-core.plugins.ltac2" "fmap_string_tag".
+End Tags.
+
+Ltac2 Type 'a t.
+
+Ltac2 @ external empty : 'a tag -> 'a t := "coq-core.plugins.ltac2" "fset_empty".
+
+Ltac2 @ external is_empty : 'a t -> bool := "coq-core.plugins.ltac2" "fset_is_empty".
+
+Ltac2 @ external mem : 'a -> 'a t -> bool := "coq-core.plugins.ltac2" "fset_mem".
+
+Ltac2 @ external add : 'a -> 'a t -> 'a t := "coq-core.plugins.ltac2" "fset_add".
+
+Ltac2 @ external remove : 'a -> 'a t -> 'a t := "coq-core.plugins.ltac2" "fset_remove".
+
+Ltac2 @ external union : 'a t -> 'a t -> 'a t := "coq-core.plugins.ltac2" "fset_union".
+
+Ltac2 @ external inter : 'a t -> 'a t -> 'a t := "coq-core.plugins.ltac2" "fset_inter".
+
+Ltac2 @ external diff : 'a t -> 'a t -> 'a t := "coq-core.plugins.ltac2" "fset_diff".
+
+Ltac2 @ external equal : 'a t -> 'a t -> bool := "coq-core.plugins.ltac2" "fset_equal".
+
+Ltac2 @ external subset : 'a t -> 'a t -> bool := "coq-core.plugins.ltac2" "fset_subset".
+
+Ltac2 @ external cardinal : 'a t -> int := "coq-core.plugins.ltac2" "fset_cardinal".
+
+Ltac2 @ external elements : 'a t -> 'a list := "coq-core.plugins.ltac2" "fset_elements".
