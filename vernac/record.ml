2d291437c42d497497588bd891a23c544404c328
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2023-04-06 15:32:29 +0200

    Fix record declaration with non cumulative prop

--- vernac/record.ml
+++ vernac/record.ml
@@ -220,9 +220,12 @@ let typecheck_params_and_fields def poly udecl ps (records : DataI.t list) : tc_
     let sort = EConstr.ESorts.kind sigma esort in
     let univ = ComInductive.Internal.compute_constructor_level env_ar sigma newfs in
     let univ = if Sorts.is_sprop sort then univ else if Sorts.is_sprop univ then Sorts.prop else univ in
-      if not def && is_impredicative_sort env0 sort then
-        sigma, (sort, typ)
-      else
+    if not def
+    && (is_impredicative_sort env0 sort ||
+        (Sorts.is_prop univ && not (Sorts.is_sprop sort)))
+    then
+      sigma, (sort, typ)
+    else
         let sigma = Evd.set_leq_sort env_ar sigma (EConstr.ESorts.make univ) esort in
         if Sorts.is_small univ &&
            Option.cata (Evd.is_flexible_level sigma) false (Evd.is_sort_variable sigma esort) then
