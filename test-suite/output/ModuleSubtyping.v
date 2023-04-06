d545a2d5e9588cdbeab242fd91de574a1574ad18
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2023-04-06 14:13:17 +0200

    Put current output/ModuleSubtyping test in a submodule

--- test-suite/output/ModuleSubtyping.v
+++ test-suite/output/ModuleSubtyping.v
@@ -1,27 +1,31 @@
 
-Module Type EasyT. Definition x := O. End EasyT.
-Module EasyM. Definition x := S O. End EasyM.
+Module Qualification.
+  (* test that field mismatch errors print the qualified fields *)
 
-Fail Module Easytest <: EasyT := EasyM.
+  Module Type EasyT. Definition x := O. End EasyT.
+  Module EasyM. Definition x := S O. End EasyM.
 
-Module Type A. Module B. Module C. Definition x := O. End C. End B. End A.
-Module Type A'. Module B. Module C. Definition x := S O. End C. End B. End A'.
-Module Av. Include A'. End Av.
+  Fail Module Easytest <: EasyT := EasyM.
 
-Fail Module test <: A := Av.
-(* was Error: Signature components for field C do not match: the body of definitions differs. *)
+  Module Type A. Module B. Module C. Definition x := O. End C. End B. End A.
+  Module Type A'. Module B. Module C. Definition x := S O. End C. End B. End A'.
+  Module Av. Include A'. End Av.
 
-Module Type FT (X:A). End FT.
-Module F (X:A'). End F.
+  Fail Module test <: A := Av.
+  (* was Error: Signature components for field C do not match: the body of definitions differs. *)
 
-Fail Module Ftest <: FT := F.
-
-Module Type FXT.
-  Module F (X:A). End F.
-End FXT.
-
-Module FX.
+  Module Type FT (X:A). End FT.
   Module F (X:A'). End F.
-End FX.
 
-Fail Module FXtest <: FXT := FX.
+  Fail Module Ftest <: FT := F.
+
+  Module Type FXT.
+    Module F (X:A). End F.
+  End FXT.
+
+  Module FX.
+    Module F (X:A'). End F.
+  End FX.
+
+  Fail Module FXtest <: FXT := FX.
+End Qualification.
