3317854d842d73ba9e2eaa72fc7c9f2896e78a7f
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2023-05-09 20:44:00 +0200

    Applies the "Tactic Notation" fix for numerals to "Ltac2 Notation" too.

--- test-suite/bugs/bug_17583.v
+++ test-suite/bugs/bug_17583.v
@@ -1,3 +1,7 @@
 (*"2" should not break the parsing of commands referring to "2" *)
 Tactic Notation "foo" "2" := idtac.
 Global Hint Extern 2 (True) => exact I : core.
+
+Require Import Ltac2.Ltac2.
+Ltac2 Notation "foo" "2" := intros.
+Global Hint Extern 2 (True) => exact I : core.
