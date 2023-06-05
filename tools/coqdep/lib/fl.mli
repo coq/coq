493d9947fb5e2c10a5de17c1bb8fb7448cf0fff8
Rodolphe Lepigre <rodolphe@bedrocksystems.com>
2023-06-05 11:21:36 +0200

    [coqdep] relative paths to META and .cmxs files.
    NOTE: the paths are also normalized since a [Makefile] will
    generally choke on paths containing [..] (or even [.]).

--- tools/coqdep/lib/fl.mli
+++ tools/coqdep/lib/fl.mli
@@ -8,17 +8,17 @@
 (*         *     (see LICENSE file for the text of the license)         *)
 (************************************************************************)
 
-(** [findlib_resolve meta_files file package plugin_name] tries to
-   locate a [.cmxs] for a given [package.plugin_name]
+(** [findlib_resolve ~meta_files ~file ~package ~plugin_name] tries to locate
+    a [.cmxs] for a given [package.plugin_name].
 
-    If a [META] file for [package] is found, it will try to use it to
-   resolve the path to the [.cmxs], and return both. If not, it
-   errors.
+    If a [META] file for [package] is found, it will try to use it to resolve
+    the path to the [.cmxs], and return a relative path to both. If not, it
+    errors.
 
-    The [META] file for [package] is search among the list of
-   [meta_files] first, then using [Findlib.package_meta_file]. note
-   that coqdep doesn't initialize findlib so that function performs
-   implicity initialization.  *)
+    The [META] file for [package] is first searched in the [meta_files] list,
+    and if it is not found then [Findlib.package_meta_file] is used. note that
+    coqdep does not initialize findlib, so that function performs implicity
+    initialization. *)
 val findlib_resolve
   : meta_files:string list
   -> file:string
