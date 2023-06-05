8331118188da813cd8d10db7b5054def85c35697
Maxime Dénès <mail@maximedenes.fr>
2023-06-05 14:08:21 +0200

    Update flake.nix
    Co-authored-by: Ali Caglayan <alizter@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.flake-utils.url = github:numtide/flake-utils;
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
