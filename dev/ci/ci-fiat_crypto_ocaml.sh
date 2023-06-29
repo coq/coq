607252748f96914b940668730d829d2d55be1949
Théo Zimmermann <theo.zimmermann@telecom-paris.fr>
2023-06-29 16:12:05 +0200

    Split Docker images in two: one for base and one for edge.
    The base image will use the old Ubuntu LTS (currently 20.04) and the
    edge image will use the most recent Ubuntu version (currently 23.04).
    
    We temporarily switch the dev job to use the base image because the
    documentation jobs depend on it, but should be tested with our minimal
    dependency version bounds.

--- dev/ci/ci-fiat_crypto_ocaml.sh
+++ dev/ci/ci-fiat_crypto_ocaml.sh
@@ -8,6 +8,10 @@ if [ "$DOWNLOAD_ONLY" ]; then exit 0; fi
 ci_dir="$(dirname "$0")"
 . "${ci_dir}/ci-common.sh"
 
+# We set the stack size to 128MiB to be able to build with flambda
+# See https://github.com/ocaml/ocaml/issues/7842
+ulimit -s 131072
+
 # we explicitly pass OCAMLFIND so that we pick up the opam
 # (non-flambda one) rather than the one used to build coq
 make_args=(EXTERNAL_REWRITER=1 EXTERNAL_COQPRIME=1 OCAMLFIND=ocamlfind)
