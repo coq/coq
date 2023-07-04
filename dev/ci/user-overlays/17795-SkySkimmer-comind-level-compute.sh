2154901589381a3ff6eb32466bf93131e5815531
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2023-07-04 15:57:36 +0200

    overlays for inductive_levels refactor

---
+++ dev/ci/user-overlays/17795-SkySkimmer-comind-level-compute.sh
@@ -0,0 +1,7 @@
+overlay elpi https://github.com/SkySkimmer/coq-elpi comind-level-compute 17795
+
+overlay category_theory https://github.com/SkySkimmer/category-theory type0 17795 comind-level-compute
+
+overlay serapi https://github.com/SkySkimmer/coq-serapi comind-level-compute 17795
+
+overlay quickchick https://github.com/SkySkimmer/QuickChick comind-level-compute 17795
