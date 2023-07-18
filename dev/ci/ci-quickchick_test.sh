b78c8dc89d5b92a987cad0eb0a79f9ff7b2642c2
Yishuai Li <liyishuai.lys@alibaba-inc.com>
2023-07-18 17:07:20 +0800

    [ci] Add coq-http

--- dev/ci/ci-quickchick_test.sh
+++ dev/ci/ci-quickchick_test.sh
@@ -5,11 +5,6 @@ set -e
 ci_dir="$(dirname "$0")"
 . "${ci_dir}/ci-common.sh"
 
-git_download quickchick
-
-if [ "$DOWNLOAD_ONLY" ]; then exit 0; fi
-
 ( cd "${CI_BUILD_DIR}/quickchick"
-  make
-  make install
+  make tests
 )
