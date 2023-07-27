219bc2fb3f959eb24d7c47be763e008eb56d6ad9
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2023-07-27 09:04:55 +0200

    Overlays for Equations, HoTT and metacoq

---
+++ dev/ci/user-overlays/17888-herbelin-master+discharge-on-the-fly.sh
@@ -0,0 +1,3 @@
+overlay equations https://github.com/herbelin/Coq-Equations main+discharge+plug-set_strategy-on-section-mechanism 17888 master+discharge-on-the-fly
+overlay hott https://github.com/herbelin/HoTT master+qualifying-idtoiso 17888 master+discharge-on-the-fly
+overlay metacoq https://github.com/herbelin/template-coq main+compatibility-reduce_stack_full_unfold-transparent 17888 master+discharge-on-the-fly
