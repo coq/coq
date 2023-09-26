f9bc0eb5297687250de912e48806c0cbda5eeff0
Pierre Roux <pierre.roux@onera.fr>
2023-09-26 15:07:39 +0200

    [CI] Make VST use CompCert from the CI
    instead of recompiling the bundled one.

--- dev/ci/ci-compcert.sh
+++ dev/ci/ci-compcert.sh
@@ -9,9 +9,12 @@ git_download compcert
 
 if [ "$DOWNLOAD_ONLY" ]; then exit 0; fi
 
+# CompCert does compile with -native-compiler yes
+# but with excessive memory requirements
 export COQCOPTS='-native-compiler no -w -undeclared-scope -w -omega-is-deprecated'
 ( cd "${CI_BUILD_DIR}/compcert"
-  [ -e Makefile.config ] || ./configure -ignore-coq-version x86_32-linux -use-external-MenhirLib -use-external-Flocq
+  [ -e Makefile.config ] || ./configure -ignore-coq-version x86_32-linux -install-coqdev -clightgen -use-external-MenhirLib -use-external-Flocq -prefix ${CI_INSTALL_DIR} -coqdevdir ${CI_INSTALL_DIR}/lib/coq/user-contrib/compcert
   make
   make check-proof COQCHK='"$(COQBIN)coqchk" -silent -o $(COQINCLUDES)'
+  make install
 )
