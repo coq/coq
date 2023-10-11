1f39f2185041b18fcb38e94a8bc93c91a45496e1
Pierre Roux <pierre.roux@onera.fr>
2023-10-11 10:06:56 +0200

    Add overlays

--- theories/Unicode/Utf8_core.v
+++ theories/Unicode/Utf8_core.v
@@ -13,10 +13,10 @@
 
 (* Logic *)
 Notation "∀ x .. y , P" := (forall x, .. (forall y, P) ..)
-  (at level 200, x binder, y binder, right associativity,
+  (at level 10, x binder, y binder, P at level 200,
   format "'[  ' '[  ' ∀  x  ..  y ']' ,  '/' P ']'") : type_scope.
 Notation "∃ x .. y , P" := (exists x, .. (exists y, P) ..)
-  (at level 200, x binder, y binder, right associativity,
+  (at level 10, x binder, y binder, P at level 200,
   format "'[  ' '[  ' ∃  x  ..  y ']' ,  '/' P ']'") : type_scope.
 
 Notation "x ∨ y" := (x \/ y) (at level 85, right associativity) : type_scope.
@@ -30,5 +30,5 @@ Notation "x ≠ y" := (x <> y) (at level 70) : type_scope.
 
 (* Abstraction *)
 Notation "'λ' x .. y , t" := (fun x => .. (fun y => t) ..)
-  (at level 200, x binder, y binder, right associativity,
+  (at level 10, x binder, y binder, t at level 200,
   format "'[  ' '[  ' 'λ'  x  ..  y ']' ,  '/' t ']'").
