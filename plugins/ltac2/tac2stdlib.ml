47fc98587906feed6ab9ca1624472f57968d0862
quarkcool <quarkcool@yahoo.fr>
2023-10-20 19:19:16 +0200

    Remove useless helper function

--- plugins/ltac2/tac2stdlib.ml
+++ plugins/ltac2/tac2stdlib.ml
@@ -397,12 +397,9 @@ let () =
     (bool @-> list rewriting @-> clause @-> option (thunk unit) @-> tac unit)
     Tac2tactics.rewrite
 
-let to_helper v = uthaw constr_with_bindings v
-let helper = make_to_repr to_helper
-
 let () =
   define "tac_setoid_rewrite"
-    (bool @-> helper @-> occurrences @-> option ident @-> tac unit)
+    (bool @-> uthaw constr_with_bindings @--> occurrences @-> option ident @-> tac unit)
     Tac2tactics.setoid_rewrite
 
 let () =
