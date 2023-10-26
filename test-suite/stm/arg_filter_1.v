db8576b55714c420a5a46f2f7986c9a4daa15547
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2023-10-26 15:10:52 +0200

    "failure" "stm" and "coqchk" tests are not special
    "failure" has used Fail instead of expecting a non-zero exit code
    since 56b3ad583852438a1378280e9b269f8020a9524c
    
    "stm" implied async proofs on but we make it explicit instead.
    
    "coqchk" used `-Q coqchk coqchk` instead of `-Q coqchk ""` but this
    doesn't matter.

--- test-suite/stm/arg_filter_1.v
+++ test-suite/stm/arg_filter_1.v
@@ -1,4 +1,4 @@
-(* coq-prog-args: ("-async-proofs-tac-j" "1") *)
+(* -*- coq-prog-args: ("-async-proofs" "on" "-async-proofs-tac-j" "1"); -*- *)
 
 Lemma foo (A B : Prop) n : n + 0 = n /\ (A -> B -> A).
 Proof. split. par: now auto. Qed.
