950763127eb2f5aea660173906a0448af052cdbf
Andres Erbsen <andres-github@andres.systems>
2023-10-31 15:40:03 +0000

    ci: build and reuse bedrock2 in fiat-crypto

--- dev/ci/ci-bedrock2.sh
+++ dev/ci/ci-bedrock2.sh
@@ -6,12 +6,12 @@ ci_dir="$(dirname "$0")"
 . "${ci_dir}/ci-common.sh"
 
 WITH_SUBMODULES=1
-git_download bedrock2
+git_download fiat_crypto
 
 if [ "$DOWNLOAD_ONLY" ]; then exit 0; fi
 
 export COQEXTRAFLAGS='-native-compiler no'
-( cd "${CI_BUILD_DIR}/bedrock2"
+( cd "${CI_BUILD_DIR}/fiat_crypto/rupicola/bedrock2"
   COQMF_ARGS='-arg "-async-proofs-tac-j 1"' make
   make install
 )
--- dev/ci/ci-fiat_crypto.sh
+++ dev/ci/ci-fiat_crypto.sh
@@ -5,9 +5,6 @@ set -e
 ci_dir="$(dirname "$0")"
 . "${ci_dir}/ci-common.sh"
 
-WITH_SUBMODULES=1
-git_download fiat_crypto
-
 if [ "$DOWNLOAD_ONLY" ]; then exit 0; fi
 
 # We need a larger stack size to not overflow ocamlopt+flambda when
