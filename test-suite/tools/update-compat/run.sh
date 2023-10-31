7fad8e2358760ca61953e7322d4dc6c140a6236d
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2023-10-31 15:28:11 +0100

    update-compat --release for 8.19

--- test-suite/tools/update-compat/run.sh
+++ test-suite/tools/update-compat/run.sh
@@ -6,4 +6,4 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 # we assume that the script lives in test-suite/tools/update-compat/,
 # and that update-compat.py lives in dev/tools/
 cd "${SCRIPT_DIR}/../../.."
-dev/tools/update-compat.py --assert-unchanged --master || exit $?
+dev/tools/update-compat.py --assert-unchanged --release || exit $?
