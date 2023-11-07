357011fd44555abe1766af59401da524f92db960
Jason Gross <jgross@mit.edu>
2023-11-07 06:48:31 -0800

    [ci-fiat_crypto_ocaml] use lite-c-files
    It should be much faster than lite-generated-files and we don't lose any significant code coverage.  Fiat Crypto could provide an even more trimmed down target if this one is still too slow.

--- dev/ci/ci-fiat_crypto_ocaml.sh
+++ dev/ci/ci-fiat_crypto_ocaml.sh
@@ -17,5 +17,5 @@ ulimit -s 131072
 make_args=(EXTERNAL_REWRITER=1 EXTERNAL_COQPRIME=1 OCAMLFIND=ocamlfind)
 
 ( cd "${CI_BUILD_DIR}/fiat_crypto"
-  make "${make_args[@]}" -j 2 standalone-ocaml lite-generated-files
+  make "${make_args[@]}" -j 2 standalone-ocaml lite-c-files
 )
