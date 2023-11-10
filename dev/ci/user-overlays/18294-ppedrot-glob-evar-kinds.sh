44543951d9c39cc5e505159d64cd25114734a46c
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2023-11-10 16:36:17 +0100

    Adding overlays.

---
+++ dev/ci/user-overlays/18294-ppedrot-glob-evar-kinds.sh
@@ -0,0 +1,13 @@
+overlay bignums https://github.com/ppedrot/bignums glob-evar-kinds 18294
+
+overlay elpi https://github.com/ppedrot/coq-elpi glob-evar-kinds 18294
+
+overlay equations https://github.com/ppedrot/Coq-Equations glob-evar-kinds 18294
+
+overlay metacoq https://github.com/ppedrot/metacoq glob-evar-kinds 18294
+
+overlay quickchick https://github.com/ppedrot/QuickChick glob-evar-kinds 18294
+
+overlay serapi https://github.com/ppedrot/coq-serapi glob-evar-kinds 18294
+
+overlay tactician https://github.com/ppedrot/coq-tactician glob-evar-kinds 18294
