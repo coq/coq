7e33565ddeb276bb08d66fbca366c8cd5638aa60
Pierre Roux <pierre@roux01.fr>
2023-11-19 11:17:03 +0100

    [CI] Make `make ci-compcert` work locally
    Following https://coq.zulipchat.com/#narrow/stream/237656-Coq-devs-.26-plugin-devs/topic/make.20ci-vst

--- dev/ci/ci-compcert.sh
+++ dev/ci/ci-compcert.sh
@@ -12,8 +12,9 @@ if [ "$DOWNLOAD_ONLY" ]; then exit 0; fi
 # CompCert does compile with -native-compiler yes
 # but with excessive memory requirements
 export COQCOPTS='-native-compiler no -w -undeclared-scope -w -omega-is-deprecated'
+if [ -z "${COQLIB}" ]; then COQLIB="${CI_INSTALL_DIR}/lib/coq"; fi
 ( cd "${CI_BUILD_DIR}/compcert"
-  [ -e Makefile.config ] || ./configure -ignore-coq-version x86_32-linux -install-coqdev -clightgen -use-external-MenhirLib -use-external-Flocq -prefix ${CI_INSTALL_DIR} -coqdevdir ${CI_INSTALL_DIR}/lib/coq/user-contrib/compcert
+  [ -e Makefile.config ] || ./configure -ignore-coq-version x86_32-linux -install-coqdev -clightgen -use-external-MenhirLib -use-external-Flocq -prefix ${CI_INSTALL_DIR} -coqdevdir ${COQLIB}/user-contrib/compcert
   make
   make check-proof COQCHK='"$(COQBIN)coqchk" -silent -o $(COQINCLUDES)'
   make install
