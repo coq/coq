herbelin/github-coq

Experimenting removing the special case of Context outside sections for Module Type.
This means:
- Context outside section does not declare defined _instances_ anymore
  in Module Type.
- Context outside section does not declare axiomatic _instances_ anymore
  with a local flag in Module Type.

Note: Declarations themselves made by Context outside sections in
Module Type remain exceptionally "Global" while they are "Local"
otherwise (not visible w/o qualifications). See #10888. It seems
difficult to change this behavior in one way or the other.

vernac/comAssumption.ml
@@ -244,9 +244,8 @@ let context_nosection sigma ~poly ctx =
     let () = Declare.assumption_message name in
     let env = Global.env () in
     (* why local when is_modtype? *)
-    let locality = if Lib.is_modtype () then Goptions.OptLocal else Goptions.OptGlobal in
-    let () = if Lib.is_modtype() || Option.is_empty b then
-        Classes.declare_instance env sigma None locality (GlobRef.ConstRef cst)
+    let () = if Option.is_empty b then
+        Classes.declare_instance env sigma None Goptions.OptGlobal (GlobRef.ConstRef cst)
     in
     Constr.mkConstU (cst,instance_of_univ_entry univs) :: subst
   in

coq/coq

vernac/comAssumption.ml
@@ -225,10 +225,8 @@ let context_nosection sigma ~poly ctx =
     let cst = Declare.declare_constant ~name ~kind ~local decl in
     let () = Declare.assumption_message name in
     let env = Global.env () in
-    (* why local when is_modtype? *)
-    let locality = if Lib.is_modtype () then Hints.Local else Hints.SuperGlobal in
-    let () = if Lib.is_modtype() || Option.is_empty b then
-        Classes.declare_instance env sigma None locality (GlobRef.ConstRef cst)
+    let () = if Option.is_empty b then
+        Classes.declare_instance env sigma None Hints.SuperGlobal (GlobRef.ConstRef cst)
     in
     Constr.mkConstU (cst,instance_of_univ_entry univ_entry) :: subst
   in