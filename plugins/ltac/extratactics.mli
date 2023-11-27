78d2c7d6254d0109ec64e498f931995b00ba0d3b
Emilio Jesus Gallego Arias <e+git@x80.org>
2023-11-27 19:03:04 +0100

    [ltac] (re)-export AST witnesses.
    Fix downfall of #17293

--- plugins/ltac/extratactics.mli
+++ plugins/ltac/extratactics.mli
@@ -7,3 +7,15 @@
 (*         *     GNU Lesser General Public License Version 2.1          *)
 (*         *     (see LICENSE file for the text of the license)         *)
 (************************************************************************)
+
+type cmp =
+  | Eq
+  | Lt | Le
+  | Gt | Ge
+
+val wit_comparison : (cmp, cmp, cmp) Genarg.genarg_type
+
+type 'i test =
+  | Test of cmp * 'i * 'i
+
+val wit_test : (int Locus.or_var test, int Locus.or_var test, int test) Genarg.genarg_type
