cf7a7f966665a0e60eda0568305ae4b4ae2a33ed
Pierre Roux <pierre.roux@onera.fr>
2023-12-05 12:44:32 +0100

    Readd a deprecation warning for transitive require
    This one is silenced by default.

--- lib/deprecation.mli
+++ lib/deprecation.mli
@@ -12,7 +12,7 @@ type t = { since : string option ; note : string option }
 
 val make : ?since:string -> ?note:string -> unit -> t
 
-val create_warning : object_name:string -> warning_name_if_no_since:string ->
+val create_warning : ?default:CWarnings.status -> object_name:string -> warning_name_if_no_since:string ->
   ('b -> Pp.t) -> ?loc:Loc.t -> 'b * t -> unit
 
 module Version : sig
