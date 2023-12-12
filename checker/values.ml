1090276340b05e038881b3595dbcbc3bf3d4fe34
Emilio Jesus Gallego Arias <e+git@x80.org>
2023-12-12 18:00:35 +0100

    [checker] Fix discrepancy in raw_level definition.
    Thanks to Mario Carneiro for pointing this out.
    
    (cherry picked from commit e3726838aff6a87c6fe114e1c8304ba92432cf2a)

--- checker/values.ml
+++ checker/values.ml
@@ -88,7 +88,7 @@ let v_cons = v_tuple "constructor" [|v_ind;Int|]
 
 (** kernel/univ *)
 let v_level_global = v_tuple "Level.Global.t" [|v_dp;String;Int|]
-let v_raw_level = v_sum "raw_level" 3 (* SProp, Prop, Set *)
+let v_raw_level = v_sum "raw_level" 1 (* Set *)
   [|(*Level*)[|v_level_global|]; (*Var*)[|Int|]|]
 let v_level = v_tuple "level" [|Int;v_raw_level|]
 let v_expr = v_tuple "levelexpr" [|v_level;Int|]
