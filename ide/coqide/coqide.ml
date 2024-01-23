e42451d1265ff26ff928e6052ba49617e22f8052
Sylvain Chiron <chironsylvain@orange.fr>
2024-01-23 12:39:52 +0100

    More explicit message when trying in vain to open a file without extension

--- ide/coqide/coqide.ml
+++ ide/coqide/coqide.ml
@@ -117,9 +117,12 @@ let make_coqtop_args fname =
         (* at initialization of coqtop (see #10286) *)
         (* If the file name is a valid identifier, use it as toplevel name; *)
         (* otherwise the default “Top” will be used. *)
-        match Unicode.ident_refutation (Filename.chop_extension (Filename.basename fname)) with
-        | Some _ -> args
-        | None -> "-topfile"::fname::args
+        try
+          match Unicode.ident_refutation (Filename.chop_extension (Filename.basename fname)) with
+          | Some _ -> args
+          | None -> "-topfile"::fname::args
+        with Invalid_argument _ ->
+          failwith "CoqIDE cannot open files which do not have an extension in their filename."
   in
   proj, args
 
