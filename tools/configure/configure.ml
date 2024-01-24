a58838d5e34b3e01232886d18bc95aa93f05e378
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2024-01-24 13:46:38 +0100

    configure is_a_released_version = false after 8.19.0

--- tools/configure/configure.ml
+++ tools/configure/configure.ml
@@ -24,7 +24,7 @@ let (/) = Filename.concat
 
 let coq_version = "8.19.0"
 let vo_magic = 81999
-let is_a_released_version = true
+let is_a_released_version = false
 
 (** Default OCaml binaries *)
 
