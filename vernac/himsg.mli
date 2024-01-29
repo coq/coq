18278cbcc88922ad649ca61c33dcce63d8cf2941
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2024-01-29 15:58:54 +0100

    Nicer elimination error messages for sort poly inductives
    We stop going through sort families as InQSort is not useful enough
    for our purposes.
    
    Close #18515
    
    (cherry picked from commit 77a0aafc6240d75d3186c8bbaa368e2c8bb2a504)

--- vernac/himsg.mli
+++ vernac/himsg.mli
@@ -16,12 +16,3 @@ val explain_type_error : Environ.env -> Evd.evar_map -> Pretype_errors.type_erro
 val explain_pretype_error : Environ.env -> Evd.evar_map -> Pretype_errors.pretype_error -> Pp.t
 
 val explain_refiner_error : Environ.env -> Evd.evar_map -> Logic.refiner_error -> Pp.t
-
-(* XXX add cases for SProp (and sort poly?) *)
-type arity_error =
-  | NonInformativeToInformative
-  | StrongEliminationOnNonSmallType
-  | WrongArity
-
-val error_elim_explain : Sorts.family -> Sorts.family -> arity_error
-(** Second argument is the familty of the inductive. *)
