herbelin/github-coq

ComAssumption: Factorize code between context_nosection and declare_axiom.
One little change: changing again kind of declarations made by Context
(see 2cdccb3f050).

vernac/comAssumption.mli
@@ -53,6 +53,19 @@ val declare_axiom
   -> variable
   -> GlobRef.t * Univ.Instance.t
 
+val declare_global
+  : coercion_flag
+  -> poly:bool
+  -> local:Locality.import_status
+  -> kind:Decls.logical_kind
+  -> Constr.constr option
+  -> Constr.types
+  -> Entries.universes_entry * UnivNames.universe_binders
+  -> Impargs.manual_implicits
+  -> Declaremods.inline
+  -> variable
+  -> GlobRef.t * Univ.Instance.t
+
 (** Context command *)
 
 val context

coq/coq

vernac/comAssumption.mli
@@ -55,6 +55,20 @@ val declare_axiom
   -> variable
   -> GlobRef.t * UVars.Instance.t
 
+val declare_global
+  : coercion_flag
+  -> try_assum_as_instance:bool
+  -> local:Locality.import_status
+  -> kind:Decls.logical_kind
+  -> ?user_warns:UserWarn.t
+  -> Constr.constr option
+  -> Constr.types
+  -> UState.universes_entry * UnivNames.universe_binders
+  -> Impargs.manual_implicits
+  -> Declaremods.inline
+  -> variable
+  -> GlobRef.t * UVars.Instance.t
+
 (** Context command *)
 
 val do_context