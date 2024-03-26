ea6148b3ec7eb92fa1d8011f24d16e2a1f928402
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2024-03-26 22:20:49 +0100

    Move homogeneous relation test to hipattern.ml for reusability.

--- vernac/comProgramFixpoint.ml
+++ vernac/comProgramFixpoint.ml
@@ -11,7 +11,6 @@
 open Pp
 open CErrors
 open Util
-open Constr
 open Context
 open Declare
 open Names
@@ -76,20 +75,7 @@ let build_wellfounded pm (recname,pl,bl,arityc,body) poly ?typing_flags ?user_wa
   let binders = letbinders @ [arg] in
   let binders_env = push_rel_context binders_rel env in
   let sigma, (rel, _) = interp_constr_evars_impls ~program_mode:true env sigma r in
-  let relty = Retyping.get_type_of env sigma rel in
-  let relargty =
-    let error () =
-      user_err ?loc:(constr_loc r)
-        (Printer.pr_econstr_env env sigma rel ++ str " is not an homogeneous binary relation.")
-    in
-    let ctx, ar =
-      try Reductionops.whd_decompose_prod_n env sigma 2 relty
-      with Invalid_argument _ -> error () in
-    match ctx, EConstr.kind sigma ar with
-      | [(_,t); (_,u)], Sort s
-        when Sorts.is_prop (ESorts.kind sigma s) && Reductionops.is_conv env sigma t u -> t
-      | _, _ -> error ()
-  in
+  let relargty = Hipattern.is_homogeneous_relation ?loc:(constr_loc r) env sigma rel in
   let sigma, measure = interp_casted_constr_evars ~program_mode:true binders_env sigma measure relargty in
   let sigma, wf_rel, wf_rel_fun, measure_fn =
     let measure_body, measure =
