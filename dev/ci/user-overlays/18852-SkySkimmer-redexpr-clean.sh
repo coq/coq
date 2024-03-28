e2ba9083acc348c49165384f22eab6f07a378ea1
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2024-03-28 17:14:14 +0100

    Overlays for redexpr changes

---
+++ dev/ci/user-overlays/18852-SkySkimmer-redexpr-clean.sh
@@ -0,0 +1,5 @@
+overlay metacoq https://github.com/SkySkimmer/metacoq redexpr-clean 18852
+
+overlay elpi https://github.com/SkySkimmer/coq-elpi redexpr-clean 18852
+
+overlay serapi https://github.com/SkySkimmer/coq-serapi redexpr-clean 18852
