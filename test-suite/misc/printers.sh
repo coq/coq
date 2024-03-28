89049284d80e0a7f5fde7df1c4bb6982be63ddb0
Anton Danilkin <afdw@yandex.ru>
2024-03-28 11:52:12 +0100

    Clean up the include files, move them to a separate directory `dev/ml_toplevel`

--- test-suite/misc/printers.sh
+++ test-suite/misc/printers.sh
@@ -4,11 +4,11 @@ command -v "${BIN}coqtop.byte" || { echo "Missing coqtop.byte"; exit 1; }
 
 f=$(mktemp)
 {
-    printf 'Drop.\n#directory "../dev";;\n#use "include";;\n#quit;;\n' | "${BIN}coqtop.byte" -q
+    printf 'Drop.\n#directory "../dev";;\n#use "ml_toplevel/include";;\n#quit;;\n' | "${BIN}coqtop.byte" -q
 } 2>&1 | tee "$f"
 
-# if there's an issue in base_include 'go' won't be defined
-# if there's an issue in include_printers it will be an undefined printer
+# if there's an issue in `include_utilities`, 'go' won't be defined
+# if there's an issue in `include_printers`, it will be an undefined printer
 if ! grep -q -F 'val go : unit -> unit = <fun>' "$f" ||
-        grep -q -E "Error|Unbound" "$f";
+        grep -q -E 'Error|Unbound' "$f";
 then exit 1; fi
