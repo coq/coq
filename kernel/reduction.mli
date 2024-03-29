f5f78d910c1a61195fe70127483f01ea93098ba7
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2024-03-29 14:35:36 +0100

    Add optional ?evars argument to functions in reduction.ml

--- kernel/reduction.mli
+++ kernel/reduction.mli
@@ -16,7 +16,7 @@ open Environ
 
 (** None of these functions do eta reduction *)
 
-val whd_all                 : env -> constr -> constr
+val whd_all : ?evars:constr CClosure.evar_handler -> env -> constr -> constr
 
 (************************************************************************)
 
@@ -30,13 +30,13 @@ val beta_appvect : constr -> constr array -> constr
 val beta_app : constr -> constr -> constr
 
 (** Pseudo-reduction rule  Prod(x,A,B) a --> B[x\a] *)
-val hnf_prod_applist : env -> types -> constr list -> types
+val hnf_prod_applist : ?evars:constr CClosure.evar_handler -> env -> types -> constr list -> types
 
 (** In [hnf_prod_applist_decls n c args], [c] is supposed to (whd-)reduce to
     the form [∀Γ.t] with [Γ] of length [n] and possibly with let-ins; it
     returns [t] with the assumptions of [Γ] instantiated by [args] and
     the local definitions of [Γ] expanded. *)
-val hnf_prod_applist_decls : env -> int -> types -> constr list -> types
+val hnf_prod_applist_decls : ?evars:constr CClosure.evar_handler -> env -> int -> types -> constr list -> types
 
 (** Compatibility alias for Term.lambda_appvect_decls *)
 val betazeta_appvect : int -> constr -> constr array -> constr
@@ -44,20 +44,20 @@ val betazeta_appvect : int -> constr -> constr array -> constr
 (***********************************************************************
   s Recognizing products and arities modulo reduction *)
 
-val whd_decompose_prod         : env -> types -> Constr.rel_context * types
-val whd_decompose_prod_decls   : env -> types -> Constr.rel_context * types
-val whd_decompose_lambda       : env -> constr -> Constr.rel_context * constr
-val whd_decompose_lambda_decls : env -> constr -> Constr.rel_context * constr
+val whd_decompose_prod : ?evars:constr CClosure.evar_handler -> env -> types -> Constr.rel_context * types
+val whd_decompose_prod_decls : ?evars:constr CClosure.evar_handler -> env -> types -> Constr.rel_context * types
+val whd_decompose_lambda : ?evars:constr CClosure.evar_handler -> env -> constr -> Constr.rel_context * constr
+val whd_decompose_lambda_decls : ?evars:constr CClosure.evar_handler -> env -> constr -> Constr.rel_context * constr
 
 (** This is typically the function to use to extract the context of a
     Fix not already in normal form up to and including the decreasing
     argument, counting as many lambda's as given by the decreasing
     index + 1 *)
-val whd_decompose_lambda_n_assum : env -> int -> constr -> Constr.rel_context * constr
+val whd_decompose_lambda_n_assum : ?evars:constr CClosure.evar_handler -> env -> int -> constr -> Constr.rel_context * constr
 
 exception NotArity
 
-val dest_arity : env -> types -> Term.arity (* raises NotArity if not an arity *)
-val is_arity   : env -> types -> bool
+val dest_arity : ?evars:constr CClosure.evar_handler -> env -> types -> Term.arity (* raises NotArity if not an arity *)
+val is_arity : ?evars:constr CClosure.evar_handler -> env -> types -> bool
 
-val eta_expand : env -> constr -> types -> constr
+val eta_expand : ?evars:constr CClosure.evar_handler -> env -> constr -> types -> constr
