af945144d00ddd87bc213c0dcc2aee2aed1f7945
Pierre Roux <pierre.roux@onera.fr>
2024-05-15 19:20:38 +0200

    Update compat infrastructure prior to branching

--- test-suite/success/CompatOldOldFlag.v
+++
@@ -1,6 +0,0 @@
-(* -*- coq-prog-args: ("-compat" "8.17") -*- *)
-(** Check that the current-minus-three compatibility flag actually requires the relevant modules. *)
-Import Coq.Compat.Coq820.
-Import Coq.Compat.Coq819.
-Import Coq.Compat.Coq818.
-Import Coq.Compat.Coq817.
