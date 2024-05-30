3218c656ceb0528fd80979221e5bb5f931e4a15e
Théo Zimmermann <theo.zimmermann@telecom-paris.fr>
2024-05-30 15:48:24 +0200

    Start a GoodDefaults file collecting recommended option settings.
    Follow-up of Zulip discussion at: https://coq.zulipchat.com/#narrow/stream/237977-Coq-users/topic/Better.20default.20options

---
+++ theories/Compat/GoodDefaults_8_20.v
@@ -0,0 +1,14 @@
+(************************************************************************)
+(*         *   The Coq Proof Assistant / The Coq Development Team       *)
+(*  v      *         Copyright INRIA, CNRS and contributors             *)
+(* <O___,, * (see version control and CREDITS file for authors & dates) *)
+(*   \VV/  **************************************************************)
+(*    //   *    This file is distributed under the terms of the         *)
+(*         *     GNU Lesser General Public License Version 2.1          *)
+(*         *     (see LICENSE file for the text of the license)         *)
+(************************************************************************)
+
+(** File exporting recommended option settings at the time of releasing Coq v8.20 *)
+
+
+#[ export ] Set Default Goal Selector "!".
