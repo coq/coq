b36b42120addb1f20273229f9d04e8ae371786c3
Enrico Tassi <Enrico.Tassi@Inria.fr>
2024-06-24 14:56:55 +0200

    ci: update elpi branch

--- dev/ci/ci-elpi.sh
+++ dev/ci/ci-elpi.sh
@@ -12,5 +12,5 @@ if [ "$DOWNLOAD_ONLY" ]; then exit 0; fi
 ( cd "${CI_BUILD_DIR}/elpi"
   make build-core
   make build-apps
-  make install
+  make install DUNE_install_FLAGS=--prefix="$CI_INSTALL_DIR"
 )
