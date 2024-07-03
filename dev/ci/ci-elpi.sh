1a3a3d422dd9c7a91a1993e8d22ccfa95e9c6945
Enrico Tassi <Enrico.Tassi@Inria.fr>
2024-07-03 11:37:35 +0200

    ci: update elpi branch
    Co-authored-by: Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>

--- dev/ci/ci-elpi.sh
+++ dev/ci/ci-elpi.sh
@@ -10,7 +10,6 @@ git_download elpi
 if [ "$DOWNLOAD_ONLY" ]; then exit 0; fi
 
 ( cd "${CI_BUILD_DIR}/elpi"
-  make build-core
-  make build-apps
-  make install
+  make build DUNE_build_FLAGS="--root ."
+  make install DUNE_install_FLAGS=--prefix="$CI_INSTALL_DIR"
 )
