013f89adbe945c57fef61057bdca5addedaf75dc
Pierre Roux <pierre.roux@onera.fr>
2024-07-04 10:07:01 +0200

    [test-suite] Remove tests requiring Lia

--- test-suite/success/search_lia.v
+++
@@ -1,7 +0,0 @@
-(** The example in the Reference Manual *)
-
-Require Import ZArith.
-
-Search Z.mul Z.add "distr".
-Search "+"%Z "*"%Z "distr" -positive -Prop.
-Search (?x * _ + ?x * _)%Z outside Lia.
