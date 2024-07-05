1609266056e9632c9d412cf2b37592fda0176ad6
Pierre Roux <pierre.roux@onera.fr>
2024-07-05 19:44:37 +0200

    [test-suite] Cleanup dependencies on Uint63

--- test-suite/bugs/bug_12770.v
+++ test-suite/bugs/bug_12770.v
@@ -1,11 +1,11 @@
-Require Import Uint63 PrimFloat PArray ssreflect.
+Require Import PrimInt63 PrimFloat PrimArray ssreflect.
 
 Open Scope uint63_scope.
 
-Lemma test : 2 = 1 + 1.
+Lemma test : 2 = PrimInt63.add 1 1.
 Proof.
   set X := 2.
-  rewrite -[X]/(1 + 1).
+  rewrite -[X]/(PrimInt63.add 1 1).
   by [].
 Qed.
 
