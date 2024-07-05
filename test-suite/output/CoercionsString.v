7bfd3b8cfe89abc2897c951d3473d3251429a89a
Pierre Roux <pierre.roux@onera.fr>
2024-07-05 19:24:12 +0200

    [test-suite] Remove tests requiring String

--- test-suite/output/CoercionsString.v
+++
@@ -1,8 +0,0 @@
-(* Check both removal of coercions with target Funclass and mixing
-   string and numeral scopes *)
-
-Require Import String.
-Open Scope string_scope.
-Inductive PAIR := P (s:string) (n:nat).
-Coercion P : string >-> Funclass.
-Check ("1" 0).
