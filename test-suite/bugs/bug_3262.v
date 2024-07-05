32262a9ad5a9ad2b5f8adefa53a3e2e0ab5f28e9
Pierre Roux <pierre.roux@onera.fr>
2024-07-05 22:23:18 +0200

    [test-suite] Cleanup dependency on Relations

--- test-suite/bugs/bug_3262.v
+++ test-suite/bugs/bug_3262.v
@@ -1,7 +1,7 @@
 (* Not having a [return] clause causes the [refine] at the bottom to stack overflow before f65fa9de8a4c9c12d933188a755b51508bd51921 *)
 
-Require Import Stdlib.Lists.List.
-Require Import Relations RelationClasses.
+Require Import Stdlib.Lists.ListDef.
+Require Import Relation_Definitions RelationClasses.
 
 Set Implicit Arguments.
 Set Strict Implicit.
