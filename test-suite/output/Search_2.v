f04a1ee7422a53f0c513abb604954fc7787be332
Pierre Roux <pierre.roux@onera.fr>
2024-07-06 15:43:15 +0200

    [test-suite] Split tests requiring Arith

--- test-suite/output/Search_2.v
+++ test-suite/output/Search_2.v
@@ -1,12 +1,3 @@
-
-(* Reproduce the example of the doc *)
-
-Search "_assoc".
-Search "+".
-Search hyp:bool -headhyp:bool.
-Search concl:bool -headconcl:bool.
-Search [ is:Definition headconcl:nat | is:Lemma (_ + _) ].
-
 Require Import PeanoNat.
 
 Search (_ ?n ?m = _ ?m ?n).
