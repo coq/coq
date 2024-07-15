41cb5944091b3176b3344f4108b6a8c8e31549b1
Pierre Roux <pierre.roux@onera.fr>
2024-07-15 13:38:31 +0200

    [test-suite] Cleanup dependencies on ZArith

--- test-suite/coq-makefile/timing-aggregate/Slow.v
+++ test-suite/coq-makefile/timing-aggregate/Slow.v
@@ -1,4 +1,4 @@
-Require Stdlib.ZArith.BinInt.
-Definition foo0 := Eval vm_compute in Stdlib.ZArith.BinInt.Z.div_eucl.
-Definition foo1 := Eval vm_compute in Stdlib.ZArith.BinInt.Z.div_eucl.
-Definition foo2 := Eval vm_compute in Stdlib.ZArith.BinInt.Z.div_eucl.
+Require IntDef.
+Definition foo0 := Eval vm_compute in IntDef.Z.div_eucl.
+Definition foo1 := Eval vm_compute in IntDef.Z.div_eucl.
+Definition foo2 := Eval vm_compute in IntDef.Z.div_eucl.
