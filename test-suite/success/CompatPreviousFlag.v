fc8c4b13af5dccff6ba46e6b203a6e1c72e15d0a
Pierre Roux <pierre@roux01.fr>
2024-07-15 22:17:53 +0200

    Try rm compat

--- test-suite/success/CompatPreviousFlag.v
+++
@@ -1,4 +0,0 @@
-(* -*- coq-prog-args: ("-compat" "8.20") -*- *)
-(** Check that the current-minus-one compatibility flag actually requires the relevant modules. *)
-Import Coq.Compat.Coq821.
-Import Coq.Compat.Coq820.
