8952083e522c7431ee3525b5bd78d20f423367f9
Pierre Roux <pierre.roux@onera.fr>
2024-07-15 14:24:21 +0200

    [test-suite] Cleanup dependency on FunExt

--- test-suite/prerequisite/funext.v
+++ test-suite/prerequisite/funext.v
@@ -1,3 +1,9 @@
 Axiom functional_extensionality_dep :
   forall {A : Type} {B : A -> Type} (f g : forall x : A, B x),
     (forall x : A, f x = g x) -> f = g.
+
+Lemma functional_extensionality {A B} (f g : A -> B) :
+  (forall x, f x = g x) -> f = g.
+Proof.
+  intros ; eauto using @functional_extensionality_dep.
+Qed.
