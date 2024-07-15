810c927af5bf23ed1cff06e366d31af9a6c4fa0e
Pierre Roux <pierre.roux@onera.fr>
2024-07-15 13:38:31 +0200

    [test-suite] Cleanup dependencies on ZArith

--- test-suite/coq-makefile/timing-per-line/after/A.v
+++ test-suite/coq-makefile/timing-per-line/after/A.v
@@ -1,4 +1,4 @@
-Require Stdlib.ZArith.BinInt.
+Require IntDef.
 Declare Reduction comp := native_compute.
-Definition foo0 := Eval comp in (Stdlib.ZArith.BinInt.Z.div_eucl, Stdlib.ZArith.BinInt.Z.div_eucl).
+Definition foo0 := Eval comp in (IntDef.Z.div_eucl, IntDef.Z.div_eucl).
 Definition foo1 := Eval comp in (foo0, foo0).
