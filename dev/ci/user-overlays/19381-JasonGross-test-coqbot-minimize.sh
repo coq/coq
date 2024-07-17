1401da84f78b3ca38a1d17ccd324da48970e5017
Jason Gross <jgross@mit.edu>
2024-07-16 21:22:08 -0700

    adjust overlays again

--- dev/ci/user-overlays/19381-JasonGross-test-coqbot-minimize.sh
+++ dev/ci/user-overlays/19381-JasonGross-test-coqbot-minimize.sh
@@ -2,4 +2,4 @@
 #overlay quickchick https://github.com/proux01/QuickChick coq_19310 19310
 #overlay metacoq https://github.com/proux01/metacoq coq_19310 19310
 #overlay elpi https://github.com/proux01/coq-elpi coq_19310 19310
-overlay coqutil https://github.com/mit-plv/coqutil 126561ce8d32df8be7ea7de10eebd0e35b9fa8e9 19381
+overlay coqutil https://github.com/JasonGross/coqutil temp-revert-19310 19381
