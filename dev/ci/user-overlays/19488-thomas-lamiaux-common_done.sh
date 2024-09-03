339058ade36e5bd97bc88e64192a6d4247164ac4
thomas-lamiaux <thomas.lamiaux@ens-paris-saclay.fr>
2024-09-03 21:30:48 +0200

    add coquelicot stdpp iris to overlay

--- dev/ci/user-overlays/19488-thomas-lamiaux-common_done.sh
+++ dev/ci/user-overlays/19488-thomas-lamiaux-common_done.sh
@@ -1,2 +1,5 @@
 overlay mathcomp https://github.com/thomas-lamiaux/math-comp common_done 19488
-overlay coquelicot https://github.com/thomas-lamiaux/coquelicot common_done 19488
+overlay coquelicot https://github.com/thomas-lamiaux/coquelicot/coquelicot common_done 19488
+overlay quickchick https://github.com/thomas-lamiaux/QuickChick/QuickChick common_done 19488
+overlay stdpp https://gitlab.mpi-sws.org/thomas-lamiaux/stdpp common_done 19488
+overlay iris https://gitlab.mpi-sws.org/thomas-lamiaux/iris common_done 19488
