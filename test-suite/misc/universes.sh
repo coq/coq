bae06fcb49f6ff6a1a62f28bb7e9dc1af8279d63
Matthieu Sozeau <matthieu.sozeau@inria.fr>
2024-09-17 17:53:11 +0200

    Fix universes test (now with 1 representing Set + 1)

--- test-suite/misc/universes.sh
+++ test-suite/misc/universes.sh
@@ -4,6 +4,6 @@ EXPECTED_UNIVERSES=3 # Prop is not counted
 $coqc -R misc/universes Universes misc/universes/all_stdlib 2>&1
 $coqc -R misc/universes Universes misc/universes/universes 2>&1
 mv universes.txt misc/universes
-N=$(awk '{print $3}' misc/universes/universes.txt | sort -u | wc -l)
+N=$(awk '{print $3}' misc/universes/universes.txt | sort -u | grep "Type" | wc -l)
 printf "Found %s/%s universes\n" "$N" "$EXPECTED_UNIVERSES"
 if [ "$N" -eq $EXPECTED_UNIVERSES ]; then exit 0; else exit 1; fi
