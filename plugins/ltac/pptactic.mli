2485d123b7d24989cdeb11c34991043810953e0d
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2024-10-04 13:54:10 +0200

    Remove Pptactic.declare_extra_vernac_genarg_pprule
    The users did not get any advantage from using it over directly using
    Genprint.register_vernac_print0

--- plugins/ltac/pptactic.mli
+++ plugins/ltac/pptactic.mli
@@ -77,11 +77,7 @@ val declare_extra_genarg_pprule_with_level :
   'a raw_extra_genarg_printer_with_level ->
   'b glob_extra_genarg_printer_with_level ->
   'c extra_genarg_printer_with_level ->
-  (* surroounded *) entry_relative_level -> (* non-surroounded *) entry_relative_level -> unit
-
-val declare_extra_vernac_genarg_pprule :
-  ('a, 'b, 'c) genarg_type ->
-  'a raw_extra_genarg_printer -> unit
+  (* surrounded *) entry_relative_level -> (* non-surrounded *) entry_relative_level -> unit
 
 type grammar_terminals = Genarg.ArgT.any Extend.user_symbol grammar_tactic_prod_item_expr list
 
