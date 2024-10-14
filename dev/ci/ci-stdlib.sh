a34fd9127d585f526569f9f56ba59126d8d54b8a
Pierre Roux <pierre.roux@onera.fr>
2024-10-14 13:11:39 +0200

    Adapt CI

--- dev/ci/ci-stdlib.sh
+++ dev/ci/ci-stdlib.sh
@@ -5,9 +5,11 @@ set -e
 ci_dir="$(dirname "$0")"
 . "${ci_dir}/ci-common.sh"
 
+git_download stdlib
+
 if [ "$DOWNLOAD_ONLY" ]; then exit 0; fi
 
-( cd "stdlib"
+( cd "${CI_BUILD_DIR}/stdlib"
   dev/with-rocq-wrap.sh dune build --root . --only-packages=coq-stdlib @install
   dev/with-rocq-wrap.sh dune install --root . coq-stdlib --prefix="$CI_INSTALL_DIR"
 )
