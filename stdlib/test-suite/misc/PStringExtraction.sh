112a1716950a3cd9b30e80bd2bcb5d6533f3b6e2
Pierre Roux <pierre.roux@onera.fr>
2024-10-14 13:09:04 +0200

    Remove stdlib (moved to its own repo)

--- stdlib/test-suite/misc/PStringExtraction.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-$coqc misc/PStringExtraction.v > misc/PStringExtraction.real 2>&1
-
-if [[ "$(ocamlc -config-var word_size)" = "64" ]]; then
-  diff -u misc/PStringExtraction.out misc/PStringExtraction.real
-fi
