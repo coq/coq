124d98c6fd126491dbe792936b0f139217714ca4
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2024-10-15 11:52:31 +0200

    Update the vernacular file headers to reflect the fact Coq is now Rocq.

--- theories/FSets/FMaps.v
+++ theories/FSets/FMaps.v
@@ -1,5 +1,5 @@
 (************************************************************************)
-(*         *   The Coq Proof Assistant / The Coq Development Team       *)
+(*         *      The Rocq Prover / The Rocq Development Team           *)
 (*  v      *         Copyright INRIA, CNRS and contributors             *)
 (* <O___,, * (see version control and CREDITS file for authors & dates) *)
 (*   \VV/  **************************************************************)
