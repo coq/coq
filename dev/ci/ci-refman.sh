c3dda1f4720e8d309cf76f437fc1f380b9ad4a74
Pierre Roux <pierre.roux@onera.fr>
2024-10-23 14:56:43 +0200

    [CI] Add refman with all extras

---
+++ dev/ci/ci-refman.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+ci_dir="$(dirname "$0")"
+. "${ci_dir}/ci-common.sh"
+
+if [ "$DOWNLOAD_ONLY" ]; then exit 0; fi
+
+sed -i.bak doc/dune -e '/package coq-core/ d'
+sed -i.bak doc/dune -e '/package rocq-core/ d'
+COQRST_EXTRA=1 dune build --no-buffer @refman-html
+COQRST_EXTRA=1 dune build --no-buffer @refman-pdf
