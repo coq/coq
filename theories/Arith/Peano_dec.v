ca74bb1d582281bc6ae534e39169633c2033dd29
Jean Abou Samra <jean@abou-samra.fr>
2024-10-30 13:03:45 +0100

    Simplify proof of Peano_dec.le_unique

--- theories/Arith/Peano_dec.v
+++ theories/Arith/Peano_dec.v
@@ -8,7 +8,8 @@
 (*         *     (see LICENSE file for the text of the license)         *)
 (************************************************************************)
 
-Require Import Decidable PeanoNat.
+Require Import Decidable PeanoNat Program.Equality.
+
 Require Eqdep_dec.
 Local Open Scope nat_scope.
 
@@ -35,28 +36,15 @@ Register dec_eq_nat as num.nat.eq_dec.
 
 Definition UIP_nat:= Eqdep_dec.UIP_dec Nat.eq_dec.
 
-Import EqNotations.
-
 Lemma le_unique: forall m n (le_mn1 le_mn2 : m <= n), le_mn1 = le_mn2.
 Proof.
-intros m n.
-generalize (eq_refl (S n)).
-generalize n at -1.
-induction (S n) as [|n0 IHn0]; try discriminate.
-clear n; intros n [= <-] le_mn1 le_mn2.
-pose (def_n2 := eq_refl n0); transitivity (eq_ind _ _ le_mn2 _ def_n2).
-  2: reflexivity.
-generalize def_n2; revert le_mn1 le_mn2.
-generalize n0 at 1 4 5 7; intros n1 le_mn1.
-destruct le_mn1 as [|? le_mn1]; intros le_mn2; destruct le_mn2 as [|? le_mn2].
-+ now intros def_n0; rewrite (UIP_nat _ _ def_n0 eq_refl).
-+ intros def_n0; generalize le_mn2; rewrite <-def_n0; intros le_mn0.
-  now destruct (Nat.nle_succ_diag_l _ le_mn0).
-+ intros def_n0; generalize le_mn1; rewrite def_n0; intros le_mn0.
-  now destruct (Nat.nle_succ_diag_l _ le_mn0).
-+ intros def_n0. injection def_n0 as [= ->].
-  rewrite (UIP_nat _ _ def_n0 eq_refl); simpl.
-  assert (H : le_mn1 = le_mn2).
-  * now apply IHn0.
-  * now rewrite H.
+  enough (forall n m, n <= m -> forall le_mn1 le_mn2 : n <= m, le_mn1 = le_mn2) by firstorder.
+  intros n m H.
+  induction H as [|m H IH];
+    dependent destruction le_mn1; dependent destruction le_mn2;
+    try match goal with
+      [ H_imp : S ?n <= ?n |- _ ] => contradiction (Nat.nle_succ_diag_l n H_imp)
+    end.
+  - reflexivity.
+  - f_equal. apply IH.
 Qed.
