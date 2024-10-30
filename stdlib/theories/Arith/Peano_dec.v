6443ea17b972a06e0c1a0d8603c2160fb947243c
Jean Abou Samra <jean@abou-samra.fr>
2024-10-30 13:03:45 +0100

    Simplify proof of Peano_dec.le_unique

--- stdlib/theories/Arith/Peano_dec.v
+++ stdlib/theories/Arith/Peano_dec.v
@@ -35,28 +35,23 @@ Register dec_eq_nat as num.nat.eq_dec.
 
 Definition UIP_nat:= Eqdep_dec.UIP_dec Nat.eq_dec.
 
-Import EqNotations.
+Scheme le_ind_dep := Induction for le Sort Prop.
 
-Lemma le_unique: forall m n (le_mn1 le_mn2 : m <= n), le_mn1 = le_mn2.
+Lemma le_unique: forall (m n : nat) (le_mn1 le_mn2 : m <= n), le_mn1 = le_mn2.
 Proof.
-intros m n.
-generalize (eq_refl (S n)).
-generalize n at -1.
-induction (S n) as [|n0 IHn0]; try discriminate.
-clear n; intros n [= <-] le_mn1 le_mn2.
-pose (def_n2 := eq_refl n0); transitivity (eq_ind _ _ le_mn2 _ def_n2).
-  2: reflexivity.
-generalize def_n2; revert le_mn1 le_mn2.
-generalize n0 at 1 4 5 7; intros n1 le_mn1.
-destruct le_mn1 as [|? le_mn1]; intros le_mn2; destruct le_mn2 as [|? le_mn2].
-+ now intros def_n0; rewrite (UIP_nat _ _ def_n0 eq_refl).
-+ intros def_n0; generalize le_mn2; rewrite <-def_n0; intros le_mn0.
-  now destruct (Nat.nle_succ_diag_l _ le_mn0).
-+ intros def_n0; generalize le_mn1; rewrite def_n0; intros le_mn0.
-  now destruct (Nat.nle_succ_diag_l _ le_mn0).
-+ intros def_n0. injection def_n0 as [= ->].
-  rewrite (UIP_nat _ _ def_n0 eq_refl); simpl.
-  assert (H : le_mn1 = le_mn2).
-  * now apply IHn0.
-  * now rewrite H.
+  intros m. refine (le_ind_dep m _ _ _).
+  - enough (forall n le_mn2 (E : m = n), le_n m = eq_rect_r _ le_mn2 E) as H.
+    { intros le_mm. apply (H m le_mm eq_refl). }
+    intros n le_mn2. destruct le_mn2 as [|n]; intros E.
+    + replace E with (eq_refl m) by apply UIP_nat. reflexivity.
+    + exfalso. apply (Nat.nle_succ_diag_l n). rewrite <- E. apply le_mn2.
+  - intros n le_mn1 IH.
+    enough (forall p le_mp (E : S n = p),
+               le_S m n le_mn1 = eq_rect_r (le m) le_mp E) as H.
+    { intros le_mp. apply (H (S n) le_mp eq_refl). }
+    intros p le_mp. destruct le_mp as [|p]; intros E.
+    + exfalso. apply (Nat.nle_succ_diag_l n). rewrite E. apply le_mn1.
+    + injection E as E'. subst p.
+      replace E with (eq_refl (S n)) by apply UIP_nat.
+      compute. f_equal. apply IH.
 Qed.
