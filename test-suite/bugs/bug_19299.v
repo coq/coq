966cecd66baca3e302ec8a48ad0ffcdc43660b4e
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2024-11-18 15:00:38 +0100

    Add test for template poly extracted from mtac2
    Close #19299

---
+++ test-suite/bugs/bug_19299.v
@@ -0,0 +1,15 @@
+Require Import List.
+
+Import ListNotations.
+
+Class MBind (M : Type -> Type) :=
+  mbind : forall {A B}, (A -> M B) -> M A -> M B.
+#[global] Instance list_bind : MBind list.
+exact (fun A B f =>
+  fix go (l : list A) :=
+    match l with nil => nil | cons x l => f x ++ go l end%list).
+Defined.
+
+Polymorphic Record dyn := Dyn { type : Type; elem : type }.
+
+Definition fails : list dyn := [Dyn _ (@List.app)].
