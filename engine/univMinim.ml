919268ae1bd69d48d6beeab8a431cbbc6b549f6c
Matthieu Sozeau <matthieu.sozeau@inria.fr>
2024-11-25 17:54:21 +0100

    Remove dead code from univMinim

--- engine/univMinim.ml
+++ engine/univMinim.ml
@@ -47,116 +47,6 @@ let choose_canonical ctx flexible s =
           let canon = choose_expr flexible in
           canon, (global, rigid, LevelExpr.Set.remove canon flexible)
 
-type lowermap = int Level.Map.t
-
-let lower_union =
-  let merge k a b =
-    match a, b with
-    | Some _, None -> a
-    | None, Some _ -> b
-    | None, None -> None
-    | Some l, Some r ->
-       if Int.compare l r >= 0 then a
-       else b
-  in Level.Map.merge merge
-
-let lower_add l c m =
-  try let c' = Level.Map.find l m in
-      if Int.compare c c' > 0 then
-        Level.Map.add l c m
-      else m
-  with Not_found -> Level.Map.add l c m
-
-let lower_of_list l =
-  List.fold_left (fun acc (k,l) -> Level.Map.add l k acc) Level.Map.empty l
-
-type lbound = { enforce : bool; inst: Universe.t; lower : lowermap }
-
-module LBMap :
-sig
-  type t = private { lbmap : lbound Level.Map.t; lbrev : (Level.t * lowermap) Universe.Map.t }
-  val empty : t
-  val add : Level.t -> lbound -> t -> t
-end =
-struct
-  type t = { lbmap : lbound Level.Map.t; lbrev : (Level.t * lowermap) Universe.Map.t }
-  (* lbrev is uniquely given from lbmap as a partial reverse mapping *)
-  let empty = { lbmap = Level.Map.empty; lbrev = Universe.Map.empty }
-  let add u bnd m =
-    let lbmap = Level.Map.add u bnd m.lbmap in
-    let lbrev = m.lbrev in
-    { lbmap; lbrev }
-end
-
-let compute_lbound left =
- (* The universe variable was not fixed yet.
-    Compute its level using its lower bound. *)
-  let sup l lbound =
-    match lbound with
-    | None -> Some l
-    | Some l' -> Some (Universe.sup l l')
-  in
-    List.fold_left (fun lbound (k, l) -> sup (Universe.addn l k) lbound) None left
-
-let instantiate u inst lower ~enforce (ctx, us, seen, insts, cstrs) =
-  (debug Pp.(fun () -> str"Instantiating " ++ Level.raw_pr u ++ str" with " ++ Universe.pr Level.raw_pr inst);
-  (Level.Set.remove u ctx, UnivFlex.define u inst us, seen,
-   LBMap.add u {enforce;inst;lower} insts, cstrs),
-  {enforce; inst; lower})
-
-
-let instantiate_with_lbound u inst lower ~enforce (ctx, us, seen, insts, cstrs as acc) =
-  let flexible = try not (UnivFlex.is_defined u us) with Not_found -> false in
-  if enforce || not flexible then
-    let uinst = Universe.make u in
-    let cstrs' = enforce_leq inst uinst cstrs in
-      (ctx, us, seen, LBMap.add u {enforce;inst;lower} insts, cstrs'),
-      {enforce; inst; lower}
-  else (* Actually instantiate *)
-  instantiate u inst lower ~enforce acc
-
-type constraints_map = (constraint_type * Level.Map.key) list Level.Map.t
-
-let _pr_constraints_map (cmap:constraints_map) =
-  let open Pp in
-  Level.Map.fold (fun l cstrs acc ->
-    Level.raw_pr l ++ str " => " ++
-      prlist_with_sep spc (fun (d,r) -> pr_constraint_type d ++ Level.raw_pr r) cstrs ++
-      fnl () ++ acc)
-    cmap (mt ())
-
-let not_lower lower (k,l) =
-  (* We're checking if (k,l) is already implied by the lower
-     constraints on some level u. If it represents l < u (k is > 0
-     or k = 0 and i > 0, the i < 0 case is impossible due to
-     invariants of Univ), and the lower constraints only have l <=
-     u then it is not implied. *)
-  Universe.exists
-    (fun (l,i) ->
-      let k = k + i in
-       try let k' = Level.Map.find l lower in
-         (* If k is stronger than the already implied lower
-          * constraints we must keep it. *)
-         k > k'
-       with Not_found ->
-         (* No constraint existing on l *) true) l
-
-(** [enforce_uppers upper lbound cstrs] interprets [upper] as upper
-   constraints to [lbound], adding them to [cstrs]. *)
-let enforce_uppers upper lbound cstrs =
-  List.fold_left (fun cstrs (k, r) -> (* lbound + k <= r *)
-      if Int.equal k 0 then
-        enforce_leq lbound (Universe.make r) cstrs
-      else
-        Constraints.add (Universe.addn lbound k, Le, Universe.make r) cstrs)
-    cstrs upper
-
-let is_set_increment u =
-  match Universe.repr u with
-  | [] -> false
-  | [(l, k)] -> Level.is_set l
-  | _ -> false
-
 let term_type_variances_irrel u us (variances : InferCumulativity.variances) =
   let open UVars.Variance in
   match Level.Map.find_opt u variances with
@@ -164,134 +54,6 @@ let term_type_variances_irrel u us (variances : InferCumulativity.variances) =
   | Some occs ->
     let termv, typev, principal = term_type_variances occs in
     Option.default Irrelevant termv, Option.default Irrelevant typev, principal
-
-(* let term_type_variances u us variances =
-  match Level.Map.find_opt u variances with
-  | None -> None, None
-  | Some (_mode, pos) -> term_type_variances pos *)
-
-let minimize_univ_variables partial ctx us variances left right cstrs =
-  let left, lbounds =
-    Level.Map.fold (fun r lower (left, lbounds as acc)  ->
-      if UnivFlex.mem r us || not (Level.Set.mem r ctx) then acc
-      else (* Fixed universe, just compute its glb for sharing *)
-        let lbounds =
-          match compute_lbound (List.map (fun (d,l) -> d, Universe.make l) lower) with
-          | None -> lbounds
-          | Some inst -> LBMap.add r {enforce=true; inst; lower=lower_of_list lower}
-                                   lbounds
-        in (Level.Map.remove r left, lbounds))
-      left (left, LBMap.empty)
-  in
-  let rec instance (ctx, us, seen, insts, cstrs as acc) u =
-    let acc, left, lower =
-      match Level.Map.find u left with
-      | exception Not_found -> acc, [], Level.Map.empty
-      | l ->
-        let acc, left, newlow, lower =
-          List.fold_left
-          (fun (acc, left, newlow, lower') (d, l) ->
-           let acc', {enforce=enf;inst=l';lower} = aux acc l in
-           let l' =
-             if enf then Universe.make l
-             else l'
-           in acc', (d, l') :: left,
-              lower_add l d newlow, lower_union lower lower')
-          (acc, [], Level.Map.empty, Level.Map.empty) l
-        in
-        let left = CList.uniquize (List.filter (not_lower lower) left) in
-        (acc, left, Level.Map.lunion newlow lower)
-    in
-    let term_variance, type_variance, principal = term_type_variances_irrel u us variances in
-    let instantiate_lbound lbound =
-      if is_set_increment lbound && not (get_set_minimization()) then
-        (* Minim to Set disabled, do not instantiate with Set *)
-        instantiate_with_lbound u lbound lower ~enforce:(term_variance <> Irrelevant) acc
-      else (* u is algebraic: we instantiate it with its lower bound, if any,
-              or enforce the constraints if it is bounded from the top. *)
-        let lower = Level.Set.fold Level.Map.remove (Universe.levels lbound) lower in
-        instantiate_with_lbound u lbound lower ~enforce:false acc
-    in
-    let enforce_uppers ((ctx,us,seen,insts,cstrs), b as acc) =
-      debug Pp.(fun () -> str"Looking for " ++ Level.raw_pr u ++ str" in right map");
-      match Level.Map.find u right with
-      | exception Not_found ->
-        debug Pp.(fun () -> str"Looking for " ++ Level.raw_pr u ++ str" in right map: not found");
-        acc
-      | upper ->
-        debug Pp.(fun () -> str"Enforcing upper bound of " ++ Universe.pr Level.raw_pr b.inst ++ str":" ++ Universe.pr Level.raw_pr (Universe.of_list (List.map (fun (d, r) -> r, d) upper)));
-        let upper = List.filter (fun (d, r) -> not (UnivFlex.mem r us)) upper in
-        debug Pp.(fun () -> str"Enforcing upper bound of " ++ Universe.pr Level.raw_pr b.inst ++ str":" ++ Universe.pr Level.raw_pr (Universe.of_list (List.map (fun (d, r) -> r, d) upper)));
-        let cstrs = enforce_uppers upper b.inst cstrs in
-        (ctx, us, seen, insts, cstrs), b
-    in
-    if not (Level.Set.mem u ctx)
-    then enforce_uppers (acc, {enforce=true; inst=Universe.make u; lower})
-    else
-      let lbound = match compute_lbound left with None -> Universe.type0 | Some lbound -> lbound in
-      debug Pp.(fun () -> str"Lower bound of " ++ Level.raw_pr u ++ str":" ++ Universe.pr Level.raw_pr lbound);
-      if Level.Set.mem u (Universe.levels lbound) then
-        (* let cstrs' = enforce_leq (Univ.univ_level_rem u lbound lbound) (Universe.make u) cstrs in *)
-        (ctx, us, seen, insts, cstrs), { enforce = true; inst = Universe.make u; lower }
-      else
-      let open UVars.Variance in
-      match term_variance, type_variance with
-      | Irrelevant, Irrelevant ->
-        (* This keeps principal typings, as instantiating to e.g. Set in a covariant position allows to use Set <= i for any i *)
-        enforce_uppers (instantiate_lbound lbound)
-      | (Covariant | Irrelevant), Covariant when not partial ->
-        enforce_uppers (instantiate_lbound lbound)
-      | _, _ ->
-        if Universe.is_type0 lbound then enforce_uppers (acc, {enforce = true; inst = Universe.make u; lower})
-        else enforce_uppers (instantiate_with_lbound u lbound lower ~enforce:true acc)
-  and aux (ctx, us, seen, insts, cstrs as acc) u =
-    debug Pp.(fun () -> str"Calling minim on " ++ Level.raw_pr u);
-    try let lbound = Level.Map.find u insts.LBMap.lbmap in
-      debug Pp.(fun () -> str" = " ++ Universe.pr Level.raw_pr lbound.inst);
-      acc, lbound
-    with Not_found ->
-      debug Pp.(fun () -> str"Calling minim on " ++ Level.raw_pr u ++ str" not found");
-      if Level.Set.mem u seen then
-        (debug Pp.(fun () -> str"Calling minim on " ++ Level.raw_pr u ++ str" not found but seen");
-        (* Loop in the contraints *)
-        let bnd = {enforce=true; inst=Universe.make u; lower = Level.Map.empty} in
-        acc, bnd)
-      else
-        (let acc = instance (ctx, us, Level.Set.add u seen, insts, cstrs) u in
-          debug Pp.(fun () -> str"Calling minim on " ++ Level.raw_pr u ++ str" computed an instance");
-          acc)
-  in
-  let (ctx, us, _, _, cstrs as acc) =
-    UnivFlex.fold (fun u ~is_defined (ctx, us, seen, insts, cstrs as acc) ->
-      if not is_defined then
-        (debug Pp.(fun () -> str"Calling aux on " ++ Level.raw_pr u);
-         fst (aux acc u))
-      else Level.Set.remove u ctx, us, seen, insts, cstrs)
-      us (ctx, us, Level.Set.empty, lbounds, cstrs)
-  in
-  let () = debug Pp.(fun () -> str"Maximising contravariant universes") in
-  let instantiate_ubound (ctx, us, cstrs as acc) u ubound =
-    match ubound with
-    | Some [(0,e)] -> (* u + i <= e *)
-      let univ = Universe.of_expr (e, 0) in
-      let univ = UnivFlex.normalize_universe us univ in
-      if Universe.mem u univ then acc else
-      let () = debug Pp.(fun () -> str"Instantiating "++ Level.raw_pr u ++ str" with " ++ Level.raw_pr e) in
-      (Level.Set.remove u ctx, UnivFlex.define u univ us,
-       subst_univs_level_constraints (Level.Map.singleton u univ) cstrs)
-    | _ ->  acc
-  in
-  let maximize_contravariant (ctx, us, cstrs as acc) u =
-    let term_variance, type_variance, _ = term_type_variances_irrel u us variances in
-    match term_variance, type_variance with
-    | (Covariant | Irrelevant), Contravariant when not partial ->
-      let ubound = Level.Map.find_opt u right in
-      instantiate_ubound acc u ubound
-    | _, _ -> acc
-  in
-    UnivFlex.fold (fun u ~is_defined acc ->
-      if not is_defined then maximize_contravariant acc u
-      else acc) us (ctx, us, cstrs)
 
 let _warn_not_minimizable u =
   Feedback.msg_notice Pp.(str"Universe " ++ Level.raw_pr u ++ str" is not mimimizable as its lower bound \
@@ -438,13 +200,6 @@ let extra_union a b = {
   weak_constraints = UPairSet.union a.weak_constraints b.weak_constraints;
   above_prop = Level.Set.union a.above_prop b.above_prop;
 }
-
-let add_list_map u t map =
-  try
-    let l = Level.Map.find u map in
-    Level.Map.set u (t :: l) map
-  with Not_found ->
-    Level.Map.add u [t] map
 
 let pr_partition prl m =
   let open Pp in
@@ -687,56 +442,29 @@ let normalize_context_set ~lbound ~variances ~partial g ctx (us:UnivFlex.t) ?bin
   debug Pp.(fun () -> str "Starting minimization with: " ++ pr_universe_context_set prl (ctx, noneqs) ++
     UnivFlex.pr Level.raw_pr us);
   let ctx', us, variances, noneqs =
-    if not (CDebug.get_flag switch_minim) then
-      (* Compute the left and right set of flexible variables, constraints
-        mentioning other variables remain in noneqs. *)
-       let filter, noneqs, ucstrsl, ucstrsr =
-        Constraints.fold (fun (l,d,r) (filter, noneq, ucstrsl, ucstrsr as acc) ->
-          let analyse l k r (filter, noneq, ucstrsl, ucstrsr) =
-            let lus = UnivFlex.mem l us and rus = UnivFlex.mem r us in
-            let ucstrsl' =
-              if lus then add_list_map l (k, r) ucstrsl
-              else ucstrsl
-            and ucstrsr' =
-              add_list_map r (k, l) ucstrsr
-            in
-            let noneqs =
-              if lus || rus then noneq
-              else Constraints.add (Universe.addn (Universe.make l) k, Le, Universe.make r) noneq
-            in (filter, noneqs, ucstrsl', ucstrsr')
-          in
-          match Universe.level r with
-          | Some r -> List.fold_left (fun acc (l, k) -> analyse l k r acc) acc (Univ.Universe.repr l)
-          | None -> (Level.Set.union filter (Universe.levels r), Constraints.add (l, d, r) noneqs, ucstrsl, ucstrsr))
-        noneqs (Level.Set.empty, Constraints.empty, Level.Map.empty, Level.Map.empty)
-      in
-          let us = Level.Set.fold (fun l us -> UnivFlex.remove l us) filter us in
-      let ctx, us, noneqs = minimize_univ_variables partial ctx us variances ucstrsr ucstrsl noneqs in
-      ctx, us, variances, noneqs
-    else
-      let smalllesu = Constraints.fold (fun (l, d, r) acc ->
-        match Universe.level r with Some r -> Level.Set.add r acc | None -> acc) smallles Level.Set.empty in
-      (* debug Pp.(fun () -> str"Model before removal: " ++ UGraph.pr_model graph); *)
-      let graph = UnivFlex.fold (fun l ~is_defined graph ->
-        if not is_defined && not (Level.Set.mem l smalllesu) then
-          (debug Pp.(fun () -> str"Removing " ++ Level.raw_pr l ++ str" -> Set constraints");
-           UGraph.remove_set_clauses l graph)
-        else graph)
-        us graph
-      in
-      (* debug Pp.(fun () -> str"Model after removal: " ++ UGraph.pr_model graph); *)
-      let ctx', us, variances, graph = simplify_variables partial ctx us variances graph in
-      debug_graph Pp.(fun () -> str"Model after simplification: " ++ UGraph.pr_model graph ++ fnl () ++ UnivFlex.pr Level.raw_pr us);
-      let ctx', us, variances, graph = new_minimize_weak ctx' us weak (graph, variances) in
-      let locals, cstrs, partition = UGraph.constraints_of_universes ~only_local:true graph in
-      let cstrs = partition_to_constraints partition cstrs in
-      let cstrs =
-        Constraints.filter
-          (fun (l,d,r) -> not (d == Le && Universe.is_type0 l))
-          cstrs
-      in
-      let cstrs = Constraints.union smallles cstrs in
-      ctx', us, variances, cstrs
+    let smalllesu = Constraints.fold (fun (l, d, r) acc ->
+      match Universe.level r with Some r -> Level.Set.add r acc | None -> acc) smallles Level.Set.empty in
+    (* debug Pp.(fun () -> str"Model before removal: " ++ UGraph.pr_model graph); *)
+    let graph = UnivFlex.fold (fun l ~is_defined graph ->
+      if not is_defined && not (Level.Set.mem l smalllesu) then
+        (debug Pp.(fun () -> str"Removing " ++ Level.raw_pr l ++ str" -> Set constraints");
+          UGraph.remove_set_clauses l graph)
+      else graph)
+      us graph
+    in
+    (* debug Pp.(fun () -> str"Model after removal: " ++ UGraph.pr_model graph); *)
+    let ctx', us, variances, graph = simplify_variables partial ctx us variances graph in
+    debug_graph Pp.(fun () -> str"Model after simplification: " ++ UGraph.pr_model graph ++ fnl () ++ UnivFlex.pr Level.raw_pr us);
+    let ctx', us, variances, graph = new_minimize_weak ctx' us weak (graph, variances) in
+    let locals, cstrs, partition = UGraph.constraints_of_universes ~only_local:true graph in
+    let cstrs = partition_to_constraints partition cstrs in
+    let cstrs =
+      Constraints.filter
+        (fun (l,d,r) -> not (d == Le && Universe.is_type0 l))
+        cstrs
+    in
+    let cstrs = Constraints.union smallles cstrs in
+    ctx', us, variances, cstrs
   in
   let us = UnivFlex.normalize us in
   let noneqs = UnivSubst.subst_univs_constraints (UnivFlex.normalize_univ_variable us) noneqs in
