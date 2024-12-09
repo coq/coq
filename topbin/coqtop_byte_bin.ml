32e71e61c5a89a8500c38cc3ce808663b8d00718
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2024-12-09 17:46:13 +0100

    Add `rocq repl-with-drop` subcommand, put coqtop.byte back in coq-core

--- topbin/coqtop_byte_bin.ml
+++ topbin/coqtop_byte_bin.ml
@@ -8,48 +8,13 @@
 (*         *     (see LICENSE file for the text of the license)         *)
 (************************************************************************)
 
-(* We register this handler for lower-level toplevel loading code *)
-let () = CErrors.register_handler (function
-    | Symtable.Error e ->
-      Some (Pp.str (Format.asprintf "%a" Symtable.report_error e))
-    | _ ->
-      None
-  )
+open Rocqshim
 
-(* Another bit of text is printed in the [include_utilities] file, so the default one is not need *)
-let () = Clflags.noversion := true
-
-let load_module fmt name =
-  if not ((Topdirs.load_file [@ocaml.warning "-3"]) fmt name) then
-    CErrors.user_err Pp.(str ("Could not load plugin " ^ name))
-
-let load_plugin fmt ps =
-  let lib = Mltop.PluginSpec.to_package ps in
-  Topfind.load_deeply [lib]
-
-let ml_loop fmt ?init_file () =
-  let init_file = ref init_file in
-  Toploop.add_hook (fun event ->
-    if event = Toploop.After_setup then begin
-      match !init_file with
-      | None -> ()
-      | Some f ->
-        init_file := None; (* Run the initialization file only once *)
-        ignore (Coq_byte_config.toploop_use_silently fmt f)
-    end
-  );
-  Coq_byte_config.compenv_handle_exit_with_status_0 (fun () -> Toploop.loop fmt)
-
-let drop_setup () =
-  let ppf = Format.std_formatter in
-  Mltop.set_top
-    { load_plugin = load_plugin ppf
-    ; load_module = load_module ppf
-    ; add_dir = Topdirs.dir_directory
-    ; ml_loop = ml_loop ppf
-    }
-
-(* Main coqtop initialization *)
-let _ =
-  drop_setup ();
-  Coqtop.(start_coq coqtop_toplevel (List.tl (Array.to_list Sys.argv)))
+let () =
+  let args = List.tl (Array.to_list Sys.argv) in
+  let opts, args = Rocqshim.parse_opts args in
+  let () = Rocqshim.init opts args in
+  let prog = get_worker_path { package = "rocq-runtime"; basename = "coqworker_with_drop" } in
+  let () = if opts.debug_shim then Printf.eprintf "Using %s\n%!" prog in
+  let argv = Array.of_list (prog :: args) in
+  exec_or_create_process prog argv
