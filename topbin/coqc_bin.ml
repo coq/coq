37db20f81ea37aa6be057afd1f8246d9a1b1cb1d
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2024-12-09 15:23:08 +0100

    Add `rocq preprocess-mlg` subcommand (also pp-mlg)

--- topbin/coqc_bin.ml
+++ topbin/coqc_bin.ml
@@ -12,8 +12,9 @@ open Rocqshim
 
 let () =
   let args = List.tl (Array.to_list Sys.argv) in
-  let {debug_shim}, args = Rocqshim.init args in
+  let opts, args = Rocqshim.parse_opts args in
+  let () = Rocqshim.init opts args in
   let prog = get_worker_path { package = "rocq-runtime"; basename = "coqworker" } in
-  let () = if debug_shim then Printf.eprintf "Using %s\n%!" prog in
+  let () = if opts.debug_shim then Printf.eprintf "Using %s\n%!" prog in
   let argv = Array.of_list (prog :: "--kind=compile" :: args) in
   exec_or_create_process prog argv
