53d9356cc14101468e1110ec513219d8aa4f3dfa
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2024-12-09 17:46:13 +0100

    Add `rocq repl-with-drop` subcommand, put coqtop.byte back in coq-core

--- topbin/rocq.ml
+++ topbin/rocq.ml
@@ -13,12 +13,15 @@ let fatal_error fmt = Printf.kfprintf (fun _ -> exit 1) stderr (fmt^^"\n")
 let error_usage () =
   fatal_error "Usage: rocq [-debug-shim] {-v|--version|NAME} [ARGUMENTS...]"
 
-let with_worker opts kind args =
+let with_worker_gen opts basename args =
   Rocqshim.init opts args;
-  let prog = Rocqshim.get_worker_path { package = "rocq-runtime"; basename = "coqworker" } in
+  let prog = Rocqshim.get_worker_path { package = "rocq-runtime"; basename } in
   let () = if opts.debug_shim then Printf.eprintf "Using %s\n%!" prog in
-  let argv = Array.of_list (prog :: ("--kind="^kind) :: args) in
+  let argv = Array.of_list (prog :: args) in
   Rocqshim.exec_or_create_process prog argv
+
+let with_worker opts kind args =
+  with_worker_gen opts "coqworker" (("--kind="^kind) :: args)
 
 let () =
   if Array.length Sys.argv < 2 then error_usage ();
@@ -29,6 +32,7 @@ let () =
   | "-v" :: _ | "--version" :: _ -> Boot.Usage.version ()
   | ("c" | "compile") :: args -> with_worker opts "compile" args
   | ("top"|"repl") :: args -> with_worker opts "repl" args
+  | ("top-with-drop"|"repl-with-drop") :: args -> with_worker_gen opts "coqworker_with_drop" args
   | ("preprocess-mlg"|"pp-mlg") :: args -> Coqpp_main.main args
   | "dep" :: args -> Coqdeplib.Rocqdep_main.main args
   | "doc" :: args -> Coqdoclib.Rocqdoc_main.main ~prog:(Sys.argv.(0) ^ " doc") args
