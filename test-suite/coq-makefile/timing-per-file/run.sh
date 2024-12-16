093dd6ede3fa068bca50e7c67dd273b6ddb1d4e4
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2024-12-16 13:17:13 +0100

    coq_makefile produce timing info for coqdep

--- test-suite/coq-makefile/timing-per-file/run.sh
+++ test-suite/coq-makefile/timing-per-file/run.sh
@@ -2,10 +2,19 @@
 
 . ../timing-template/init.sh
 
+rm -rf _test
+mkdir _test
+find . -maxdepth 1 -not -name . -not -name _test -exec cp -r '{}' -t _test ';'
+cd _test
+
+# we delete the timings for coqdep because they're too close to 0
+# so testing them is hard
+
 cd before/
 rocq makefile -f _CoqProject -o Makefile
 make cleanall
 make make-pretty-timed-before TGTS="all" -j1 || exit $?
+sed -i.bak 's/.Makefile.d .*/.Makefile.d (ignored)/' time-of-build-before.log
 
 cd ../after/
 
@@ -14,6 +23,7 @@ make cleanall
 
 make make-pretty-timed-after TGTS="all" -j1 || exit $?
 rm -f time-of-build-before.log
+sed -i.bak 's/.Makefile.d .*/.Makefile.d (ignored)/' time-of-build-after.log
 
 make print-pretty-timed-diff TIMING_SORT_BY=diff TIME_OF_BUILD_BEFORE_FILE=../before/time-of-build-before.log
 cp ../before/time-of-build-before.log ./
