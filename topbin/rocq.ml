43f80b473ee77ccb26145500e4266e827a2e892c
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2024-12-20 14:08:39 +0100

    More useful usage message for `rocq`

--- topbin/rocq.ml
+++ topbin/rocq.ml
@@ -8,10 +8,7 @@
 (*         *     (see LICENSE file for the text of the license)         *)
 (************************************************************************)
 
-let fatal_error fmt = Printf.kfprintf (fun _ -> exit 1) stderr (fmt^^"\n")
-
-let error_usage () =
-  fatal_error "Usage: rocq [-debug-shim] {-v|--version|NAME} [ARGUMENTS...]"
+let fatal_error fmt = Printf.kfprintf (fun _ -> exit 1) stderr (fmt^^"%!")
 
 let with_worker_gen opts basename args =
   Rocqshim.init opts args;
@@ -29,6 +26,79 @@ let with_sibling_exe opts prog args =
   let argv = Array.of_list (prog :: args) in
   Rocqshim.exec_or_create_process prog argv
 
+type subcommand =
+  | Compile
+  | Repl
+  | ReplWithDrop
+  | NativePrecompile
+  | Check
+  | Votour
+  | PpMlg
+  | Dep
+  | Doc
+  | Wc
+  | Workmgr
+  | Tex
+  | Makefile
+  | Timelog2Html
+
+let subcommands = [
+  ("compile", "Compile a Rocq source file", Compile);
+  ("c", "Alias for compile", Compile);
+  ("repl", "Interactive read-eval-print loop", Repl);
+  ("top", "Alias for repl", Repl);
+  ("repl-with-drop", "repl with Drop command", ReplWithDrop);
+  ("top-with-drop", "Alias for repl-with-drop", ReplWithDrop);
+  ("native-precompile", "Preprocess a compiled Rocq file for use by native_compute", NativePrecompile);
+  ("check", "Check a compiled Rocq file (alias for command rocqchk)", Check);
+  ("votour", "Low level debugging of compiled files", Votour);
+  ("preprocess-mlg", "Preprocess Rocq grammar files (.mlg) to produce OCaml sources", PpMlg);
+  ("pp-mlg", "Alias for preprocess-mlg", PpMlg);
+  ("dep", "Print dependencies for compiling Rocq files", Dep);
+  ("doc", "Generate documentation from a Rocq source file", Doc);
+  ("wc", "Count lines of code in a Rocq source file", Wc);
+  ("workmgr", "Control the number of parallel workers used by Rocq", Workmgr);
+  ("tex", "Process Rocq code in a Latex document", Tex);
+  ("makefile", "Generate a Makefile to compile a Rocq project", Makefile);
+  ("timelog2html", "Combine timing information and a Rocq source file", Timelog2Html);
+]
+
+let print_usage fmt () =
+  let longest_subcmd =
+    List.fold_left (fun acc (cmd,_,_) -> max acc (String.length cmd)) 0 subcommands
+  in
+  let subcommands =
+    List.map (fun (cmd,doc,_) ->
+        let separator = String.make (3 + longest_subcmd - String.length cmd) ' ' in
+        "    "^cmd^separator^doc)
+      subcommands
+  in
+  Printf.fprintf fmt "Usage: rocq [-debug-shim] {-v|--version|--help|SUBCOMMAND} [ARGUMENTS...]\n\
+\n\
+Supported subcommands:\n\
+%s\n\
+\n\
+Use \"rocq subcommand --help\" to get more information about a given subcommand.\n" (String.concat "\n" subcommands)
+
+let error_usage () =
+  fatal_error "%a" print_usage ()
+
+let run_subcommand opts args = function
+  | Compile -> with_worker opts "compile" args
+  | Repl -> with_worker opts "repl" args
+  | ReplWithDrop -> with_worker_gen opts "coqworker_with_drop" args
+  | NativePrecompile -> with_worker_gen opts "rocqnative" args
+  | Check -> with_sibling_exe opts "rocqchk" args
+  | Votour -> with_sibling_exe opts "votour" args
+  | PpMlg -> Coqpp_main.main args
+  | Dep -> Coqdeplib.Rocqdep_main.main args
+  | Doc -> Coqdoclib.Rocqdoc_main.main ~prog:(Sys.argv.(0) ^ " doc") args
+  | Wc -> Rocqwc.main args
+  | Workmgr -> Rocqworkmgr.main ~prog:(Sys.argv.(0) ^ " workmgr") args
+  | Tex -> Rocqtex.main ~prog:(Sys.argv.(0) ^ " tex") args
+  | Makefile -> Rocqmakefile.main ~prog:[Sys.argv.(0); "makefile"] args
+  | Timelog2Html -> Benchlib.Timelog2html.main ~prog:(Sys.argv.(0) ^ " timelog2html") args
+
 let () =
   if Array.length Sys.argv < 2 then error_usage ();
 
@@ -37,27 +107,14 @@ let () =
   match args with
   (* help prints *)
   | "-v" :: _ | "--version" :: _ -> Boot.Usage.version ()
+  | ("-h" | "-H" | "-help" | "--help") :: _ ->
+    Printf.printf "%a%!" print_usage ();
+    exit 0
 
-  (* workers *)
-  | ("c" | "compile") :: args -> with_worker opts "compile" args
-  | ("top"|"repl") :: args -> with_worker opts "repl" args
-  | ("top-with-drop"|"repl-with-drop") :: args -> with_worker_gen opts "coqworker_with_drop" args
-  | "native-precompile" :: args -> with_worker_gen opts "rocqnative" args
+  | cmd :: args ->
+    begin match List.find_opt (fun (cmd',_,_) -> String.equal cmd cmd') subcommands with
+    | None -> fatal_error "Unknown subcommand %s\n%a%!" cmd print_usage ()
+    | Some (_,_,cmd) -> run_subcommand opts args cmd
+    end
 
-  (* public executables *)
-  | "check" :: args -> with_sibling_exe opts "rocqchk" args
-  | "votour" :: args -> with_sibling_exe opts "votour" args
-
-  (* statically linked subcommands *)
-  | ("preprocess-mlg"|"pp-mlg") :: args -> Coqpp_main.main args
-  | "dep" :: args -> Coqdeplib.Rocqdep_main.main args
-  | "doc" :: args -> Coqdoclib.Rocqdoc_main.main ~prog:(Sys.argv.(0) ^ " doc") args
-  | "wc" :: args -> Rocqwc.main args
-  | "workmgr" :: args -> Rocqworkmgr.main ~prog:(Sys.argv.(0) ^ " workmgr") args
-  | "tex" :: args -> Rocqtex.main ~prog:(Sys.argv.(0) ^ " tex") args
-  | "makefile" :: args -> Rocqmakefile.main ~prog:[Sys.argv.(0); "makefile"] args
-  | "timelog2html" :: args -> Benchlib.Timelog2html.main ~prog:(Sys.argv.(0) ^ " timelog2html") args
-
-  | prog :: _ ->
-    fatal_error "Unknown argument %s" prog
   | [] -> error_usage ()
