9af8e675c23deac98abf739bdf7485ccbe75fae0
Pierre Roux <pierre.roux@onera.fr>
2025-01-06 14:29:58 +0100

    Add overlays

---
+++ dev/ci/user-overlays/19801-andres-erbsen-Zeq_bool.sh
@@ -0,0 +1,4 @@
+overlay flocq https://github.com/proux01/flocq coq_19801 19801
+overlay vst https://github.com/andres-erbsen/VST less-ZArith_base 19801
+overlay compcert https://github.com/andres-erbsen/CompCert less-ZArith_base 19801
+overlay coquelicot https://github.com/proux01/coquelicot coq_19801 19801
