295212d7930b4ae21491d6714fdf88ccef031bbb
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2025-01-08 16:53:01 +0100

    Adding overlays.

---
+++ dev/ci/user-overlays/19995-ppedrot-module-abstract-type.sh
@@ -0,0 +1,7 @@
+overlay coq_lsp https://github.com/ppedrot/coq-lsp module-abstract-type 19995
+
+overlay elpi https://github.com/ppedrot/coq-elpi module-abstract-type 19995
+
+overlay metacoq https://github.com/ppedrot/metacoq module-abstract-type 19995
+
+overlay paramcoq https://github.com/ppedrot/paramcoq module-abstract-type 19995
