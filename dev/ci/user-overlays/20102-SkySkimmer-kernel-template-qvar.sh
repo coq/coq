82c4d182a37eca14d6ede04e0e9f86cb147daa94
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2025-01-22 13:54:45 +0100

    Overlays for kernel pseudo sort poly

---
+++ dev/ci/user-overlays/20102-SkySkimmer-kernel-template-qvar.sh
@@ -0,0 +1,5 @@
+overlay paramcoq https://github.com/SkySkimmer/paramcoq cominductive-lbound 20102 kernel-template-qvar
+
+overlay coq_lsp https://github.com/SkySkimmer/coq-lsp cominductive-lbound 20102 kernel-template-qvar
+
+overlay metacoq https://github.com/SkySkimmer/metacoq kernel-template-qvar 20102
