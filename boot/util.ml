2b3637bd0085ae092c92ab506d603950540ec536
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2025-02-03 14:43:56 +0100

    Move coqbin/coqroot from Boot.Util to Boot.Env
    These values have to do with the execution environment so they
    shouldn't be in Util.

--- boot/util.ml
+++ boot/util.ml
@@ -61,12 +61,10 @@ let getenv_rocq_gen ~rocq ~coq =
 let getenv_rocq name =
   getenv_rocq_gen ~rocq:("ROCQ"^name) ~coq:("COQ"^name)
 
-(** Add a local installation suffix (unless the suffix is itself
-    absolute in which case the prefix does not matter) *)
 let use_suffix prefix suffix =
-  if String.length suffix > 0 && suffix.[0] = '/'
-  then suffix
-  else Filename.concat prefix suffix
+  if Filename.is_relative suffix
+  then Filename.concat prefix suffix
+  else suffix
 
 let canonical_path_name p =
   let current = Sys.getcwd () in
@@ -78,20 +76,3 @@ let canonical_path_name p =
   with Sys_error _ ->
     (* We give up to find a canonical name and just simplify it... *)
     Filename.concat current p
-
-let coqbin =
-  canonical_path_name (Filename.dirname Sys.executable_name)
-
-(** The following only makes sense when executables are running from
-    source tree (e.g. during build or in local mode). *)
-let coqroot =
-  Filename.dirname coqbin
-
-(** [check_file_else ~dir ~file oth] checks if [file] exists in
-    the installation directory [dir] given relatively to [coqroot],
-    which maybe has been relocated.
-    If the check fails, then [oth ()] is evaluated.
-    Using file system equality seems well enough for this heuristic *)
-let check_file_else ~dir ~file oth =
-  let path = use_suffix coqroot dir in
-  if Sys.file_exists (Filename.concat path file) then path else oth ()
