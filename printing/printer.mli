18a845b5f489076129be9e718c855bee20958c62
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2025-02-12 15:57:16 +0100

    Print/About print binding instances in binder style
    ie with extra `|` when it binds qvars
    
    Close #20112

--- printing/printer.mli
+++ printing/printer.mli
@@ -120,7 +120,7 @@ val pr_sort                : evar_map -> Sorts.t -> Pp.t
 (** Universe constraints *)
 
 val pr_universe_instance   : evar_map -> UVars.Instance.t -> Pp.t
-val pr_universe_instance_constraints : evar_map -> UVars.Instance.t -> Univ.Constraints.t -> Pp.t
+val pr_universe_instance_binder : evar_map -> UVars.Instance.t -> Univ.Constraints.t -> Pp.t
 val pr_universe_ctx        : evar_map -> ?variance:UVars.Variance.t array ->
   UVars.UContext.t -> Pp.t
 val pr_abstract_universe_ctx : evar_map -> ?variance:UVars.Variance.t array ->
