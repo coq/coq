1979c1b3ead874250ebb1b7d9516963c8dcaf1a1
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2025-02-21 12:53:11 +0100

    timelog2html add -o option

--- dev/bench/timelog2html.ml
+++ dev/bench/timelog2html.ml
@@ -10,13 +10,16 @@
 
 let die fmt = Printf.kfprintf (fun _ -> exit 1) stderr fmt
 
-let usage () = die "Usage: rocq timelog2html VFILE DATAFILES\n\n%a\n%s\n"
+let usage () = die "Usage: rocq timelog2html [options] VFILE DATAFILES\n\n%a\n%s\n"
     (fun fmt len -> Printf.fprintf fmt "(1 to %d data files are supported.)" len)
     Htmloutput.max_data_count
     "Data files may be JSON profile files (as produced by rocq c -profile) with extension .json or .json.gz,\
-\nor timing files (as produced by rocq c -time-file)"
+     \nor timing files (as produced by rocq c -time-file).\
+     \n\
+     \nOptions:\
+     \n  -o FILE: output to FILE (default is - meaning stdout)"
 
-let parse_args = function
+let parse_files = function
   | [] | [_] -> usage ()
   | vfile :: data_files ->
     let data_files = Array.of_list data_files in
@@ -24,6 +27,24 @@ let parse_args = function
       then usage ()
     in
     vfile, data_files
+
+type output = Stdout | File of string
+
+let with_output out f =
+  match out with
+  | Stdout -> f stdout
+  | File fname ->
+    let ch = open_out fname in
+    Fun.protect ~finally:(fun () -> close_out ch) (fun () -> f ch)
+
+let parse_output = function
+  | "-" -> Stdout
+  | f -> File f
+
+let parse_args = function
+  | "-o" :: f :: args -> parse_output f, parse_files args
+  | ["-o"] -> usage()
+  | args -> Stdout, parse_files args
 
 let file_data data_file =
   if List.exists (fun suf -> CString.is_suffix suf data_file) [".json"; ".json.gz"] then
@@ -34,7 +55,7 @@ let file_data data_file =
     data_file, CArray.of_list data
 
 let main args =
-  let vfile, data_files = parse_args args in
+  let out, (vfile, data_files) = parse_args args in
 
   let source = BenchUtil.read_whole_file vfile in
 
@@ -48,5 +69,5 @@ let main args =
 
   let vname = Filename.basename vfile in
 
-  let () = Htmloutput.output stdout ~vname ~data_files all_data in
+  let () = with_output out (fun ch -> Htmloutput.output ch ~vname ~data_files all_data) in
   ()
