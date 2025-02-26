07d174c01c4c02bdf5e241f1e2d325692f549328
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2025-02-26 14:39:04 +0100

    manually disable warning for eq_pars definition

--- theories/Classes/Morphisms.v
+++ theories/Classes/Morphisms.v
@@ -540,7 +540,7 @@ Class PartialApplication.
 CoInductive normalization_done : Prop := did_normalization.
 
 Class Params {A : Type} (of : A) (arity : nat).
-#[global] Instance eq_pars : Params (@eq) 1 := {}.
+#[global, warnings="-bad-template-constraint"] Instance eq_pars : Params (@eq) 1 := {}.
 #[global] Instance iff_pars : Params (@iff) 0 := {}.
 #[global] Instance impl_pars : Params (@impl) 0 := {}.
 #[global] Instance flip_pars : Params (@flip) 4 := {}.
