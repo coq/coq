eb45a2ff2d57c1b3a7d18230bf184a9dcf63ad7c
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2025-02-28 14:52:36 +0100

    Ltac2 in term be more precise about used notation variables

--- test-suite/bugs/bug_17833.v
+++ test-suite/bugs/bug_17833.v
@@ -3,7 +3,7 @@ Notation "'silly' i1 .. i_ => x"
   := (let v := match (fun i1 => .. (fun i_ => x) ..) return _ with
                | y => y
                end in
-      let _ := ltac2:(()) in
+      let _ := ltac2:(let _ := x in ()) in
       v)
        (at level 10, i1 binder, i_ binder, only parsing).
 
