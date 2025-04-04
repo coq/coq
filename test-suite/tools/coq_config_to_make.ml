035ca23aa10039805a5f8d6879a3947700b0a783
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2025-04-04 17:37:07 +0200

    test suite print more info when failing

--- test-suite/tools/coq_config_to_make.ml
+++ test-suite/tools/coq_config_to_make.ml
@@ -23,6 +23,7 @@ let write_makefile coqprefix coqlibinstall best_compiler ocamlfind caml_flags co
   pr "OCAMLFIND=%S\n" ocamlfind;
   pr "# Caml flags\n";
   pr "CAMLFLAGS=%s %s\n" caml_flags coq_caml_flags;
+  pr "# coqc was said to be %s\n" Sys.argv.(1);
   ()
 
 let coq_warn_error (prefs : Prefs.t) =
