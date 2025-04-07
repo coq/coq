3daceb429dff72c5e087bb8858674fbff39ebba6
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2025-04-07 16:12:38 +0200

    Expand the tests for rewriting on primitive projections.

--- test-suite/success/setoid_rewrite_proj.v
+++ test-suite/success/setoid_rewrite_proj.v
@@ -1,5 +1,7 @@
 Require Export Corelib.Setoids.Setoid.
 
+Module Test1.
+
 #[projections(primitive=yes)]
 Structure Test := {
   test : unit
@@ -15,3 +17,45 @@ Proof.
   setoid_rewrite H.
   exact eq_refl.
 Qed.
+
+End Test1.
+
+Module Test2.
+
+#[projections(primitive=yes)]
+Structure Test (n : nat) := {
+  test : unit
+}.
+
+Axiom tst : Test 0.
+Axiom tst' : Test 0.
+Axiom H : tst = tst'.
+
+Theorem a :
+  test _ tst = test _ tst'.
+Proof.
+  setoid_rewrite H.
+  exact eq_refl.
+Qed.
+
+End Test2.
+
+Module Test3.
+
+#[projections(primitive=yes)]
+Structure Test (n : nat) := {
+  test : unit
+}.
+
+Axiom n : nat.
+Axiom tst : Test n.
+Axiom tst' : Test n.
+Axiom H : n = 0.
+
+Theorem a :
+  test _ tst = test _ tst'.
+Proof.
+  Fail setoid_rewrite H. (* no rewriting in parameters *)
+Abort.
+
+End Test3.
