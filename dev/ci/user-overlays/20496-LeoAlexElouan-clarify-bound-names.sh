6a561ca94d4cc5bef5721b7fde06835e11fd40c3
Soudant <soudant.leo@gmail.com>
2025-04-16 15:15:26 +0200

    Overlays for clarify-bound-names

---
+++ dev/ci/user-overlays/20496-LeoAlexElouan-clarify-bound-names.sh
@@ -0,0 +1,4 @@
+overlay coq_lsp https://github.com/LeoAlexElouan/coq-lsp clarify-bound-names 20496
+overlay lean_importer https://github.com/LeoAlexElouan/rocq-lean-import clarify-bound-names 20496
+overlay metarocq https://github.com/LeoAlexElouan/metarocq clarify-bound-names 20496
+overlay paramcoq https://github.com/LeoAlexElouan/paramcoq clarify-bound-names 20496
