1ae2bb63f3485e7b326242cf240fb167a46b1e83
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2026-07-21 15:07:34 +0200

    Restore interleaving of libobject and kernel load in Require
    kinda dubious behaviour but easy to restore so why not

--- vernac/library.ml
+++ vernac/library.ml
@@ -390,7 +390,7 @@ let register_library_syntax (root, m) =
      the module or module type
    - not called from a library (i.e. a module identified with a file) *)
 let load_require _ needed =
-  List.iter register_library needed
+  register_library needed
 
   (* [needed] is the ordered list of libraries not already loaded *)
 let cache_require o =
@@ -400,7 +400,7 @@ let discharge_require o = Some o
 
 (* open_function is never called from here because an Anticipate object *)
 
-type require_obj = library_t list
+type require_obj = library_t
 
 let in_require : require_obj -> obj =
   declare_object
@@ -412,7 +412,7 @@ let in_require : require_obj -> obj =
      classify_function = (fun o -> Anticipate) }
 
 let load_require_syntax _ needed =
-  List.iter register_library_syntax needed
+  register_library_syntax needed
 
 let cache_require_syntax o =
   load_require_syntax 1 o
@@ -421,8 +421,7 @@ let discharge_require_syntax o = Some o
 
 (* open_function is never called from here because an Anticipate object *)
 
-type require_obj_syntax = (bool * library_t) list
-
+type require_obj_syntax = bool * library_t
 let in_require_syntax : require_obj_syntax -> obj =
   declare_object
     {(default_object "REQUIRE-SYNTAX") with
@@ -449,13 +448,21 @@ let kernel_load_require m =
 
 let require_library_from_dirpath needed =
   if Lib.is_module_or_modtype () then warn_require_in_module ();
-  List.iter kernel_load_require needed;
-  Lib.add_leaf (in_require needed)
+  (* Note that putting the list in the libobject would need to split the iter
+     (ie do [List.iter kernel_load_require needed; List.iter add_leaf needed])
+     which changes behaviour because libobjects can change kernel flags
+     (eg Global Unset Universe Checking).
+     TBH I'm not sure how much we care about preserving such behaviours
+     if it ever becomes inconvenient though. *)
+  List.iter (fun m ->
+      kernel_load_require m;
+      Lib.add_leaf (in_require m))
+    needed
 
 let require_library_syntax_from_dirpath ~intern modrefl =
   let needed, contents = List.fold_left (rec_intern_library ~intern) ([], DirPath.Map.empty) modrefl in
   let needed = List.rev_map (fun (root, dir) -> root, DirPath.Map.find dir contents) needed in
-  Lib.add_leaf (in_require_syntax needed);
+  List.iter (fun m -> Lib.add_leaf (in_require_syntax m)) needed;
   List.map snd needed
 
 (************************************************************************)
